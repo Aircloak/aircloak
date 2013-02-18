de0f8933e3d157ed2b6755466504bcfd50036ac9
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2013-02-18 16:10:03 +0100

    Enable assets pipeline in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,11 @@ Web::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.enabled = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
