1fa8d7887d77c44569bac8df3f3b73ab81bbcc4c
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2013-05-01 11:02:54 +0200

    Adds timestamps to verification views

--- app/models/staging_machine.rb
+++ app/models/staging_machine.rb
@@ -4,7 +4,7 @@ class StagingMachine < ActiveRecord::Base
   def result_for file, event_name
     file.client_file_events.where(staging_machine_id: id).inject(:missing) do |previous, event|
       if event.event == event_name
-        event.positive
+        event
       else
         previous
       end
