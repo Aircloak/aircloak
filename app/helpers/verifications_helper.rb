17788c2ab28dc77296bd6bc85ff4e621cb0325b3
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2013-06-06 15:45:33 +0200

    Display description and time per event-machine pair
    Previously we showed the description once per event.
    We are now using the events in a much more sensible
    manner, to also relay interesting meta data.
    This is now shown on a per event basis.

--- app/helpers/verifications_helper.rb
+++ app/helpers/verifications_helper.rb
@@ -1,10 +1,2 @@
 module VerificationsHelper
-  def description_for_event_name name
-    event = ClientFileEvent.where(event: name).first
-    if event
-      event.description
-    else
-      "Unkown event"
-    end
-  end
 end
