85da9662d449a52bfc4016a430a0c0ad6d1a048b
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2013-06-17 15:41:08 +0200

    Load properties and percentiles using `where`

--- app/models/percentile.rb
+++ app/models/percentile.rb
@@ -3,7 +3,7 @@ class Percentile < ActiveRecord::Base
   belongs_to :query
 
   def self.from_proto query_id, prop
-    p = Percentile.first(bucket: prop.name, query_id: query_id)
+    p = Percentile.where(bucket: prop.name, query_id: query_id).first
     p = Percentile.create(bucket: prop.name, query_id: query_id) unless p
     p
   end
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,7 +3,7 @@ class Property < ActiveRecord::Base
   has_many :property_results, dependent: :destroy
 
   def self.from_proto query_id, prop
-    p = Property.first(property: prop.name, query_id: query_id)
+    p = Property.where(property: prop.name, query_id: query_id).first
     p = Property.create(property: prop.name, query_id: query_id) unless p
     p
   end
