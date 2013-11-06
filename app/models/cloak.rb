99865f6c73561d85f3194f6ed0359531c8991bb6
Matthias Kretschmer <matthias@aircloak.com>
2013-11-06 14:23:09 +0100

    cloaks belong to clusters

--- app/models/cloak.rb
+++ app/models/cloak.rb
@@ -1,4 +1,5 @@
 class Cloak < ActiveRecord::Base
+  belongs_to :cluster
   validates :ip, format: { with: /\A(((25[0-5])|(2[0-4][0-9])|([01]?[0-9][0-9]?))\.){3}((25[0-5])|(2[0-4][0-9])|([01]?[0-9][0-9]?))/}
   validates_presence_of :name
   validates_uniqueness_of :name, :ip
