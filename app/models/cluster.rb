13272c82ee5ce0163820d803b40f946ed4650c4f
Matthias Kretschmer <matthias@aircloak.com>
2013-11-06 12:50:31 +0100

    added model of clusters

---
+++ app/models/cluster.rb
@@ -0,0 +1,13 @@
+class Cluster < ActiveRecord::Base
+  has_many :cloaks
+  has_one :build
+
+  validates_associated :cloaks
+  validates :name, uniqueness: true
+  validates_each :cloak do |record, attr, value|
+    record.error.add(attr, 'must match tpm configuration') unless value.tpm == record.tpm
+  end
+  validates_each :build do |record, attr, value|
+    record.error.add(attr, 'must match tpm configuration') unless value.tpm == record.tpm
+  end
+end
