f424ce5c4ab0cb04bac6639ed7631ae0a8b442c2
Matthias Kretschmer <matthias@aircloak.com>
2013-11-08 13:29:18 +0100

    add health status to cloaks and show that

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -10,4 +10,13 @@ class Cluster < ActiveRecord::Base
   validates_each :build_id do |record, attr, value|
     record.error.add(attr, 'must match tpm configuration') unless Build.find(value).tpm == record.tpm
   end
+
+  def health_of_cloaks
+    res = {:ok => 0, :changing => 0, :down => 0}
+    cloaks.all.each do |cloak|
+      health = cloak.health
+      res[health] = res[health] + 1
+    end
+    res
+  end
 end
