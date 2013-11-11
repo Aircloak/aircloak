b914b90fabf391625f6a3025049f5ac19f9bdd5e
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2013-11-11 17:45:36 +0100

    Add /machines endpoint for manny-air
    The /machines endpoint servers a MachinesProto
    message with all the cloaks that exist in the system

---
+++ spec/requests/machines_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require './lib/proto/air/management_messages.pb'
+require './lib/machine_packer.rb'
+
+describe "ResultsController" do
+  describe "GET /machines" do
+    it "should provide a list of machines" do
+      machine = MachineProto.new(
+        machine_id: 1,
+        ip: "1.2.3.4",
+        name: "tpm-monster.mpi-sws.org",
+        type: MachineProto::MachineType::PHYSICAL
+      )
+      p = MachinesProto.new machines: [machine]
+
+      m = double
+      Cloak.stub(:all).and_return [m]
+      MachinePacker.stub(:package_cloaks).with([m]).and_return(p)
+
+      get "/machines"
+
+      resp = MachinesProto.decode(response.body)
+      resp.should eq p
+      response.status.should be(200)
+    end
+  end
+end
