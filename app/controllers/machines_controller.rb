fe39b8d11666a969064e5737a120a597a164e2b4
Matthias Kretschmer <matthias@aircloak.com>
2013-11-15 14:13:08 +0100

    fixing test of updating machine health

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -3,6 +3,7 @@ require './lib/machine_packer'
 
 class MachinesController < ApplicationController
   filter_access_to :index, require: :anon_read
+  filter_access_to :update, require: :anon_read
 
   def index
     render text: MachinePacker.package_cloaks(Cloak.all).encode.buf, layout: false
@@ -25,5 +26,6 @@ private
     health = :changing if state == MachineStateProto::State::CHANGING
     health = :sw_failing if state == MachineStateProto::State::SW_FAILING
     health = :hw_failing if state == MachineStateProto::State::HW_FAILING
+    health
   end
 end
--- spec/controllers/machines_controller_spec.rb
+++ spec/controllers/machines_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe MachinesController do
+  describe "UPDATE /machines/MACHINEID" do
+    it "should update the health of MACHINEID" do
+      machine_state = MachineStateProto.new(state: MachineStateProto::State::GOOD)
+      Cloak.destroy_all
+      cloak = Cloak.create(name: "cloak", ip: "1.1.1.1")
 
+      request.env['RAW_POST_DATA'] = machine_state.encode.buf
+      post :update, id: cloak.id
+      
+      response.status.should be(200)
+      cloak.reload.health.should == :good
+    end
+  end
 end
