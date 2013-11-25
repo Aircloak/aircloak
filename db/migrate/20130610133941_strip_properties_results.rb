ec411949e06190d423bbfe1fcd69b7b4af1b7a5b
Matthias Kretschmer <matthias@aircloak.com>
2013-11-25 19:25:09 +0100

    fix a problem due to classes no longer available

--- db/migrate/20130610133941_strip_properties_results.rb
+++ db/migrate/20130610133941_strip_properties_results.rb
@@ -4,10 +4,12 @@ class StripPropertiesResults < ActiveRecord::Migration
       change_table :property_results do |t|
         dir.up { 
           add_reference :property_results, :property
-          PropertyResult.all.each do |pr|
-            p = Property.where(property: pr.bucket, query_id: query_id).first
-            pr.property = p
-            pr.save
+          if Object.const_defined?('PropertyResult')
+            PropertyResult.all.each do |pr|
+              p = Property.where(property: pr.bucket, query_id: query_id).first
+              pr.property = p
+              pr.save
+            end
           end
           remove_column :property_results, :bucket 
           remove_reference :property_results, :query
@@ -15,10 +17,12 @@ class StripPropertiesResults < ActiveRecord::Migration
         dir.down   { 
           add_column :property_results, :bucket, :string
           add_reference :property_results, :query
-          PropertyResult.all.each do |pr|
-            pr.bucket = pr.property.property
-            pr.query_id = pr.query_id
-            pr.save
+          if Object.const_defined?('PropertyResult')
+            PropertyResult.all.each do |pr|
+              pr.bucket = pr.property.property
+              pr.query_id = pr.query_id
+              pr.save
+            end
           end
           remove_reference :property_results, :property
         }
