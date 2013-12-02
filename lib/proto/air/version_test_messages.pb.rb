9a081fd214b2526008f0c9094b32834515610652
Matthias Kretschmer <matthias@aircloak.com>
2013-12-02 18:24:12 +0100

    send reply URI in TestRequestPB

--- lib/proto/air/version_test_messages.pb.rb
+++ lib/proto/air/version_test_messages.pb.rb
@@ -13,7 +13,8 @@ end
 class TestRequestPB
   required :id, :uint32, 1
   required :cluster_id, :uint32, 2
-  repeated :cluster_nodes, :string, 3
+  required :reply_uri, :string, 3
+  repeated :cluster_nodes, :string, 4
 end
 
 
