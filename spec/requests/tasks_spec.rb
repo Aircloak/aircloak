35cba6baa2aec01cb7217401fe14420e4c904e89
Matthias Kretschmer <matthias@aircloak.com>
2014-02-12 14:33:18 +0100

    refactoring the task tests as suggested by @sebastian

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -9,98 +9,64 @@ describe "TasksController" do
       Task.destroy_all
     end
 
-    it "should create a new task binary upon upload if there isn't already one with the same main package" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin])
+    let (:bin) { QueryBinary.new(package: "task", data: "1234") }
 
+    def query_data params = {}
+      QueryData.new(
+        main_package: "task",
+        data: [params.delete(:bin) || bin],
+        payload_identifier: params.delete(:payload_identifier),
+        mutator: params.delete(:mutator),
+        system_task: params.delete(:system_task)
+      )
+    end
+
+    def post_and_validate params
       expect {
-        post "/tasks/update_task_binary", data.encode.buf
+        post "/tasks/update_task_binary", (query_data params).encode.buf
       }.to change {Task.count}.from(0).to(1)
-
       response.status.should be(200)
+    end
 
+    it "should create a new task binary upon upload if there isn't already one with the same main package" do
+      post_and_validate({})
       Task.all.map(&:ready).should eq([false])
     end
 
     it "should update a task binary upon upload" do
-      bin1 = QueryBinary.new(package: "task", data: "1234")
-      data1 = QueryData.new(main_package: "task", data: [bin1])
-      task = Task.new(main_package: "task", packaged_data: data1.encode.buf, ready: true)
+      task = Task.new(main_package: "task", packaged_data: (query_data {}).encode.buf, ready: true)
       task.save.should eq true
 
       bin2 = QueryBinary.new(package: "task", data: "5678")
-      data2 = QueryData.new(main_package: "task", data: [bin2])
-
-      post "/tasks/update_task_binary", data2.encode.buf
-
+      post "/tasks/update_task_binary", query_data(bin: bin2).encode.buf
       response.status.should be(200)
-      task.reload.ready.should eq true
 
+      task.reload.ready.should eq true
       QueryData.decode(task.packaged_data.dup).data.should eq [bin2]
     end
 
     it "should create/update a new task binary upon upload and set it ready if enough information" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin], system_task: true, mutator: false)
-
-      expect {
-        post "/tasks/update_task_binary", data.encode.buf
-      }.to change {Task.count}.from(0).to(1)
-
-      response.status.should be(200)
-
+      post_and_validate(system_task: true, mutator: false)
       Task.all.map(&:ready).should eq([true])
     end
 
     it "should create/update a new task to be an update task if a payload identifier is given" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin], payload_identifier: "foo")
-
-      expect {
-        post "/tasks/update_task_binary", data.encode.buf
-      }.to change {Task.count}.from(0).to(1)
-
-      response.status.should be(200)
-
+      post_and_validate(payload_identifier: "foo")
       Task.all.map(&:update_task).should eq([true])
     end
 
     it "should create/update a new task to be no update task if no payload identifier is given" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin])
-
-      expect {
-        post "/tasks/update_task_binary", data.encode.buf
-      }.to change {Task.count}.from(0).to(1)
-
-      response.status.should be(200)
-
+      post_and_validate({})
       Task.all.map(&:update_task).should eq([false])
     end
 
     it "should create/update a new task that is not ready if system_task is not set" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin], mutator: false)
-
-      expect {
-        post "/tasks/update_task_binary", data.encode.buf
-      }.to change {Task.count}.from(0).to(1)
-
-      response.status.should be(200)
-
+      post_and_validate(mutator: false)
       Task.all.map(&:ready).should eq([false])
     end
 
     it "should create/update a new task that is not ready if mutator is not set" do
-      bin = QueryBinary.new(package: "task", data: "1234")
-      data = QueryData.new(main_package: "task", data: [bin], system_task: false)
-
-      expect {
-        post "/tasks/update_task_binary", data.encode.buf
-      }.to change {Task.count}.from(0).to(1)
-
-      response.status.should be(200)
-
+      post_and_validate(system_task: false)
       Task.all.map(&:ready).should eq([false])
     end
   end
