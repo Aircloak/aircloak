831435100590fae3645b1cc39feb509cf54c15e7
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-04-15 15:57:45 +0200

    Sanitize results before inserting to db

--- lib/result_handler.rb
+++ lib/result_handler.rb
@@ -13,19 +13,19 @@ class ResultHandler
   def self.create_buckets_for_properties properties, result_id
     inserts = []
     properties.each do |property|
-      label = property.label
-      str_answer = property.string
-      range_min = property.range ? property.range.min : 0
-      range_max = property.range ? property.range.max : 0
+      label = Bucket.sanitize property.label
+      str_answer = Bucket.sanitize property.string
+      range_min = property.range ? Bucket.sanitize(property.range.min) : 0
+      range_max = property.range ? Bucket.sanitize(property.range.max) : 0
       if property.joiners_leavers
-        joiners = property.joiners_leavers.joiners
-        leavers = property.joiners_leavers.leavers
+        joiners = Bucket.sanitize property.joiners_leavers.joiners
+        leavers = Bucket.sanitize property.joiners_leavers.leavers
       else
         joiners = 0
         leavers = 0
       end
-      accumulated_count = property.accumulated_count
-      inserts.push "(#{result_id}, '#{label}', '#{str_answer}'," +
+      accumulated_count = Bucket.sanitize property.accumulated_count
+      inserts.push "(#{result_id}, #{label}, #{str_answer}," +
           " #{range_min}, #{range_max}, #{joiners}, #{leavers}, #{accumulated_count})"
     end
     sql = "INSERT INTO buckets (result_id, label, str_answer, range_min, range_max, " +
