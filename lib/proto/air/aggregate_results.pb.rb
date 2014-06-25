5e69d7ebec479cf9d878a768393a40c9f7508f96
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-06-25 11:01:26 +0200

    Compile proto without result_id

--- lib/proto/air/aggregate_results.pb.rb
+++ lib/proto/air/aggregate_results.pb.rb
@@ -28,30 +28,24 @@ class BucketPB
     required :min, :sint64, 1
     required :max, :sint64, 2
   end
-
   required :label, :string, 1
   optional :string, :string, 2
   optional :range, BucketPB::RangeProto, 3
   optional :accumulated_count, :sint64, 4
 end
 
-
 class ExceptionPB
   required :stackEntry, :string, 1
   optional :accumulated_count, :sint64, 2
 end
 
-
 class ResultPB
   required :analyst_id, :uint64, 1
   required :task_id, :string, 2
-  required :result_id, :uint64, 3
-  repeated :buckets, BucketPB, 4
-  repeated :exceptions, ExceptionPB, 5
+  repeated :buckets, BucketPB, 3
+  repeated :exceptions, ExceptionPB, 4
 end
-
 
 class ResultsPB
   repeated :result_ids, :uint64, 1
 end
-
