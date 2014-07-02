f394f2fb3a1e6f61842f04ef8f538e7959e7f7ae
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-07-02 16:48:39 +0200

    Add helper for generating popovers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,53 @@ module ApplicationHelper
     errors = object.errors
     group_class = "control-group#{" error" if errors.include?(name)} #{name.to_s.gsub("_", "-")}-group"
     content_tag :div, class: group_class do
-      block.call 
+      block.call
       errors.include?(name) ? "#{name} #{errors.messages[name].first}" : ""
     end
+  end
+
+  def help_tooltip field
+    tip = tip_for(request.params["controller"], field)
+    return "" if tip.nil?
+    result = "<a class=\"btn btn-mini btn-info tip\"" +
+        " data-content=\"" + tip[:content] + "\"" +
+        " data-toggle=\"button\"" +
+        " data-placement=\"" + tip[:placement] + "\"" +
+        ">?</a>"
+    result.html_safe
+  end
+
+private
+  def tip_for controller, field
+    data = yaml(controller)[field]
+    if data.nil?
+      nil
+    else
+      {
+        content: data["markdown"] ? (h markdown.render(data["content"])) : data["content"],
+        placement: data["placement"] || "bottom"
+      }
+    end
+  end
+
+  def yaml controller
+    YAML.load_file "app/help/#{controller}/tooltips.yml"
+  end
+
+  def markdown
+    return @markdown if defined?(@markdown)
+    options = {
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      autolink: true,
+      strikethrough: true,
+      lax_spacing: true,
+      superscript: true,
+      highlight: true,
+      quote: true,
+      footnotes: true
+    }
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
   end
 end
