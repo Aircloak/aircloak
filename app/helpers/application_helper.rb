e30149733fe57f4999a1bca6ebee8b2d2bec1731
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-07-03 13:16:06 +0200

    Remove stale old unused helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,4 @@
 module ApplicationHelper
-  def labeled_form_for(object, options = {}, &block)
-    options[:builder] = LabeledFormBuilder
-    html = options[:html] || {}
-    html_class = html[:class] || ""
-    html_class += " form-horizontal"
-    html[:class] = html_class
-    options[:html] = html
-    form_for(object, options, &block)
-  end
-
-  def control_group(name, object, &block)
-    errors = object.errors
-    group_class = "control-group#{" error" if errors.include?(name)} #{name.to_s.gsub("_", "-")}-group"
-    content_tag :div, class: group_class do
-      block.call
-      errors.include?(name) ? "#{name} #{errors.messages[name].first}" : ""
-    end
-  end
-
   def help_tooltip field
     tip = tip_for(request.params["controller"], field)
     return "" if tip.nil?
