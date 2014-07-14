b2663d42dc47295435f78a7b96cfd9f6d71fe3a9
Cristian Berneanu <cristian@aircloak.com>
2014-07-14 10:19:20 +0300

    Respect top-level controller page-layout setting
    Application controller should respect the decision of the top-level
    controller to disable the page layout for certain paths.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -74,7 +74,7 @@ private
   def set_layout
     if current_user && current_user.analyst != nil
       self.class.layout "analyst"
-    else
+    elseif @layout != false
       self.class.layout "application"
     end
   end
