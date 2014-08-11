44878a5dfd53c056b5ccdb9742ead243267fe37f
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-08-11 10:29:45 +0200

    Move utility functions to help utility
    This cleans up the controller and moves
    helper method used when rendering ERB
    files into the helper utility.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -4,122 +4,26 @@ class HelpController < ApplicationController
   before_action :load_guides
 
   def show
-    b = binding
+    b = @help_utils.create_binding
     @current_guide = params[:id]
-    guide = HelpUtils.instance.guide_for_id @current_guide
-    @content = HelpUtils.instance.markdown.render(ERB.new(guide["content"], 0, "<>-").result b)
-    @toc = HelpUtils.instance.toc_markdown.render(guide["content"])
+    guide = @help_utils.guide_for_id @current_guide
+    @content = @help_utils.markdown.render(ERB.new(guide["content"], 0, "<>-").result b)
+    @toc = @help_utils.toc_markdown.render(guide["content"])
   end
 
 private
   def load_guides
-    guide_names = Dir.entries(HelpUtils.instance.guide_disk_dir).select do |file_name|
-      File.file?(HelpUtils.instance.full_guide_path file_name)
+    @help_utils = HelpUtils.new(current_user, self)
+    # @help_utils = HelpUtils.new current_user
+    guide_names = Dir.entries(@help_utils.guide_disk_dir).select do |file_name|
+      File.file?(@help_utils.full_guide_path file_name)
     end
     @guides = guide_names.map do |guide_name|
-      raw_yaml = HelpUtils.instance.yaml HelpUtils.instance.full_guide_path guide_name
+      raw_yaml = @help_utils.yaml @help_utils.full_guide_path guide_name
       raw_yaml["path"] = guide_name.gsub("\.yml", "")
       raw_yaml
     end.sort do |a,b|
       a["order"] <=> b["order"]
     end
-  end
-
-
-  #####################################################################
-  # Functions to make writing guides easier
-  #####################################################################
-
-  # These functions are available in the ERB context
-  # in the help guides themselves.
-
-  def help_link page
-    HelpUtils.instance.help_link page
-  end
-
-  def site_link url, name
-    "<a href=\"#{url}\">#{name}</a>"
-  end
-
-  def key_count
-    current_user.analyst.key_materials.count
-  end
-
-  def has_tables?
-    current_user.analyst.undeleted_analyst_tables.count != 0
-  end
-
-  def sample_table_name
-    unless has_tables?
-      "locations"
-    else
-      current_user.analyst.undeleted_analyst_tables.first.table_name
-    end
-  end
-
-  def sample_json
-    table = current_user.analyst.undeleted_analyst_tables.where(table_name: sample_table_name).first
-    json = if table then
-      table.generate_sample_json
-    else
-      # We invent some json for a fictive location table
-      {
-        user1: {
-          locations: [
-            {
-              x: 1,
-              y: 2
-            },
-            {
-              x: 42,
-              y: 40
-            }
-          ]
-        },
-        user2: {
-          locations: [
-            {
-              x: 100,
-              y: 200
-            }
-          ]
-        }
-      }
-    end
-    JSON.pretty_generate(json)
-  end
-
-  def has_cluster?
-    current_user.analyst.clusters.count != 0
-  end
-
-  def sample_cloak_name
-    if has_cluster?
-      current_user.analyst.clusters.first.cloaks.first.name
-    else
-      "cluster.example.com"
-    end
-  end
-
-  def sample_key_name
-    if key_count == 0
-      "MyKey.pfx"
-    else
-      current_user.analyst.key_materials.first.name
-    end
-  end
-
-  def sample_constraint
-    if has_tables?
-      table = current_user.analyst.undeleted_analyst_tables.where(table_name: sample_table_name).first
-      column = JSON.parse(table.table_data).first
-      AnalystTable.sample_constraint column
-    else
-      "x > 10"
-    end
-  end
-
-  def has_multiple_clusters?
-    current_user.analyst.clusters.count > 0
   end
 end
--- lib/help_utils.rb
+++ lib/help_utils.rb
@@ -5,7 +5,23 @@ class CodeRayify < Redcarpet::Render::HTML
 end
 
 class HelpUtils
-  include Singleton
+  def initialize user, controller
+  # def initialize user
+    @current_user = user
+    @controller = controller
+  end
+
+  def create_binding
+    binding
+  end
+
+  def controller
+    @controller
+  end
+
+  def current_user
+    @current_user
+  end
 
   def guide_for_id id
     yaml full_guide_path "#{id}.yml"
@@ -32,7 +48,7 @@ class HelpUtils
   end
 
   def markdown
-    # return @markdown if defined?(@markdown)
+    return @markdown if defined?(@markdown)
     options = {
       with_toc_data: true,
       prettify: true,
@@ -48,12 +64,97 @@ class HelpUtils
       footnotes: true
     }
     code_ray_renderer = CodeRayify.new(options)
-    # @markdown = Redcarpet::Markdown.new(OtherHTML.new(options), options)
     @markdown = Redcarpet::Markdown.new(code_ray_renderer, options)
   end
 
   def help_link page
     guide = guide_for_id page
     "<a href=\"/help/#{page}\">#{guide["title"]}</a>"
+  end
+
+  def site_link url, name
+    "<a href=\"#{url}\">#{name}</a>"
+  end
+
+  def key_count
+    @current_user.analyst.key_materials.count
+  end
+
+  def has_tables?
+    current_user.analyst.undeleted_analyst_tables.count != 0
+  end
+
+  def sample_table_name
+    unless has_tables?
+      "locations"
+    else
+      @current_user.analyst.undeleted_analyst_tables.first.table_name
+    end
+  end
+
+  def sample_json
+    table = @current_user.analyst.undeleted_analyst_tables.where(table_name: sample_table_name).first
+    json = if table then
+      table.generate_sample_json
+    else
+      # We invent some json for a fictive location table
+      {
+        user1: {
+          locations: [
+            {
+              x: 1,
+              y: 2
+            },
+            {
+              x: 42,
+              y: 40
+            }
+          ]
+        },
+        user2: {
+          locations: [
+            {
+              x: 100,
+              y: 200
+            }
+          ]
+        }
+      }
+    end
+    JSON.pretty_generate(json)
+  end
+
+  def has_cluster?
+    @current_user.analyst.clusters.count != 0
+  end
+
+  def sample_cloak_name
+    if has_cluster?
+      @current_user.analyst.clusters.first.cloaks.first.name
+    else
+      "cluster.example.com"
+    end
+  end
+
+  def sample_key_name
+    if key_count == 0
+      "MyKey.pfx"
+    else
+      @current_user.analyst.key_materials.first.name
+    end
+  end
+
+  def sample_constraint
+    if has_tables?
+      table = @current_user.analyst.undeleted_analyst_tables.where(table_name: sample_table_name).first
+      column = JSON.parse(table.table_data).first
+      AnalystTable.sample_constraint column
+    else
+      "x > 10"
+    end
+  end
+
+  def has_multiple_clusters?
+    @current_user.analyst.clusters.count > 0
   end
 end
