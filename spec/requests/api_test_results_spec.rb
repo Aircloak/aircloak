f5d1bfb6d21897ea817d4c4c3087c50718a709c5
Matthias Kretschmer <matthias@aircloak.com>
2014-08-28 17:26:22 +0200

    adding tests for upload test_results

---
+++ spec/requests/api_test_results_spec.rb
@@ -0,0 +1,143 @@
+require 'spec_helper'
+
+describe "ApiTestResultsController" do
+  describe "POST /api/test_results" do
+    before(:each) do
+      TestResult.delete_all
+      TestItem.delete_all
+      TestItemVm.delete_all
+      TestVm.delete_all
+    end
+
+    let (:request) {
+      {
+        timestamp: 1234,
+        duration: 232,
+        benchmark: {
+          success: true,
+          duration: 2,
+          coverage: 0.1,
+          memory_usage: 200
+        },
+        vms: [
+          {name: "foo", duration: 1, disk_size: 100, disk_usage: 10, log: "foo"},
+          {name: "bar", duration: 2, disk_size: 100, disk_usage: 20}
+        ],
+        tests: [
+          {
+            name: "foobar",
+            success: true,
+            duration: 23,
+            log: "flarg"
+          },
+          {
+            name: "foobaz",
+            success: true,
+            duration: 23,
+            vms: [{name: "foo", cpus: 13, memory_size: 1024, memory_usage: 10, disk_usage: 99}]
+          }
+        ]
+      }
+    }
+
+    it "should take a valid request" do
+      post "/api/test_results", request.to_json
+      response.status.should eq(200)
+    end
+
+    it "requires a JSON" do
+      post "/api/test_results", "foo"
+      response.status.should eq(403)
+    end
+
+    it "requires .timestamp" do
+      request.delete(:timestamp)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .duration" do
+      request.delete(:duration)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .benchmark.duration" do
+      request[:benchmark].delete(:duration)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .benchmark.coverage" do
+      request[:benchmark].delete(:coverage)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .benchmark.memory_usage" do
+      request[:benchmark].delete(:memory_usage)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .vms[].name" do
+      request[:vms][1].delete(:name)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .vms[].duration" do
+      request[:vms][1].delete(:duration)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .vms[].disk_size" do
+      request[:vms][1].delete(:disk_size)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .vms[].disk_usage" do
+      request[:vms][1].delete(:disk_usage)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].name" do
+      request[:tests][1].delete(:name)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].duration" do
+      request[:tests][1].delete(:duration)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].vms[].cpus" do
+      request[:tests][1][:vms][0].delete(:cpus)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].vms[].memory_size" do
+      request[:tests][1][:vms][0].delete(:memory_size)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].vms[].memory_usage" do
+      request[:tests][1][:vms][0].delete(:memory_usage)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+
+    it "requires .tests[].vms[].disk_usage" do
+      request[:tests][1][:vms][0].delete(:disk_usage)
+      post "/api/test_results", request.to_json
+      response.status.should eq(403)
+    end
+  end
+end
