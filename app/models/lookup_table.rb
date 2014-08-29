23a3f94373eb548d7af667c1a3dac5384a8ef0d0
Sasa Juric <sasa.juric@gmail.com>
2014-08-29 16:20:14 +0200

    verify table uniqueness

--- app/models/lookup_table.rb
+++ app/models/lookup_table.rb
@@ -3,6 +3,7 @@ class LookupTable < ActiveRecord::Base
   belongs_to :analyst
 
   validates_presence_of :table_name, :cluster, :analyst
+  validate :unique_table
   validate :upload_data_format
 
   attr_reader :upload_data
@@ -12,6 +13,21 @@ class LookupTable < ActiveRecord::Base
   end
 
 private
+  def unique_table
+    return if deleted == true
+    return if analyst.nil?
+    return if table_name.nil? || table_name.empty?
+
+    unless analyst.lookup_tables.
+          where(
+                "table_name = ? and cluster_id = ? and id <> ? and deleted=?",
+                table_name, cluster.id, id || -1, false
+              ).
+          empty?
+      errors.add(:table_name, "must be unique")
+    end
+  end
+
   def upload_data_format
     return unless errors[:upload_data].empty?
     error = upload_data_error
