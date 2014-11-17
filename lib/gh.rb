f15d881cf1539ebed3393b5f76238523f78a52df
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2014-11-17 10:10:32 +0100

    Make possible to build from tag
    This way we can build releases easily for testing.
    
    Closes #412

--- lib/gh.rb
+++ lib/gh.rb
@@ -20,22 +20,36 @@ class Gh
     commits.first.sha
   end
 
-  def self.branchinfo_for_deployable_entity de
-    branches = github.repos.branches user: "aircloak", repo: de.repo
-    branch_infos = []
-    threads = []
-    branches.each do |branch|
-      threads << Thread.new do
-        commit = get_commit de.repo, branch.commit.sha
-        branch_infos << {
-          branch_name: branch.name,
-          sha: branch.commit.sha,
-          commit_title: commit.commit.message.split("\n").first
+  def self.buildpoint_for_deployable_entity de
+    build_infos = []
+    branch_thread = Thread.new do
+      threads = []
+      branches = github.repos.branches user: "aircloak", repo: de.repo
+      branches.each do |branch|
+        threads << Thread.new do
+          commit = get_commit de.repo, branch.commit.sha
+          build_infos << {
+            branch_name: branch.name,
+            sha: branch.commit.sha,
+            commit_title: commit.commit.message.split("\n").first
+          }
+        end
+      end
+      threads.each {|thread| thread.join}
+    end
+    tag_thread = Thread.new do
+      tags = github.repos.tags user: "aircloak", repo: de.repo
+      tags.each do |tag|
+        build_infos << {
+          branch_name: tag.name,
+          sha: tag.commit.sha,
+          commit_title: "release tag"
         }
       end
     end
-    threads.each {|thread| thread.join}
-    branch_infos
+    branch_thread.join
+    tag_thread.join
+    build_infos
   end
 
   def self.get_commit repo, sha
