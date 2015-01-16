f7e8fbecae12003551272f788594665f5164d1d0
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2015-01-16 10:17:40 +0100

    Hide aircloak employees from activity monitor
    Also adds a button to toggle them back in.
    This makes it easier to see what our real users
    are actually doing, rather than what we are testing.
    
    Closes #467

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,10 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.paginate(:page => params[:page], per_page: 30).order(created_at: :desc)
-    @activities = @activities.where(user_id: params[:user_id]) if params[:user_id]
+    @activities = Activity.all
+    @activities = @activites.where(user_id: params[:user_id]) if params[:user_id]
+    @activities = @activities.where("user_id not in (select user_id from " +
+        "user_permissions, (select * from permissions where name = 'admin') as p " +
+        "where permission_id = p.id)") unless params[:show_aircloak]
+    @activities = @activities.paginate(:page => params[:page], per_page: 30).order(created_at: :desc)
   end
 end
