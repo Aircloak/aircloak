681b8ead6a61509e2b90a8974a7649867b331734
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2015-02-24 10:43:44 +0100

    Ensure only @aircloak.com'ers can be admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   has_many :activities
   belongs_to :analyst
 
+  validate :only_aircloakers_can_be_admin
+
   before_destroy :remove_tracked_activity
 
   acts_as_authentic do |c|
@@ -107,5 +109,12 @@ class User < ActiveRecord::Base
 
   def remove_tracked_activity
     Activity.where(user_id: self.id).delete_all
+  end
+
+private
+  def only_aircloakers_can_be_admin
+    unless email =~ /aircloak\.com/
+      errors[:base] = "Non-aircloakers are not allow to be administrators" if permissions.where(name: "admin")
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,30 @@
 require 'spec_helper'
 
 describe User do
+  before :each do
+    Permission.delete_all
+    Permission.create name: :admin
+    User.delete_all
+  end
+
   it "attempt to create a legible name from the login" do
     User.new(login: "root").attempt_to_make_a_human_name_from_login.should eq "Root"
     User.new(login: "root.me").attempt_to_make_a_human_name_from_login.should eq "Root Me"
     User.new(login: "f.broth").attempt_to_make_a_human_name_from_login.should eq "F Broth"
+  end
+
+  it "should prevent non-aircloakers from being admin" do
+    u = User.new(login: "test", email: "hello@example.com", password: "1234",
+        password_confirmation: "1234")
+    u.permission_ids << Permission.find_by_name("admin").id
+    u.save.should eq false
+    u.errors[:base].count.should_not eq 0
+  end
+
+  it "should allow aircloakers to be admins" do
+    u = User.new(login: "test", email: "sebastian@aircloak.com", password: "1234",
+        password_confirmation: "1234")
+    u.permission_ids << Permission.find_by_name("admin").id
+    u.save.should eq true
   end
 end
