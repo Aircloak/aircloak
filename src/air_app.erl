44e653d0aca44036a4aa317a1723db38d659da96
Sasa Juric <sasa.juric@gmail.com>
2015-04-30 16:40:37 +0200

    cluster cron job for purging tasks

--- src/air_app.erl
+++ src/air_app.erl
@@ -8,6 +8,8 @@
   stop/1
 ]).
 
+-include("air.hrl").
+
 
 %% -------------------------------------------------------------------
 %% Application callbacks
@@ -16,8 +18,23 @@
 %% @hidden
 start(_StartType, _StartArgs) ->
   air:load_cloak_config(),
-  air_sup:start_link().
+  case air_sup:start_link() of
+    {ok, Pid} ->
+      start_cron_jobs(),
+      {ok, Pid};
+    Other -> Other
+  end.
 
 %% @hidden
 stop(_State) ->
+  ok.
+
+
+%% -------------------------------------------------------------------
+%% Internal functions
+%% -------------------------------------------------------------------
+
+start_cron_jobs() ->
+  cluster_cron:init(),
+  task_purger:setup_cron(),
   ok.
---
+++ src/task_purger.erl
@@ -0,0 +1,90 @@
+%% @doc Global (cluster-wide) service which purges deleted tasks.
+-module(task_purger).
+-behaviour(gen_server).
+
+%% Internal API
+-export([
+  setup_cron/0
+]).
+
+%% Internal API
+-export([
+  start_link/2,
+  run/1
+]).
+
+%% Callbacks
+-export([
+  init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3
+]).
+
+-include("air.hrl").
+
+
+%% -------------------------------------------------------------------
+%% API functions
+%% -------------------------------------------------------------------
+
+%% @doc Sets up the cluster cron job.
+-spec setup_cron() -> ok.
+setup_cron() ->
+  cluster_cron:install(air_conf:get_val(tasks, purge_cron_spec), ?MODULE, ?MODULE).
+
+
+%% -------------------------------------------------------------------
+%% Internal API
+%% -------------------------------------------------------------------
+
+%% @hidden
+start_link(GlobalServiceKey, ?MODULE) ->
+  gen_server:start_link({via, global_service, GlobalServiceKey}, ?MODULE, undefined, []).
+
+%% @hidden
+run(Pid) ->
+  gen_server:cast(Pid, run).
+
+
+%% -------------------------------------------------------------------
+%% Callbacks
+%% -------------------------------------------------------------------
+
+%% @hidden
+init(_) ->
+  {ok, undefined}.
+
+%% @hidden
+-spec handle_call(any(), any(), any()) -> no_return().
+handle_call(Message, From, _State) ->
+  throw({unexpected_call, Message, from, From, to, ?MODULE}).
+
+%% @hidden
+handle_cast(run, State) ->
+  purge_deleted(),
+  {noreply, State};
+handle_cast(Message, _State) ->
+  throw({unexpected_cast, Message, to, ?MODULE}).
+
+%% @hidden
+handle_info(_, State) -> {noreply, State}.
+
+%% @hidden
+terminate(_Reason, _State) ->
+  ok.
+
+%% @hidden
+code_change(_, State, _) -> {ok, State}.
+
+
+%% -------------------------------------------------------------------
+%% Internal functions
+%% -------------------------------------------------------------------
+
+purge_deleted() ->
+  {{delete, _}, []} = air_db:call(fun(Connection) ->
+        pgsql_connection:simple_query("DELETE FROM tasks WHERE deleted=true and purged=true", Connection)
+      end).
