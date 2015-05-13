a24f88241f73e7903209dccd8c48122ef2b872a3
Sasa Juric <sasa.juric@gmail.com>
2015-05-13 13:34:06 +0200

    fetch db settings from etcd

--- backend/src/air_db.erl
+++ backend/src/air_db.erl
@@ -7,9 +7,11 @@
 ]).
 
 -include("air.hrl").
+-include_lib("etcd/include/etcd_types.hrl").
 
 -type request_fun() :: fun((pgsql_connection:pgsql_connection()) -> any()).
 -type request_fun_spec() :: request_fun() | {request_fun(), non_neg_integer() | infinity}.
+
 
 %% -------------------------------------------------------------------
 %% API functions
@@ -18,4 +20,14 @@
 %% @doc Calls the given function on the local cloak database using connection parameters from `app.config'.
 -spec call(request_fun_spec()) -> any().
 call(Fun) ->
-  cloak_db:call(undefined, air_conf:get_val(air_db, connection), Fun).
+  cloak_db:call(undefined, db_config(), Fun).
+
+
+db_config() ->
+  [
+    {host, binary_to_list(air_etcd:get_cached("/settings/rails/db/host"))},
+    {user, binary_to_list(air_etcd:get_cached("/settings/rails/db/username"))},
+    {password, binary_to_list(air_etcd:get_cached("/settings/rails/db/password"))},
+    {database, binary_to_list(air_etcd:get_cached("/settings/rails/db/database"))},
+    {port, 5432}
+  ].
