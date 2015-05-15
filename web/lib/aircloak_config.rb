2a3827ab24a4ad03b4b24d4a99baf11b331f182a
Sasa Juric <sasa.juric@gmail.com>
2015-05-15 16:07:59 +0200

    don't read from etcd or db when precompiling assets

--- web/lib/aircloak_config.rb
+++ web/lib/aircloak_config.rb
@@ -6,6 +6,10 @@ class Conf
   @@client = nil
 
   def self.get path
+    # In precompile env we just compile assets, so we don't want to connect to etcd (it's possible that it's
+    # not running)
+    return "" if Rails.env == "precompile"
+
     setup
     @@client.get(path).value
   end
@@ -13,13 +17,22 @@ class Conf
 private
   def self.setup
     return if @@setup
+
     unless Rails.env == "test"
-      host = ENV['ETCD_HOST'] || "127.0.0.1"
-      port = ENV['ETCD_PORT'] || 4001
+      host = ENV['ETCD_HOST']
+      port = ENV['ETCD_PORT']
+
+      # In dev, we don't require env vars. If they're not present, we just use local ETCD
+      if Rails.env == "development"
+        host ||= "127.0.0.1"
+        port ||= "4001"
+      end
     else
+      # Hardcoded test ETCD endpoint
       host = "127.0.0.1"
       port = "4002"
     end
+
     @@client = Etcd.client(host: host, port: port)
     @@setup = true
   end
