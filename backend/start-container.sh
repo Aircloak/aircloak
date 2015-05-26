74992c6bb3019991fb274f9604769459f3b4705c
Sasa Juric <sasa.juric@gmail.com>
2015-05-26 11:48:35 +0200

    ensure named containers are stopped

--- backend/start-container.sh
+++ backend/start-container.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-docker run --rm --name air -it -p 11000:11000 -p 9000:9000 "$@" aircloak/air:latest
+cd $(dirname $0)
+. ../common/docker_helper.sh
+stop_named_container air_backend
+
+docker run --rm --name air_backend -it -p 11000:11000 -p 9000:9000 "$@" aircloak/air:latest
