80da9a6733d0a474ad8cdf8a5a81de30364265a3
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2015-07-08 10:23:48 +0200

    Remove remnants from old cap deploy on air1
    There were a lot of lingering recipes and
    templates that no longer are in use after
    we started using the dockerized deploys

--- frontend/config/recipes/ca.rb
+++
@@ -1,8 +0,0 @@
-namespace :ca do
-  desc "Symlink the ca folder into latest release"
-  task :symlink, roles: :app do
-    run "mkdir -p #{shared_path}/config/ca"
-    run "ln -nfs #{shared_path}/config/ca #{release_path}/config/ca"
-  end
-  after "deploy:finalize_update", "ca:symlink"
-end
--- frontend/config/recipes/nginx.rb
+++
@@ -1,8 +0,0 @@
-namespace :nginx do
-  %w[start stop restart].each do |command|
-    desc "#{command} nxing"
-    task command, roles: :web do
-      sudo "/etc/init.d/nginx #{command}"
-    end
-  end
-end
