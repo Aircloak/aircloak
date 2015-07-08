658db792dc7fe1e0070bbeb497aec534dae6fe06
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2015-07-08 10:24:38 +0200

    Add revision check to current cap deploy

--- config/check.rb
+++ config/check.rb
@@ -1,6 +1,6 @@
 namespace :check do
   desc "Make sure local git is in sync with remote."
-  task :revision, roles: :web do
+  task :revision do
     unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
       puts "WARNING: HEAD is not the same as origin/#{branch}"
       puts "Run `git push` to sync changes."
@@ -8,6 +8,4 @@ namespace :check do
     end
   end
   before "deploy", "check:revision"
-  before "deploy:migrations", "check:revision"
-  before "deploy:cold", "check:revision"
 end
