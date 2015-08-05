9c16663ce72c39550060eaa62d839375da814398
Sasa Juric <sasa.juric@gmail.com>
2015-08-05 09:42:42 +0200

    properly escape values

--- coreos/create_user_data.sh
+++ coreos/create_user_data.sh
@@ -7,12 +7,12 @@ cat <<-EOF > user-data
 coreos:
   etcd2:
     discovery: https://discovery.etcd.io/df692a97fdfa404321ab3040a5c67f0e
-    advertise-client-urls: http://$public_ipv4:2379,http://$public_ipv4:4001
-    initial-advertise-peer-urls: http://$public_ipv4:2380
+    advertise-client-urls: http://\$public_ipv4:2379,http://\$public_ipv4:4001
+    initial-advertise-peer-urls: http://\$public_ipv4:2380
     listen-client-urls: http://0.0.0.0:2379,http://0.0.0.0:4001
-    listen-peer-urls: http://$public_ipv4:2380
+    listen-peer-urls: http://\$public_ipv4:2380
   fleet:
-    public-ip: $public_ipv4
+    public-ip: \$public_ipv4
   units:
   - name: docker.service
     drop-ins:
