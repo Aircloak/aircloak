d3a870d92f641bfc86e220b626212ca45e3d8725
Sasa Juric <sasa.juric@gmail.com>
2015-08-13 14:58:19 +0200

    set proper frontend for docker env

--- etcd/config_docker.sh
+++ etcd/config_docker.sh
@@ -7,6 +7,7 @@ cd $(dirname $0)
 
 init_env
 cat etcd_values_dev | sed s/127.0.0.1/172.17.42.1/ > ./etcd_values_docker
+echo 'etcd_set /service/frontend/endpoint "http://${GATEWAY}:8080"' >> ./etcd_values_docker
 . etcd_values_docker
 if [ -f local_settings/docker ]; then
   . ./local_settings/docker
