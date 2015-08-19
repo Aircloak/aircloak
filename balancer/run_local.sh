5177e10d823d3b6ee1af1929cb94b7b3586d12f9
Sasa Juric <sasa.juric@gmail.com>
2015-08-19 11:52:42 +0200

    aircloak site

--- balancer/run_local.sh
+++ balancer/run_local.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 function upstream_contents {
   paste -d " " \
     <(cat docker/nginx/sites/upstreams.tmpl | grep upstream | awk '{print $2}') \
-    <(cat docker/nginx/sites/upstreams.tmpl | grep 'server $HOST_IP:' | awk '{print $2}' | sed s/\$HOST_IP://) \
+    <(cat docker/nginx/sites/upstreams.tmpl | grep 'server $HOST_IP:' | awk '{print $2}' | sed "s/\$HOST_IP://; s/;//") \
     | while read upstream; do
         upstream_name=$(echo $upstream | awk '{print $1}')
         forward_port=$(echo $upstream | awk '{print $2}')
@@ -54,29 +54,34 @@ EOF
 }
 
 function check_etc_hosts {
-  grep -nr server_name ./nginx_local | egrep -o '[a-zA-Z0-9]+\.local' | sort | uniq |
-    while read alias; do
-      if [ $(cat /etc/hosts | egrep -c "^127.0.0.1.*$alias$" || true) == "0" ]; then
-        echo "Add following to your /etc/hosts:"
-        echo "  127.0.0.1 $alias"
-        echo
-      fi
-    done
+  missing_sites=$(
+    grep -nr server_name ./nginx_local | egrep -o '[a-zA-Z0-9]+\.local' | sort | uniq |
+      while read alias; do
+        if [ $(cat /etc/hosts | egrep -c "^127.0.0.1.*$alias$" || true) == "0" ]; then
+          sites="$sites  127.0.0.1 $alias\n"
+          echo "$sites"
+        fi
+      done | tail -n 1
+  )
+
+  if [[ ! -z $missing_sites ]]; then
+    printf "You need to add following entries to your /etc/hosts:\n$missing_sites\n"
+  fi
 }
 
 nginx -c $(pwd)/nginx_local/nginx.conf -s stop > /dev/null 2>&1 || true
 
 generate_nginx_conf
+
+echo
 check_etc_hosts
 
 nginx -c $(pwd)/nginx_local/nginx.conf
 
-echo "You can access the site at:
-  Frontend:
-    http://frontend.local:8203
-    https://frontend.local:8202
+echo "You can access following sites:
+  http://frontend.local:8203
+  http://api.local:8203
+  http://aircloak.local:8203
 
-  API:
-    http://api.local:8203
-    https://api.local:8202
+You can also access these sites via https at the port 8202
 "
