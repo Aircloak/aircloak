c520db0d1c4edb71fc1031a47c9a4dc2dfff67ef
Sasa Juric <sasa.juric@gmail.com>
2015-08-20 09:14:49 +0200

    optimize start_dependencies

--- start_dependencies.sh
+++ start_dependencies.sh
@@ -4,7 +4,9 @@ set -eo pipefail
 
 cd $(dirname $0)
 
+# We always restart etcd (since settings may have changed). In contrast, db and
+# docker_registry are not restarted since these change practically never.
 ./etcd/container.sh start
-./db/container.sh start
-./docker_registry/container.sh start
+./db/container.sh ensure_started
+./docker_registry/container.sh ensure_started
 ./balancer/run_local.sh
