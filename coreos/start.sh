df10f59236f7555943a1ad8d68efd199272d32b7
Sasa Juric <sasa.juric@gmail.com>
2015-08-21 16:18:08 +0200

    change IP example

--- coreos/start.sh
+++ coreos/start.sh
@@ -66,9 +66,9 @@ if [ -z $COREOS_HOST_IP ]; then
   echo "
 COREOS_HOST_IP not set. Please run with:
 
-  COREOS_HOST_IP=x.y.z $0
+  COREOS_HOST_IP=w.x.y.z $0
 
-where x.y.z is the IP of your host on your local network.
+where w.x.y.z is the IP of your host on your local network.
 "
 exit 1
 fi
