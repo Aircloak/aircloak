c41290a5aa5dcd067099b07f938f6262d348fd33
Sasa Juric <sasa.juric@gmail.com>
2015-08-31 13:49:41 +0200

    use config ports in travis tests

--- etcd/config_travis.sh
+++ etcd/config_travis.sh
@@ -6,9 +6,9 @@ cd $(dirname $0)
 . ./etcd_lib.sh
 . ../config/config.sh
 
-export ETCD_PORT=4004
-export HOST_IP="127.0.0.1"
-export ETCD=$HOST_IP:$ETCD_PORT
+init_env test
 
-cat etcd_values_test | sed s/$(get_tcp_port prod database/tcp)/5432/ > ./etcd_values_travis
-. etcd_values_travis
+. ./etcd_values_test
+
+# overrides
+etcd_set /settings/air/db/port 5432
