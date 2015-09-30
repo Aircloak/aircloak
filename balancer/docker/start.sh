4afe7785d0a961489063137f561953e570f5df0f
Sasa Juric <sasa.juric@gmail.com>
2015-09-30 15:15:43 +0200

    haproxy based balancer

--- balancer/docker/start.sh
+++ balancer/docker/start.sh
@@ -13,17 +13,55 @@ function log {
 AIR_ROUTERS=${AIR_ROUTERS:-"127.0.0.1"}
 
 function upstreams {
+  index=1
   for server in $(echo $AIR_ROUTERS | tr ";" "\n"); do
-    printf "server $server:$1;\\\n"
+    printf "server router_$index $server:$1 check send-proxy\\\n"
+    index=$(($index+1))
   done
 }
 
-cat /aircloak/balancer/nginx.conf.tmpl \
+
+# Helper functions for running haproxy daemon in the docker.
+# We're running as daemon since it allows us to start multiple haproxy processes.
+# To make it work with docker, we do following:
+#   - keep this bash script running for as long as haproxy is running
+#   - trap SIGQUIT signals
+#   - on SIGQUIT, send SIGUSR1 to haproxy processes (soft stop)
+
+function haproxy_running {
+  pgrep haproxy > /dev/null
+  return $?
+}
+
+function wait_for_haproxy {
+  pgrep haproxy > /dev/null
+  while [ ! haproxy_running ]; do
+    echo "Waiting for haproxy to start..."
+    sleep 1
+  done
+}
+
+function soft_stop_haproxy {
+  echo "Sending SIGUSR1 to haproxy"
+  kill -SIGUSR1 $(pgrep haproxy)
+}
+trap soft_stop_haproxy SIGQUIT
+
+# need syslog for logs
+rsyslogd
+
+cat /aircloak/balancer/haproxy.cfg.tmpl \
   | sed "s#\$BALANCER_HTTPS_PORT#$(get_tcp_port prod balancer/https)#" \
   | sed "s#\$BALANCER_HTTP_PORT#$(get_tcp_port prod balancer/http)#" \
-  | sed "s#\$HTTPS_UPSTREAMS#$(upstreams $(get_tcp_port prod router/https))#" \
-  | sed "s#\$HTTP_UPSTREAMS#$(upstreams $(get_tcp_port prod router/http))#" \
-  > /aircloak/balancer/nginx.conf
+  | sed "s#\$HTTPS_UPSTREAMS#$(upstreams $(get_tcp_port prod router/proxy_https))#" \
+  | sed "s#\$HTTP_UPSTREAMS#$(upstreams $(get_tcp_port prod router/proxy_http))#" \
+  > /aircloak/balancer/haproxy.cfg
 
-log "Starting nginx"
-exec /usr/local/nginx/sbin/nginx -c "/aircloak/balancer/nginx.conf"
+haproxy -f /aircloak/balancer/haproxy.cfg
+wait_for_haproxy
+log "haproxy started"
+
+while haproxy_running; do
+  sleep 5
+done
+log "Haproxy not running -> stopping the container"
