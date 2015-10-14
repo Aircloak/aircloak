b9c3ff55a35de7f0fb751feae3c525a9fc340991
Sebastian Probst Eide <sebastian@aircloak.com>
2015-10-14 10:48:14 +0200

    Add text/csv as content-type for /infra/cloaks

--- frontend/app/controllers/infrastructure_api/cloaks_controller.rb
+++ frontend/app/controllers/infrastructure_api/cloaks_controller.rb
@@ -10,6 +10,6 @@ class InfrastructureApi::CloaksController < ApplicationController
     Cloak.all.each do |cloak|
       cloak_lines << "#{cloak.name};#{cloak.ip};#{cloak.tpm};#{cloak.good}"
     end
-    render text: cloak_lines.join("\n")
+    render text: cloak_lines.join("\n"), content_type: "text/csv"
   end
 end
