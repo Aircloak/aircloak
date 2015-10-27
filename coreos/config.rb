43e557169292caf7a58a5e84561a7b53f459c88a
Sasa Juric <sasa.juric@gmail.com>
2015-10-27 11:35:10 +0100

    use beta channel and the latest release

--- coreos/config.rb
+++ coreos/config.rb
@@ -6,12 +6,20 @@ $num_instances=5
 # "core-01" through to "core-${num_instances}".
 $instance_name_prefix="air"
 
-# Official CoreOS channel from which updates should be downloaded
-$update_channel='alpha'
+# CoreOS channel from which updates should be downloaded
+$update_channel='beta'
+
+# Determine latest CoreOS version from the specified channel
+require 'open-uri'
+most_recent_version =
+  open("http://#{$update_channel}.release.core-os.net/amd64-usr/").
+      read.
+      scan(/href="(.*?)\/"/)[-2][0]
+
+puts "Starting with CoreOS #{most_recent_version}" if ARGV[0] == "up"
 
 $vm_memory = 768
-
-$image_version = "833.0.0"
+$image_version = $most_recent_version
 $enable_serial_logging = false
 $share_home = false
 $vm_gui = false
