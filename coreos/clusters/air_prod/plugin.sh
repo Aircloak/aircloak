a9f7683f9d2433ddd92c6798ab37144d4a88e073
Sasa Juric <sasa.juric@gmail.com>
2015-11-30 12:03:14 +0100

    coreos prod plugin

---
+++ coreos/clusters/air_prod/plugin.sh
@@ -0,0 +1,15 @@
+SSH_OPTS="
+  -o PasswordAuthentication=no
+  -o IdentitiesOnly=yes
+  -o User=aircloak
+  -i /mnt/nfs/cosprod/id_rsa
+"
+
+function etcd_settings {
+  cat ../etcd/etcd_values_prod
+}
+
+function prepare_machine {
+  # copy docker registry auth info to the proper place
+  machine_ssh $1 "cp -rp /mnt/nfs/cosprod/.docker/config.json /aircloak/docker_config.json"
+}
