324aad449c7729a4bf66e1422a9f70cec2f3d8ff
Sebastian Probst Eide <sebastian@aircloak.com>
2016-02-04 17:45:50 +0100

    Remove migrations needed missing models
    We have removed models which are no longer
    needed, and need to remove their previous
    migrations as well. Otherwise there
    is no way to migrate our system from
    scratch.

--- frontend/db/migrate/20140908140123_add_prefetch_to_repeated_answer_task_code.rb
+++
@@ -1,5 +0,0 @@
-class AddPrefetchToRepeatedAnswerTaskCode < ActiveRecord::Migration
-  def change
-    add_column :repeated_answer_task_codes, :prefetch, :string
-  end
-end
--- frontend/db/migrate/20150303113408_add_cluster_to_repeated_answers.rb
+++
@@ -1,5 +0,0 @@
-class AddClusterToRepeatedAnswers < ActiveRecord::Migration
-  def change
-    add_belongs_to :repeated_answers, :cluster, index: true
-  end
-end
