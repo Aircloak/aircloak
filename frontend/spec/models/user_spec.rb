9aff0703f761e8d83df9b4a96c154efadd843c09
Sebastian Probst Eide <sebastian@aircloak.com>
2016-02-05 14:23:39 +0100

    Removes login requirement for help guides
    The help guides tried to be too smart.
    This pull removes the auto-generated
    content from the guides. This in turn
    allows us to share them with external
    users without requiring them to have
    a login. This is a big win :)

--- frontend/spec/models/user_spec.rb
+++ frontend/spec/models/user_spec.rb
@@ -14,19 +14,13 @@ describe User do
     # be able to validate the behaviour that only aircloakers can
     # be administrators, which the spec helper attempts to hide
     # from other tests.
-    u = User.new(
+    User.new(
       login: (params.delete(:login) || "test"),
       email: (params.delete(:email) || "example@example.com"),
       password: "1234",
       password_confirmation: "1234",
       analyst: Analyst.first
     )
-  end
-
-  it "attempt to create a legible name from the login" do
-    User.new(login: "root").attempt_to_make_a_human_name_from_login.should eq "Root"
-    User.new(login: "root.me").attempt_to_make_a_human_name_from_login.should eq "Root Me"
-    User.new(login: "f.broth").attempt_to_make_a_human_name_from_login.should eq "F Broth"
   end
 
   it "should prevent non-aircloakers from being admin" do
