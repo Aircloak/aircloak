a22524dba720f1796c94ef93bc3973df3cc787a1
Sasa Juric <sasa.juric@gmail.com>
2016-03-09 10:18:59 +0100

    make some functions private

--- air/site/web/views/user_view.ex
+++ air/site/web/views/user_view.ex
@@ -4,14 +4,14 @@ defmodule Air.UserView do
   # bug in the current Phoenix
   @dialyzer :no_match
 
-  def select_org_values(conn) do
+  defp select_org_values(conn) do
     organisations = conn.assigns[:organisations]
     Enum.reduce(organisations, [], fn(org, acc) ->
           [{org.name, org.id} | acc]
         end)
   end
 
-  def select_org_options(changeset, options \\ []) do
+  defp select_org_options(changeset, options) do
     options = [{:prompt, "Please select an organisation"} | options]
     case changeset.model.organisation_id do
       nil -> options
@@ -19,7 +19,7 @@ defmodule Air.UserView do
     end
   end
 
-  def organisation_name(user) do
+  defp organisation_name(user) do
     case user.organisation do
       nil -> "Not assigned an organisation"
       org -> org.name
