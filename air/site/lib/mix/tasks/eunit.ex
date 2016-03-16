25b6679a24a810cade738b8cd6de86e227d08c97
Sasa Juric <sasa.juric@gmail.com>
2016-03-16 12:24:26 +0100

    fix eunit task issue

--- air/site/lib/mix/tasks/eunit.ex
+++ air/site/lib/mix/tasks/eunit.ex
@@ -14,11 +14,22 @@ defmodule Mix.Tasks.Eunit do
 
   @doc false
   def run(args) do
+    project_config = Mix.Project.config
+
+    # It seems that modules are already loaded at this point, so eunit doesn't pick up new versions if the
+    # code is changed. To fix this, we're reloading all modules of this app.
+    for {module, path} <- :code.all_loaded,
+        String.starts_with?(to_string(path), "#{Mix.Project.build_path}/lib/#{project_config[:app]}/ebin/")
+    do
+      :code.purge(module)
+      :code.soft_purge(module)
+      {:module, _} = :code.load_file(module)
+    end
+
     Mix.Task.run("loadpaths", args)
     Mix.Task.run("app.start", args)
 
     Application.ensure_all_started(:eunit)
-    project_config = Mix.Project.config
     :eunit.test({:application, project_config[:app]}, project_config[:eunit_options] || [])
   end
 end
