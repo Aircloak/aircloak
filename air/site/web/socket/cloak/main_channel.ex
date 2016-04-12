1245187a7ee65f2145dcc522b6bef06cafa730fa
Sasa Juric <sasa.juric@gmail.com>
2016-04-12 11:14:41 +0200

    register cloak channel in etcd

--- air/site/web/socket/cloak/main_channel.ex
+++ air/site/web/socket/cloak/main_channel.ex
@@ -5,6 +5,7 @@ defmodule Air.Socket.Cloak.MainChannel do
   use Phoenix.Channel
   require Logger
 
+
   # -------------------------------------------------------------------
   # Phoenix.Channel callback functions
   # -------------------------------------------------------------------
@@ -12,7 +13,14 @@ defmodule Air.Socket.Cloak.MainChannel do
   @doc false
   def join("main", cloak_info, socket) do
     cloak_id = socket.assigns.cloak_id
-    Logger.info("cloak '#{cloak_id}' joined air")
+
+    # Using `ServiceRegistration` for strongly consistent discovery of the channel process.
+    {:ok, _} = Air.ServiceRegistration.start_link(
+          registration_key(cloak_id),
+          registration_value(),
+          crash_on_error: true
+        )
+
     {:ok, %{}, assign(socket, :cloak_info, cloak_info)}
   end
 
@@ -35,5 +43,19 @@ defmodule Air.Socket.Cloak.MainChannel do
     cloak_id = socket.assigns.cloak_id
     Logger.info("unhandled info #{message} from '#{cloak_id}'")
     {:noreply, socket}
+  end
+
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp registration_key(cloak_id) do
+    # base16 is used because the supported character set in the key is limited
+    "/settings/air/cloaks/#{Base.encode16(cloak_id)}/main"
+  end
+
+  defp registration_value do
+    self() |> :erlang.term_to_binary() |> Base.encode64()
   end
 end
