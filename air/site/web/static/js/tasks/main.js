433f886b2793696d3ee0a995f11d1cdcdd5908b7
Sebastian Probst Eide <sebastian@aircloak.com>
2016-04-14 11:00:41 +0200

    Adds manual task save
    Ask user for confirmation if they are leaving
    the page, having made unsaved changes.

--- air/site/web/static/js/tasks/main.js
+++ air/site/web/static/js/tasks/main.js
@@ -7,30 +7,61 @@ class TaskEditor extends React.Component {
     super();
 
     this.state = {
+      // active state used in the query editor
+      // and task name field
       query: props.query,
       name: props.name,
+
+      // We keep some stats on whether or not
+      // the task is running. This is used
+      // throughout the interface to toggle
+      // buttons and show progress bars
       running_percent: -1,
-      isSaved: true
     }
+    // we keep a backup copy of the last saved
+    // state of the task, in order to be able to
+    // verify whether the task needs saving or not
+    this.state.savedState = this.queryData();
 
     // Bind the handlers, so we can pass them, and have `this`
     // bound to the right context
+    this.checkForUnsavedChanges = this.checkForUnsavedChanges.bind(this);
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleCodeChange = this.handleCodeChange.bind(this);
     this.handleRunTask = this.handleRunTask.bind(this);
-    this.updateServer = this.updateServer.bind(this);
+    this.isSaved = this.isSaved.bind(this);
+    this.saveTask = this.saveTask.bind(this);
 
-    // We throttle updates, so that we ensure we don't blast the server
-    this.pending_save_request = false
-    setInterval(this.updateServer, 1000);
+    // To prevent the user loosing changes, we ask whether
+    // the page should be closed, if changes have been
+    // made since the last time the task was saved.
+    window.onbeforeunload = this.checkForUnsavedChanges;
+  }
+  checkForUnsavedChanges() {
+    if (!this.isSaved()) {
+      return "Your task has unsaved changes!";
+    }
   }
   handleNameChange(name) {
     this.setState({name: name});
-    this.scheduleUpdate();
   }
   handleCodeChange(query) {
     this.setState({query: query});
-    this.scheduleUpdate();
+  }
+  updateSavedState() {
+    this.setState({savedState: this.queryData()});
+  };
+  queryData() {
+    return {
+      task: {
+        query: this.state.query,
+        name: this.state.name
+      }
+    };
+  }
+  isSaved() {
+    // Hack to get object comparison in order to see whether or not the object is changed
+    return (JSON.stringify(this.state.savedState) == JSON.stringify(this.queryData()));
   }
   handleRunTask() {
     this.setState({running_percent: 0});
@@ -45,35 +76,19 @@ class TaskEditor extends React.Component {
           }
         });
   }
-  scheduleUpdate() {
-    this.setState({isSaved: false});
-    this.pending_save_request = true;
-  }
-  updateServer() {
-    if (this.pending_save_request) {
-      this.pending_save_request = false;
-      $.ajax(`/tasks/${this.props.id}`, {
-            context: this,
-            method: "PUT",
-            headers: {
-              "X-CSRF-TOKEN": this.props.csrf_token
-            },
-            data: {
-              task: {
-                query: this.state.query,
-                name: this.state.name
-              }
-            },
-            success: (responseData, textStatus) => {
-              if (!this.pending_save_request) {
-                this.setState({isSaved: true});
-              }
-            },
-            error: (jqXHR, textStatus) => {
-              console.log(`Error: ${textStatus}`);
-            }
-        });
-    }
+  saveTask() {
+    $.ajax(`/tasks/${this.props.id}`, {
+          context: this,
+          method: "PUT",
+          headers: {
+            "X-CSRF-TOKEN": this.props.csrf_token
+          },
+          data: this.queryData(),
+          success: this.updateSavedState,
+          error: (jqXHR, textStatus) => {
+            console.log(`Error: ${textStatus}`);
+          }
+      });
   }
   render() {
     return (
@@ -81,8 +96,9 @@ class TaskEditor extends React.Component {
         <StatusLine
             running_percent={this.state.running_percent}
             name={this.state.name}
-            isSaved={this.state.isSaved}
+            isSavedCheck={this.isSaved}
             onNameChange={this.handleNameChange}
+            onTaskSaveClick={this.saveTask}
             onRunTaskClick={this.handleRunTask} />
         <CodeEditor query={this.state.query} onChange={this.handleCodeChange} />
         <SidePane />
@@ -101,31 +117,47 @@ class StatusLine extends React.Component {
     this.props.onNameChange(name);
   }
   render() {
-    var saveState = "";
-    if (this.props.isSaved) {
-      saveState = "Task saved";
-    } else {
-      saveState = "Saving...";
-    }
     return (
       <div id="task-status-bar">
         <div className="task-status-bar-component">
           <input type="text" onChange={this.handleNameChange} value={this.props.name} />
         </div>
-        <div id="save-state">
-          {saveState}
-        </div>
+        <SaveButton {...this.props} />
         <RunButton {...this.props} />
       </div>
     );
   }
 }
 
+class SaveButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSaveTaskClick = this.handleSaveTaskClick.bind(this);
+  }
+  handleSaveTaskClick() {
+    if (!this.props.isSavedCheck()) {
+      this.props.onTaskSaveClick();
+    }
+  }
+  render() {
+    var classNames = "btn btn-primary";
+    if (this.props.isSavedCheck()) {
+      classNames += " disabled";
+    }
+    return (
+      <div className="task-status-bar-button-group">
+        <button type="button" className={classNames} onClick={this.handleSaveTaskClick}>
+          Save task
+        </button>
+      </div>
+    );
+  }
+}
 class RunButton extends React.Component {
   render() {
     if (this.props.running_percent < 0) {
       return (
-        <div id="task-run">
+        <div className="task-status-bar-button-group">
           <button type="button" className="btn btn-primary" onClick={this.props.onRunTaskClick}>
             Run task
           </button>
@@ -133,7 +165,7 @@ class RunButton extends React.Component {
       );
     } else {
       return (
-        <div id="task-run">
+        <div className="task-status-bar-button-group">
           <button type="button" className="btn btn-primary disabled">
             Run task
           </button>
