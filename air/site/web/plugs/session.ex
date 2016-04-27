fe428f7995b73687e2b7e4ca8e59f779909ffc11
Sebastian Probst Eide <sebastian@aircloak.com>
2016-04-27 15:39:34 +0200

    Improve error message layout

--- air/site/web/plugs/session.ex
+++ air/site/web/plugs/session.ex
@@ -24,24 +24,36 @@ defmodule Air.Plug.Session do
         [] ->
           conn
           |> put_status(Plug.Conn.Status.code(:unauthorized))
-          |> Phoenix.Controller.json(%{success: false, description: "The Aircloak API's are authenticated with \
-auth-tokens. You can get create auth-tokens for your account at https://insights.aircloak.com/api_tokens. \
-The token should be sent with your request via the HTTP header 'auth-token'. \
-For example, using curl, you would make your request like this: `curl -H 'auth-token: <token-value>' ...` \
-where <token-value> is your auth token."})
+          |> Phoenix.Controller.json(%{success: false, description: missing_auth_header_error()})
           |> halt()
         [token] ->
           case Air.ApiToken.user_for_token(conn, token) do
             :error ->
               conn
               |> put_status(Plug.Conn.Status.code(:unauthorized))
-              |> Phoenix.Controller.json(%{success: false, description: "Invalid auth-token. This could be a \
-result of the auth-token being incorrectly sent to the API backend, or the auth-token having been revoked. \
-You can validate that your auth-token is still valid by visiting https://insights.aircloak.com/api_tokens."})
+              |> Phoenix.Controller.json(%{success: false, description: invalid_auth_token_error()})
               |> halt()
             user -> assign(conn, :current_user, user)
           end
       end
+    end
+
+
+    # -------------------------------------------------------------------
+    # API error messages
+    # -------------------------------------------------------------------
+
+    defp missing_auth_header_error() do
+      "The Aircloak API's are authenticated with auth-tokens. You can get create auth-tokens for your account " <>
+      "at https://insights.aircloak.com/api_tokens. The token should be sent with your request via the HTTP " <>
+      "header 'auth-token'. For example, using curl, you would make your request like this: " <>
+      "`curl -H 'auth-token:<token-value>' ...` where <token-value> is your auth token."
+    end
+
+    defp invalid_auth_token_error() do
+      "Invalid auth-token. This could be a result of the auth-token being incorrectly sent to the API backend, " <>
+      "or the auth-token having been revoked. You can validate that your auth-token is still valid by visiting " <>
+      "https://insights.aircloak.com/api_tokens."
     end
   end
 
