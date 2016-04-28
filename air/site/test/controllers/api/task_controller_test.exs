bec46386334afef03c9f28e32e3bc58b9a6107ef
Sebastian Probst Eide <sebastian@aircloak.com>
2016-04-28 15:37:11 +0200

    Adds task execution to the task execution API
    This change allows tasks to be executed synchronously
    using the API. It will block for up to 10 minutes, and
    then return an error, should the task not have completed
    until then.

--- air/site/test/controllers/api/task_controller_test.exs
+++ air/site/test/controllers/api/task_controller_test.exs
@@ -1,17 +1,83 @@
 defmodule Air.API.TaskControllerTest do
   use Air.ConnCase
-  import Air.{TestAuthHelper, TestRepoHelper}
 
-  test "authenticates properly", %{conn: conn} do
+  import Air.{TestAuthHelper, TestRepoHelper}
+  alias Air.{Task, TestSocketHelper}
+
+  test "requires parameters", %{conn: conn} do
     conn = add_auth_to_conn(conn)
     token = create_token(conn, create_user!())
 
-    %{"success" => success} = conn
+    %{"success" => success, "errors" => errors} = conn
         |> put_req_header("auth-token", token)
         |> post("/api/task")
-        |> response(200)
+        |> response(422)
         |> Poison.decode!
 
-    assert success
+    assert errors
+        |> Enum.map(fn(str) -> Regex.run(~r"The '(\w+)' parameter", str, capture: :all_but_first) end)
+        |> Enum.map(&hd(&1))
+        |> Enum.sort == ["cloak_id", "data_source", "query", "tables"]
+
+    refute success
+  end
+
+  test "executes task", %{conn: conn} do
+    conn = add_auth_to_conn(conn)
+    user = create_user!()
+    token = create_token(conn, user)
+
+    # Open the cloak mock socket so we can schedule a task execution against it
+    socket = TestSocketHelper.connect!(%{cloak_name: "cloak_1"})
+    TestSocketHelper.join!(socket, "main", %{"data_sources" => [
+        %{"id" => "local", "tables" => [%{"columns" => [
+          %{"name" => "item", "type" => "text"},
+          %{"name" => "price", "type" => "number"}
+        ], "id" => "test"}]}], "name" => "nonode@nohost"})
+
+    query_params = %{
+      query: "report_property(\"Hello\", \"world\")",
+      cloak_id: "unknown_org/nonode@nohost",
+      data_source: "local",
+      tables: ["test"]
+    }
+
+    # Since the call scheduling the task is blocking, we need to perform it
+    # in a secondary thread.
+    me = self()
+    spawn_link(fn ->
+          response = conn
+              |> put_req_header("auth-token", token)
+              |> put_req_header("content-type", "application/json")
+              |> post("/api/task", query_params)
+              |> response(200)
+              |> Poison.decode!
+          send(me, {:response, response})
+        end)
+
+    # We block waiting for the task to have been created by the request,
+    # otherwise we don't know which ID we should send our fake results to
+    task_id = wait_for_task_id(query_params.query)
+
+    # Simulate that the cloak has accepted the task
+    TestSocketHelper.respond_to_start_task_request!(socket, task_id, "ok")
+
+    # Simulate that the cloak broadcasts the response.
+    Air.Endpoint.broadcast_from!(self(), "task:#{task_id}", "result", %{bucket: "value"})
+
+    # Verify the cloak response
+    assert_receive {:response, %{"success" => status, "bucket" => value}}
+    assert status
+    assert value == "value"
+  end
+
+  defp wait_for_task_id(query) do
+    case Repo.one(from task in Task, where: task.query == ^query, select: task) do
+      nil ->
+        :timer.sleep(100)
+        wait_for_task_id(query)
+      task ->
+        task.id
+    end
   end
 end
--- air/site/web/controllers/api/task_controller.ex
+++ air/site/web/controllers/api/task_controller.ex
@@ -3,6 +3,7 @@ defmodule Air.API.TaskController do
   use Air.Web, :controller
 
   require Logger
+  alias Air.Task
 
 
   # -------------------------------------------------------------------
@@ -20,7 +21,168 @@ defmodule Air.API.TaskController do
   # Actions
   # -------------------------------------------------------------------
 
-  def run_task(conn, _params) do
-    json(conn, %{success: true})
+  @doc """
+  Executes a task sent in by the API user, reusing the standard task execution pipeline.
+  The requester will receive an error should the task not finish executing within 10 minutes.
+  This does not mean that the task doesn't infact still run.
+
+  The following parameters are required:
+
+  - query: the lua task to be executed
+  - cloak_id: the ID of the cloak on which the task should execute.
+      No checks are performed to ensure that the cloak belongs to the user's organisation.
+      The cloak_id takes the format `unknown_org/<node-name>`
+  - data_source: the name of the data source in the selected cloak
+  - tables: a list of tables that should be made available to the task
+
+  You can test this endpoint in your local development environment using the following curl command:
+  curl --data '{"query":"report_property(\"Hello\", \"world\")", "cloak_id":"unknown_org/nonode@nohost", \
+      "data_source":"local", "tables":["test"]}' \
+      -H "auth-token: <valid auth token>" \
+      -H "content-type: application/json" -k -XPOST \
+      https://insights.air-local:20000/api/task
+  """
+  def run_task(conn, task_payload) do
+    case validate_params(conn, task_payload) do
+      {:ok, validated_params} -> save_and_run_task(conn, validated_params)
+      {:error, conn} -> conn
+    end
+  end
+
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp param_validations() do
+    [
+      %{name: "query", description: "should contain the task code you want to execute, for example: " <>
+          "\"query\":\"report_property(\"hello\", \"world\")\""},
+      %{name: "cloak_id", description: "should contain the id of the cloak you have installed. " <>
+          "The id usually takes the form '<the name of organisation>/<hostname>', so you could for " <>
+          "example include the following statement: \"cloak_id\":\"my-org/hostname\""},
+      %{name: "data_source", description: "should contain the name of the data source you want to use. " <>
+          "A data source mostly corresponds to a database. Your 'data_source' parameter could for example " <>
+          "look like: \"data_source\":\"my-db\""},
+      %{name: "tables", description: "should consist of a list the tables contained within your data source " <>
+          "that you want made available to your task. For example: \"tables\":[\"my-table\"]", validator: fn(tables) ->
+            case is_list(tables) do
+              true -> :ok
+              false -> {:error, "should be a list of tables declared as strings, for example \"tables\":" <>
+                  "[\"my-table1\", \"my-table2\"]"}
+            end
+          end}
+    ]
+  end
+
+  defp validate_params(conn, payload) do
+    validation_fun = fn(%{name: name, description: description} = req,
+        %{params: params, errors: existing_errors} = acc) ->
+          add_error = fn(error) -> %{acc| errors: [%{parameter: name, problem: error}|existing_errors]} end
+          add_value = fn(value) -> %{acc| params: Map.put(params, name, value)} end
+          case payload[name] do
+            nil -> add_error.(description)
+            value ->
+              case req[:validator] do
+                nil -> add_value.(value)
+                validator -> case validator.(value) do
+                  :ok -> add_value.(value)
+                  {:error, error} -> add_error.(error)
+                end
+              end
+          end
+        end
+    %{params: params, errors: errors} = List.foldl(param_validations(), %{params: %{}, errors: []}, validation_fun)
+    case errors do
+      [] -> {:ok, params}
+      _ ->
+        response = %{
+          success: false,
+          description: "Your request needs some attention - your task was not executed. Please consult the API " <>
+              "documentation for further information on how to use this API endpoint. " <>
+              "The specific errors are described in more defail in the 'errors' field",
+          errors: Enum.map(errors, fn(error) -> "The '#{error.parameter}' parameter #{error.problem}" end)
+        }
+
+        {:error, conn
+            |> put_status(:unprocessable_entity)
+            |> json(response)}
+    end
+  end
+
+
+  defp save_and_run_task(conn, params) do
+    changeset = build_assoc(conn.assigns.current_user, :tasks)
+    |> Task.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, task} ->
+        execute_task(conn, task)
+      {:error, changeset} ->
+        # The task was validated, but didn't save. Let's crash and report
+        Logger.error([
+              "Error saving task: #{inspect(changeset)}\n",
+              Exception.format_stacktrace(System.stacktrace())
+            ])
+
+        send_resp(conn, Plug.Conn.Status.code(:internal_server_error), "")
+    end
+  end
+
+  defp execute_task(conn, task) do
+    try do
+      # We have to subscribe to task results before we schedule the task, otherwise
+      # there is a race condition, where the task result is returned before we have
+      # time to notice, and then end up waiting indefinitely for a result that has
+      # already been sent.
+      Air.Endpoint.subscribe(self(), "task:#{task.id}")
+      case Air.Socket.Cloak.MainChannel.run_task(task.cloak_id, Task.to_cloak_query(task)) do
+        :ok ->
+          await_response(send_chunked(conn, 200), task, 0)
+        {:error, :not_connected} ->
+          json(conn, %{success: false, reason: "the cloak is not connected"})
+        {:error, other} ->
+          Logger.error("Task start error: #{other}")
+          json(conn, %{success: false, reason: "an error has occurred"})
+      end
+    catch type, error ->
+      # We'll make a nice error log report and return 500
+      Logger.error([
+            "Error starting a task: #{inspect(type)}:#{inspect(error)}\n",
+            Exception.format_stacktrace(System.stacktrace())
+          ])
+
+      send_resp(conn, Plug.Conn.Status.code(:internal_server_error), "")
+    end
+  end
+
+  @ten_minutes_in_ms 600000
+
+  defp await_response(conn, _task, time_waited) when time_waited > @ten_minutes_in_ms do
+    response = %{
+      success: false,
+      description: "The task execution timed out. In the future you will also be able to run " <>
+          "tasks asynchronously using this API end-point. Until then, please run your tasks via " <>
+          "the web interface at https://insights.aircloak.com"
+    }
+    {:ok, conn} = chunk(conn, Poison.encode!(response))
+    conn
+  end
+  defp await_response(conn, task, time_waited) do
+    receive do
+      %Phoenix.Socket.Broadcast{event: "result", payload: payload} ->
+        {:ok, conn} = chunk(conn, Poison.encode!(Map.merge(%{success: true}, payload)))
+        conn
+      other ->
+        Logger.warn("Received unexpected response while waiting for query to complete: #{inspect(other)}")
+        keep_connection_alive(conn, task, time_waited)
+    after
+      10_000 -> keep_connection_alive(conn, task, time_waited + 10_000)
+    end
+  end
+
+  defp keep_connection_alive(conn, task, time_waited) do
+    {:ok, conn} = chunk(conn, " ")
+    await_response(conn, task, time_waited)
   end
 end
