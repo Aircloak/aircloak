4ad01463fa256f7c59b61cf3fb4122df1c2db90b
Sasa Juric <sasa.juric@gmail.com>
2016-05-02 09:54:04 +0200

    js_vm_worker
    Almost a carbon port from the backend project with a few minor changes and tests written in Elixir.

---
+++ common/js_sandbox/lib/logger.ex
@@ -0,0 +1,9 @@
+defmodule Aircloak.JsSandbox.Logger do
+  @moduledoc false
+  # TODO: replace this temp workaround by extracting Erlang -> Logger bridge from cloak
+  #       to common, and reusing it here.
+
+  require Logger
+
+  defdelegate error(msg), to: Logger
+end
--- common/js_sandbox/test/js_sandbox_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule JsSandboxTest do
-  use ExUnit.Case
-  doctest JsSandbox
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
