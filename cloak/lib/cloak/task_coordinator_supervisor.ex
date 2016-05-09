e2b5e1c7836749da0fec43c167a2d25c45f801a9
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-09 11:49:56 +0200

    Add docs for TaskCoordinatorSupervisor

--- cloak/lib/cloak/task_coordinator_supervisor.ex
+++ cloak/lib/cloak/task_coordinator_supervisor.ex
@@ -1,4 +1,10 @@
 defmodule Cloak.TaskCoordinatorSupervisor do
+  @moduledoc """
+  All task_coordinators are started under this supervisor. In the usual case you should use
+  Cloak.TaskCoordinatorSupervisor.start_worker/1 instead of task_coordinator:start_link/2 to make sure newly
+  created task_coordinators are part of the supervision tree.
+  """
+
   def start_link do
     import  Supervisor.Spec
 
