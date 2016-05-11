a9ebc6dbce32a6e901668e873bcbc7ad34558b16
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-11 15:49:59 +0200

    Add docs for Air.Utils.Process

--- air/site/lib/air/utils/process.ex
+++ air/site/lib/air/utils/process.ex
@@ -1,4 +1,13 @@
 defmodule Air.Utils.Process do
+  @moduledoc """
+  Utilities for working with processes
+  """
+
+  @doc """
+  Checks if process `pid` on node `node_name` is alive. Works just like `Process.alive?` but also for remote
+  processes.
+  """
+  @spec alive?(node_name :: atom, pid :: pid) :: boolean
   def alive?(node_name, pid) do
     case :rpc.call(node_name, Process, :alive?, [pid]) do
       true -> true
