e02a86d9151f67a055964dd4145a2e3cf5d25caf
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-12 11:02:16 +0200

    Turn off support for timestamps
    ...for now. It's not implemented in job_data_streamer yet

--- cloak/lib/cloak/data_source/postgresql.ex
+++ cloak/lib/cloak/data_source/postgresql.ex
@@ -80,7 +80,6 @@ defmodule Cloak.DataSource.PostgreSQL do
   defp parse_type("float8"), do: :number
   defp parse_type("money"), do: :number
   defp parse_type("numeric"), do: :number
-  defp parse_type("timestamp"), do: :timestamp
   defp parse_type(type), do: {:unsupported, type}
 
   defp build_metadata_query(table, filter, 0) do
