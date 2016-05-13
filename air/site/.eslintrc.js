c083595044b8210b8fd0ca09954601c93ee9e586
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-13 11:23:18 +0200

    Allow multiple components per file in eslint

--- air/site/.eslintrc.js
+++ air/site/.eslintrc.js
@@ -9,5 +9,7 @@ module.exports = {
       "quotes": ["error", "double"],
       "object-curly-spacing": ["error", "never"],
       "max-len": ["error", {"code": 110}],
+      /* FIXME break out all components to separate files and turn this on */
+      "react/no-multi-comp": ["off"],
     }
 };
