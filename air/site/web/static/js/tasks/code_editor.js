eacef18c6ad6e7e3514235d1b737b803aa9aea9c
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-13 12:42:17 +0200

    Fix eslint errors in code_editor.js

--- air/site/web/static/js/tasks/code_editor.js
+++ air/site/web/static/js/tasks/code_editor.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Codemirror from 'react-codemirror';
-import _ from 'lodash';
+import React from "react";
+import Codemirror from "react-codemirror";
+import _ from "lodash";
 
-require('codemirror/mode/lua/lua');
-require('codemirror/addon/hint/show-hint');
-require('codemirror/addon/hint/anyword-hint');
+require("codemirror/mode/lua/lua");
+require("codemirror/addon/hint/show-hint");
+require("codemirror/addon/hint/anyword-hint");
 
 export class CodeEditor extends React.Component {
   constructor(props) {
@@ -25,19 +25,19 @@ export class CodeEditor extends React.Component {
     this.editor = instance;
     instance.commands.save = (_cm) => {
       this.props.onSave();
-    }
+    };
     instance.commands.run = (_cm) => {
       this.props.onRun();
-    }
+    };
     instance.commands.autoComplete = (cm) => {
       cm.showHint({hint: this.completionList});
-    }
+    };
   }
 
   completionList(cm) {
-    let regex = /(\w|\.)/;
-    let cur = cm.getCursor();
-    let curLine = cm.getLine(cur.line);
+    const regex = /(\w|\.)/;
+    const cur = cm.getCursor();
+    const curLine = cm.getLine(cur.line);
     let start = cur.ch;
     let end = start;
 
@@ -49,69 +49,86 @@ export class CodeEditor extends React.Component {
       start--;
     }
 
-    let curWord = curLine.slice(start, end);
-    let fuzzyMatcher = new RegExp(curWord.replace(/(.)/g, '$1.*'), 'i');
+    const curWord = curLine.slice(start, end);
+    const fuzzyMatcher = new RegExp(curWord.replace(/(.)/g, "$1.*"), "i");
 
-    let sortOrder = (text) => {
+    const sortOrder = (text) => {
       // Place items that start with the given word at the top of the completion list.
       if (text.startsWith(curWord)) {
-        return '0' + text;
+        return `0${text}`;
       } else {
-        return '1' + text;
+        return `1${text}`;
       }
-    }
-
-    let
-      dataSource = this.props.settings.selectedDataSource(),
-      selectedTables = Array.from(this.props.settings.tables),
-      list = _.chain([]).
-        union(
-          this.props.completions,
-          _.map(selectedTables, (table) => {return {text: `load_user_table("${dataSource.id}/${table}")`}}),
-          _.map(selectedTables, (table) => {return {text: `user_table("${dataSource.id}/${table}")`}}),
-          _.map(this.editor.hint.anyword(cm, {word: /[a-zA-Z_](\w)*/}).list,
-            (word) => {return {text: word}})
-        ).
-        filter((candidate) => {return candidate.text.match(fuzzyMatcher)}).
-        uniqBy((el) => {return el.displayText || el.text}).
-        sortBy((el) => {return sortOrder(el.text)}).
-        value();
-
-    let returnValue = {
-      list: list,
-      from: this.editor.Pos(cur.line, start),
-      to: this.editor.Pos(cur.line, end)
     };
-    return returnValue;
+
+    const dataSource = this.props.settings.selectedDataSource();
+    const selectedTables = Array.from(this.props.settings.tables);
+    const list = _.chain([]).
+      union(
+        this.props.completions,
+        _.map(selectedTables, (table) => ({text: `load_user_table("${dataSource.id}/${table}")`})),
+        _.map(selectedTables, (table) => ({text: `user_table("${dataSource.id}/${table}")`})),
+        _.map(this.editor.hint.anyword(cm, {word: /[a-zA-Z_](\w)*/}).list, (word) => ({text: word}))
+      ).
+      filter((candidate) => candidate.text.match(fuzzyMatcher)).
+      uniqBy((el) => el.displayText || el.text).
+      sortBy((el) => sortOrder(el.text)).
+      value();
+
+    return {
+      list,
+      /* eslint-disable new-cap */
+      from: this.editor.Pos(cur.line, start),
+      to: this.editor.Pos(cur.line, end),
+      /* eslint-enable new-cap */
+    };
   }
 
   render() {
-    var options = {
+    const options = {
       autofocus: true,
       indentUnit: 2,
       indentWithTabs: false,
       lineNumbers: true,
       lineWrapping: true,
       matchBrackets: true,
-      mode: 'lua',
+      mode: "lua",
       showCursorWhenSelecting: true,
       smartIndent: true,
       viewportMargin: Infinity,
 
       extraKeys: {
-        'Ctrl-S': 'save',
-        'Ctrl-R': 'run',
-        'Ctrl-Space': 'autoComplete'
-      }
+        "Ctrl-S": "save",
+        "Ctrl-R": "run",
+        "Ctrl-Space": "autoComplete",
+      },
     };
     return (
-      <div className={this.props.sidePaneHidden() ? 'side-panel-hidden' : 'side-panel-shown' }>
+      <div className={this.props.sidePaneHidden() ? "side-panel-hidden" : "side-panel-shown"}>
         <Codemirror
           ref={this.setupComponent}
           value={this.props.query}
           onChange={this.props.onChange}
-          options={options} />
+          options={options}
+        />
       </div>
     );
   }
+}
+
+CodeEditor.propTypes = {
+  onSave: React.PropTypes.func.isRequired,
+  onRun: React.PropTypes.func.isRequired,
+  onChange: React.PropTypes.func.isRequired,
+
+  sidePaneHidden: React.PropTypes.func.isRequired,
+  settings: React.PropTypes.shape({
+    selectedDataSource: React.PropTypes.func.isRequired,
+    tables: React.PropTypes.object,
+  }),
+  completions: React.PropTypes.arrayOf(React.PropTypes.shape({
+    displayText: React.PropTypes.string,
+    text: React.PropTypes.string,
+  })),
+  query: React.PropTypes.string,
 };
