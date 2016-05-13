ef8652d21d8974fbda157bd79ba5d92262ca5ab8
Cristian Berneanu <cristian@aircloak.com>
2016-05-13 13:45:15 +0300

    Move old result check into view

--- air/site/web/views/result_view.ex
+++ air/site/web/views/result_view.ex
@@ -7,4 +7,8 @@ defmodule Air.ResultView do
   defp to_js_timestamp(date_time) do
     Ecto.DateTime.to_iso8601(date_time)
   end
+
+  defp is_old_result?(result, task) do
+    result.timestamp < task.updated_at
+  end
 end
