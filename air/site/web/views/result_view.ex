456f1aa1cd2fc997e51b086a9795352d84c5be31
Cristian Berneanu <cristian@aircloak.com>
2016-05-13 13:31:41 +0300

    Move timestamp conversion into the view

--- air/site/web/views/result_view.ex
+++ air/site/web/views/result_view.ex
@@ -3,4 +3,8 @@ defmodule Air.ResultView do
   use Air.Web, :view
   # bug in the current Phoenix
   @dialyzer :no_match
+
+  defp to_js_timestamp(date_time) do
+    Ecto.DateTime.to_iso8601(date_time)
+  end
 end
