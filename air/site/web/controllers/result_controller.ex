a2902b07fe48a1a3764db63148ac3d5c9f543607
Cristian Berneanu <cristian@aircloak.com>
2016-05-13 15:03:52 +0300

    Reduce the number of maximum columns

--- air/site/web/controllers/result_controller.ex
+++ air/site/web/controllers/result_controller.ex
@@ -27,7 +27,7 @@ defmodule Air.ResultController do
   def index(conn, _params) do
     # Preload and unpack the task results.
     task = conn.assigns.task |> Repo.preload(results: (from r in Result, order_by: [desc: r.id]))
-    results = for result <- task.results, do: Result.unpack(result, 15)
+    results = for result <- task.results, do: Result.unpack(result, 10)
 
     # Create the list of unique bucket names.
     headers = (for %{buckets: buckets} <- results, {label, value, _count} <- buckets, do: {label, value}) |> Enum.uniq
