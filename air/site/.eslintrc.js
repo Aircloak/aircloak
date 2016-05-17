6d4841f6578cfa9b3943ce89c5130322f6317f54
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-17 09:49:50 +0200

    Reformat .eslintrc

--- air/site/.eslintrc.js
+++ air/site/.eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-      "allow-unused-underscore-vars": ["error"],
-      "no-else-return": ["off"],
-      "quotes": ["error", "double"],
-      "object-curly-spacing": ["error", "never"],
-      "max-len": ["error", {"code": 110}],
-      /* FIXME break out all components to separate files and turn this on */
-      "react/no-multi-comp": ["off"],
-    }
+  "extends": "airbnb",
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "allow-unused-underscore-vars": ["error"],
+    "no-else-return": ["off"],
+    "quotes": ["error", "double"],
+    "object-curly-spacing": ["error", "never"],
+    "max-len": ["error", {"code": 110}],
+    /* FIXME break out all components to separate files and turn this on */
+    "react/no-multi-comp": ["off"],
+  }
 };
