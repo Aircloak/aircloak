92fd8bdf56da6a4dff3adf5361725408df20c912
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-17 11:22:36 +0200

    Add a timeout to Process.alive?

--- air/site/lib/air/utils/process.ex
+++ air/site/lib/air/utils/process.ex
@@ -3,12 +3,14 @@ defmodule Air.Utils.Process do
   Utilities for working with processes
   """
 
+  @timeout :timer.seconds(1)
+
   @doc """
   Checks if process `pid` is alive. Works just like `Process.alive?` but also for remote processes.
   """
   @spec alive?(pid :: pid) :: boolean
   def alive?(pid) do
-    case :rpc.call(node(pid), Process, :alive?, [pid]) do
+    case :rpc.call(node(pid), Process, :alive?, [pid], @timeout) do
       true -> true
       _ -> false
     end
