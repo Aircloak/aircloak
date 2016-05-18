8a36c928266620feaded976544c46d8d937e1131
Sasa Juric <sasa.juric@gmail.com>
2016-05-18 11:50:06 +0200

    basic sql queries parser
    resolves #147

---
+++ cloak/test/cloak/sql_query_test.exs
@@ -0,0 +1,35 @@
+defmodule Cloak.SqlQueryTest do
+  use ExUnit.Case, async: true
+
+  alias Cloak.SqlQuery
+
+  test "simple select query" do
+    assert %Cloak.SqlQuery{select: ["foo"], from: "baz"} == SqlQuery.parse!("select foo from baz")
+  end
+
+  test "multiple fields" do
+    assert %Cloak.SqlQuery{select: ["foo", "bar"], from: "baz"} == SqlQuery.parse!("select foo, bar from baz")
+  end
+
+  test "multiline query" do
+    assert %Cloak.SqlQuery{select: ["foo"], from: "baz"} == SqlQuery.parse!("select foo\nfrom\n\nbaz")
+  end
+
+  test "all allowed identifier characters" do
+    assert %Cloak.SqlQuery{select: ["foO1_"], from: "Ba_z2"} == SqlQuery.parse!("select foO1_ from Ba_z2")
+  end
+
+
+  for {description, statement} <- [
+    {"comma is not allowed in the identifier", "select fo'o from baz"},
+    {"identifier can't start with a number", "select 1foo from baz"},
+    {"from table is required", "select foo"},
+    {"at least one column must be specified", "select from baz"},
+    {"columns must be separated with a comma", "select foo bar from baz"},
+    {"query must start with a select", "foo select foo bar from baz"}
+  ] do
+    test description do
+      assert {:error, _} = SqlQuery.parse(unquote(statement))
+    end
+  end
+end
