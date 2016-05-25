6a83dd6f290b1e0c0ced09d8d81e147e9bd81cee
Paweł Obrok <pawel.obrok@gmail.com>
2016-05-25 16:10:25 +0200

    Remove air/backend

--- air/backend/apps/airpub/include/types.hrl
+++
@@ -1,7 +0,0 @@
--record(article, {
-  path :: string(),
-  content_type = "undefined" :: string(),
-  content_encoding = "undefined" :: string(),
-  content :: binary(),
-  published_at = os:timestamp() :: erlang:timestamp()
-}).
--- air/backend/make_test_config.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p apps/air/test/
-cp -rp rel/files/sys.config apps/air/test/
