7f7232f2e294ee0b0e9c182194da8eec14e2a692
Cristian Berneanu <cristian@aircloak.com>
2016-05-25 17:33:38 +0300

    Split long line

--- cloak/lib/cloak/data_source.ex
+++ cloak/lib/cloak/data_source.ex
@@ -38,7 +38,8 @@ defmodule Cloak.DataSource do
   @type num_rows :: non_neg_integer
   @type column :: String.t
   @type row :: [String.t]
-  @type data_type :: :text | :integer | :real | :boolean | :timestamp | :time | :date | :interval | {:unsupported, String.t}
+  @type data_type :: :text | :integer | :real | :boolean | :timestamp |
+    :time | :date | :interval | {:unsupported, String.t}
   @type query_result :: {num_rows, [column], [row]}
 
 
