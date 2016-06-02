d80d23493e0f150ce6c70ac74f3bc85eefa12d53
Sebastian Probst Eide <sebastian@aircloak.com>
2016-06-02 11:26:13 +0200

    Only show first 10 rows by default

--- air/site/web/static/js/queries/result.js
+++ air/site/web/static/js/queries/result.js
@@ -5,6 +5,49 @@ import {CodeEditor} from "../code_editor";
 export class Result extends React.Component {
   constructor(props) {
     super(props);
+
+    if (props.rows.length > 10) {
+      this.state = {showAll: false};
+    } else {
+      this.state = {showAll: true};
+    }
+
+    this.renderRows = this.renderRows.bind(this);
+    this.renderShowAll = this.renderShowAll.bind(this);
+    this.clickShowAll = this.clickShowAll.bind(this);
+  }
+
+  clickShowAll() {
+    this.setState({showAll: true});
+  }
+
+  renderRows() {
+    let rows = this.props.rows;
+    if (! this.state.showAll) {
+      rows = rows.slice(0, 10);
+    }
+    const tableRows = rows.map((row, i) =>
+      <tr key={i}>
+        {row.map((value, j) => <td key={j}>{value}</td>)}
+      </tr>
+    );
+    return tableRows;
+  }
+
+  renderShowAll() {
+    if (this.state.showAll) {
+      return (
+        <div className="row-count">
+          {this.props.rows.length} rows.
+        </div>
+      );
+    } else {
+      return (
+        <div className="row-count">
+          Showing 10 of {this.props.rows.length} rows. <a onClick={this.clickShowAll}>Show all rows</a>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -29,18 +72,10 @@ export class Result extends React.Component {
             </thead>
 
             <tbody>
-              {this.props.rows.
-                map((row, i) =>
-                  <tr key={i}>
-                    {row.map((value, j) => <td key={j}>{value}</td>)}
-                  </tr>
-                )
-              }
+              {this.renderRows()}
             </tbody>
           </table>
-          <div className="row-count">
-            {this.props.rows.length} rows after anonymization
-          </div>
+          {this.renderShowAll()}
         </div>
       </div>
     );
