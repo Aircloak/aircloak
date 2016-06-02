e828ceff77d8fc2f71252ef9a124dca6e121561b
Sebastian Probst Eide <sebastian@aircloak.com>
2016-06-02 11:09:47 +0200

    Make result into a class

--- air/site/web/static/js/queries/result.js
+++ air/site/web/static/js/queries/result.js
@@ -2,41 +2,51 @@ import React from "react";
 
 import {CodeEditor} from "../code_editor";
 
-export const Result = (props) =>
-  <div className="panel panel-success">
-    <div className="panel-heading" />
-    <div className="panel-body">
-      <CodeEditor
-        onRun={() => {}}
-        onSave={() => {}}
-        onChange={() => {}}
-        statement={props.statement}
-        readOnly
-      />
-      <table className="table table-striped table-hover">
-        <thead>
-          <tr>
-            {props.columns.map((column) =>
-              <th key={column}>{column}</th>
-            )}
-          </tr>
-        </thead>
+export class Result extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-        <tbody>
-          {props.rows.
-            map((row, i) =>
-              <tr key={i}>
-                {row.map((value, j) => <td key={j}>{value}</td>)}
+  render() {
+    return (
+      <div className="panel panel-success">
+        <div className="panel-heading" />
+        <div className="panel-body">
+          <CodeEditor
+            onRun={() => {}}
+            onSave={() => {}}
+            onChange={() => {}}
+            statement={this.props.statement}
+            readOnly
+          />
+          <table className="table table-striped table-hover">
+            <thead>
+              <tr>
+                {this.props.columns.map((column) =>
+                  <th key={column}>{column}</th>
+                )}
               </tr>
-            )
-          }
-        </tbody>
-      </table>
-      <div className="row-count">
-        {props.rows.length} rows after anonymization
+            </thead>
+
+            <tbody>
+              {this.props.rows.
+                map((row, i) =>
+                  <tr key={i}>
+                    {row.map((value, j) => <td key={j}>{value}</td>)}
+                  </tr>
+                )
+              }
+            </tbody>
+          </table>
+          <div className="row-count">
+            {this.props.rows.length} rows after anonymization
+          </div>
+        </div>
       </div>
-    </div>
-  </div>;
+    );
+  }
+
+}
 
 Result.propTypes = {
   statement: React.PropTypes.string,
