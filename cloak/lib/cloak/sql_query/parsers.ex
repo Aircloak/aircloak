8e0398012e20b5c21e6edd808219874486efc12a
Sasa Juric <sasa.juric@gmail.com>
2016-06-06 09:41:43 +0200

    simplify nested conditional

--- cloak/lib/cloak/sql_query/parsers.ex
+++ cloak/lib/cloak/sql_query/parsers.ex
@@ -103,26 +103,25 @@ defmodule Cloak.SqlQuery.Parsers do
   @spec token(any) :: Base.parser
   @spec token(Base.parser, any) :: Base.parser
   defparser token(%ParserState{status: :ok, input: input, results: results} = state, category) do
-    if Enum.empty?(input) do
-      %{state | status: :error, error: "Expected #{category}, but hit end of input"}
-    else
-      case input do
-        [%Token{category: ^category} = token | next_tokens] ->
-          {next_line, next_column} =
-            case next_tokens do
-              [next_token | _] -> {next_token.line, next_token.column}
-              [] -> {token.line, token.column}
-            end
-          %{state | line: next_line, column: next_column, input: next_tokens, results: [token | results]}
+    case input do
+      [] ->
+        %{state | status: :error, error: "Expected #{category}, but hit end of input"}
 
-        [%Token{} = token | _] ->
-          %{state |
-            status: :error,
-            error: "Unexpected token `#{to_string(token)}` at line #{state.line}, column #{state.column}}"
-          }
+      [%Token{category: ^category} = token | next_tokens] ->
+        {next_line, next_column} =
+          case next_tokens do
+            [next_token | _] -> {next_token.line, next_token.column}
+            [] -> {token.line, token.column}
+          end
+        %{state | line: next_line, column: next_column, input: next_tokens, results: [token | results]}
 
-        [other | _] -> raise "Input contained non-token #{inspect other}"
-      end
+      [%Token{} = token | _] ->
+        %{state |
+          status: :error,
+          error: "Unexpected token `#{to_string(token)}` at line #{state.line}, column #{state.column}}"
+        }
+
+      [other | _] -> raise "Input contained non-token #{inspect other}"
     end
   end
 
