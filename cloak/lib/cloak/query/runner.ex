6d218dd7d6f7f190f64cdaecc57265ccdaa50bc5
Cristian Berneanu <cristian@aircloak.com>
2016-06-10 12:22:43 +0300

    Extract count aggregation step to separate method

--- cloak/lib/cloak/query/runner.ex
+++ cloak/lib/cloak/query/runner.ex
@@ -35,31 +35,37 @@ defmodule Cloak.Query.Runner do
   end
   defp execute_sql_query(%{command: :select, data_source: data_source} = select_query) do
     with {:ok, {_count, [_user_id | columns], rows}} <- DataSource.select(data_source, select_query) do
-      lcf_data = LCFData.new()
 
-      reportable_buckets = try do
-        rows
-        |> NegativeCondition.apply(select_query)
-        |> NegativeCondition.drop_unsafe_filter_columns(select_query)
-        |> group_by_user()
-        |> AccumulateCount.pre_process()
-        |> anonymize(lcf_data)
-        |> AccumulateCount.post_process()
-        |> add_lcf_buckets(lcf_data, length(columns))
-        |> Result.apply_aggregation(select_query)
-        |> Result.apply_order(select_query)
-        |> Result.to_map()
-      after
-        LCFData.delete(lcf_data)
-      end
+    reportable_buckets = rows
+      |> NegativeCondition.apply(select_query)
+      |> NegativeCondition.drop_unsafe_filter_columns(select_query)
+      |> group_by_user()
+      |> count_aggregator(length(columns))
+      |> Result.apply_aggregation(select_query)
+      |> Result.apply_order(select_query)
+      |> Result.to_map()
 
       {:ok, {:buckets, Cloak.SqlQuery.column_titles(select_query), reportable_buckets}}
     end
   end
 
+  defp count_aggregator(properties, columns_count) do
+    lcf_data = LCFData.new()
+    try do
+      properties
+      |> count_properties(lcf_data)
+      |> add_lcf_buckets(lcf_data, columns_count)
+    after
+      LCFData.delete(lcf_data)
+    end
+  end
+
+  defp count_properties(properties, lcf_data) do
+    properties |> AccumulateCount.pre_process() |> anonymize(lcf_data) |> AccumulateCount.post_process()
+  end
+
   defp anonymize(properties, lcf_data) do
     aggregator = Aggregator.new()
-
     try do
       for {user_id, property} <- properties, do: Aggregator.add_property(aggregator, user_id, property)
       Aggregator.gather_buckets(aggregator, lcf_data) |> :anonymizer.anonymize(lcf_data)
@@ -70,12 +76,10 @@ defmodule Cloak.Query.Runner do
 
   defp add_lcf_buckets(buckets, lcf_data, columns_count) do
     lcf_property = List.duplicate(:*, columns_count)
-    low_count_filter_data = LCFData.filtered_property_counts(lcf_data)
-    |> Enum.map(fn({user, count}) -> {user, List.duplicate(lcf_property, count)} end)
 
-    lcf_buckets = AccumulateCount.pre_process(low_count_filter_data)
-    |> anonymize(:undefined)
-    |> AccumulateCount.post_process
+    lcf_buckets = LCFData.filtered_property_counts(lcf_data)
+    |> Enum.map(fn({user, count}) -> {user, List.duplicate(lcf_property, count)} end)
+    |> count_properties(:undefined)
 
     buckets ++ lcf_buckets
   end
