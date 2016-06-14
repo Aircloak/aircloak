3fa1b2a0a28fd2e4e5d5926ab6785424520dcdc4
Paweł Obrok <pawel.obrok@gmail.com>
2016-06-14 11:14:22 +0200

    Simplify ConnHelper.api_conn

--- air/site/test/support/conn_helper.ex
+++ air/site/test/support/conn_helper.ex
@@ -1,6 +1,7 @@
 defmodule Air.TestConnHelper do
   @moduledoc "Helpers for working with the connection."
 
+  alias Air.Endpoint
   import Phoenix.ConnTest
 
   @doc "Logs-in as the given user, and returns the recycled connection."
@@ -15,9 +16,8 @@ defmodule Air.TestConnHelper do
   @doc "Simulates a connection authenticated with the given token"
   defmacro api_conn(token) do
     quote do
-      token_salt = Application.get_env(:air, Air.Endpoint) |> Keyword.fetch!(:api_token_secret)
-      fake_connection = %Plug.Conn{private: %{phoenix_endpoint: Air.Endpoint}}
-      token_text = Phoenix.Token.sign(fake_connection, token_salt, unquote(token).id)
+      token_salt = Application.get_env(:air, Endpoint) |> Keyword.fetch!(:api_token_secret)
+      token_text = Phoenix.Token.sign(Endpoint, token_salt, unquote(token).id)
 
       put_req_header(conn(), "auth-token", token_text)
     end
