668778493b6394913e834551037627a6f60ae929
Sasa Juric <sasa.juric@gmail.com>
2016-06-15 16:36:30 +0200

    remove confusing asserts

--- air/site/test/socket/cloak_test.exs
+++ air/site/test/socket/cloak_test.exs
@@ -42,35 +42,38 @@ defmodule Air.Socket.CloakTest do
     query = Air.TestRepoHelper.create_organisation!()
     |> Air.TestRepoHelper.create_user!()
     |> Air.TestRepoHelper.create_query!()
-    query_id = query.id
+
+    nil = Air.Repo.get!(Air.Query, query.id).result
 
     socket = TestSocketHelper.connect!()
-    assert {:ok, %{}} == join_main_channel(socket)
+    join_main_channel!(socket)
 
     request = %{
       request_id: "foobar", event: "query_result",
-      payload: %{query_id: query_id, rows: [], columns: []}
+      payload: %{query_id: query.id, rows: [], columns: []}
     }
-    assert Air.Repo.get!(Air.Query, query_id).result == nil
 
     Air.Endpoint.subscribe(self(), "user:#{query.user_id}")
     TestSocket.push(socket, "main", "cloak_call", request)
+
     assert {:ok, {"main", "call_response", response}} = TestSocket.await_message(socket, 100)
     assert %{"request_id" => "foobar", "status" => "ok"} = response
 
+    query_id = query.id
     assert_receive %Phoenix.Socket.Broadcast{event: "result", payload: %{completed: true, id: ^query_id}}
-    assert Air.Repo.get!(Air.Query, query_id).result != nil
+
+    assert Air.Repo.get!(Air.Query, query.id).result != nil
   end
 
   test "getting data for connected cloaks" do
     socket1 = TestSocketHelper.connect!(%{cloak_name: "cloak_3"})
-    assert {:ok, %{}} == join_main_channel(socket1, "cloak_3")
+    join_main_channel!(socket1, "cloak_3")
     assert [%Air.CloakInfo{name: "cloak_3"} = cloak_3] = CloakInfo.all(Air.TestRepoHelper.admin_organisation())
     assert cloak_3 == CloakInfo.get(cloak_3.id)
     assert nil == CloakInfo.get("non-existing cloak")
 
     socket2 = TestSocketHelper.connect!(%{cloak_name: "cloak_4"})
-    assert {:ok, %{}} == join_main_channel(socket2, "cloak_4")
+    join_main_channel!(socket2, "cloak_4")
 
     # admin org fetches all cloaks
     all_cloaks = CloakInfo.all(Air.TestRepoHelper.admin_organisation())
@@ -87,6 +90,10 @@ defmodule Air.Socket.CloakTest do
     assert_receive {:DOWN, ^mref, _, _, _}
     assert [%Air.CloakInfo{name: "cloak_4"}] = CloakInfo.all(Air.TestRepoHelper.admin_organisation())
     assert nil == CloakInfo.get(cloak_3.id)
+  end
+
+  defp join_main_channel!(socket, cloak_name \\ "cloak_1", data_sources \\ []) do
+    {:ok, %{}} = join_main_channel(socket, cloak_name, data_sources)
   end
 
   defp join_main_channel(socket, cloak_name \\ "cloak_1", data_sources \\ []) do
