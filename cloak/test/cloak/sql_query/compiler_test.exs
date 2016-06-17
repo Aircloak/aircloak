84ab98132442ce08d5853f8fe8912b0c7da0dd79
Paweł Obrok <pawel.obrok@gmail.com>
2016-06-17 18:53:49 +0200

    Refactor compiler test

--- cloak/test/cloak/sql_query/compiler_test.exs
+++ cloak/test/cloak/sql_query/compiler_test.exs
@@ -2,19 +2,14 @@ defmodule Cloak.SqlQuery.Compiler.Test do
   use ExUnit.Case, async: true
 
   alias Cloak.SqlQuery.Compiler
-  alias Cloak.SqlQuery.Parsers.Token
+  alias Cloak.SqlQuery.Parser
   alias Timex.{DateTime, Timezone}
 
   test "casts timestamp where conditions" do
-    data_source = %{
-      tables: %{table: %{columns: [{"column", :timestamp}]}}
-    }
-    literal = %Token{category: :constant, value: %{type: :string, value: "2015-01-01"}}
-    query = %{
-      select: [:*],
-      from: "table",
-      where: [{:comparison, "column", :>, literal}]
-    }
+    data_source = %{tables: %{table: %{
+      columns: [{"column", :timestamp}]
+    }}}
+    query = Parser.parse!("select * from table where column > '2015-01-01'")
 
     {:ok, result} = Compiler.compile(data_source, query)
     assert result[:where] ==
