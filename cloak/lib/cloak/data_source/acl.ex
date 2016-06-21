2509638ce19b303a3f74e6ee5dbc32847bc53521
Sasa Juric <sasa.juric@gmail.com>
2016-06-21 08:28:30 +0200

    convert acl driver to http interface

--- cloak/lib/cloak/data_source/acl.ex
+++ cloak/lib/cloak/data_source/acl.ex
@@ -11,10 +11,7 @@ defmodule Cloak.DataSource.Acl do
       "data_sources": {
         "acl": {
           driver: "acl",
-          "parameters": {
-            "hostname": "localhost",
-            "port": 8000
-          },
+          "parameters": {"url": "http://localhost:8000"},
           "tables": {
             "<table-display-name>": {
               "name": "<actual-table-name>",
@@ -40,26 +37,22 @@ defmodule Cloak.DataSource.Acl do
 
   @doc false
   def child_spec(source_id, parameters) do
-    worker(__MODULE__, [[source_id | parameters]], id: proc_name(source_id), restart: :permanent)
+    worker(__MODULE__, [source_id, parameters], id: proc_name(source_id), restart: :permanent)
   end
 
   @doc false
-  def start_link([source_id | parameters]) do
+  def start_link(source_id, parameters) do
     Agent.start_link(fn() -> parameters end, name: proc_name(source_id))
   end
 
   @doc false
   def get_columns(source_id, full_table_name) do
-    with {:ok, params} <- Agent.get(proc_name(source_id), &({:ok, &1})) do
-      load_column_definitions(params, full_table_name)
-    end
+    load_column_definitions(params(source_id), full_table_name)
   end
 
   @doc false
   def select(source_id, sql_query) do
-    with {:ok, params} <- Agent.get(proc_name(source_id), &({:ok, &1})) do
-      run_query(params, sql_query)
-    end
+    run_query(params(source_id), sql_query)
   end
 
 
@@ -67,35 +60,21 @@ defmodule Cloak.DataSource.Acl do
   # Internal functions
   #-----------------------------------------------------------------------------------------------------------
 
+  defp params(source_id) do
+    Agent.get(proc_name(source_id), &(&1))
+  end
+
   defp load_column_definitions(params, full_table_name) do
-    request = %{
-      type: "show:columns",
-      table: full_table_name,
-    }
-    with {:ok, response} <- tcp_send_receive(params, request) do
-      case response["success"] do
-        true ->
-          Enum.map(response["columns"], &({&1["name"], parse_type(&1["type"])}))
-        false ->
-          raise "Couldn't determine columns for table #{full_table_name}"
-      end
-    end
+    response = %{"success" => true} = post!(params.url, "list_columns", %{table: full_table_name})
+    Enum.map(response["columns"], &({&1["name"], parse_type(&1["type"])}))
   end
 
   defp run_query(params, %{columns: columns} = query) do
-    request = %{
-      type: "query",
-      columns: columns |> Enum.map(&Builder.select_column_to_string/1),
+    response = %{"success" => true} = post!(params.url, "run_query", %{
+      columns: Enum.map(columns, &Builder.select_column_to_string/1),
       statement: sql_statement(query)
-    }
-    with {:ok, response} <- tcp_send_receive(params, request) do
-      case response["success"] do
-        true ->
-          {:ok, {length(response["rows"]), response["columns"], response["rows"]}}
-        false ->
-          {:error, response["error"]}
-      end
-    end
+    })
+    {:ok, {length(response["rows"]), response["columns"], response["rows"]}}
   end
 
   defp sql_statement(%{from: {:subquery, unsafe_select}}) do
@@ -113,37 +92,14 @@ defmodule Cloak.DataSource.Acl do
     }
   end
 
-  defp tcp_send_receive(params, message) do
-    case :gen_tcp.connect(String.to_char_list(params[:hostname]), params[:port],
-        [:binary, {:packet, 0}, {:active, true}]) do
-      {:ok, socket} ->
-        :ok = :gen_tcp.send(socket, Poison.encode!(message))
-        :ok = :gen_tcp.shutdown(socket, :write)
-        response = case receive_data([]) do
-          {:ok, data} ->
-            Poison.decode(data)
-          {:error, _error} ->
-            {:error, "Communication with ACL backend failed"}
-        end
-        :gen_tcp.close(socket)
-        response
-      {:error, reason} ->
-        {:error, "Unable to connect to ACL backend: #{inspect(reason)}"}
-    end
-  end
+  defp post!(url, operation, payload) do
+    %HTTPoison.Response{status_code: 200, body: body} = HTTPoison.post!(
+      "#{url}/#{operation}",
+      Poison.encode!(payload),
+      [{"Content-Type", "application/json"}]
+    )
 
-  defp receive_data(acc) do
-    receive do
-      {:tcp, _socket, packet} ->
-        receive_data([packet | acc])
-      {:tcp_closed, _socket} ->
-        data = acc
-        |> Enum.reverse()
-        |> Enum.join()
-        {:ok, data}
-      _other ->
-        {:error, "Unexpected response from ACL backend"}
-    end
+    Poison.decode!(body)
   end
 
   defp proc_name(source_id), do: {:via, :gproc, {:n, :l, {Cloak.DataSource, source_id}}}
---
+++ cloak/test/cloak/data_source/acl_test.exs
@@ -0,0 +1,104 @@
+defmodule Cloak.DataSource.AclTest do
+  use ExUnit.Case, async: true
+
+  alias Cloak.DataSource.Acl
+
+  setup do
+    data_source_id = :"data_source_#{:erlang.unique_integer()}"
+    bypass = Bypass.open
+    url = "http://localhost:#{bypass.port}"
+    {:ok, _} = Acl.start_link(data_source_id, %{url: url})
+    {:ok, data_source_id: data_source_id, url: url, bypass: bypass}
+  end
+
+  test "get columns", context do
+    expect_json_post(context, "/list_columns",
+      fn(payload) ->
+        assert %{"table" => "table_name"} == payload
+
+        {200, %{
+          success: true,
+          columns: [%{name: "column1", type: "varchar"}, %{name: "column2", type: "integer"}]
+        }}
+      end
+    )
+
+    columns = Acl.get_columns(context.data_source_id, "table_name")
+    assert [{"column1", :text}, {"column2", :integer}] == columns
+  end
+
+  test "parsed select", context do
+    expect_json_post(context, "/run_query",
+      fn(payload) ->
+        assert %{"columns" => ["user_id", "foo"], "statement" => statement} = payload
+        assert %{"params" => [], "type" => "parsed", "val" => "SELECT user_id,foo FROM bar "} == statement
+
+        {200, %{success: true, columns: ["user_id", "foo"], rows: Enum.map(1..100, &[&1, &1])}}
+      end
+    )
+
+    query_result = run_query(context, "select count(foo) from bar")
+    assert {:ok, {:buckets, ["count(foo)"], [%{occurrences: 1, row: [100]}]}} = query_result
+  end
+
+  test "unsafe select", context do
+    expect_json_post(context, "/run_query",
+      fn(payload) ->
+        assert %{"columns" => ["foo"], "statement" => statement} = payload
+        assert %{"type" => "unsafe", "val" => "select foo from bar"} == statement
+
+        {200, %{success: true, columns: ["user_id", "foo"], rows: Enum.map(1..100, &[&1, &1])}}
+      end
+    )
+
+    query_result = run_query(context, "select count(foo) from (select foo from bar) as baz")
+    assert {:ok, {:buckets, ["count(foo)"], [%{occurrences: 1, row: [100]}]}} = query_result
+  end
+
+
+  ## ----------------------------------------------------------------
+  ## Internal functions
+  ## ----------------------------------------------------------------
+
+  defp expect_json_post(test_context, path, callback) do
+    Bypass.expect(test_context.bypass,
+      fn(conn) ->
+        assert "POST" == conn.method
+        assert path == conn.request_path
+
+        headers = Map.new(conn.req_headers)
+        assert "application/json" == headers["content-type"]
+
+        {:ok, body, conn} = Plug.Conn.read_body(conn)
+        {status, response} = callback.(Poison.decode!(body))
+        Plug.Conn.resp(conn, status, Poison.encode!(response))
+      end
+    )
+  end
+
+  defp run_query(test_context, statement, data_source_opts \\ []) do
+    %Cloak.Query{
+      id: "foobar",
+      statement: statement,
+      data_source:
+        default_data_source(test_context)
+        |> Map.merge(Enum.into(data_source_opts, %{}))
+    }
+    |> Cloak.Query.Runner.run()
+  end
+
+  defp default_data_source(test_context) do
+    %{
+      id: test_context.data_source_id,
+      driver: Acl,
+      parameters: %{url: test_context.url},
+      tables: %{
+        bar: %{
+          name: "bar",
+          user_id: "user_id",
+          columns: %{"foo" => :integer}
+        }
+      }
+    }
+  end
+end
