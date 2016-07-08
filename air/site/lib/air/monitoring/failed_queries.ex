1a2d0a7392bc1d24c2b6fa3c739896538fc688ac
Paweł Obrok <pawel.obrok@gmail.com>
2016-07-08 15:58:28 +0200

    Mark json logs in a generic way

--- air/site/lib/air/monitoring/failed_queries.ex
+++ air/site/lib/air/monitoring/failed_queries.ex
@@ -3,19 +3,19 @@ defmodule Air.Monitoring.FailedQueries do
 
   defp work do
     for {:result, result} <- Air.QueryEvents.stream do
-      if result["error"] do
-        log_error(result)
-      end
+      if result["error"], do: log_error(result)
     end
   end
 
   defp log_error(%{"error" => error, "query_id" => query_id}) do
     import Logger, warn: false
 
-    Air.Repo.get!(Air.Query, query_id)
-    |> Map.take([:statement, :cloak_id, :data_source])
-    |> Map.merge(%{type: "failed_query", message: error})
-    |> Poison.encode!()
-    |> Logger.error()
+    message =
+      Air.Repo.get!(Air.Query, query_id)
+      |> Map.take([:statement, :cloak_id, :data_source])
+      |> Map.merge(%{type: "failed_query", message: error})
+      |> Poison.encode!()
+
+    Logger.error("JSON_LOG #{message}")
   end
 end
