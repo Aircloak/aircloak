537a012b3525ae891bec28ef70910a1ad3e7a41f
Paweł Obrok <pawel.obrok@gmail.com>
2016-07-28 18:39:54 +0200

    Add docs and specs

--- cloak/lib/cloak/query/function.ex
+++ cloak/lib/cloak/query/function.ex
@@ -1,4 +1,8 @@
 defmodule Cloak.Query.Function do
+  @moduledoc "Includes information about functions and implementation of non-aggregation functions."
+
+  alias Cloak.SqlQuery.Parser
+
   @functions %{
     ~w(count) => %{aggregate: true, extraction: false, type: :any},
     ~w(sum avg min max stddev median) => %{aggregate: true, extraction: false, type: :numeric},
@@ -7,18 +11,44 @@ defmodule Cloak.Query.Function do
   |> Enum.flat_map(fn({functions, traits}) -> Enum.map(functions, &{&1, traits}) end)
   |> Enum.into(%{})
 
+  @type argument_type :: :any | :numeric | :timestamp
+
+
+  # -------------------------------------------------------------------
+  # Info functions
+  # -------------------------------------------------------------------
+
+  @doc "Returns true if the given column definition is a function call, false otherwise."
+  @spec function?(Parser.column) :: boolean
   def function?({:function, _, _}), do: true
   def function?(_), do: false
 
+  @doc "Returns true if the given function call to a known function, false otherwise."
+  @spec valid_function?(Parser.column) :: boolean
   def valid_function?({:function, function, _}), do: Map.has_key?(@functions, function)
 
+  @doc """
+  Returns true if the given column definition is a function call to an aggregate function, false otherwise.
+  """
+  @spec aggregate_function?(Parser.column) :: boolean
   def aggregate_function?({:function, function, _}), do: @functions[function].aggregate
   def aggregate_function?(_), do: false
 
+  @doc "Returns the argument type required by the given function call."
+  @spec argument_type(Parser.column) :: argument_type
   def argument_type({:function, function, _}), do: @functions[function].type
 
+  @doc "Returns the argument specifiaction of the given function call."
+  @spec argument(Parser.column) :: Parser.column
   def argument({:function, _, argument}), do: argument
 
+
+  # -------------------------------------------------------------------
+  # Implementations
+  # -------------------------------------------------------------------
+
+  @doc "Returns the result of applying the given function definition to the given value."
+  @spec apply(term, Parser.column) :: term
   def apply(value, {:function, "year", _}), do: value.year
   def apply(value, {:function, "month", _}), do: value.month
   def apply(value, {:function, "day", _}), do: value.day
