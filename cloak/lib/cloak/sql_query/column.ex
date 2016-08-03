36728272b88fbafd5ab8d0527d779519fba11724
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-03 14:27:52 +0200

    Add missing spec

--- cloak/lib/cloak/sql_query/column.ex
+++ cloak/lib/cloak/sql_query/column.ex
@@ -1,10 +1,11 @@
 defmodule Cloak.SqlQuery.Column do
   @moduledoc "Represents a column in a compiled query."
 
+  @type column_type :: Cloak.DataSource.data_type | nil
   @type t :: %__MODULE__{
     table: :unknown | Cloak.DataSource.table,
     name: String.t,
-    type: Cloak.DataSource.data_type | nil,
+    type: column_type,
     user_id?: boolean,
     constant?: boolean,
     value: any,
@@ -12,6 +13,7 @@ defmodule Cloak.SqlQuery.Column do
   defstruct [table: :unknown, name: nil, type: nil, user_id?: false, constant?: false, value: nil]
 
   @doc "Returns a column struct representing the constant `value`."
+  @spec constant(column_type, any) :: t
   def constant(type, value) do
     %__MODULE__{table: :unknown, name: :constant, type: type, user_id?: false, constant?: true, value: value}
   end
