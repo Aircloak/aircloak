e692f1e373479917a4fe451bf2147f2aacc2f882
Sebastian Probst Eide <sebastian@aircloak.com>
2016-08-04 09:42:50 +0200

    Add LEFT, RIGHT, OUTER to lexer
    This allows us to parse for the remaining,
    as of yet, unsupported JOIN types

--- cloak/lib/cloak/sql_query/lexer.ex
+++ cloak/lib/cloak/sql_query/lexer.ex
@@ -27,7 +27,7 @@ defmodule Cloak.SqlQuery.Lexer do
     "SELECT", "SHOW",
     "TABLES", "COLUMNS",
     "FROM",
-    "INNER", "JOIN", "ON",
+    "INNER", "OUTER", "LEFT", "RIGHT", "FULL", "JOIN", "ON",
     "WHERE", "AND", "NOT",
     "LIKE", "ILIKE", "IN", "IS",
     "ORDER", "GROUP", "BY",
