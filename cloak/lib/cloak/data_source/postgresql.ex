f7f786eb77082b359bcf113ca5e87cb1c2a64f4d
Cristian Berneanu <cristian@aircloak.com>
2016-08-04 11:41:59 +0300

    Change the name of the prepared data selection query

--- cloak/lib/cloak/data_source/postgresql.ex
+++ cloak/lib/cloak/data_source/postgresql.ex
@@ -49,7 +49,7 @@ defmodule Cloak.DataSource.PostgreSQL do
   defp run_query(source_id, statement, params \\ [], decode_mapper, result_processor) do
     options = [timeout: 4 * 60 * 60_000, pool_timeout: 5 * 60_000, pool: @pool_name]
     Postgrex.transaction(proc_name(source_id), fn(conn) ->
-      with {:ok, query} <- Postgrex.prepare(conn, "unknown", statement, []) do
+      with {:ok, query} <- Postgrex.prepare(conn, "data select", statement, []) do
         result = Postgrex.stream(conn, query, params, [decode_mapper: decode_mapper, max_rows: 25_000])
         |> Stream.flat_map(fn (%Postgrex.Result{rows: rows}) -> rows end)
         |> result_processor.()
