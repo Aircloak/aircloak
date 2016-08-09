aa7f49f05820dc2d967c45275f4983ff71e91594
Sebastian Probst Eide <sebastian@aircloak.com>
2016-08-09 10:01:12 +0200

    Add execution time to queries

---
+++ air/site/priv/repo/migrations/20160809074306_add_execution_time_to_query.exs
@@ -0,0 +1,9 @@
+defmodule Air.Repo.Migrations.AddExecutionTimeToQuery do
+  use Ecto.Migration
+
+  def change do
+    alter table(:queries) do
+      add :execution_time, :integer
+    end
+  end
+end
