aa8ac0161f9138431da4c6671f2c87b77d14a8ff
Sebastian Probst Eide <sebastian@aircloak.com>
2016-08-09 11:53:26 +0200

    Add the ability to record audit log events

--- air/site/test/models/audit_log_test.exs
+++ air/site/test/models/audit_log_test.exs
@@ -1,7 +1,8 @@
 defmodule Air.AuditLogTest do
   use Air.ModelCase
 
-  alias Air.AuditLog
+  import Air.{TestRepoHelper}
+  alias Air.{AuditLog, Repo}
 
   @valid_attrs %{event: "some content", metadata: "some content", user: "some content"}
   @invalid_attrs %{}
@@ -14,5 +15,48 @@ defmodule Air.AuditLogTest do
   test "changeset with invalid attributes" do
     changeset = AuditLog.changeset(%AuditLog{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "creating audit log entries should save a db record" do
+    organisation = create_organisation!()
+    user = create_user!(organisation)
+    conn = %Plug.Conn{}
+
+    assert AuditLog.log(conn, "event", [meta: true], user) == :ok
+    entry = Repo.one!(AuditLog)
+
+    assert entry.event == "event"
+    assert entry.user == user.email
+    assert %{"meta" => true} = Poison.decode!(entry.metadata)
+  end
+
+  test "reads user from connection if none provided" do
+    organisation = create_organisation!()
+    user = create_user!(organisation)
+    conn = assign_user(%Plug.Conn{}, user)
+
+    assert AuditLog.log(conn, "event") == :ok
+    entry = Repo.one!(AuditLog)
+
+    assert entry.event == "event"
+    assert entry.user == user.email
+  end
+
+  test "sets IP and remote_ip in metadata" do
+    organisation = create_organisation!()
+    user = create_user!(organisation)
+    conn = %{%Plug.Conn{} | remote_ip: {127, 0, 0, 1}, peer: {{127, 0, 0, 1}, 1234}}
+    conn = assign_user(conn, user)
+
+    assert AuditLog.log(conn, "event") == :ok
+    entry = Repo.one!(AuditLog)
+
+    assert entry.event == "event"
+    assert entry.user == user.email
+    assert Poison.decode!(entry.metadata) == %{"peer" => "127.0.0.1:1234", "remote_ip" => "127.0.0.1"}
+  end
+
+  defp assign_user(conn, user) do
+    %{conn | assigns: %{current_user: user}}
   end
 end
--- air/site/web/models/audit_log.ex
+++ air/site/web/models/audit_log.ex
@@ -1,5 +1,9 @@
 defmodule Air.AuditLog do
+  @moduledoc "Model for recording events having taken place"
   use Air.Web, :model
+  require Logger
+
+  alias Air.{User, Repo}
 
   schema "audit_logs" do
     field :event, :string
@@ -22,4 +26,50 @@ defmodule Air.AuditLog do
     model
     |> cast(params, @required_fields, @optional_fields)
   end
+
+
+  # -------------------------------------------------------------------
+  # Auditable events
+  # -------------------------------------------------------------------
+
+  @doc "Saves an audit log entry, but allows the operation to fail."
+  @spec log(Plug.Conn.t, String.t, User.t, Keyword.t) :: :ok
+  def log(%Plug.Conn{} = conn, event, options \\ [], user \\ nil) do
+    user = case user do
+      nil -> conn.assigns.current_user
+      _ -> user
+    end
+
+    peer = case conn.peer do
+      {ip, port} -> "#{ip_to_string(ip)}:#{port}"
+      _ -> "Unknown"
+    end
+    remote_ip = case conn.remote_ip do
+      ip when is_tuple(ip) -> ip_to_string(ip)
+      _ -> "Unknown"
+    end
+
+    defaults = %{
+      peer: peer,
+      remote_ip: remote_ip,
+    }
+
+    metadata = options
+    |> Enum.into(%{})
+    |> Map.merge(defaults)
+
+    params = %{
+      user: user.email,
+      event: event,
+      metadata: Poison.encode!(metadata),
+    }
+    case Repo.insert(changeset(%__MODULE__{}, params)) do
+      {:ok, _} -> :ok
+      {:error, _} ->
+        Logger.error("Failed at storing audit log entry")
+        :ok
+    end
+  end
+
+  defp ip_to_string({a, b, c, d}), do: "#{a}.#{b}.#{c}.#{d}"
 end
