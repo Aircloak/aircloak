3f924dc772fe0bb0b19030d46aadd6772028d8e9
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-15 18:47:38 +0200

    Remove dead code

--- cloak/lib/cloak/time/timex_datetime_poison_encoder.ex
+++
@@ -1,7 +0,0 @@
-defimpl Poison.Encoder, for: Timex.DateTime do
-  def encode(datetime, options) do
-    datetime
-    |> Timex.format!("{ISOz}")
-    |> Poison.Encoder.BitString.encode(options)
-  end
-end
