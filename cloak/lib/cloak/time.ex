2a48cc25d90330c5208bf6c1ac07ec591d58321c
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-18 16:31:34 +0200

    Add docs to Cloak.Time

--- cloak/lib/cloak/time.ex
+++ cloak/lib/cloak/time.ex
@@ -1,4 +1,8 @@
 defmodule Cloak.Time do
+  @moduledoc "Contains utilities for normalizing date/times"
+
+  @doc "Parses string as an ISO8601 time."
+  @spec parse_time(String.t) :: {:ok, Time.t} | {:error, atom}
   def parse_time(string) do
     case Time.from_iso8601(string) do
       {:ok, result} -> {:ok, max_precision(result)}
@@ -6,6 +10,8 @@ defmodule Cloak.Time do
     end
   end
 
+  @doc "Parses string as an ISO8601 date with time."
+  @spec parse_datetime(String.t) :: {:ok, NaiveDateTime.t} | {:error, atom}
   def parse_datetime(string) do
     case NaiveDateTime.from_iso8601(string) do
       {:ok, result} -> {:ok, max_precision(result)}
@@ -13,8 +19,12 @@ defmodule Cloak.Time do
     end
   end
 
+  @doc "Parses string as an ISO8601 date with time."
+  @spec parse_date(String.t) :: {:ok, Date.t} | {:error, atom}
   def parse_date(string), do: Date.from_iso8601(string)
 
+  @doc "Sets the microsecond precision of the given Time or NaiveDateTime to 6."
+  @spec max_precision(x) :: x when x: Time.t | NaiveDateTime.t
   def max_precision(datetime = %{microsecond: {usecs, _precision}}), do:
     %{datetime | microsecond: {usecs, 6}}
 end
