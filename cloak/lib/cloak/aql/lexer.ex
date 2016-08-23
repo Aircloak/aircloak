e42dca8f1e9ca87aeb0c26f8426de337cf7a307c
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-23 17:25:03 +0200

    Fix dialyzer error

--- cloak/lib/cloak/aql/lexer.ex
+++ cloak/lib/cloak/aql/lexer.ex
@@ -163,9 +163,9 @@ defmodule Cloak.Aql.Lexer do
 
   defp quoted_identifier() do
     sequence([
-      ignore(char("`")),
+      ignore(char(?`)),
       word_of(~r/[^`]/),
-      ignore(char("`"))
+      ignore(char(?`))
     ])
     |> map(fn([identifier]) -> {:identifier, identifier} end)
     |> output_token()
