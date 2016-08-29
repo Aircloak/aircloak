7d1fde5104437d19b3a8ce9ebf502280b7cd1141
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-29 18:20:04 +0200

    Add linting and dialysis to BOM

--- bom/mix.exs
+++ bom/mix.exs
@@ -2,12 +2,15 @@ defmodule Bom.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bom,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :bom,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      aliases: aliases(Mix.env),
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,7 +31,27 @@ defmodule Bom.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:poison, "~> 2.2.0"}
+      {:poison, "~> 2.2.0", override: true},
+      {:aircloak_common, path: "../common/elixir"},
     ]
   end
+
+  #
+  # Aliases are shortcut or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases(env) when env in [:dev, :test] do
+    [
+      "lint": ["credo --strict --ignore #{Enum.join(ignored_credo_checks(Mix.env), ",")}"]
+    ]
+  end
+  defp aliases(:prod), do: []
+
+  defp ignored_credo_checks(:test), do:
+    ["ModuleDoc" | ignored_credo_checks(:dev)]
+  defp ignored_credo_checks(_), do:
+    ["NameRedeclarationBy", "AliasUsage", "PipeChain", "ABCSize", "Nesting"]
 end
