4c6b74ef6c77bff3fa00891685bd7e831fcc0868
Paweł Obrok <pawel.obrok@gmail.com>
2016-08-30 12:40:41 +0200

    Add docs and specs

--- air/site/lib/air/bom.ex
+++ air/site/lib/air/bom.ex
@@ -1,7 +1,27 @@
 defmodule Air.BOM do
+  @moduledoc """
+  An Agent keeping the data about open source dependencies of the project.  See priv/bom.json.example for how
+  the data is structured.
+  """
+
+
+  # -------------------------------------------------------------------
+  # API
+  # -------------------------------------------------------------------
+
+  @doc false
+  @spec start_link :: Agent.on_start
   def start_link, do: Agent.start_link(&read_bom/0, name: __MODULE__)
 
+  @doc "Returns the data about open source dependencies transformed by `f` (or identity if `f` isn't provided)."
+  @spec get :: %{}
+  @spec get((%{} -> x)) :: x when x: var
   def get(f \\ &(&1)), do: Agent.get(__MODULE__, f)
+
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
 
   defp read_bom do
     Application.app_dir(:air)
