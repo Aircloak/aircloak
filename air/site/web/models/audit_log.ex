2aa479e90d89d8f7bf510eaa47996c56a7104511
Sebastian Probst Eide <sebastian@aircloak.com>
2016-08-31 14:01:28 +0200

    Add missing typespecs

--- air/site/web/models/audit_log.ex
+++ air/site/web/models/audit_log.ex
@@ -5,6 +5,8 @@ defmodule Air.AuditLog do
 
   alias Air.Repo
 
+  @type t :: %__MODULE__{}
+
   schema "audit_logs" do
     field :event, :string
     field :user, :string
@@ -22,6 +24,7 @@ defmodule Air.AuditLog do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
+  @spec changeset(t | Changeset.t, Map.t) :: Changeset.t
   def changeset(model, params \\ %{}) do
     model
     |> cast(params, @required_fields, @optional_fields)
