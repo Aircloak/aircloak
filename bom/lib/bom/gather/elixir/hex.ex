7964893e53353416e5c6b0c5634b3dcf6c61d26e
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-02 15:42:44 +0200

    Simplify hex.pm rate limiting

--- bom/lib/bom/gather/elixir/hex.ex
+++ bom/lib/bom/gather/elixir/hex.ex
@@ -3,30 +3,24 @@ defmodule BOM.Gather.Elixir.Hex do
 
   def start_link, do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
 
-  def package(name), do: GenServer.call(__MODULE__, {:package, name}, :timer.minutes(1))
+  def package(name) do
+    send(__MODULE__, {:package, name, self()})
 
-  def handle_call({:package, name}, from, state) do
-    case Hex.API.Package.get(name) do
-      {200, result, _headers} -> {:reply, {:ok, result}, state}
-      {404, _content, _headers} -> {:reply, {:error, :not_found}, state}
-      {429, _content, _headers} ->
-        retry({:package, name}, from)
-        :timer.sleep(:timer.seconds(5))
-        {:noreply, state}
+    receive do
+      message -> message
+    after
+      :timer.minutes(1) -> raise "Timeout while fetching from hex.pm"
     end
   end
 
-  def handle_cast({:retry, {:package, name}, from}, state) do
+  def handle_info({:package, name, from}, state) do
     case Hex.API.Package.get(name) do
-      {200, result, _headers} -> GenServer.reply(from, {:ok, result})
-      {404, _content, _headers} -> GenServer.reply(from, {:error, :not_found})
+      {200, result, _headers} -> send(from, {:ok, result})
+      {404, _content, _headers} -> send(from, {:error, :not_found})
       {429, _content, _headers} ->
-        retry({:package, name}, from)
-        :timer.sleep(:timer.seconds(5))
+        Process.send_after(self(), {:package, name, from}, :timer.seconds(5))
     end
 
     {:noreply, state}
   end
-
-  defp retry(message, from), do: GenServer.cast(__MODULE__, {:retry, message, from})
 end
