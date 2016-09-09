ca6717072f9adc6a160059a02417d27dbb97c087
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-09 16:33:47 +0200

    Display a summary of license types on the licenses page

--- air/site/web/views/license_view.ex
+++ air/site/web/views/license_view.ex
@@ -3,4 +3,11 @@ defmodule Air.LicenseView do
   use Air.Web, :view
   # bug in the current Phoenix
   @dialyzer :no_match
+
+  def type_counts(packages) do
+    packages
+    |> Enum.group_by(&(&1.license_type))
+    |> Enum.map(fn({type, packages}) -> {type, length(packages)} end)
+    |> Enum.sort_by(fn({type, count}) -> count end, &>=/2)
+  end
 end
