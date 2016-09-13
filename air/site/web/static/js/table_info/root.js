5b8b113276a7c37c04ca7e1d05a7c5a7e7d285d7
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-13 16:22:22 +0200

    Show a collapsible list of columns per-table

--- air/site/web/static/js/table_info/root.js
+++ air/site/web/static/js/table_info/root.js
@@ -1,22 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Columns = (props) =>
+  <ul>
+    <li>Column 1</li>
+    <li>Column 2</li>
+    <li>Column 3</li>
+    <li>Column 4</li>
+  </ul>
+
 const Table = (props) =>
   <li>
-    <a href="#">
-      <span className="glyphicon glyphicon-plus"></span>
+    <a onClick={props.onClick} href="#">
+      {(() => {
+        if (props.expanded) {
+          return <span className="glyphicon glyphicon-minus"></span>;
+        } else {
+          return <span className="glyphicon glyphicon-plus"></span>;
+        }
+      })()}
+
       {props.name}
     </a>
+
+    {(() => {
+      if (props.expanded) {
+        return <Columns table={props.table}/>;
+      } else {
+        return null;
+      }
+    })()}
   </li>;
 
-const TableInfo = (props) =>
-  <div>
-    <h2>Tables</h2>
+class TableInfo extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <ul className="list-unstyled">
-      {props.tableNames.map((name, i) => <Table key={i} name={name} />)}
-    </ul>
-  </div>;
+    this.state = {expanded: new Set()};
+
+    this.toggleExpand = this.toggleExpand.bind(this);
+  }
+
+  toggleExpand(table) {
+    return () => {
+      const expanded = this.state.expanded;
+      if (this.expanded(table)) {
+        expanded.delete(table);
+      } else {
+        expanded.add(table);
+      }
+      this.setState({expanded})
+    }
+  }
+
+  expanded(table) {
+    return this.state.expanded.has(table);
+  }
+
+  render() {
+    return (<div>
+      <h2>Tables</h2>
+
+      <ul className="list-unstyled">
+        {this.props.tableNames.map((name, i) =>
+          <Table key={i} name={name} expanded={this.expanded(name)} onClick={this.toggleExpand(name)} />
+        )}
+      </ul>
+    </div>);
+  }
+}
 
 export default function renderTableInfo(data, elem) {
   ReactDOM.render(<TableInfo {...data} />, elem);
