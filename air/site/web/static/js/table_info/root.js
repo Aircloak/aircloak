f186e4959b50bcc3e7a09c4ad04fea469a59292c
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-13 15:56:02 +0200

    Display a list of tables in the side panel

--- air/site/web/static/js/table_info/root.js
+++ air/site/web/static/js/table_info/root.js
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const TableInfo = (props) => <div>
-  <h2>Tables</h2>
+const Table = (props) =>
+  <li>
+    <a href="#">
+      <span className="glyphicon glyphicon-plus"></span>
+      {props.name}
+    </a>
+  </li>;
 
-  <ul className="list-unstyled">
-  </ul>
-</div>
+const TableInfo = (props) =>
+  <div>
+    <h2>Tables</h2>
+
+    <ul className="list-unstyled">
+      {props.tableNames.map((name, i) => <Table key={i} name={name} />)}
+    </ul>
+  </div>;
 
 export default function renderTableInfo(data, elem) {
   ReactDOM.render(<TableInfo {...data} />, elem);
