f89d80a15a97a0d68e28c0ec4e698c5eaa168374
Sasa Juric <sasa.juric@gmail.com>
2016-09-14 15:22:11 +0200

    use linked containers locally
    Allows us reliable host resolving without needing to curl anything from within the container.

--- air/site/docker/start.sh
+++ air/site/docker/start.sh
@@ -10,8 +10,6 @@ function log {
 export HTTP_HOST_IP=${AIR_HOST_NAME:-"127.0.0.1"}
 export AIR_INSIGHTS_ENV="prod"
 
-echo "$(ip route get 8.8.8.8 | grep via | awk '{print $3}') air-db.local" >> /etc/hosts
-
 log "Booting container."
 mkdir -p /aircloak/insights/lib/air-0.0.1/priv/config/
 cp -rp /runtime_config/* /aircloak/insights/lib/air-0.0.1/priv/config/
