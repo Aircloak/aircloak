648ae396e5d1c5f8bdbb6aab1f2cb635ac63c826
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-14 15:32:03 +0200

    Fix table DOM in table_info/columns

--- air/site/web/static/js/table_info/columns.js
+++ air/site/web/static/js/table_info/columns.js
@@ -2,16 +2,21 @@ import React from "react";
 
 export const Columns = (props) =>
   <table className="table">
-    <tr>
-      <th>Column</th>
-      <th>Type</th>
-    </tr>
-    {props.columns.map((column, i) =>
-      <tr key={i}>
-        <td>{column.name}</td>
-        <td>{column.type}</td>
+    <thead>
+      <tr>
+        <th>Column</th>
+        <th>Type</th>
       </tr>
-    )}
+    </thead>
+
+    <tbody>
+      {props.columns.map((column, i) =>
+        <tr key={i}>
+          <td>{column.name}</td>
+          <td>{column.type}</td>
+        </tr>
+      )}
+    </tbody>
   </table>;
 
 Columns.propTypes = {
