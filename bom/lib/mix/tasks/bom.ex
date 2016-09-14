30237d55b07f7afbe2030858eae1c20f50cb5974
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-14 17:46:21 +0200

    Extend the docs for bom with version information

--- bom/lib/mix/tasks/bom.ex
+++ bom/lib/mix/tasks/bom.ex
@@ -5,8 +5,12 @@ defmodule Mix.Tasks.Bom do
 
       mix bom [--node <path>]+ [--elixir <path>]+ <output>
 
-      Add a --node switch for every node_modules directory to be searched.
-      Add a --elixir switch for every elixir deps directory to be searched.
+      Add a --node switch for every node_modules directory to be searched. An npm-shrinkwrap.json file is
+      assumed to exist at the same level as this directory.
+
+      Add a --elixir switch for every elixir deps directory to be searched. A mix.lock file is assumed to
+      exist at the same level as this directory.
+
       The file will be generated at <output>
   """
 
