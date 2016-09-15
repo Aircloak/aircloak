4ead0dfa21595ddb4a29830bdb7d089fdc8f73f7
Sebastian Probst Eide <sebastian@aircloak.com>
2016-09-15 13:18:17 +0200

    Remove aliases when only use in schema def
    When the module name was aliased, but only
    referenced in schema relationship, then the
    compiler would issue a warning, which in turn
    trips up our settings on Travis.

--- air/site/web/models/user.ex
+++ air/site/web/models/user.ex
@@ -6,7 +6,7 @@ defmodule Air.User do
 
   alias Ecto.Changeset
   alias Comeonin.Pbkdf2, as: Hash
-  alias Air.{Organisation, Query, Group}
+  alias Air.Organisation
 
   @type t :: %__MODULE__{}
   @type role_id :: non_neg_integer
@@ -22,8 +22,8 @@ defmodule Air.User do
 
     belongs_to :organisation, Organisation
 
-    has_many :queries, Query
-    many_to_many :groups, Group,
+    has_many :queries, Air.Query
+    many_to_many :groups, Air.Group,
       join_through: "groups_users",
       on_delete: :delete_all,
       on_replace: :delete
