2e40aaaa5096304eb85a303fb0e009a442c889c8
Sebastian Probst Eide <sebastian@aircloak.com>
2016-09-20 10:32:03 +0200

    Fix/remove org related migrations

--- air/site/priv/repo/migrations/20160919155841_remove_role_id_from_user.exs
+++ air/site/priv/repo/migrations/20160919155841_remove_role_id_from_user.exs
@@ -1,9 +1,15 @@
 defmodule Air.Repo.Migrations.RemoveRoleIdFromUser do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:users) do
       remove :role_id
+    end
+  end
+
+  def down do
+    alter table(:users) do
+      add :role_id, :integer
     end
   end
 end
