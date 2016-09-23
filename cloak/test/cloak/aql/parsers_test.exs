332727790a21e1abee8a420046693bb40443f143
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-23 19:06:36 +0200

    Improve cosmetics

--- cloak/test/cloak/aql/parsers_test.exs
+++ cloak/test/cloak/aql/parsers_test.exs
@@ -5,20 +5,16 @@ defmodule Cloak.Aql.Parsers.Test do
   import Cloak.Aql.Parsers
 
   describe "sep_by1_failing" do
-    test "a single item" do
+    test "a single item", do:
       assert [["a"]] = Combine.parse("a", sep_by1_failing(char("a"), char(",")))
-    end
 
-    test "a single wrong item" do
+    test "a single wrong item", do:
       assert {:error, _} = Combine.parse("b", sep_by1_failing(char("a"), char(",")))
-    end
 
-    test "multiple items" do
+    test "multiple items", do:
       assert [["a", "a", "a"]] = Combine.parse("a,a,a", sep_by1_failing(char("a"), char(",")))
-    end
 
-    test "multiple items with a wrong one" do
+    test "multiple items with a wrong one", do:
       assert {:error, _} = Combine.parse("a,b,a", sep_by1_failing(char("a"), char(",")))
-    end
   end
 end
