22cbbae07441d9db71ca727c8ccab02ecfaac47d
Sebastian Probst Eide <sebastian@aircloak.com>
2016-09-27 17:34:43 +0200

    Show users given access to in data sources

--- air/lib/air/utils/checkbox_mapper.ex
+++ air/lib/air/utils/checkbox_mapper.ex
@@ -16,7 +16,7 @@ defmodule Air.Utils.CheckboxMapper do
       label(form, field, label_opts) do
         [
           tag(:input, input_opts),
-          group_label_text(group)
+          data_source_content(group)
         ]
       end
     end
@@ -27,7 +27,7 @@ defmodule Air.Utils.CheckboxMapper do
       label(form, field, label_opts) do
         [
           tag(:input, input_opts),
-          users_label_text(group)
+          users_content(group)
         ]
       end
     end
@@ -37,6 +37,20 @@ defmodule Air.Utils.CheckboxMapper do
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
+
+  defp data_source_content(group) do
+    [
+      group_label_text(group),
+      raw(" &ndash; ") | data_sources_given_access_to(group)
+    ]
+  end
+
+  defp users_content(group) do
+    [
+      group_label_text(group),
+      raw(" &ndash; ") | users_given_access_to(group)
+    ]
+  end
 
   defp group_label_text(%Group{admin: false, name: name}), do: content_tag(:strong, html_escape(name))
   defp group_label_text(%Group{admin: true, name: name}) do
@@ -48,28 +62,33 @@ defmodule Air.Utils.CheckboxMapper do
     ]
   end
 
-  defp users_label_text(group) do
-    [
-      group_label_text(group),
-      raw(" &ndash; ") | users_given_access_to(group)
-    ]
-  end
 
   @num_to_take 5
+
+  defp data_sources_given_access_to(group) do
+    group = Repo.preload(group, :data_sources)
+    case group.data_sources do
+      [] -> ["Doesn't give access to any data sources"]
+      data_sources when length(data_sources) <= @num_to_take ->
+        [raw("Gives access to #{length(data_sources)} data_sources: ") | names(data_sources)]
+      data_sources ->
+        [raw("Gives access to #{length(data_sources)} data_sources, including: ") | names(data_sources)]
+    end
+  end
 
   defp users_given_access_to(group) do
     group = Repo.preload(group, :users)
     case group.users do
       [] -> ["Doesn't give access to any users"]
       users when length(users) <= @num_to_take ->
-        [raw("Gives access to #{length(users)} users: ") | user_names(users)]
+        [raw("Gives access to #{length(users)} users: ") | names(users)]
       users ->
-        [raw("Gives access to #{length(users)} users, including: ") | user_names(users)]
+        [raw("Gives access to #{length(users)} users, including: ") | names(users)]
     end
   end
 
-  defp user_names(users) do
-    users
+  defp names(entities) do
+    entities
     |> Enum.take(@num_to_take)
     |> Enum.map(&content_tag(:strong, html_escape(&1.name)))
     |> Enum.intersperse(raw(", "))
