1bb040b388a60ecc6b53cabec0f2032415b5ec7b
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-29 11:18:11 +0200

    Add tests for FixAlign of float intervals

--- cloak/test/cloak/aql/fix_align_test.exs
+++ cloak/test/cloak/aql/fix_align_test.exs
@@ -4,36 +4,40 @@ defmodule Cloak.Aql.FixAlign.Test do
 
   alias Cloak.Aql.FixAlign
 
-  property "aligned interval contains both ends of input" do
-    for_all {x, y} in int_interval do
-      {left, right} = FixAlign.align({x, y})
-      left <= x && y <= right
+  for {type_name, interval_type} <- [{:int, :int_interval}, {:float, :float_interval}] do
+    property "aligned #{type_name} interval contains both ends of input" do
+      for_all {x, y} in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+        {left, right} = FixAlign.align({x, y})
+        left <= x && y <= right
+      end
+    end
+
+    property "the endpoints of an #{type_name} interval are multiples of half the size" do
+      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+        {left, right} = FixAlign.align(interval)
+        half_size = (right - left) / 2
+        even_multiple?(left, half_size) && even_multiple?(right, half_size)
+      end
+    end
+
+    property "the size of an aligned #{type_name} interval is money-aligned" do
+      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+        {left, right} = FixAlign.align(interval)
+        size = right - left
+        even_power_of_10?(size) || even_power_of_10?(size / 2) || even_power_of_10?(size / 5)
+      end
+    end
+
+    property "fix align is idempotent on #{type_name} intervals" do
+      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+        FixAlign.align(interval) == FixAlign.align(FixAlign.align(interval))
+      end
     end
   end
 
-  property "the endpoints are multiples of half the size" do
-    for_all interval in int_interval do
-      {left, right} = FixAlign.align(interval)
-      half_size = (right - left) / 2
-      even_multiple?(left, half_size) && even_multiple?(right, half_size)
-    end
-  end
+  def int_interval, do: such_that({x, y} in {int, int} when x < y)
 
-  property "the size is an even power of ten, 2 times an even power or 5 times an even power (money-aligned)" do
-    for_all interval in int_interval do
-      {left, right} = FixAlign.align(interval)
-      size = right - left
-      even_power_of_10?(size) || even_power_of_10?(size / 2) || even_power_of_10?(size / 5)
-    end
-  end
-
-  property "fix align is idempotent" do
-    for_all interval in int_interval do
-      FixAlign.align(interval) == FixAlign.align(FixAlign.align(interval))
-    end
-  end
-
-  defp int_interval, do: such_that({x, y} in {int, int} when x < y)
+  def float_interval, do: such_that({x, y} in {float, float} when x < y)
 
   defp even_power_of_10?(x) do
     log = :math.log10(x)
