0925945aee357b88529f06fc509a41739019e554
Paweł Obrok <pawel.obrok@gmail.com>
2016-09-29 11:24:42 +0200

    Refactor FixAlign tests

--- cloak/test/cloak/aql/fix_align_test.exs
+++ cloak/test/cloak/aql/fix_align_test.exs
@@ -4,40 +4,43 @@ defmodule Cloak.Aql.FixAlign.Test do
 
   alias Cloak.Aql.FixAlign
 
-  for {type_name, interval_type} <- [{:int, :int_interval}, {:float, :float_interval}] do
-    property "aligned #{type_name} interval contains both ends of input" do
-      for_all {x, y} in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+  for interval_type <- [:int, :float] do
+    property "aligned #{interval_type} interval contains both ends of input" do
+      for_all {x, y} in interval(unquote(interval_type)) do
         {left, right} = FixAlign.align({x, y})
         left <= x && y <= right
       end
     end
 
-    property "the endpoints of an #{type_name} interval are multiples of half the size" do
-      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+    property "the endpoints of an #{interval_type} interval are multiples of half the size" do
+      for_all interval in interval(unquote(interval_type)) do
         {left, right} = FixAlign.align(interval)
         half_size = (right - left) / 2
         even_multiple?(left, half_size) && even_multiple?(right, half_size)
       end
     end
 
-    property "the size of an aligned #{type_name} interval is money-aligned" do
-      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
+    property "the size of an aligned #{interval_type} interval is money-aligned" do
+      for_all interval in interval(unquote(interval_type)) do
         {left, right} = FixAlign.align(interval)
         size = right - left
         even_power_of_10?(size) || even_power_of_10?(size / 2) || even_power_of_10?(size / 5)
       end
     end
 
-    property "fix align is idempotent on #{type_name} intervals" do
-      for_all interval in Kernel.apply(__MODULE__, unquote(interval_type), []) do
-        FixAlign.align(interval) == FixAlign.align(FixAlign.align(interval))
+    property "fix align is idempotent on #{interval_type} intervals" do
+      for_all interval in interval(unquote(interval_type)) do
+        interval |> FixAlign.align() == interval |> FixAlign.align() |> FixAlign.align()
       end
     end
   end
 
-  def int_interval, do: such_that({x, y} in {int, int} when x < y)
+  defp interval(:int), do: int_interval
+  defp interval(:float), do: float_interval
 
-  def float_interval, do: such_that({x, y} in {float, float} when x < y)
+  defp int_interval, do: such_that({x, y} in {int, int} when x < y)
+
+  defp float_interval, do: such_that({x, y} in {float, float} when x < y)
 
   defp even_power_of_10?(x) do
     log = :math.log10(x)
