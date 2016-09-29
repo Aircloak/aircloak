ed5d58cf5f60e6c486c42f0233d44d825b10bea6
Sasa Juric <sasa.juric@gmail.com>
2016-09-29 09:22:01 +0200

    remove leftover comment

--- air/start_dependencies.sh
+++ air/start_dependencies.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 
 cd $(dirname $0)
 
-# We always restart etcd (since settings may have changed). In contrast, db and
 ./db/build-image.sh
 DB_ENV="dev" ./db/container.sh ensure_started
 DB_ENV="test" ./db/container.sh ensure_started
