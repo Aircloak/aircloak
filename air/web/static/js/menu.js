2b5c276a7698ad76bb65f4b89adcfe8a0c909431
Paweł Obrok <pawel.obrok@gmail.com>
2016-10-04 14:18:39 +0200

    Remove dead code

--- air/web/static/js/menu.js
+++ air/web/static/js/menu.js
@@ -2,11 +2,6 @@
 
 import React from "react";
 
-type Info = {
-  action: () => void;
-  message: string;
-};
-
 export const Menu = (props: {children: Node}) => <div id="task-menu">{props.children}</div>;
 
 export const MenuButton = (props: {onClick: () => void, isActive: boolean, children?: Node}) =>
@@ -18,26 +13,3 @@ export const MenuButton = (props: {onClick: () => void, isActive: boolean, child
   >
     {props.children}
   </button>;
-
-export const PaneSelectButton = (props: {isActive: boolean, onClick: () => void, children?: Node}) => {
-  const classes = props.isActive ? "selection active" : "selection";
-
-  return (
-    <button onClick={props.onClick} className={classes}>
-      {props.children}
-    </button>
-  );
-};
-
-export const InfoBox = (props: {info: Info}) => {
-  if (props.info == null) {
-    return null;
-  } else {
-    return (
-      <div id="infobox">
-        <span className="label label-default">Notice</span>
-        <span onClick={props.info.action} id="infobox-message">{props.info.message}</span>
-      </div>
-    );
-  }
-};
