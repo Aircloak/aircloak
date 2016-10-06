67ad3d20435a2e636b77685b8071d66596955b10
Sebastian Probst Eide <sebastian@aircloak.com>
2016-10-06 09:07:34 +0200

    Add 404 page

--- air/test/views/error_view_test.exs
+++ air/test/views/error_view_test.exs
@@ -5,7 +5,7 @@ defmodule Air.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(Air.ErrorView, "404.html", []) == "Page not found"
+    assert render_to_string(Air.ErrorView, "404.html", []) =~ "Not found"
   end
 
   test "render 500.html" do
