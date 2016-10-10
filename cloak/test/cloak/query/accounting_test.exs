b62fb1d7df7e161524f499638e2f320462bfdc36
Sebastian Probst Eide <sebastian@aircloak.com>
2016-10-10 15:56:50 +0200

    Change table used for accounting tests

--- cloak/test/cloak/query/accounting_test.exs
+++ cloak/test/cloak/query/accounting_test.exs
@@ -4,24 +4,24 @@ defmodule Cloak.Query.AccountingTest do
   import Cloak.Test.QueryHelpers
 
   setup_all do
-    :ok = Cloak.Test.DB.create_table("heights", "height INTEGER, name TEXT, male BOOLEAN")
+    :ok = Cloak.Test.DB.create_table("numerics", "number INTEGER")
     :ok
   end
 
   setup do
-    Cloak.Test.DB.clear_table("heights")
+    Cloak.Test.DB.clear_table("numerics")
     :ok
   end
 
   test "account for the number of users processed" do
-    :ok = insert_rows(_user_ids = 1..50, "heights", ["height"], [180])
-    :ok = insert_rows(_user_ids = 51..100, "heights", ["height"], [190])
-    assert_query "select height from heights", %{au_count: 100}
+    :ok = insert_rows(_user_ids = 1..50, "numerics", ["number"], [1])
+    :ok = insert_rows(_user_ids = 51..100, "numerics", ["number"], [2])
+    assert_query "select number from numerics", %{au_count: 100}
   end
 
   test "only count each user once" do
-    :ok = insert_rows(_user_ids = 1..100, "heights", ["height"], [180])
-    :ok = insert_rows(_user_ids = 1..100, "heights", ["height"], [190])
-    assert_query "SELECT height FROM heights WHERE height IN (180, 190)", %{au_count: 100}
+    :ok = insert_rows(_user_ids = 1..100, "numerics", ["number"], [1])
+    :ok = insert_rows(_user_ids = 1..100, "numerics", ["number"], [2])
+    assert_query "SELECT number FROM numerics WHERE number IN (1, 2)", %{au_count: 100}
   end
 end
