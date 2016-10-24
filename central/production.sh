88ed4e9a85414604398cf0b34b68273e841a1481
Sebastian Probst Eide <sebastian@aircloak.com>
2016-10-24 09:47:31 +0200

    Adjust deployment config for central

--- central/production.sh
+++ central/production.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 cd $(dirname $0)
 . ../docker/production_helper.sh
 
-RUNTIME_CONFIG_PATH="/opt/share/air_runtime_configs/$DEPLOYMENT_NAME/"
-DOCKER_ARGS="-p $AIR_HTTP_PORT:8080"
+RUNTIME_CONFIG_PATH="/opt/share/central_runtime_config/"
+DOCKER_ARGS="-p $CENTRAL_HTTP_PORT:7080"
 
-run_production_command air air "${DEPLOYMENT_NAME}_air" $@
+run_production_command central central "central" $@
