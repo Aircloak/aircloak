1a6c1cc92cbda9e90d632fb0a4c8ae7b7b805423
Cristian Berneanu <cristian@aircloak.com>
2016-10-31 13:52:32 +0200

    Extend comparison module API

--- cloak/lib/cloak/aql/comparison.ex
+++ cloak/lib/cloak/aql/comparison.ex
@@ -13,9 +13,23 @@ defmodule Cloak.Aql.Comparison do
   def inequality?({:comparison, _, operator, _}), do: Enum.member?(@inequalities, operator)
   def inequality?(_), do: false
 
-  @doc "Returns the rhs value the given comparison compares against."
+  @doc "Returns the term the given comparison compares against."
   @spec value(Parser.where_clause) :: any
-  def value({:comparison, _, _, rhs}), do: rhs.value
+  def value({:comparison, _lhs, _, rhs}), do: rhs.value
+  def value({:not, comparison}), do: value(comparison)
+  def value({:is, _lhs, :null}), do: nil
+  def value({:in, _lhs, rhs}), do: Enum.map(rhs, & &1.value)
+  def value({:like, _lhs, rhs}), do: rhs.value
+  def value({:ilike, _lhs, rhs}), do: rhs.value
+
+  @doc "Returns the term the given comparison acts on."
+  @spec column(Parser.where_clause) :: any
+  def column({:comparison, lhs, _, _rhs}), do: lhs.name
+  def column({:not, comparison}), do: column(comparison)
+  def column({:is, lhs, :null}), do: lhs.name
+  def column({:in, lhs, _rhs}), do: lhs.name
+  def column({:like, lhs, _rhs}), do: lhs.value
+  def column({:ilike, lhs, _rhs}), do: lhs.value
 
   @doc "Returns a representation of the direction of the given inequality as `:<` or `:>`."
   @spec direction(Parser.where_clause) :: direction
