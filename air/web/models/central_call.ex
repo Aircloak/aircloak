3d6f364df9c6622f1ab52f89f949d6c8a11593b5
Sebastian Probst Eide <sebastian@aircloak.com>
2016-11-03 17:10:54 +0100

    Add database buffer for failed RPCs to Central

---
+++ air/web/models/central_call.ex
@@ -0,0 +1,34 @@
+defmodule Air.CentralCall do
+  @moduledoc """
+  This model holds RPC calls from the air to the central that failed
+  due to the server not being connected, or other temporary glitches.
+  """
+  use Air.Web, :model
+
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
+
+  schema "central_calls" do
+    field :event, :string
+    field :payload, :map
+
+    timestamps
+  end
+
+  @required_fields ~w(event payload)a
+  @optional_fields ~w()a
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  @spec changeset(t | Changeset.t, Map.t) :: Changeset.t
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
