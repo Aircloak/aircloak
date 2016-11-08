744c15f59b339d9552d43b7e7240cf0f5c7aafc6
Paweł Obrok <pawel.obrok@gmail.com>
2016-11-08 11:45:48 +0100

    Revert "Filter messages by user in the session topic"
    This reverts commit e8e3d9b72165da3b71bbd16b2c54cbb8be81c79f. The commit
    is not really needed and there was some disagreement with reviewer on
    how it should be handled if at all.

--- air/web/socket/frontend/user_channel.ex
+++ air/web/socket/frontend/user_channel.ex
@@ -20,8 +20,7 @@ defmodule Air.Socket.Frontend.UserChannel do
   """
   @spec broadcast_result(Query.t) :: :ok
   def broadcast_result(query) do
-    payload = query |> Query.for_display() |> Map.put(:user_id, query.user_id)
-    Air.Endpoint.broadcast_from!(self(), "session:#{query.session_id}", "result", payload)
+    Air.Endpoint.broadcast_from!(self(), "session:#{query.session_id}", "result", Query.for_display(query))
     :ok
   end
 
@@ -37,18 +36,5 @@ defmodule Air.Socket.Frontend.UserChannel do
       {:ok, _} -> {:ok, socket}
       _ -> {:error, %{success: false, description: "Channel not found"}}
     end
-  end
-
-  intercept ["result"]
-
-  @doc false
-  def handle_out("result", msg, socket) do
-    if socket.assigns.user.id == msg.user_id do
-      push(socket, "result", Map.delete(msg, :user_id))
-    else
-      # Drop message
-    end
-
-    {:noreply, socket}
   end
 end
