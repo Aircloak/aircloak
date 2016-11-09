dc0a3ddbdf14dc8a4c0423932fab2dd879277deb
Sebastian Probst Eide <sebastian@aircloak.com>
2016-11-09 14:56:57 +0100

    Move container class logic from template to view

--- air/web/views/layout_view.ex
+++ air/web/views/layout_view.ex
@@ -4,6 +4,14 @@ defmodule Air.LayoutView do
   # bug in the current Phoenix
   @dialyzer :no_match
 
+  defp container_class(assigns) do
+    if assigns[:full_width] do
+      "container-fluid"
+    else
+      "container"
+    end
+  end
+
   defp help_links(conn) do
     case Air.Plug.HelpPages.help_pages(conn) do
       [] -> ""
