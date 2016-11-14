74d1c72ab7604bfb34ca1250f3290559d026e106
Sebastian Probst Eide <sebastian@aircloak.com>
2016-11-14 13:45:13 +0100

    Remove dead code

--- central/web/controllers/stats_controller.ex
+++ central/web/controllers/stats_controller.ex
@@ -2,8 +2,6 @@ defmodule Central.StatsController do
   @moduledoc false
   use Central.Web, :controller
 
-  alias Central.Service.{Stats, Customer}
-
   plug :set_layout
 
 
@@ -14,11 +12,6 @@ defmodule Central.StatsController do
   def index(conn, _params) do
     dash_url = Central.site_setting("kibana_main_dash")
     render(conn, "index.html", dash: dash_url)
-  end
-
-  def show(conn, %{"id" => id}) do
-    {:ok, customer} = Customer.get(id)
-    render(conn, "show.html", customer: customer, stats: Stats.for_customer(customer))
   end
 
 
