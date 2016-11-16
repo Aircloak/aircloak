f98ea5b50a0dabb1264bc8087e40a1eeffca7107
Sasa Juric <sasa.juric@gmail.com>
2016-11-16 17:06:11 +0100

    remove unused functions
    They were failing in dialyzer, but weren't used anywhere in the project, so I just removed them.

--- air/lib/air/utils/date_time.ex
+++ air/lib/air/utils/date_time.ex
@@ -14,32 +14,4 @@ defmodule Air.Utils.DateTime do
     |> NaiveDateTime.from_erl!()
     |> Timex.from_now()
   end
-
-  @doc """
-  Returns an Ecto.DateTime dated N days in the past.
-
-  Please note that the hour, minutes and seconds are all set to 0.
-  Therefore if you want to use the returned value in a database compairson,
-  and want all records for the current day included, then you should use
-  -1 as your parameter, or alternative use `datetime_days_in_the_future with 1
-  as the parameter in order to get a comparison like this:
-
-  ... where: inserted_at <= <tomorrow's date at midnight>
-  """
-  @spec datetime_days_ago(integer) :: Ecto.DateTime.t
-  def datetime_days_ago(days) do
-    interval = Timex.Time.to_timestamp(days, :days)
-    now = Timex.Date.now()
-    Timex.subtract(now, interval)
-    |> Timex.to_erlang_datetime()
-    |> Ecto.DateTime.from_erl()
-  end
-
-  @doc """
-  Returns an Ecto.DateTime dated N days in the future.
-  The results are the same as you would get when calling datetime_days_ago
-  with the parameter negated.
-  """
-  @spec datetime_days_in_the_future(integer) :: Ecto.DateTime.t
-  def datetime_days_in_the_future(days), do: datetime_days_ago(-1 * days)
 end
