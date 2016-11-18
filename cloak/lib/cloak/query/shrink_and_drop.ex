4c440e40fe2826a89375d0e5dbea22c856049640
Paweł Obrok <pawel.obrok@gmail.com>
2016-11-18 17:12:59 +0100

    Implement buffering strategy for shrink and drop

--- cloak/lib/cloak/query/shrink_and_drop.ex
+++ cloak/lib/cloak/query/shrink_and_drop.ex
@@ -2,39 +2,126 @@ defmodule Cloak.Query.ShrinkAndDrop do
   alias Cloak.Query.Anonymizer
   alias Cloak.Aql.{FixAlign, Function}
 
-  def apply(rows, query) do
-    for_suppression =
-      query.ranges
-      |> Stream.map(fn {column, range} -> for_suppression(rows, column, range) end)
-      |> Enum.reduce(MapSet.new, &MapSet.union/2)
+  defmodule HalfBuffer do
+    defstruct [:size, :min, :max, :users]
 
-    Enum.reject(rows, &MapSet.member?(for_suppression, &1))
-  end
+    def new(size), do: %HalfBuffer{size: size, min: nil, max: nil, users: %{}}
 
-  defp for_suppression(_rows, _column, {%Time{}, _}), do: MapSet.new
-  defp for_suppression(_rows, _column, {%Date{}, _}), do: MapSet.new
-  defp for_suppression(_rows, _column, {%NaiveDateTime{}, _}), do: MapSet.new
-  defp for_suppression(rows, column, range) do
-    for {x, y} <- attack_intervals(rows, column, range) do
-      {in_range, out_of_range} = Enum.partition(rows, fn(row) ->
-        row_value = Function.apply_to_db_row(column, row)
-        row_value >= x && row_value < y
-      end)
+    def under?(%{min: nil}, _), do: false
+    def under?(%{max: max}, value), do: value <= max
 
-      anonymizer = in_range |> Enum.map(&Enum.at(&1, 0)) |> MapSet.new() |> Anonymizer.new()
+    def over?(%{max: nil}, _), do: false
+    def over?(%{max: max}, value), do: value >= max
 
-      case Anonymizer.sufficiently_large?(anonymizer, Enum.count(out_of_range)) do
-        {true, _} -> MapSet.new
-        {false, _} -> MapSet.new(out_of_range)
+    def add(buffer = %{min: nil}, value, user_id, row, _comparator), do:
+      {%{buffer | min: value, max: value, users: %{user_id => %{value: value, rows: [row]}}}, []}
+    def add(buffer, value, user_id, row, comparator) do
+      %{
+        buffer |
+        min: min(buffer.min, value),
+        max: max(buffer.max, value),
+        users: Map.update(buffer.users, user_id, %{value: value, rows: [row]}, fn(%{value: old_value, rows: rows}) ->
+          %{value: (if comparator.(old_value, value), do: old_value, else: value), rows: [row | rows]}
+        end),
+      }
+      |> pop_as_needed(comparator)
+    end
+
+    def includes?(%{users: users}, user_id), do: Map.has_key?(users, user_id)
+
+    def all(%{users: users}), do: users |> Map.values() |> Enum.map(&(&1[:rows])) |> Enum.concat()
+
+    defp pop_as_needed(buffer = %{size: size, users: users}, comparator) do
+      if Enum.count(users) > size do
+        pop(buffer, comparator)
+      else
+        {buffer, []}
       end
     end
-    |> Enum.reduce(MapSet.new, &MapSet.union/2)
+
+    defp pop(buffer, comparator) do
+      {user_to_pop, _} = Enum.reduce(buffer.users, Enum.at(buffer.users, 0), fn
+        (user = {_, %{value: x}}, found = {_, %{value: y}}) -> if comparator.(y, x), do: user, else: found
+      end)
+      {%{rows: rows}, new_users} = Map.pop(buffer.users, user_to_pop)
+
+      {
+        %{
+          buffer |
+          users: new_users,
+          min: new_users |> Map.values() |> Enum.map(&(&1[:value])) |> Enum.min(),
+          max: new_users |> Map.values() |> Enum.map(&(&1[:value])) |> Enum.max(),
+        },
+        rows
+      }
+    end
   end
 
-  defp attack_intervals(rows, column, {x, y}) do
-    shift = (y - x) / 2
-    FixAlign.subintervals(compact_range(rows, column)) ++ [{x - shift, y - shift}, {x + shift, y + shift}]
+  defmodule Buffer do
+    defstruct [:left, :right]
+
+    @extremes_to_keep 11
+
+    def new, do: %Buffer{left: HalfBuffer.new(@extremes_to_keep), right: HalfBuffer.new(@extremes_to_keep)}
+
+    def add(row, buffer) do
+      {new_buffer, popped} = do_add(row, buffer)
+      {new_buffer, popped} = Enum.reduce(popped, {new_buffer, []}, fn(row = {_, user_id, value, _}, {buffer, popped}) ->
+        cond do
+          HalfBuffer.includes?(buffer.left, user_id) ->
+            {%{buffer | left: HalfBuffer.add(buffer.left, user_id, value, row, &Kernel.</2)}, popped}
+          HalfBuffer.includes?(buffer.right, user_id) ->
+            {%{buffer | right: HalfBuffer.add(buffer.right, user_id, value, row, &Kernel.>/2)}, popped}
+          true -> {buffer, [row | popped]}
+        end
+      end)
+      {new_buffer, Enum.uniq(popped)}
+    end
+
+    def all(%{left: left, right: right}) do
+      HalfBuffer.all(left) ++ HalfBuffer.all(right) |> Enum.uniq()
+    end
+
+    defp do_add(row = {_, user_id, value, _}, buffer = %{left: left, right: right}) do
+      cond do
+        HalfBuffer.over?(left, value) && HalfBuffer.under?(right, value) -> {buffer, [row]}
+        not HalfBuffer.over?(left, value) && not HalfBuffer.under?(right, value) ->
+          {new_left, popped_left} = HalfBuffer.add(left, value, user_id, row, &Kernel.</2)
+          {new_right, popped_right} = HalfBuffer.add(right, value, user_id, row, &Kernel.>/2)
+          {%{buffer | left: new_left, right: new_right}, popped_left ++ popped_right |> Enum.uniq}
+        not HalfBuffer.over?(left, value) ->
+          {new_left, popped_left} = HalfBuffer.add(left, value, user_id, row, &Kernel.</2)
+          {%{buffer | left: new_left}, popped_left}
+        not HalfBuffer.under?(right, value) ->
+          {new_right, popped_right} = HalfBuffer.add(right, value, user_id, row, &Kernel.>/2)
+          {%{buffer | right: new_right}, popped_right}
+      end
+    end
   end
+
+  def apply(rows, query), do: Enum.reduce(query.ranges, rows, &suppress_outliers/2)
+
+  defp suppress_outliers({column, _range}, rows) do
+    rows
+    |> Stream.concat([:done])
+    |> Stream.transform(%{next_id: 0, buffer: Buffer.new}, &do_suppress_outliers(&1, &2, column))
+  end
+
+  defp do_suppress_outliers(:done, %{buffer: buffer}, _column) do
+    {buffer |> Buffer.all() |> Enum.map(&undecorate_row/1), nil}
+  end
+  defp do_suppress_outliers(row, %{next_id: next_id, buffer: buffer}, column) do
+    {new_buffer, popped_rows} = row |> decorate_row(next_id, column) |> Buffer.add(buffer)
+    {Enum.map(popped_rows, &undecorate_row/1), %{next_id: next_id + 1, buffer: new_buffer}}
+  end
+
+  defp decorate_row(row, id, column), do: {id, user_id(row), value(row, column), row}
+
+  defp undecorate_row({_, _, _, row}), do: row
+
+  defp user_id([id | _rest]), do: id
+
+  defp value(row, column), do: Function.apply_to_db_row(column, row)
 
   defp compact_range(rows, column) do
     values = Enum.map(rows, &Function.apply_to_db_row(column, &1))
---
+++ cloak/test/cloak/query/shrink_and_drop_test.exs
@@ -0,0 +1,60 @@
+defmodule Cloak.Query.ShrinkAndDrop.Test do
+  use ExUnit.Case, async: true
+
+  alias Cloak.Aql.Column
+  alias Cloak.Query.ShrinkAndDrop
+  alias Cloak.Query.ShrinkAndDrop.HalfBuffer
+
+  test "when all values are the same nothing is dropped" do
+    data = [["user1", 10], ["user2", 10], ["user3", 10]]
+    query = %{ranges: %{%Column{db_row_position: 1} => [0, 20]}}
+
+    assert ShrinkAndDrop.apply(data, query) |> Enum.sort() == Enum.sort(data)
+  end
+
+  test "a basic scenario" do
+    data = [["user1", 10], ["user1", 15], ["user2", 20], ["user2", 25], ["user3", 30]]
+    query = %{ranges: %{%Column{db_row_position: 1} => [0, 50]}}
+
+    assert ShrinkAndDrop.apply(data, query) |> Enum.sort() == Enum.sort(data)
+  end
+
+  describe "HalfBuffer" do
+    test "adding to an empty buffer" do
+      assert HalfBuffer.add(HalfBuffer.new(3), 10, "user1", :some_data, :not_used) ==
+        {%HalfBuffer{size: 3, min: 10, max: 10, users: %{"user1" => %{value: 10, rows: [:some_data]}}}, []}
+    end
+
+    test "adding when the user already in the buffer" do
+      buffer = %HalfBuffer{size: 3, min: 10, max: 20, users: %{"user1" => %{value: 20, rows: [:some_data]}}}
+      assert HalfBuffer.add(buffer, 25, "user1", :more_data, &Kernel.>/2) ==
+        {%HalfBuffer{size: 3, min: 10, max: 25, users: %{"user1" => %{value: 25, rows: [:more_data, :some_data]}}}, []}
+    end
+
+    test "adding when the user is not in the buffer" do
+      buffer = %HalfBuffer{size: 3, min: 10, max: 20, users: %{"user1" => :some_data}}
+      assert HalfBuffer.add(buffer, 5, "user2", :more_data, :not_used) ==
+        {%HalfBuffer{size: 3, min: 5, max: 20, users: %{
+          "user1" => :some_data,
+          "user2" => %{value: 5, rows: [:more_data]}}}, []}
+    end
+
+    test "adding to a full buffer" do
+      buffer = %HalfBuffer{size: 3, min: 1, max: 3, users: %{
+        "user1" => %{value: 1, rows: [:some_data]},
+        "user2" => %{value: 2, rows: [:other_data]},
+        "user3" => %{value: 3, rows: [:more_data]},
+      }}
+
+      assert HalfBuffer.add(buffer, 0, "user4", :even_more_data, &Kernel.</2) ==
+        {
+          %HalfBuffer{size: 3, min: 0, max: 2, users: %{
+            "user4" => %{value: 0, rows: [:even_more_data]},
+            "user1" => %{value: 1, rows: [:some_data]},
+            "user2" => %{value: 2, rows: [:other_data]},
+          }},
+          [:more_data]
+        }
+    end
+  end
+end
