ca9459d50842f87191f89edd85fcf104d396516c
Cristian Berneanu <cristian@aircloak.com>
2016-11-22 17:56:08 +0200

    Implement aggregation for inner MongoDB queries

--- cloak/lib/cloak/data_source/mongodb/projector.ex
+++ cloak/lib/cloak/data_source/mongodb/projector.ex
@@ -29,6 +29,13 @@ defmodule Cloak.DataSource.MongoDB.Projector do
     end
   end
 
+  @doc "Creates a MongoDB projection from a column."
+  @spec map_column(Column.t) :: {String.t, atom | map}
+  def map_column(%Column{name: name, alias: nil}), do: {name, true}
+  def map_column(%Column{aggregate?: true, db_function: fun, db_function_args: [arg], alias: alias}), do:
+    {get_field_name(alias), parse_function(fun, begin_parse_column(arg))}
+  def map_column(column), do: {get_field_name(column.alias), begin_parse_column(column)}
+
 
   #-----------------------------------------------------------------------------------------------------------
   # Internal functions
@@ -36,18 +43,14 @@ defmodule Cloak.DataSource.MongoDB.Projector do
 
   defp map_array_size(name), do: %{'$size': %{'$ifNull': ["$" <> name, []]}}
 
-  defp map_column(%Column{name: name, alias: nil}), do: {name, true}
-  defp map_column(%Column{db_function: fun} = column) when fun != nil do
+  defp begin_parse_column(%Column{db_function: fun} = column) when fun != nil do
     non_null_args =
       column
       |> extract_fields()
       |> Enum.map(&%{'$gt': ["$" <> &1, nil]})
-    {
-      get_field_name(column.alias),
-      %{'$cond': [%{'$and': non_null_args}, parse_column(column), nil]}
-    }
+    %{'$cond': [%{'$and': non_null_args}, parse_column(column), nil]}
   end
-  defp map_column(column), do: {get_field_name(column.alias), parse_column(column)}
+  defp begin_parse_column(column), do: parse_column(column)
 
   defp valid_alias?(name), do:
     String.match?(name, ~r/^[a-zA-Z_#][a-zA-Z0-9_.#]*$/) and
@@ -62,30 +65,43 @@ defmodule Cloak.DataSource.MongoDB.Projector do
     name
   end
 
+  defp extract_fields({:distinct, column}), do: extract_fields(column)
+  defp extract_fields(:*), do: []
   defp extract_fields(%Column{constant?: true}), do: []
   defp extract_fields(%Column{db_function: fun, db_function_args: args}) when fun != nil, do:
     Enum.flat_map(args, &extract_fields/1)
   defp extract_fields(%Column{name: name}) when is_binary(name), do: [name]
 
+  defp parse_column(:*), do: :*
+  defp parse_column({:distinct, column}), do: {:distinct, parse_column(column)}
   defp parse_column(%Column{constant?: true, value: value}), do: %{'$literal': value}
   defp parse_column(%Column{db_function: "length", db_function_args: [%Column{name: name}]})
     when is_binary(name), do: "$" <> name <> ".length"
   defp parse_column(%Column{db_function: {:cast, type}, db_function_args: [value]}), do:
     parse_function("cast", [parse_column(value), value.type, type])
+  defp parse_column(%Column{db_function: fun, db_function_args: [arg]}) when fun != nil, do:
+    parse_function(fun, parse_column(arg))
   defp parse_column(%Column{db_function: fun, db_function_args: args}) when fun != nil, do:
     parse_function(fun, Enum.map(args, &parse_column/1))
-  defp parse_column(%Column{name: name}) when is_binary(name), do: "$" <> name
+  defp parse_column(%Column{name: name}) when is_binary(name), do:
+    if Schema.is_array_size?(name),
+      do: name |> Schema.array_size_field() |> map_array_size(),
+      else: "$" <> name
 
   defp parse_function("left", [string, count]), do: %{"$substr" => [string, 0, count]}
   defp parse_function("substring", [string, from]), do: %{"$substr" => [string, from, -1]}
+  defp parse_function("count", :*), do: %{'$sum': 1}
+  defp parse_function(_, {:distinct, value}), do: %{'$addToSet': value}
+  defp parse_function("count", value), do: %{'$sum': %{'$cond': [%{'$gt': [value, nil]}, 1, 0]}}
   for {name, translation} <- %{
     "*" => "$multiply", "/" => "$divide", "+" => "$add", "-" => "$subtract",
-    "^" => "$pow", "%" => "$mod", "mod" => "$mod", "sqrt" => "$sqrt",
+    "^" => "$pow", "pow" => "$pow", "%" => "$mod", "mod" => "$mod", "sqrt" => "$sqrt",
     "floor" => "$floor", "ceil" => "$ceil", "trunc" => "$trunc", "abs" => "$abs",
     "||" => "$concat", "concat" => "$concat", "substring" => "$substr",
     "lower" => "$toLower", "lcase" => "$toLower", "upper" => "$toUpper", "ucase" => "$toUpper",
     "year" => "$year", "month" => "$month", "day" => "$dayOfMonth", "weekday" => "$dayOfWeek",
     "hour" => "$hour", "minute" => "$minute", "second" => "$second",
+    "sum" => "$sum", "avg" => "$avg", "min" => "$min", "max" => "$max", "stddev" => "$stdDevPop",
   }, do:
     defp parse_function(unquote(name), args), do: %{unquote(translation) => args}
   defp parse_function("cast", [value, :datetime, :text]), do:
