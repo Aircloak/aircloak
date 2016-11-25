fa24baa93dd09c559b0581253ebb4df4189c2153
Cristian Berneanu <cristian@aircloak.com>
2016-11-25 11:05:24 +0200

    Fix performance tests

--- cloak/lib/cloak/test/db.ex
+++ cloak/lib/cloak/test/db.ex
@@ -35,6 +35,22 @@ defmodule Cloak.Test.DB do
     :ok
   end
 
+  def register_test_table(table_id, db_name, decoders \\ []) do
+    table = %{db_name: db_name, user_id: "user_id", decoders: decoders}
+    sources = for source <- Application.get_env(:cloak, :data_sources) do
+      tables = Map.put(source[:tables], table_id, table)
+      Map.put(source, :tables, tables) |> DataSource.add_tables()
+    end
+    DataSource.cache_columns(sources)
+  end
+
+  def clear_test_tables() do
+    sources = for source <- Application.get_env(:cloak, :data_sources) do
+      Map.put(source, :tables, %{})
+    end
+    DataSource.cache_columns(sources)
+  end
+
 
   # -------------------------------------------------------------------
   # GenServer callbacks
@@ -51,8 +67,7 @@ defmodule Cloak.Test.DB do
     db_name = opts[:db_name] || table_name
     create_db_table(db_name, definition, opts)
     decoders = opts[:decoders] || []
-    table = %{db_name: full_table_name(db_name), user_id: "user_id", decoders: decoders}
-    register_test_table(String.to_atom(table_name), table)
+    register_test_table(String.to_atom(table_name), full_table_name(db_name), decoders)
     {:reply, :ok, state}
   end
 
@@ -60,21 +75,6 @@ defmodule Cloak.Test.DB do
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
-
-  defp register_test_table(table_id, table) do
-    sources = for source <- Application.get_env(:cloak, :data_sources) do
-      tables = Map.put(source[:tables], table_id, table)
-      Map.put(source, :tables, tables) |> DataSource.add_tables()
-    end
-    DataSource.cache_columns(sources)
-  end
-
-  defp clear_test_tables() do
-    sources = for source <- Application.get_env(:cloak, :data_sources) do
-      Map.put(source, :tables, %{})
-    end
-    DataSource.cache_columns(sources)
-  end
 
   defp create_db_table(db_name, definition, opts) do
     if opts[:skip_db_create] do
