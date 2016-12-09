0ef1612e2a6bd8219abd0c84a8f525c5516d10d2
Sasa Juric <sasa.juric@gmail.com>
2016-12-09 11:45:46 +0100

    emphasize presented control errors

--- air/web/views/error_helpers.ex
+++ air/web/views/error_helpers.ex
@@ -10,7 +10,9 @@ defmodule Air.ErrorHelpers do
   """
   def error_tag(form, field) do
     if error = form.errors[field] do
-      content_tag :span, translate_error(error), class: "help-block"
+      content_tag :span, class: "has-error" do
+        content_tag :b, translate_error(error), class: "help-block"
+      end
     end
   end
 
