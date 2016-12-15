c942b09c456ba105e0585837d3752742ce984da2
Sebastian Probst Eide <sebastian@aircloak.com>
2016-12-15 12:13:54 +0100

    Add high-level comments for lenses, and restructure

--- cloak/lib/cloak/aql/lenses/query.ex
+++ cloak/lib/cloak/aql/lenses/query.ex
@@ -10,6 +10,7 @@ defmodule Cloak.Aql.Lenses.Query do
   # Lenses into Query
   # -------------------------------------------------------------------
 
+  @doc "Lens focusing on all terminal elements of a query, including intermediate function invocations."
   deflens terminal_elements() do
     Lens.match(fn
       {:function, "count", :*} -> Lens.empty()
@@ -22,36 +23,54 @@ defmodule Cloak.Aql.Lenses.Query do
     end)
   end
 
+  @doc "Lens focusing on terminal elements that are part of WHERE-clauses"
   deflens where_terminal_elements() do
     Lens.match(fn
       {:not, _} -> Lens.at(1) |> where_terminal_elements()
-      {:comparison, _lhs, _comparator, _rhs} -> Lens.both(Lens.at(1), Lens.at(3)) |> terminal_elements()
+      {:comparison, _lhs, _comparator, _rhs} -> Lens.indices([1, 3]) |> terminal_elements()
       {op, _, _} when op in [:in, :like, :ilike, :is] -> Lens.both(Lens.at(1), Lens.at(2)) |> terminal_elements()
     end)
   end
 
+  @doc "Lens focusing on invocations of row splitting functions"
   deflens splitter_functions(), do: terminal_elements() |> Lens.satisfy(&Function.row_splitting_function?/1)
 
+  @doc "Lens focusing on invocations of the bucket function"
   deflens buckets(), do: terminal_elements() |> Lens.satisfy(&Function.bucket?/1)
 
+  @doc "Lens focusing on a query's immediate subqueries"
   deflens direct_subqueries(), do: Lens.key(:from) |> do_direct_subqueries()
 
-  deflens do_direct_subqueries() do
+  deflens do_direct_subqueries(), do:
     Lens.match(fn
       {:join, _} -> Lens.at(1) |> Lens.keys([:lhs, :rhs]) |> do_direct_subqueries()
       {:subquery, _} -> Lens.at(1)
       _ -> Lens.empty()
     end)
-  end
 
+  @doc """
+  Lens focusing on all queries found in a query. Also includes the top-level query itself.
+  More concretely, given a query containing a subquery, the main query as well as the subquery
+  will be given focus.
+  """
+  deflens queries(), do:
+    Lens.both(direct_subqueries(), Lens.root())
+
+  @doc "Lens focusing on all WHERE-clause inequalities in a query."
   deflens where_inequality_columns(), do:
     Lens.keys([:where, :lcf_check_conditions])
     |> do_where_inequality_columns()
     |> Lens.satisfy(&(not &1.constant?))
 
-  deflens queries(), do:
-    Lens.both(direct_subqueries(), Lens.root())
+  @doc "Lens focusing on the tables selected from the database. Does not include subqueries."
+  deflens leaf_tables, do: Lens.key(:from) |> do_leaf_tables()
 
+
+  # -------------------------------------------------------------------
+  # Internal lenses
+  # -------------------------------------------------------------------
+
+  @doc "Utility lens used by `where_inequality_columns`. Please use that lens instead."
   deflens do_where_inequality_columns(), do:
     Lens.match(fn
       {:comparison, _, check, _} when check in ~w(> >= < <=)a ->
@@ -61,13 +80,10 @@ defmodule Cloak.Aql.Lenses.Query do
       _ -> Lens.empty()
     end)
 
-  deflens leaf_tables, do: Lens.key(:from) |> do_leaf_tables()
-
-  deflens do_leaf_tables() do
+  deflens do_leaf_tables(), do:
     Lens.match(fn
       {:join, _} -> Lens.at(1) |> Lens.keys([:lhs, :rhs]) |> do_leaf_tables()
       {:subquery, _} -> Lens.empty()
       table when is_binary(table) -> Lens.root()
     end)
-  end
 end
