ddc329d5dbe4ee6b8dc087177d1bc4bb9cbaa51f
Sebastian Probst Eide <sebastian@aircloak.com>
2016-12-15 10:05:11 +0100

    Improve language in comments

--- cloak/lib/cloak/aql/type_checker.ex
+++ cloak/lib/cloak/aql/type_checker.ex
@@ -1,23 +1,24 @@
 defmodule Cloak.Aql.TypeChecker do
   @moduledoc """
-  Allows a user to verify whether a column that is to be selected, or
-  used as part of a WHERE-clause inequality has seen illegal transformations or not.
+  Provides functions to check whether selected columns, or expressions
+  used in WHERE-clause inequalities have undergone dangerous transformations.
 
-  The transformations that are considered illegal differ based on where
-  a value is to be used. More specifically the rules differ between values
-  used in WHERE-clauses and those that are selected for reporting.
+  What transformations are considered dangerous differ based on where
+  an expression is to be used. WHERE-clause inequalities have stricter rules
+  applied to them than selected columns.
 
   In particular this module considers whether a column has seen math
   or has had discontinuous functions applied to it. We currently only
-  consider math and discontinuous functions dangerous if the input values
-  to the function have been tainted by an analyst provided constant.
+  consider math and discontinuous functions dangerous if the inputs
+  to a function have been tainted by an analyst provided constant.
   Hence for example `abs(<pure-column>)` is ok, whereas
   `abs(<pure-column> + <constant>)` is not.
   Likewise `<pure-column> + <pure-column>` is ok, whereas
   `<pure-column> + (<pure-column> + <constant>)` is not.
 
-  Note also that `<constant> MATH <constant>` is transformed to `<constant>`, and
-  hence not considered as application of math.
+  Note also that `<constant> MATH <constant>` and `DISCONTINUOUS_FUNCTION(<constant>)`
+  are transformed to `<constant>`, and hence not considered as applications of math
+  or discontinuous functions.
   """
 
   alias Cloak.Aql.{Column, Compiler}
@@ -26,27 +27,26 @@ defmodule Cloak.Aql.TypeChecker do
     @moduledoc false
 
     @type t :: %__MODULE__{
-      # Whether the value is a constant at this point?
-      # A constant that has interacted with a non-constant column (for example
-      # through math) is no longer a constant.
+      # Whether the expressions is a constant. As soon as a constant expression
+      # interacts with a non-constant expression through math or function application
+      # it ceases to be constant.
       constant?: boolean,
 
-      # True if any of the values it has come in contact with through functions
+      # True if any of the expressions it has come in contact with through functions
       # were constant.
       touched_by_constant?: boolean,
 
-      # True if the value has been processed by a discontinuous function and the
+      # True if the expressions has been processed by a discontinuous function and the
       # parameters of the function call were such that the computation is classified
       # as potentially dangerous (i.e. an attack vector).
-      # Taints all other values, hence, if a single value in a function application
-      # is discontinuous, then the result of the function is discontinous too.
-      # Strings are handled differently here, and are only considered dangerously
-      # discontinuous if the result of the discontinuous function is later turned
-      # into a number
+      # Taints all other later expressions, hence, if a single expression in a function
+      # application is dangerously discontinuous, then the result of the function is
+      # dangerously discontinous too.
       dangerously_discontinuous?: boolean,
 
-      # Whether there has been math performed on the value, and a value that has
-      # been influenced by a constant was part of the math too.
+      # Whether there expression has had dangerous math performed on it or not.
+      # Math is considered dangerous if any of the expressions in the math application
+      # have previously been touched by a constant.
       seen_dangerous_math?: boolean,
     }
 
@@ -65,19 +65,20 @@ defmodule Cloak.Aql.TypeChecker do
   @discontinuous_string_functions ~w(btrim left ltrim right rtrim substring)
   @math_functions ~w(+ - * / ^ pow div)
 
-  @doc "Returns true if a value is safe to be reported. False otherwise"
+  @doc "Returns true if an expression of this type is safe to be reported. False otherwise"
   @spec ok_for_display?(Type.t) :: boolean
   def ok_for_display?(type), do:
     not (type.dangerously_discontinuous? and type.seen_dangerous_math?)
 
-  @doc "Returns true if a value is safe to be used in a WHERE-inequality. False otherwise"
+  @doc "Returns true if an expression of this type is safe to be used in a WHERE-inequality. False otherwise"
   def ok_for_where_inquality?(type), do:
     not (type.dangerously_discontinuous? or type.seen_dangerous_math?)
 
   @doc """
-  Produces a type characteristic for column by resolving it down to a table column.
-  The type of the the underlying value is not of interest in this case, but rather
-  the class of transformations that have been performed on the value.
+  Produces a type characteristic for an expression by resolving function applications and references
+  back to the underlying table columns and constants contributing to the expression.
+  The type of the expression itself is not of interest. Rather the class of transformations
+  that have been applied to the expression is what is can make it safe or unsafe in a query.
   """
   @spec type(Column.t, Query.t) :: Type.t
   def type(column, query), do: construct_type_hierarchy(column, query)
