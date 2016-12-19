328ee139ac04fdb82fc95fcb752a2698300a948e
Cristian Berneanu <cristian@aircloak.com>
2016-12-19 17:55:47 +0200

    Raise error on invalid column for easier debugging

--- cloak/lib/cloak/aql/column.ex
+++ cloak/lib/cloak/aql/column.ex
@@ -63,6 +63,8 @@ defmodule Cloak.Aql.Column do
   @doc "Returns the column value of a database row."
   @spec value(t, DataSource.row) :: DataSource.field
   def value(%__MODULE__{constant?: true, value: value}, _row), do: value
+  def value(%__MODULE__{db_row_position: nil} = column, _row), do:
+    raise "Invalid column specified: #{inspect(column, pretty: true)}"
   for position <- 0..99 do
     # Generates pattern matching clauses to improve sequential access to a value:
     #
