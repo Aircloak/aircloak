de6dbcf541b5123cf080dbd131e630ba6afc45bb
Sasa Juric <sasa.juric@gmail.com>
2016-12-22 16:04:50 +0100

    move emulation join transformation outside of the compiler

--- cloak/lib/cloak/query/db_emulator.ex
+++ cloak/lib/cloak/query/db_emulator.ex
@@ -8,7 +8,8 @@ defmodule Cloak.Query.DbEmulator do
   """
   require Logger
 
-  alias Cloak.{Aql.Query, DataSource, Query.DbEmulator.Selector}
+  alias Cloak.{DataSource, Query.DbEmulator.Selector}
+  alias Cloak.Aql.{Query, Expression, Function}
 
 
   # -------------------------------------------------------------------
@@ -17,19 +18,23 @@ defmodule Cloak.Query.DbEmulator do
 
   @doc "Retrieves rows according to the specification in the emulated query."
   @spec select(Query.t) :: Enumerable.t
-  def select(%Query{emulated?: true} = query), do:
+  def select(%Query{emulated?: true} = query) do
+    query = compile_emulated_joins(query)
     query.from
     |> select_rows()
     |> Selector.pick_db_columns(query)
+  end
 
 
   # -------------------------------------------------------------------
-  # Internal functions
+  # Selection of rows from subparts of an emulated query
   # -------------------------------------------------------------------
 
   defp select_rows({:subquery, %{ast: %Query{emulated?: true, from: from} = subquery}}) when not is_binary(from) do
     Logger.debug("Emulating sub-query ...")
-    rows = select_rows(from)
+    subquery = compile_emulated_joins(subquery)
+    rows = select_rows(subquery.from)
+
     Logger.debug("Processing rows ...")
     rows
     |> Selector.pick_db_columns(subquery)
@@ -54,4 +59,86 @@ defmodule Cloak.Query.DbEmulator do
     rhs_rows = Task.await(rhs_task, :infinity)
     Selector.join(lhs_rows, rhs_rows, join)
   end
+
+
+  # -------------------------------------------------------------------
+  # Transformation of joins for the purposes of emulated query selector
+  # -------------------------------------------------------------------
+
+  defp compile_emulated_joins(%Query{emulated?: true, from: {:join, _}} = query) do
+    from =
+      query.from
+      |> replace_joined_tables_with_subqueries(query.db_columns, query)
+      |> compile_join_conditions_columns()
+    %Query{query | from: from}
+  end
+  defp compile_emulated_joins(query), do: query
+
+  # The DBEmulator modules doesn't know how to select a table directly, so we need
+  # to replace any direct references to joined table with the equivalent subquery.
+  defp replace_joined_tables_with_subqueries(table_name, columns, parent_query) when is_binary(table_name) do
+    query =
+      Cloak.Aql.Compiler.make_select_query(
+        parent_query.data_source,
+        table_name,
+        (for %Expression{table: %{name: ^table_name}} = column <- columns, do: column),
+        subquery?: true
+      )
+    {:subquery, %{type: :parsed, ast: query, alias: table_name}}
+  end
+  defp replace_joined_tables_with_subqueries({:subquery, subquery}, _columns, _parent_query), do: {:subquery, subquery}
+  defp replace_joined_tables_with_subqueries({:join, join}, db_columns, parent_query) do
+    on_columns = get_in(join.conditions, [Query.Lenses.leaf_expressions()])
+    columns = Enum.uniq_by(db_columns ++ on_columns, &Expression.id/1)
+    lhs = replace_joined_tables_with_subqueries(join.lhs, columns, parent_query)
+    rhs = replace_joined_tables_with_subqueries(join.rhs, columns, parent_query)
+    {:join, %{join | lhs: lhs, rhs: rhs}}
+  end
+
+  # For emulated joins, we need to set the `row_index` field for the columns in the `ON` clause.
+  defp compile_join_conditions_columns({:join, join}) do
+    # Because this is an emulated query we only have joining of subqueries, as
+    # tables references were previously translated into subqueries.
+    columns = joined_columns({:join, join})
+    mapper_fun = &set_column_row_index(&1, columns)
+    conditions = Lens.map(Query.Lenses.conditions_terminals(), join.conditions, mapper_fun)
+    lhs = compile_join_conditions_columns(join.lhs)
+    rhs = compile_join_conditions_columns(join.rhs)
+    join = Map.put(join, :columns, columns)
+    {:join, %{join | conditions: conditions, lhs: lhs, rhs: rhs}}
+  end
+  defp compile_join_conditions_columns({:subquery, subquery}), do: {:subquery, subquery}
+
+  defp joined_columns({:join, join}) do
+    joined_columns(join.lhs) ++ joined_columns(join.rhs)
+  end
+  defp joined_columns({:subquery, subquery}) do
+    user_id_name = case Enum.find_index(subquery.ast.columns, &(&1.user_id?)) do
+      nil -> nil
+      index -> Enum.at(subquery.ast.column_titles, index)
+    end
+    columns =
+        Enum.zip(subquery.ast.column_titles, subquery.ast.columns)
+        |> Enum.map(fn ({alias, column}) -> {alias, Function.type(column)} end)
+    table = %{
+      name: subquery.alias,
+      columns: columns,
+      user_id: user_id_name,
+      decoders: [],
+      projection: nil
+    }
+    Enum.zip(subquery.ast.column_titles, subquery.ast.columns)
+    |> Enum.map(fn ({alias, column}) ->
+      %Expression{table: table, name: alias, type: Function.type(column), user_id?: user_id_name == alias}
+    end)
+  end
+
+  defp set_column_row_index(%Expression{} = column, columns) do
+    case Enum.find_index(columns, &Expression.id(&1) == Expression.id(column)) do
+      # It's not actually a needed column, so ignore for the purpose of positioning
+      nil -> column
+      position -> %Expression{column | row_index: position}
+    end
+  end
+  defp set_column_row_index(other, _columns), do: other
 end
