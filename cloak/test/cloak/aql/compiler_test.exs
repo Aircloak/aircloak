ebb498de3720157470d8412b15fcaf31ab2742d1
Sebastian Probst Eide <sebastian@aircloak.com>
2017-01-12 12:17:05 +0100

    Move test out of tests for math validation

--- cloak/test/cloak/aql/compiler_test.exs
+++ cloak/test/cloak/aql/compiler_test.exs
@@ -879,21 +879,6 @@ defmodule Cloak.Aql.Compiler.Test do
     refute select_columns_have_valid_transformations(query)
   end
 
-  test "compilation of row splitters" do
-    {:ok, query} = compile("select extract_matches(string, 'thing') from table", data_source())
-    assert [%Expression{name: "extract_matches_return_value", row_index: index}] = query.columns
-    assert [%Expression{name: "extract_matches_return_value", row_index: ^index}] = query.property
-    assert Enum.any?(query.db_columns, &match?(%Expression{name: "string"}, &1))
-    assert [%{
-      function_spec: %Expression{
-        function?: true,
-        function: "extract_matches",
-        function_args: [%Expression{name: "string"}, %Expression{value: ~r/thing/ui}]
-      },
-      row_index: ^index
-    }] = query.row_splitters
-  end
-
   describe "casts are considered dangerously discontinuous when a constant is involved" do
     Enum.each(~w(integer real boolean), fn(cast_target) ->
       test "cast from integer to #{cast_target}" do
@@ -1095,6 +1080,22 @@ defmodule Cloak.Aql.Compiler.Test do
       assert get_compilation_error(query) =~ ~r/math function '\/'/
     end
   end
+
+  test "compilation of row splitters" do
+    {:ok, query} = compile("select extract_matches(string, 'thing') from table", data_source())
+    assert [%Expression{name: "extract_matches_return_value", row_index: index}] = query.columns
+    assert [%Expression{name: "extract_matches_return_value", row_index: ^index}] = query.property
+    assert Enum.any?(query.db_columns, &match?(%Expression{name: "string"}, &1))
+    assert [%{
+      function_spec: %Expression{
+        function?: true,
+        function: "extract_matches",
+        function_args: [%Expression{name: "string"}, %Expression{value: ~r/thing/ui}]
+      },
+      row_index: ^index
+    }] = query.row_splitters
+  end
+
 
   defp scrub_aliases(query), do: put_in(query, [aliases()], nil)
 
