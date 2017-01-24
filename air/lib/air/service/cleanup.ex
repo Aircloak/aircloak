5fe7ee51b238abd8ae83c2ffe2a47833794c0a44
Paweł Obrok <pawel.obrok@gmail.com>
2017-01-24 14:39:10 +0100

    Cleanup old queries every hour

--- air/lib/air/service/cleanup.ex
+++ air/lib/air/service/cleanup.ex
@@ -1,7 +1,7 @@
 defmodule Air.Service.Cleanup do
   import Ecto.Query
 
-  def cleanup_old_queries(settings, now \\ NaiveDateTime.utc_now())
+  def cleanup_old_queries(settings \\ Air.Service.Settings.read(), now \\ NaiveDateTime.utc_now())
   def cleanup_old_queries(%Air.Settings{query_retention_days: :unlimited}, _now), do: :ok
   def cleanup_old_queries(%Air.Settings{query_retention_days: retention}, now) do
     keep_since = Timex.shift(now, days: -retention)
