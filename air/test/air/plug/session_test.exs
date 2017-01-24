fcb1514b6b064b0c0960ed9896e845c7790619ff
Paweł Obrok <pawel.obrok@gmail.com>
2017-01-24 16:58:23 +0100

    Allow passing the auth token via a query param

--- air/test/air/plug/session_test.exs
+++ air/test/air/plug/session_test.exs
@@ -11,11 +11,21 @@ defmodule Air.Plug.Session.Test do
       assert conn.status == 401
     end
 
-    test "submitting a token for a given user" do
+    test "submitting a token through a header" do
       user = TestRepoHelper.create_user!()
       token = Token.create_api_token(user, "test")
 
       conn = build_conn() |> Plug.Conn.put_req_header("auth-token", token) |> ApiAuth.call(%{})
+
+      refute conn.halted
+      assert conn.assigns.current_user.id == user.id
+    end
+
+    test "submitting a token through a param" do
+      user = TestRepoHelper.create_user!()
+      token = Token.create_api_token(user, "test")
+
+      conn = build_conn() |> put_in([Access.key!(:query_string)], "auth_token=#{token}") |> ApiAuth.call(%{})
 
       refute conn.halted
       assert conn.assigns.current_user.id == user.id
