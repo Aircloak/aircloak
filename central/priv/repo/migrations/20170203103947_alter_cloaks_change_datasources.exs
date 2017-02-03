116761967ee856fbfff865f29f835102c1ed5957
Sasa Juric <sasa.juric@gmail.com>
2017-02-03 16:06:16 +0100

    make migration reversible
    needed for tests

--- central/priv/repo/migrations/20170203103947_alter_cloaks_change_datasources.exs
+++ central/priv/repo/migrations/20170203103947_alter_cloaks_change_datasources.exs
@@ -1,10 +1,17 @@
 defmodule Central.Repo.Migrations.AlterCloaksChangeDatasources do
   use Ecto.Migration
 
-  def change do
+  def up() do
     alter table(:cloaks) do
       remove :data_sources
       add :data_source_names, {:array, :string}, null: false, default: []
+    end
+  end
+
+  def down() do
+    alter table(:cloaks) do
+      add :data_sources, :integer, null: false, default: 0
+      remove :data_source_names
     end
   end
 end
