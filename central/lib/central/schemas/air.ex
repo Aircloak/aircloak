ab42998acc3ceb8e597a604b7e80df1839eecc13
Sebastian Probst Eide <sebastian@aircloak.com>
2017-02-07 10:05:37 +0100

    Add version to air and cloak

--- central/lib/central/schemas/air.ex
+++ central/lib/central/schemas/air.ex
@@ -5,13 +5,15 @@ defmodule Central.Schemas.Air do
 
   @type t :: %__MODULE__{
     name: String.t,
+    version: String.t,
     status: Central.Schemas.OnlineStatus.t,
     customer: Central.Schemas.Customer.t,
-    cloaks: [Central.Schemas.Cloak.t]
+    cloaks: [Central.Schemas.Cloak.t],
   }
 
   schema "airs" do
     field :name, :string
+    field :version, :string
     field :status, Central.Schemas.OnlineStatus
 
     belongs_to :customer, Central.Schemas.Customer
