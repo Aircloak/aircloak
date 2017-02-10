02386590aa9288ac2d2e1264a08f3c408ded5cd4
Sasa Juric <sasa.juric@gmail.com>
2017-02-10 11:38:16 +0100

    whitelist GenServer crash logs

--- cloak/lib/logger_translator.ex
+++ cloak/lib/logger_translator.ex
@@ -18,8 +18,33 @@ defmodule Cloak.LoggerTranslator do
   ## ----------------------------------------------------------------
 
   @doc false
-  def translate(_min_level, :error, :format, _message), do:
-    :skip
+  def translate(min_level, :error, :format, message) do
+    with {:ok, filtered_message} <- filter_error_message(message), do:
+      forward_translate(min_level, :error, :format, filtered_message)
+  end
   def translate(min_level, level, kind, message), do:
+    forward_translate(min_level, level, kind, message)
+
+
+  ## ----------------------------------------------------------------
+  ## Internal functions
+  ## ----------------------------------------------------------------
+
+  defp forward_translate(min_level, level, kind, message), do:
     Logger.Translator.translate(min_level, level, kind, message)
+
+  defp filter_error_message({'** Generic server ' ++ _ = msg, [name, _last, _state, reason]}), do:
+    {:ok, {msg, [name, "filtered", "filtered", filter_reason(reason)]}}
+  defp filter_error_message(_), do: :skip
+
+  defp filter_reason({_exception, stacktrace}), do:
+    {"filtered",
+      stacktrace
+      |> Enum.map(fn
+        {_mod, _fun, arity, _location} = entry when is_integer(arity) -> entry
+        {mod, fun, args, location} when is_list(args) -> {mod, fun, length(args), location}
+        _other -> nil
+      end)
+      |> Enum.filter(&(&1 != nil))
+    }
 end
--- cloak/test/cloak/logger_translator_test.exs
+++ cloak/test/cloak/logger_translator_test.exs
@@ -1,16 +1,33 @@
 defmodule Cloak.LoggerTranslatorTest do
   use ExUnit.Case, async: false
 
-  test "crashing GenServer" do
-    {:ok, pid} = Agent.start(fn -> nil end)
-    assert capture_process_failure(pid, &Agent.cast(&1, fn(_) -> raise "bar" end)) == ""
+  describe "GenServer" do
+    setup do
+      {:ok, pid} = Agent.start(fn -> "sensitive data" end)
+      [pid: pid]
+    end
+
+    test "stacktrace is logged on crash", %{pid: pid}, do:
+      assert crash_agent(pid, fn(_) -> raise "foo" end) =~ ~r/#{Path.basename(__ENV__.file)}:#{__ENV__.line}/
+
+    test "sensitive data is removed from the exception message", %{pid: pid}, do:
+      refute crash_agent(pid, fn(_) -> Enum.count("sensitive data") end) =~ ~r/sensitive data/
+
+    test "sensitive data is removed from the stacktrace", %{pid: pid}, do:
+      refute crash_agent(pid, fn(_) -> List.last("sensitive data") end) =~ ~r/sensitive data/
+
+    defp crash_agent(pid, fun), do:
+      capture_process_failure(pid, &Agent.cast(&1, fun))
   end
 
-  defp capture_process_failure(pid, fun) do
+  defp capture_process_failure(pid, fun, timeout \\ :timer.seconds(1)) do
     ExUnit.CaptureLog.capture_log(fn ->
       mref = Process.monitor(pid)
       fun.(pid)
-      receive do {:DOWN, ^mref, _, _, _} -> :ok end
+      receive do
+        {:DOWN, ^mref, _, _, _} -> :ok
+        after timeout -> raise "Process didn't exit"
+      end
     end)
   end
 end
