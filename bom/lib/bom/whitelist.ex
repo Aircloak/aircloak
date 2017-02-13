f530f7c08bc0629d4f367cc534d74bd5cc37c555
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-13 13:09:55 +0100

    Whitelist color-convert and react-chartjs-2 licenses

--- bom/lib/bom/whitelist.ex
+++ bom/lib/bom/whitelist.ex
@@ -95,6 +95,8 @@ defmodule BOM.Whitelist do
     "8de5f23be471b6814f19b2ad82a5208a" => :mit,           # node/path-is-inside
     "ffcf739dca268cb0f20336d6c1a038f1" => :apache2,       # node/rc
     "99d097ff2dae4db019dd8ac5144f1efc" => :mit,           # node/commander
+    "330031db3ec2b47f6e9d7923b8e1f95b" => :mit,           # node/color-convert
+    "6d2716539b6e3fee1dff17903670f1cd" => :mit,           # node/react-chartjs-2
     "7c26dfe36e38a743a435b92f7e1260af" => :apache2,       # elixir/earmark
     "0b36f89594d6a8a4b5e8efa73f1f4fc5" => :mit,           # elixir/fs
     "0689a7b07fec79946ae192573e1450e8" => :bsd_3_clause,  # elixir/getopt
