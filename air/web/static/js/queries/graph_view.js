d7c7a543d83d71121673efbfdf52ccf75f565e07
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-15 14:50:58 +0100

    Display bars in different colors on the graph

--- air/web/static/js/queries/graph_view.js
+++ air/web/static/js/queries/graph_view.js
@@ -2,11 +2,32 @@
 
 import React from "react";
 import {Bar} from "react-chartjs-2";
+import _ from "lodash";
+
+const fillColors = [
+  "rgba(170, 100, 100, 0.1)",
+  "rgba(148, 193, 26, 0.1)",
+  "rgba(30, 185, 214, 0.1)",
+  "rgba(0, 170, 150, 0.1)",
+];
+
+const borderColors = [
+  "rgba(170,100,100, 0.4)",
+  "rgba(148,193,26, 0.4)",
+  "rgba(30,185,214, 0.4)",
+  "rgba(0,170,150, 0.4)",
+];
 
 const data = (graphData) => {
   return {
     labels: graphData.x(),
-    datasets: graphData.series(),
+    datasets: graphData.series().map((series, i) =>
+      _.merge(series, {
+        backgroundColor: fillColors[i % fillColors.length],
+        borderColor: borderColors[i % borderColors.length],
+        borderWidth: 2,
+      })
+    ),
   };
 };
 
