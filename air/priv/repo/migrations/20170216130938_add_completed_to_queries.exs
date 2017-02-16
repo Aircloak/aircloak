ccd3d07be2e8ea5c6a5f8a80603c28c264bd3c96
Sebastian Probst Eide <sebastian@aircloak.com>
2017-02-16 20:13:29 +0100

    Change migration to up/down

--- air/priv/repo/migrations/20170216130938_add_completed_to_queries.exs
+++ air/priv/repo/migrations/20170216130938_add_completed_to_queries.exs
@@ -1,12 +1,18 @@
 defmodule Air.Repo.Migrations.AddCompletedToQueries do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:queries) do
       add :completed, :boolean, default: false
     end
 
     # All past queries should be considered completed
     execute "UPDATE queries SET completed = true;"
+  end
+
+  def down do
+    alter table(:queries) do
+      remove :completed
+    end
   end
 end
