09212aa31df27fd46c8aad55eb85e36b4d319075
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-17 14:35:51 +0100

    Remove borders from graph bars

--- air/web/static/js/queries/graph_view.js
+++ air/web/static/js/queries/graph_view.js
@@ -8,28 +8,16 @@ import {GraphConfig} from "./graph_data";
 import type {GraphDataT} from "./graph_data";
 
 const fillColors = [
-  "rgba(170, 100, 100, 0.1)",
-  "rgba(148, 193, 26, 0.1)",
-  "rgba(30, 185, 214, 0.1)",
-  "rgba(0, 170, 150, 0.1)",
-];
-
-const borderColors = [
-  "rgba(170,100,100, 0.4)",
-  "rgba(148,193,26, 0.4)",
-  "rgba(30,185,214, 0.4)",
-  "rgba(0,170,150, 0.4)",
+  "rgba(170, 100, 100, 0.4)",
+  "rgba(148, 193, 26, 0.4)",
+  "rgba(30, 185, 214, 0.4)",
+  "rgba(0, 170, 150, 0.4)",
 ];
 
 const data = (graphData) => ({
   labels: graphData.x(),
   datasets: graphData.series().map((series, i) =>
-    _.merge(series, {
-      backgroundColor: fillColors[i % fillColors.length],
-      borderColor: borderColors[i % borderColors.length],
-      borderWidth: 2,
-    })
-  ),
+    _.merge(series, {backgroundColor: fillColors[i % fillColors.length]})),
 });
 
 const options = (graphConfig) => ({
