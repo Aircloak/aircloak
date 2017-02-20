4da6fb1aad8506ffcd096819f6ef5c5ffb3d338b
Sebastian Probst Eide <sebastian@aircloak.com>
2017-02-20 13:21:51 +0100

    Extract query rendering to a function

--- air/web/static/js/activity_monitor/queries.js
+++ air/web/static/js/activity_monitor/queries.js
@@ -6,16 +6,13 @@ import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import {QueryView} from "./query";
 import type {Query} from "./query";
 
-const hasQueries = (queries: Query[]) => queries.length > 0;
-
-export const QueriesView = (props: {queries: Query[]}) => {
-  let queries = "";
-  if (hasQueries(props.queries)) {
-    queries = props.queries.map((query) =>
+const renderQueries = (queries: Query[]) => {
+  if (queries.length > 0) {
+    return queries.map((query) =>
       <QueryView key={query.id} {...query} />
     );
   } else {
-    queries = (
+    return (
       <tr>
         <td colSpan="3">
           Currently there are no queries running.
@@ -23,27 +20,26 @@ export const QueriesView = (props: {queries: Query[]}) => {
       </tr>
     );
   }
-
-  return (
-    <div>
-      <h3>Queries</h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Data source</th>
-            <th>Analyst</th>
-            <th>Query state</th>
-          </tr>
-        </thead>
-        <ReactCSSTransitionGroup
-          component="tbody"
-          transitionName="activity-monitor-queries"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
-        >
-          {queries}
-        </ReactCSSTransitionGroup>
-      </table>
-    </div>
-  );
 };
+
+export const QueriesView = (props: {queries: Query[]}) =>
+  <div>
+    <h3>Queries</h3>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Data source</th>
+          <th>Analyst</th>
+          <th>Query state</th>
+        </tr>
+      </thead>
+      <ReactCSSTransitionGroup
+        component="tbody"
+        transitionName="activity-monitor-queries"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        {renderQueries(props.queries)}
+      </ReactCSSTransitionGroup>
+    </table>
+  </div>;
