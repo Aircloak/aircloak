0a934d7c35b2a166f704bf360d56b1245397a784
Sasa Juric <sasa.juric@gmail.com>
2017-02-23 10:35:15 +0100

    add export service

--- integration_tests/test/central_test.exs
+++ integration_tests/test/central_test.exs
@@ -34,6 +34,22 @@ defmodule IntegrationTest.CentralTest do
     )
   end
 
+  test "manual export to central" do
+    Application.put_env(:air, :auto_aircloak_export, false)
+    export = poll_for_export()
+    assert {:ok, number} = Central.CustomerMessage.import_customer_data(export.payload)
+    assert number > 0
+  end
+
+  defp poll_for_export() do
+    case Air.Service.Central.export_pending_calls() do
+      {:ok, export} -> export
+      {:error, :nothing_to_export} ->
+        :timer.sleep(10)
+        poll_for_export()
+    end
+  end
+
   defp air(), do:
     Enum.find(
       Central.Service.Customer.airs(),
