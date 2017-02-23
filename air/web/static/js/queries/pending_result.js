8fe377a98312d4766342c7a29dbcf6e744d2f273
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-23 17:47:58 +0100

    Display query states as a list

--- air/web/static/js/queries/pending_result.js
+++ air/web/static/js/queries/pending_result.js
@@ -3,7 +3,15 @@
 import React from "react";
 
 import {CodeViewer} from "../code_viewer";
-import {format} from "./state";
+import {pendingStates, later, format} from "./state";
+
+const stateItem = (state, currentState) => {
+  if (later(currentState, state)) {
+    return <s>{format(state)}</s>;
+  } else {
+    return format(state);
+  }
+};
 
 export const PendingResult = ({statement, query_state}: {query_state: string, statement: string}) =>
   <div className="panel panel-info">
@@ -11,8 +19,11 @@ export const PendingResult = ({statement, query_state}: {query_state: string, st
     <div className="panel-body">
       <CodeViewer statement={statement} />
 
-      <p className="spinner">
-        <img src="/images/loader.gif" role="presentation" /> {format(query_state)}
-      </p>
+      <p className="text-center spinner"> <img src="/images/loader.gif" role="presentation" /> </p>
+      <ul>
+        {pendingStates.map((state, i) =>
+          <li key={i}>{stateItem(state, query_state)}</li>
+        )}
+      </ul>
     </div>
   </div>;
