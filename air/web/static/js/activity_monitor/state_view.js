0756bf058206ff690d6ac6bb53dfb9e3ff9e8b6f
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-23 11:09:57 +0100

    Improve display of the awaiting_data state

--- air/web/static/js/activity_monitor/state_view.js
+++ air/web/static/js/activity_monitor/state_view.js
@@ -10,5 +10,7 @@ const stateClasses = {
 
 const stateClass = (state) => stateClasses[state] || "label label-info";
 
+const format = (state) => state.replace("_", " ");
+
 export const StateView = (props: {state: string}) =>
-  <span className={stateClass(props.state)}>{props.state}</span>;
+  <span className={stateClass(props.state)}>{format(props.state)}</span>;
