42b820ca58859b7bd8e4c4ac1c0b6fbc35504fc3
Sasa Juric <sasa.juric@gmail.com>
2017-02-24 11:56:55 +0100

    rename the Message module

--- central/lib/central/service/customer/air_message.ex
+++ central/lib/central/service/customer/air_message.ex
@@ -1,4 +1,4 @@
-defmodule Central.Service.Customer.Message do
+defmodule Central.Service.Customer.AirMessage do
   @moduledoc "Decoding and handling of messages sent by air."
   require Logger
   alias Central.Service.Customer
