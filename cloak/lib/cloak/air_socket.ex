67b87e03363de55fd31a92b227e420a505023fa3
Sebastian Probst Eide <sebastian@aircloak.com>
2017-02-27 07:46:26 +0100

    Support sending messages on multiple topics

--- cloak/lib/cloak/air_socket.ex
+++ cloak/lib/cloak/air_socket.ex
@@ -46,7 +46,7 @@ defmodule Cloak.AirSocket do
   @spec send_query_result(GenServer.server, map) :: :ok | {:error, any}
   def send_query_result(socket \\ __MODULE__, result) do
     Logger.info("sending query result to Air", query_id: result.query_id)
-    call(socket, "query_result", result)
+    call(socket, "main", "query_result", result)
   end
 
   @doc """
@@ -57,7 +57,7 @@ defmodule Cloak.AirSocket do
   """
   @spec send_query_state(GenServer.server, String.t, atom) :: :ok | {:error, any}
   def send_query_state(socket \\ __MODULE__, query_id, query_state), do:
-    call(socket, "query_state", %{query_id: query_id, query_state: query_state})
+    call(socket, "main", "query_state", %{query_id: query_id, query_state: query_state})
 
 
   # -------------------------------------------------------------------
@@ -79,6 +79,7 @@ defmodule Cloak.AirSocket do
   def handle_connected(_transport, state) do
     Logger.info("connected")
     send(self(), {:join, "main"})
+    # send(self(), {:join, "memory_reporter"})
     initial_interval = config(:min_reconnect_interval)
     {:ok, %{state | reconnect_interval: initial_interval}}
   end
@@ -155,10 +156,10 @@ defmodule Cloak.AirSocket do
     end
     {:ok, state}
   end
-  def handle_info({{__MODULE__, :call}, timeout, from, event, payload}, transport, state) do
+  def handle_info({{__MODULE__, :call}, topic, timeout, from, event, payload}, transport, state) do
     request_id = make_ref() |> :erlang.term_to_binary() |> Base.encode64()
     try do
-      GenSocketClient.push(transport, "main", "cloak_call", %{request_id: request_id, event: event, payload: payload})
+      GenSocketClient.push(transport, topic, "cloak_call", %{request_id: request_id, event: event, payload: payload})
       timeout_ref = Process.send_after(self(), {:call_timeout, request_id}, timeout)
       {:ok, put_in(state.pending_calls[request_id], %{from: from, timeout_ref: timeout_ref})}
     rescue
@@ -286,17 +287,17 @@ defmodule Cloak.AirSocket do
     send(client_pid, {mref, response})
   end
 
-  @spec call(GenServer.server, String.t, map) :: :ok | {:error, any}
-  defp call(socket, event, payload) do
-    case do_call(socket, event, payload, @timeout) do
+  @spec call(GenServer.server, String.t, String.t, map) :: :ok | {:error, any}
+  defp call(socket, topic, event, payload) do
+    case do_call(socket, topic, event, payload, @timeout) do
       {:ok, _} -> :ok
       error -> error
     end
   end
 
-  defp do_call(socket, event, payload, timeout) do
+  defp do_call(socket, topic, event, payload, timeout) do
     mref = Process.monitor(socket)
-    send(socket, {{__MODULE__, :call}, timeout, {self(), mref}, event, payload})
+    send(socket, {{__MODULE__, :call}, topic, timeout, {self(), mref}, event, payload})
     receive do
       {^mref, response} ->
         Process.demonitor(mref, [:flush])
