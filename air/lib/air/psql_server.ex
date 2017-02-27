9a18e3b954239677dc6fccc6418e63bc55b77a20
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-27 11:30:03 +0100

    Revert "Disable access to psql protocol in expired air"
    This reverts commit fe1dee4c8a6c00da9de57af12af5ffa609642e93.

--- air/lib/air/psql_server.ex
+++ air/lib/air/psql_server.ex
@@ -2,7 +2,7 @@ defmodule Air.PsqlServer do
   @moduledoc "Server for PostgreSQL protocol which allows PostgreSQL clients to query cloaks."
 
   alias Air.PsqlServer.RanchServer
-  alias Air.Service.{User, DataSource, Version}
+  alias Air.Service.{User, DataSource}
   require Logger
 
   @behaviour RanchServer
@@ -33,8 +33,7 @@ defmodule Air.PsqlServer do
 
   @doc false
   def login(conn, password) do
-    with false <- Version.expired?(),
-         data_source_id <- {:global_id, conn.login_params["database"]},
+    with data_source_id <- {:global_id, conn.login_params["database"]},
          {:ok, user} <- User.login(conn.login_params["user"], password),
          {:ok, _} <- DataSource.fetch_as_user(data_source_id, user)
     do
