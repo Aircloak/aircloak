5fc55b2ec629123c6d5390657efa7cbe3276be96
Sebastian Probst Eide <sebastian@aircloak.com>
2017-02-27 16:31:10 +0100

    Show memory in GB (we live in the future)

--- air/web/static/js/activity_monitor/cloak.js
+++ air/web/static/js/activity_monitor/cloak.js
@@ -9,17 +9,18 @@ export type Cloak = {
   total_memory: number,
 };
 
-const toMb = (memoryInBytes: number) => {
+const toGB = (memoryInBytes: number) => {
   if (memoryInBytes === null) {
     return "Unknown";
   } else {
-    return `${Math.floor(memoryInBytes / (1024 * 1024))} MB`;
+    // Not exactly accurate, but a good enough approximation
+    return `${Math.floor(memoryInBytes / (1024 * 1024 * 102)) / 10} GB`;
   }
 };
 
 export const CloakView = (props: Cloak) =>
   <tr>
     <td>{props.name}</td>
-    <td>{toMb(props.total_memory)}</td>
-    <td>{toMb(props.free_memory)}</td>
+    <td>{toGB(props.total_memory)}</td>
+    <td>{toGB(props.free_memory)}</td>
   </tr>;
