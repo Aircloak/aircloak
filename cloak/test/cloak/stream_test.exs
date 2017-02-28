0423c0cf52e078f44aefe7d13c95dbb8aa09308c
Paweł Obrok <pawel.obrok@gmail.com>
2017-02-28 15:06:14 +0100

    Add Cloak.Stream.side_effect_after_last

--- cloak/test/cloak/stream_test.exs
+++ cloak/test/cloak/stream_test.exs
@@ -27,4 +27,33 @@ defmodule Cloak.Stream.Test do
       assert Enum.take(stream1, 10) == Enum.take(stream2, 10)
     end
   end
+
+  describe "side_effect_after_last" do
+    test "the side effect doesn't if all elements aren't evaluted" do
+      this = self()
+      Stream.cycle([nil])
+      |> Stream.take(10)
+      |> Cloak.Stream.side_effect_after_last(fn() -> send(this, :message) end)
+      |> Enum.take(9)
+
+      refute_receive :message
+    end
+
+    test "the side effect happens when the whole stream is evaluated" do
+      this = self()
+      Stream.cycle([nil])
+      |> Stream.take(10)
+      |> Cloak.Stream.side_effect_after_last(fn() -> send(this, :message) end)
+      |> Stream.run()
+
+      assert_receive :message
+    end
+
+    test "doesn't change the content of the stream" do
+      stream1 = Stream.cycle([1, 2, 3]) |> Stream.take(10)
+      stream2 = Cloak.Stream.side_effect_after_last(stream1, fn() -> nil end)
+
+      assert Enum.to_list(stream1) == Enum.to_list(stream2)
+    end
+  end
 end
