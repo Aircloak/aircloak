166e3c3cc241f435d4b814396b76c6b800f2de8f
Sebastian Probst Eide <sebastian@aircloak.com>
2017-03-02 12:20:32 +0100

    Reformat pipeline

--- air/web/controllers/admin/cloaks_controller.ex
+++ air/web/controllers/admin/cloaks_controller.ex
@@ -22,15 +22,14 @@ defmodule Air.Admin.CloaksController do
   # -------------------------------------------------------------------
 
   def index(conn, _params) do
-    cloak_groups =
-      Cloak.all_cloak_infos()
-      |> Enum.map(&load_data_sources/1)
-      |> Enum.with_index()
-      |> Enum.group_by(fn({_cloak, index}) -> trunc(index / 2) end)
-      |> Enum.map(fn({_group_index, cloaks}) ->
-          {cloaks, _} = Enum.unzip(cloaks)
-          cloaks
-        end)
+    cloak_groups = Cloak.all_cloak_infos()
+    |> Enum.map(&load_data_sources/1)
+    |> Enum.with_index()
+    |> Enum.group_by(fn({_cloak, index}) -> trunc(index / 2) end)
+    |> Enum.map(fn({_group_index, cloaks}) ->
+      {cloaks, _} = Enum.unzip(cloaks)
+      cloaks
+    end)
     render(conn, "index.html", cloak_groups: cloak_groups, count: length(Cloak.all_cloak_infos()))
   end
 
