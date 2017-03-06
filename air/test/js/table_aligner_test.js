4025b27f77aaca629a4cabc93f3efaaa472dcdc4
Sebastian Probst Eide <sebastian@aircloak.com>
2017-03-06 18:21:57 +0100

    Add class to classify row alignment

---
+++ air/test/js/table_aligner_test.js
@@ -0,0 +1,40 @@
+import assert from "assert";
+
+import {TableAligner} from "queries/table_aligner";
+
+describe("TableAligner", () => {
+  it("returns left alignment class for text columns", () => {
+    const data = new TableAligner(
+      [{row: ["foo"]}],
+    );
+    assert.equal(data.alignmentClass(0), "text-left");
+  });
+
+  it("aligns as text if boolean", () => {
+    const data = new TableAligner(
+      [{row: [true]}],
+    );
+    assert.equal(data.alignmentClass(0), "text-left");
+  });
+
+  it("returns right alignment class for numerical columns", () => {
+    const data = new TableAligner(
+      [{row: [1]}],
+    );
+    assert.equal(data.alignmentClass(0), "text-right");
+  });
+
+  it("detects numbers even if first row is a null value", () => {
+    const data = new TableAligner(
+      [{row: [null]}, {row: [undefined]}, {row: [2]}],
+    );
+    assert.equal(data.alignmentClass(0), "text-right");
+  });
+
+  it("detects numbers even if first row is an anonymized value", () => {
+    const data = new TableAligner(
+      [{row: ["*"]}, {row: [2]}],
+    );
+    assert.equal(data.alignmentClass(0), "text-right");
+  });
+});
---
+++ air/web/static/js/queries/table_aligner.js
@@ -0,0 +1,40 @@
+// @flow
+
+import _ from "lodash";
+import type {Row, Column} from "./result";
+
+export type TableAlignerT = {
+  alignmentClass: (columnIndex: number) => string,
+};
+
+export const TableAligner = (rows: Row[]): TableAlignerT => {
+  // ----------------------------------------------------------------
+  // Internal functions
+  // ----------------------------------------------------------------
+
+  const isNumeric = (n) => typeof(n) === "number" && isFinite(n);
+
+  const ignorableValues = ["*", null, undefined];
+
+  const firstResultCell = (columnIndex) =>
+    _.chain(rows).
+      dropWhile((rowData) => _.includes(ignorableValues, rowData.row[columnIndex])).
+      head().
+      value().
+      row[columnIndex];
+
+
+  // ----------------------------------------------------------------
+  // API
+  // ----------------------------------------------------------------
+
+  const alignmentClass = (columnIndex) => {
+    if (isNumeric(firstResultCell(columnIndex))) {
+      return "text-right";
+    } else {
+      return "text-left";
+    }
+  };
+
+  return {alignmentClass};
+};
