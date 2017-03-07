e159a335b56255c3dd5c91d176f13abe919deeb6
Sasa Juric <sasa.juric@gmail.com>
2017-03-07 14:25:39 +0100

    cleanup js code
    It's wrapped inside an anonymous function to avoid leaking global functions.

--- user_guides/source/static/js/app.js
+++ user_guides/source/static/js/app.js
@@ -1,46 +1,46 @@
-function initClipboard() {
-  var preElements = document.getElementsByTagName('pre');
-  for (var i = 0; i < preElements.length; i++) {
-    preElements[i].appendChild(newCopyButton());
-    // need to wrap the element inside a div so we can add some styling
-    wrapInDiv(preElements[i]);
-  }
+(function() {
+  var initClipboard, newCopyButton, wrapInDiv;
 
-  new Clipboard('.copy-button', {
-    target: function(trigger) {
-      return trigger.previousElementSibling;
+  initClipboard = function() {
+    var preElements = document.getElementsByTagName('pre');
+    for (var i = 0; i < preElements.length; i++) {
+      preElements[i].appendChild(newCopyButton());
+      // need to wrap the element inside a div so we can add some styling
+      wrapInDiv(preElements[i]);
     }
-  }).
-    on('success', function(event) {
-      event.clearSelection();
-      event.trigger.textContent = 'Copied';
-      window.setTimeout(function() {
-        event.trigger.textContent = 'Copy';
-      }, 1000);
-    });
-}
 
-function newCopyButton() {
-  var button = document.createElement('button');
-  button.className = 'copy-button';
-  button.textContent = 'Copy';
-  return button;
-}
-
-function wrapInDiv(el) {
-  var wrapper = document.createElement('div');
-  wrapper.className = "codeBlock";
-
-  if (el.nextSibling) {
-    el.parentNode.insertBefore(wrapper, el.nextSibling);
-  } else {
-    el.parentNode.appendChild(wrapper);
+    new Clipboard('.copy-button', {target: function(trigger) {return trigger.previousElementSibling;}}).
+      on('success', function(event) {
+        event.clearSelection();
+        event.trigger.textContent = 'Copied';
+        window.setTimeout(function() {
+          event.trigger.textContent = 'Copy';
+        }, 1000);
+      });
   }
-  return wrapper.appendChild(el);
-};
 
-document.onreadystatechange = function() {
-  if (document.readyState === 'complete') {
-    initClipboard()
+  newCopyButton = function() {
+    var button = document.createElement('button');
+    button.className = 'copy-button';
+    button.textContent = 'Copy';
+    return button;
   }
-};
+
+  wrapInDiv = function(el) {
+    var wrapper = document.createElement('div');
+    wrapper.className = "codeBlock";
+
+    if (el.nextSibling) {
+      el.parentNode.insertBefore(wrapper, el.nextSibling);
+    } else {
+      el.parentNode.appendChild(wrapper);
+    }
+    return wrapper.appendChild(el);
+  };
+
+  document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+      initClipboard();
+    }
+  };
+})();
