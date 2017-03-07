96981cf092dacd000b9d88e496e3dbb2716453e4
Sebastian Probst Eide <sebastian@aircloak.com>
2017-03-07 17:27:58 +0100

    Add empty line

--- air/web/static/js/queries/table_aligner.js
+++ air/web/static/js/queries/table_aligner.js
@@ -8,6 +8,8 @@ export type TableAlignerT = {
 };
 
 export const TableAligner = (rows: Row[]): TableAlignerT => {
+
+
   // ----------------------------------------------------------------
   // Internal functions
   // ----------------------------------------------------------------
