9204de70f524760d1c56c8dd8a9ffb7580b5ea0a
Paweł Obrok <pawel.obrok@gmail.com>
2017-03-22 17:23:51 +0100

    Count distinct values in distinct preprocessing
    See https://github.com/Aircloak/aircloak/issues/1292

--- cloak/test/cloak/query/anonymization_test.exs
+++ cloak/test/cloak/query/anonymization_test.exs
@@ -172,5 +172,16 @@ defmodule Cloak.Query.AnonymizationTest do
       assert_query "select count(distinct number) from anonymizations",
         %{columns: ["count"], rows: [%{row: [5], occurrences: 1}]}
     end
+
+    test "bug: a user with many non-unique values would be treated as one with many distinct values" do
+      :ok = insert_rows(_user_ids = 40..40, "anonymizations", ["number"], [151])
+      :ok = insert_rows(_user_ids = 40..40, "anonymizations", ["number"], [152])
+      :ok = insert_rows(_user_ids = 40..40, "anonymizations", ["number"], [153])
+      for _ <- 1..10, do:
+        :ok = insert_rows(_user_ids = [60], "anonymizations", ["number"], [151])
+
+      assert_query "select count(distinct number) from anonymizations",
+        %{columns: ["count"], rows: [%{row: [6], occurrences: 1}]}
+    end
   end
 end
