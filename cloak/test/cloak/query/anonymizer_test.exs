02d65329fd81bbbe7cbd1f9e1688c869116f9864
Cristian Berneanu <cristian@aircloak.com>
2017-03-24 16:02:54 +0200

    Fix test and add comments

--- cloak/test/cloak/query/anonymizer_test.exs
+++ cloak/test/cloak/query/anonymizer_test.exs
@@ -9,37 +9,44 @@ defmodule Cloak.Query.AnonimyzerTest do
   end
 
   test "count" do
+    # per-user row format = count of values
     rows = [1, 2, 1, 0, 3, 1, 2, 3, 1, 2, 1]
     assert {12, 0} = Anonymizer.new([]) |> Anonymizer.count(rows)
   end
 
   test "sum" do
+    # per-user row format = sum of values
     rows = [1, 2, -1, 0, 1, 2, -4, -2, 4, 1, -1, 2]
     assert {6, 0} = Anonymizer.new([]) |> Anonymizer.sum(rows)
   end
 
   test "avg" do
+    # per-user row format = {sum of values, count of values}
     rows = [{1, 1}, {2, 1}, {-1, 1}, {0, 1}, {1, 1}, {2, 2}, {-4, 4}, {-2, 1}, {4, 3}, {-1, 2}]
     assert {0.3, 0.0} = Anonymizer.new([]) |> Anonymizer.avg(rows)
   end
 
   test "stddev" do
+    # per-user row format = {sum of values, sum of squared values, count of values}
     rows = [{1, 1, 1}, {2, 4, 1}, {-1, 1, 1}, {0, 0, 1}, {1, 1, 1}, {2, 2, 2}, {-4, 4, 4}, {-2, 4, 1}, {4, 6, 3}]
     assert {0.8988882021697692, 0.0} = Anonymizer.new([]) |> Anonymizer.stddev(rows)
   end
 
   test "min" do
+    # per-user row format = collection of values
     rows = [[1, -2], [3], [4, 2, -3], [-2, 4], [-1], [-3, -2], [3], [4, -2, 1], [5], [-4], [-5, 4], [3], [-6, 3, 2]]
     assert -1 = Anonymizer.new([]) |> Anonymizer.min(rows)
   end
 
   test "max" do
+    # per-user row format = collection of values
     rows = [[1, -2], [3], [4, 2, -3], [-2, 4], [-1], [-3, -2], [3], [4, -2, 1], [5], [-4], [-5, 4], [3], [-6, 3, 2]]
     assert 3 = Anonymizer.new([]) |> Anonymizer.max(rows)
   end
 
   test "median" do
+    # per-user row format = collection of all values
     rows = [[1, 2], [3], [4, 2, -3], [2, 4], [0], [-3, -2], [3], [4, -2, 1], [5], [-4], [-5, 4], [3]]
-    assert 2 = Anonymizer.new([]) |> Anonymizer.max(rows)
+    assert 1 = Anonymizer.new([]) |> Anonymizer.median(rows)
   end
 end
