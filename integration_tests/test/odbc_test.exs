dada0825faa44afe5212955a475f178516feb81e
Sasa Juric <sasa.juric@gmail.com>
2017-03-27 15:57:24 +0200

    explicitly cast in tests

--- integration_tests/test/odbc_test.exs
+++ integration_tests/test/odbc_test.exs
@@ -65,7 +65,7 @@ defmodule IntegrationTest.OdbcTest do
       assert param_select(context.conn, :sql_integer, 42) == '42'
 
     test "parameterized query with a boolean", context, do:
-      assert param_select(context.conn, :sql_bit, true) == true
+      assert param_select(context.conn, :sql_bit, true, "boolean") == true
 
     test "parameterized query with a float", context, do:
       assert param_select(context.conn, {:sql_float, 32}, 3.14, "real") == 3.14
@@ -74,10 +74,10 @@ defmodule IntegrationTest.OdbcTest do
       assert param_select(context.conn, :sql_double, 3.14, "real") == 3.14
 
     test "parameterized query with a decimal", context, do:
-      assert param_select(context.conn, {:sql_decimal, 10, 2}, 3.14) == 3.14
+      assert param_select(context.conn, {:sql_decimal, 10, 2}, 3.14, "real") == 3.14
 
     test "parameterized query with a real", context, do:
-      assert param_select(context.conn, :sql_real, 3.14) == 3.14
+      assert param_select(context.conn, :sql_real, 3.14, "real") == 3.14
 
     test "parameterized query with a varchar", context, do:
       assert param_select(context.conn, {:sql_varchar, 6}, 'foobar') == 'foobar'
