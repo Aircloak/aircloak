e976284141d88d14dff9ca91df86badef17c228a
Paweł Obrok <pawel.obrok@gmail.com>
2017-04-25 15:38:27 +0200

    Add absolute time to failed queries

--- air/web/views/admin/query_view.ex
+++ air/web/views/admin/query_view.ex
@@ -5,4 +5,8 @@ defmodule Air.Admin.QueryView do
   @dialyzer :no_match
 
   import Scrivener.HTML
+
+  def time_ago(entry), do: Air.Utils.DateTime.time_ago(entry.inserted_at)
+
+  def absolute_time(entry), do: Timex.format!(entry.inserted_at, "{ISOdate} {h24}:{m}:{s}")
 end
