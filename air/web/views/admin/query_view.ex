04d317bc80bb4daacb35c9a64eb8b05cae379f6a
Paweł Obrok <pawel.obrok@gmail.com>
2017-04-25 16:04:29 +0200

    Privatize view functions

--- air/web/views/admin/query_view.ex
+++ air/web/views/admin/query_view.ex
@@ -6,7 +6,7 @@ defmodule Air.Admin.QueryView do
 
   import Scrivener.HTML
 
-  def time_ago(entry), do: Air.Utils.DateTime.time_ago(entry.inserted_at)
+  defp time_ago(entry), do: Air.Utils.DateTime.time_ago(entry.inserted_at)
 
-  def absolute_time(entry), do: Timex.format!(entry.inserted_at, "{ISOdate} {h24}:{m}:{s}")
+  defp absolute_time(entry), do: Timex.format!(entry.inserted_at, "{ISOdate} {h24}:{m}:{s}")
 end
