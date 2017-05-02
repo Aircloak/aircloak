c799ccde7fee37ab0519c317546bd47d0d9707dc
Paweł Obrok <pawel.obrok@gmail.com>
2017-05-02 16:42:00 +0200

    Allow columns with no alias in SQLBuilder
    These were allowed previously but support was dropped at some point
    during a semi-related refactoring. It should not have a performance
    impact, since they are only allowed for simple columns. Some tests will
    break if aliases are not provided by the compiler for ordering by
    complex columns.

--- cloak/lib/cloak/data_source/sql_builder.ex
+++ cloak/lib/cloak/data_source/sql_builder.ex
@@ -160,12 +160,17 @@ defmodule Cloak.DataSource.SqlBuilder do
   defp order_by_fragments(%Query{subquery?: true, order_by: [_|_] = order_by} = query, sql_dialect) do
     order_by = for {index, dir} <- order_by do
       dir = if dir == :desc do " DESC" else " ASC" end
-      name = query.db_columns |> Enum.at(index) |> Map.get(:alias) |> quote_name(sql_dialect)
+      name = query.db_columns |> Enum.at(index) |> order_by_column_sql(sql_dialect)
       [name, dir]
     end
     [" ORDER BY ", Enum.intersperse(order_by, ", ")]
   end
   defp order_by_fragments(_query, _sql_dialect), do: []
+
+  defp order_by_column_sql(%Expression{alias: alias}, sql_dialect) when alias != nil, do:
+    quote_name(alias, sql_dialect)
+  defp order_by_column_sql(%Expression{name: name}, sql_dialect) when name != nil, do:
+    quote_name(name, sql_dialect)
 
   defp quote_name(name, :drill), do: "`#{name}`"
   defp quote_name(name, _sql_dialect), do: "\"#{name}\""
