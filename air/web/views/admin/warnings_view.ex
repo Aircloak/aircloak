5b0957cf508fc6de92058b2fa8d85dc39b0b46aa
Sebastian Probst Eide <sebastian@aircloak.com>
2017-05-03 11:02:39 +0200

    List problems on problems page

--- air/web/views/admin/warnings_view.ex
+++ air/web/views/admin/warnings_view.ex
@@ -3,4 +3,16 @@ defmodule Air.Admin.WarningsView do
   use Air.Web, :view
   # bug in the current Phoenix
   @dialyzer :no_match
+  import Phoenix.HTML.Link, only: [link: 2]
+
+  alias Air.Schemas.DataSource
+
+  defp type(%DataSource{}), do: "Data source"
+
+  defp name(%DataSource{} = resource), do: resource.name
+
+  defp resource_link(conn, %DataSource{} = resource), do:
+    link_to(admin_data_source_path(conn, :show, resource.name))
+
+  defp link_to(path), do: link("More", to: path)
 end
