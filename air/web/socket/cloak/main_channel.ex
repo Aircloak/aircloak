e110dc1430dceb3862545db2daf94d8c2a5c0cc9
Cristian Berneanu <cristian@aircloak.com>
2017-05-18 21:31:50 +0300

    Check salts are identical for data sources served by multiple cloaks

--- air/web/socket/cloak/main_channel.ex
+++ air/web/socket/cloak/main_channel.ex
@@ -75,6 +75,7 @@ defmodule Air.Socket.Cloak.MainChannel do
       name: socket.assigns.name,
       version: socket.assigns.version,
       online_since: Timex.now(),
+      salt: cloak_info["salt"],
     }
     data_sources = Map.fetch!(cloak_info, "data_sources")
     Air.Service.Cloak.register(cloak, data_sources)
