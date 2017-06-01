da3a98b06514d01dfb50a035119614ae6e6eda0a
Paweł Obrok <pawel.obrok@gmail.com>
2017-06-01 13:34:13 +0200

    Normalize complex constant expressions

--- cloak/lib/cloak/sql/compiler/normalization.ex
+++ cloak/lib/cloak/sql/compiler/normalization.ex
@@ -1,8 +1,10 @@
 defmodule Cloak.Sql.Compiler.Normalization do
-  alias Cloak.Sql.Query
+  alias Cloak.Sql.{Expression, Query}
 
   def normalize(query), do:
-    query |> expand_not_in()
+    query
+    |> expand_not_in()
+    |> normalize_constants()
 
   defp expand_not_in(query), do:
     update_in(query, [Query.Lenses.filter_clauses()], fn(clauses) ->
@@ -12,4 +14,16 @@ defmodule Cloak.Sql.Compiler.Normalization do
   defp do_expand_not_in({:not, {:in, lhs, exps}}), do:
     Enum.map(exps, &{:comparison, lhs, :<>, &1})
   defp do_expand_not_in(clause), do: [clause]
+
+  defp normalize_constants(query), do:
+    update_in(query, [Query.Lenses.terminals()], &do_normalize_constants/1)
+
+  defp do_normalize_constants(expression = %Expression{function?: true, aggregate?: false}) do
+    if Enum.all?(expression.function_args, &Expression.constant?/1) do
+      Expression.constant(expression.type, Expression.value(expression, []), expression.parameter_index)
+    else
+      expression
+    end
+  end
+  defp do_normalize_constants(other), do: other
 end
