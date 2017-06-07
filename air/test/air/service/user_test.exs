a433c1f18b1bdd67e4e3a7487a622c6f79f6c9e7
Sasa Juric <sasa.juric@gmail.com>
2017-06-07 09:44:55 +0200

    move tests from schema to service

--- air/test/air/service/user_test.exs
+++ air/test/air/service/user_test.exs
@@ -3,47 +3,84 @@ defmodule Air.Service.UserTest do
 
   alias Air.{Service.User, TestRepoHelper}
 
-  test "required fields", do:
-    assert errors_on(%{}) == [
-      email: "can't be blank", name: "can't be blank", password: "can't be blank",
-      password_confirmation: "can't be blank"
-    ]
+  describe "user operations" do
+    test "required fields", do:
+      assert errors_on(&User.create/1, %{}) == [
+        email: "can't be blank", name: "can't be blank", password: "can't be blank",
+        password_confirmation: "can't be blank"
+      ]
 
-  test "validates email address", do:
-    assert error_on(:email, "invalid_email") == "has invalid format"
+    test "validates email address", do:
+      assert error_on(&User.create/1, :email, "invalid_email") == "has invalid format"
 
-  test "validates password and confirmation", do:
-    assert error_on(:password_confirmation, "wrong password") == "does not match confirmation"
+    test "validates password and confirmation", do:
+      assert error_on(&User.create/1, :password_confirmation, "wrong password") == "does not match confirmation"
 
-  test "requires name to be two or more characters", do:
-    assert error_on(:name, "a") == "should be at least 2 character(s)"
+    test "requires name to be two or more characters", do:
+      assert error_on(&User.create/1, :name, "a") == "should be at least 2 character(s)"
 
-  test "requires password to be 4 or more characters", do:
-    assert error_on(:password, "123") == "should be at least 4 character(s)"
+    test "requires password to be 4 or more characters", do:
+      assert error_on(&User.create/1, :password, "123") == "should be at least 4 character(s)"
 
-  test "only update hashed password on password change" do
-    user = TestRepoHelper.create_user!()
+    test "only update hashed password on password change" do
+      user = TestRepoHelper.create_user!()
 
-    {:ok, updated_user} = User.update_profile(user, %{"name" => "foobar"})
-    assert updated_user.hashed_password == user.hashed_password
+      {:ok, updated_user} = User.update_profile(user, %{"name" => "foobar"})
+      assert updated_user.hashed_password == user.hashed_password
 
-    {:ok, updated_user} = User.update_profile(user,
-      %{"old_password" => "1234", "password" => "wxyz", "password_confirmation" => "wxyz"})
-    refute updated_user.hashed_password == user.hashed_password
+      {:ok, updated_user} = User.update_profile(user,
+        %{"old_password" => "1234", "password" => "wxyz", "password_confirmation" => "wxyz"})
+      refute updated_user.hashed_password == user.hashed_password
+    end
+
+    test "the only admin can't be deleted", do:
+      assert User.delete(TestRepoHelper.create_only_user_as_admin!()) == {:error, :forbidden_last_admin_deletion}
+
+    test "the only admin can't be updated to be a normal user", do:
+      assert User.update(TestRepoHelper.create_only_user_as_admin!(), %{groups: []}) ==
+        {:error, :forbidden_last_admin_deletion}
   end
 
-  test "the only admin can't be deleted", do:
-    assert User.delete(TestRepoHelper.create_only_user_as_admin!()) == {:error, :forbidden_last_admin_deletion}
+  describe "group operations" do
+    test "required fields", do:
+      assert errors_on(&User.create_group/1, %{}) == [admin: "can't be blank", name: "can't be blank"]
 
-  test "the only admin can't be updated to be a normal user", do:
-    assert User.update(TestRepoHelper.create_only_user_as_admin!(), %{groups: []}) ==
-      {:error, :forbidden_last_admin_deletion}
+    test "validates uniqueness of name" do
+      User.create_group!(%{name: "group name", admin: false})
+      assert errors_on(&User.create_group/1, %{name: "group name", admin: false})[:name] == "has already been taken"
+    end
 
-  defp error_on(field, value), do:
-    errors_on(%{field => value})[field]
+    test "connecting a group to users" do
+      group = TestRepoHelper.create_group!()
+      user1 = TestRepoHelper.create_user!(%{groups: [group.id]})
+      user2 = TestRepoHelper.create_user!(%{groups: [group.id]})
+      group = User.load_group(group.id)
+      assert [user1.id, user2.id] == Enum.map(group.users, &(&1.id)) |> Enum.sort()
+    end
 
-  defp errors_on(changes) do
-    assert {:error, changeset} = User.create(changes)
+    test "connecting a group to a data source" do
+      data_source = TestRepoHelper.create_data_source!()
+      group = TestRepoHelper.create_group!(%{data_sources: [data_source.id]})
+      group = User.load_group(group.id)
+      assert [loaded_data_source] = group.data_sources
+      assert loaded_data_source.id == data_source.id
+    end
+
+    test "deleting a group doesn't delete users or data sources" do
+      data_source = TestRepoHelper.create_data_source!()
+      group = TestRepoHelper.create_group!(%{data_sources: [data_source.id]})
+      user = TestRepoHelper.create_user!(%{groups: [group.id]})
+      User.delete_group!(group)
+      refute nil == User.load(user.id)
+      refute nil == Air.Repo.get(Air.Schemas.DataSource, data_source.id)
+    end
+  end
+
+  defp error_on(fun, field, value), do:
+    errors_on(fun, %{field => value})[field]
+
+  defp errors_on(fun, changes) do
+    assert {:error, changeset} = fun.(changes)
 
     changeset
     |> Ecto.Changeset.traverse_errors(&Air.ErrorHelpers.translate_error/1)
