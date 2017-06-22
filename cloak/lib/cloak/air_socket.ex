042548fa4c4883233f3a58fb2bcf2639d8947676
Sasa Juric <sasa.juric@gmail.com>
2017-06-22 15:39:33 +0200

    increase timeout for query result call

--- cloak/lib/cloak/air_socket.ex
+++ cloak/lib/cloak/air_socket.ex
@@ -40,7 +40,7 @@ defmodule Cloak.AirSocket do
   @spec send_query_result(GenServer.server, map) :: :ok | {:error, any}
   def send_query_result(socket \\ __MODULE__, result) do
     Logger.info("sending query result to Air", query_id: result.query_id)
-    call_air(socket, "main", "query_result", result)
+    call_air(socket, "main", "query_result", result, :timer.minutes(1))
   end
 
   @doc """
