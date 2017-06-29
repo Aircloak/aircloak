92c036db18a4db840a4a5b1d0fd8a52d28604ae9
Sasa Juric <sasa.juric@gmail.com>
2017-06-29 10:36:05 +0200

    allow multiple query subscribers

--- air/lib/air/service/query/events.ex
+++ air/lib/air/service/query/events.ex
@@ -9,7 +9,7 @@ defmodule Air.Service.Query.Events do
   @doc "Starts the registry for subscribers."
   @spec start_link() :: {:ok, pid}
   def start_link, do:
-    Registry.start_link(:unique, __MODULE__.Registry)
+    Registry.start_link(:duplicate, __MODULE__.Registry)
 
   @doc """
   Registers the caller process as the subscriber for the query result.
@@ -20,24 +20,21 @@ defmodule Air.Service.Query.Events do
   """
   @spec subscribe(String.t) :: :ok
   def subscribe(query_id) do
-    {:ok, _} = Registry.register(__MODULE__.Registry, query_id, nil)
+    {:ok, _} = Registry.register(__MODULE__.Registry, {:subscriber, query_id}, nil)
     :ok
   end
 
   @doc "Unsubscribes the registered query result subscriber."
   @spec unsubscribe(String.t) :: :ok
   def unsubscribe(query_id), do:
-    Registry.unregister(__MODULE__.Registry, query_id)
+    Registry.unregister(__MODULE__.Registry, {:subscriber, query_id})
 
-  @doc "Triggers a :query_result event, indicating a result has been returned from the cloak for the query."
+  @doc "Notifies subscribers that a result has been returned from the cloak. See `subscribe/1` for more details."
   @spec trigger_result(map) :: :ok
   def trigger_result(payload) do
-    # notify dedicated listener for this query first
-    case Registry.lookup(__MODULE__.Registry, payload.query_id) do
-      [] -> :ok
-      [{pid, nil}] ->
-        send(pid, {:query_result, payload})
-        :ok
-    end
+    for {pid, _} <- Registry.lookup(__MODULE__.Registry, {:subscriber, payload.query_id}), do:
+      send(pid, {:query_result, payload})
+
+    :ok
   end
 end
