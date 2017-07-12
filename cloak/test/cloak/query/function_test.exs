c2678c6611c137270958c41362425a86da8283d7
Cristian Berneanu <cristian@aircloak.com>
2017-07-12 14:55:01 +0300

    Add test for quarter

--- cloak/test/cloak/query/function_test.exs
+++ cloak/test/cloak/query/function_test.exs
@@ -330,6 +330,7 @@ defmodule Cloak.Query.FunctionTest do
   test "hour", do: assert 4 == apply_function("hour(datetime)", "datetimes_ft")
   test "minute", do: assert 5 == apply_function("minute(datetime)", "datetimes_ft")
   test "second", do: assert 6 == apply_function("second(datetime)", "datetimes_ft")
+  test "quarter", do: assert 1 == apply_function("quarter(datetime)", "datetimes_ft")
 
   test "extract(year)", do: assert 2015 == apply_function("extract(year from datetime)", "datetimes_ft")
   test "extract(month)", do: assert 2 == apply_function("extract(month from datetime)", "datetimes_ft")
