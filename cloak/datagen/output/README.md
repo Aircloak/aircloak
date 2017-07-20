45dfee5452483bea9dad5f9a18e780c3adedfa2d
Sebastian Probst Eide <sebastian@aircloak.com>
2017-07-20 16:44:12 +0200

    Add generator script for compliance testing
    A dataset that can be used to validate consistent
    cloak behaviour across multiple datasources is now
    included in the local test database.
    
    A script that generates the database is also included.
    It outputs CSV and JSON files that can be imported into
    normal databases as well as MongoDB.

---
+++ cloak/datagen/output/README.md
@@ -0,0 +1,10 @@
+# Compliance data
+
+This folder can contain a compliance dataset generated
+to validate that cloak functionality remains consistent
+across all supported datasources.
+
+If the folder is empty and you want to update it on
+one of our test servers, run `make recreate-db`.
+This will also create the same dataset in your local
+test database.
--- cloak/datagen/recreate_db.sh
+++ cloak/datagen/recreate_db.sh
@@ -3,6 +3,40 @@ set -e
 
 . ./datagen/prepare_db.funcs.sh
 
+function compliance_db_command() {
+  cmd=$1
+  psql -h $DB_HOST -p $DB_PORT -U datasource_compliance datasource_compliance -c "$cmd"
+}
+
+function generate_compliance_db() {
+  conditionally_create_user "datasource_compliance"
+  regenerate_database "datasource_compliance" "datasource_compliance"
+
+  # Create the required tables
+  compliance_db_command "CREATE TABLE users (id integer, name text, age integer, active boolean, height real);"
+  compliance_db_command "CREATE TABLE users_encoded (id integer, name text, age text, active text, height text);"
+  compliance_db_command "CREATE TABLE notes (id integer, user_id integer, title text, content text);"
+  compliance_db_command "CREATE TABLE notes_encoded (id integer, user_id integer, title text, content text);"
+  compliance_db_command "CREATE TABLE drafts_changes (id integer, note_id integer, \"changes.date\" timestamp, \"changes.change\" text);"
+  compliance_db_command "CREATE TABLE drafts_changes_encoded (id integer, note_id integer, \"changes.date\" text, \"changes.change\" text);"
+  compliance_db_command "CREATE TABLE addresses (id integer, user_id integer, \"home.city\" text, \"home.postal_code\" integer, \"work.city\" text, \"work.postal_code\" integer);"
+  compliance_db_command "CREATE TABLE addresses_encoded (id integer, user_id integer, \"home.city\" text, \"home.postal_code\" text, \"work.city\" text, \"work.postal_code\" text);"
+
+  # Generate the dataset
+  mkdir -p datagen/output
+  ruby datagen/compliance_data_generator.rb
+
+  # Copy the data into the database
+  compliance_db_command "\copy users FROM '$PWD/datagen/output/users.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy users_encoded FROM '$PWD/datagen/output/users_encoded.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy notes FROM '$PWD/datagen/output/notes.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy notes_encoded FROM '$PWD/datagen/output/notes_encoded.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy drafts_changes FROM '$PWD/datagen/output/drafts_changes.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy drafts_changes_encoded FROM '$PWD/datagen/output/drafts_changes_encoded.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy addresses FROM '$PWD/datagen/output/addresses.csv' delimiter ',' csv header;"
+  compliance_db_command "\copy addresses_encoded FROM '$PWD/datagen/output/addresses_encoded.csv' delimiter ',' csv header;"
+}
+
 function main() {
   conditionally_create_user "cloak"
   conditionally_create_user "cloaktest1"
@@ -17,6 +51,8 @@ function main() {
   # import dev schema and data
   psql -h $DB_HOST -p $DB_PORT -U postgres cloak < dev_data.sql
   mix gen_dev_data
+
+  generate_compliance_db
 }
 
 export DB_HOST=${DB_HOST:-127.0.0.1}
