865cab4d5e15020a772c823151fdfc0ce261acf9
Cristian Berneanu <cristian@aircloak.com>
2017-07-25 12:48:57 +0300

    Differenciate supported functions by number of arguments

--- cloak/lib/cloak/data_source/sql_builder/support.ex
+++ cloak/lib/cloak/data_source/sql_builder/support.ex
@@ -1,7 +1,8 @@
 defmodule Cloak.DataSource.SqlBuilder.Support do
   @moduledoc "Module for detecting when a query is supported by the SQL builder module."
 
-  alias Cloak.Sql.Query
+  alias Cloak.Sql.{Query, Expression}
+
 
   # -------------------------------------------------------------------
   # API
@@ -9,7 +10,7 @@ defmodule Cloak.DataSource.SqlBuilder.Support do
 
   def supported_query?(query) do
     sql_dialect = query.data_source.driver_dialect
-    used_functions = Query.Lenses.query_functions() |> Lens.to_list(query) |> Enum.map(& &1.function)
+    used_functions = Query.Lenses.query_functions() |> Lens.to_list(query) |> Enum.map(&function_signature/1)
     not Enum.any?(used_functions, & &1 in unsupported_functions(sql_dialect))
   end
 
@@ -17,10 +18,14 @@ defmodule Cloak.DataSource.SqlBuilder.Support do
   # Internal functions
   # -------------------------------------------------------------------
 
-  @mysql_unsupported_functions ["date_trunc", "btrim", "ltrim", "rtrim"]
-  @sqlserver_unsupported_functions ["date_trunc", "btrim"]
+  @mysql_unsupported_functions ["date_trunc/2", "btrim/2", "ltrim/2", "rtrim/2"]
+  @sqlserver_unsupported_functions ["date_trunc/2", "btrim/1", "btrim/2"]
 
   defp unsupported_functions(:mysql), do: @mysql_unsupported_functions
   defp unsupported_functions(:sqlserver), do: @sqlserver_unsupported_functions
   defp unsupported_functions(_sql_dialect), do: []
+
+  defp function_signature(%Expression{function: name, function_args: args}) when is_binary(name), do:
+    "#{name}/#{length(args)}"
+  defp function_signature(_expression), do: nil # cast, bucket, etc.
 end
