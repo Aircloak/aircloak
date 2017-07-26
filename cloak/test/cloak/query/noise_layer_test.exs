83e6c0de7f98b0f1d628daddf511431e2bfdda4e
Sebastian Probst Eide <sebastian@aircloak.com>
2017-07-26 16:04:25 +0200

    Adjust noise layer tests

--- cloak/test/cloak/query/noise_layer_test.exs
+++ cloak/test/cloak/query/noise_layer_test.exs
@@ -30,7 +30,7 @@ defmodule Cloak.Query.NoiseLayerTest do
   end
 
   test "count(*) uses a different noise layer than count(column)" do
-    :ok = insert_rows(_user_ids = 1..100, "noise_layers", ["number"], [11])
+    :ok = insert_rows(_user_ids = 1..100, "noise_layers", ["number"], [15])
     :ok = insert_rows(_user_ids = 1..10, "noise_layers", ["number"], [3])
 
     assert_query "select count(*), count(number) from noise_layers where number <> 0",
