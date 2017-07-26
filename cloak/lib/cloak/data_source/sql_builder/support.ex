782d7fc7efef509564d7c7c4bb4e7ca75a06b4e5
Cristian Berneanu <cristian@aircloak.com>
2017-07-26 13:05:01 +0300

    Use whitelisting for SQL function support
    Closes #1634

--- cloak/lib/cloak/data_source/sql_builder/support.ex
+++ cloak/lib/cloak/data_source/sql_builder/support.ex
@@ -10,22 +10,52 @@ defmodule Cloak.DataSource.SqlBuilder.Support do
 
   def supported_query?(query) do
     sql_dialect = query.data_source.driver_dialect
-    used_functions = Query.Lenses.query_functions() |> Lens.to_list(query) |> Enum.map(&function_signature/1)
-    not Enum.any?(used_functions, & &1 in unsupported_functions(sql_dialect))
+    Query.Lenses.query_functions()
+    |> Lens.to_list(query)
+    |> Enum.map(&function_signature/1)
+    |> Enum.all?(&supported_function?(&1, sql_dialect))
   end
 
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
 
-  @mysql_unsupported_functions ["date_trunc/2", "btrim/2", "ltrim/2", "rtrim/2"]
-  @sqlserver_unsupported_functions ["date_trunc/2", "btrim/1", "btrim/2"]
+  @postgresql_supported_functions ~w(
+    count sum min max avg stddev
+    year quarter month day hour minute second weekday date_trunc
+    sqrt floor ceiling ceil abs round trunc div mod pow ^ * / + - %
+    length lower lcase upper ucase btrim ltrim rtrim left right substring substring_for || concat
+    hex cast coalesce
+  )
 
-  defp unsupported_functions(:mysql), do: @mysql_unsupported_functions
-  defp unsupported_functions(:sqlserver), do: @sqlserver_unsupported_functions
-  defp unsupported_functions(_sql_dialect), do: []
+  @mysql_supported_functions ~w(
+    count sum min max avg stddev
+    year quarter month day hour minute second weekday
+    sqrt floor ceiling ceil abs round trunc div mod pow ^ * / + - %
+    length lower lcase upper ucase btrim/1 ltrim/1 rtrim/1 left right substring substring_for || concat
+    hex cast coalesce
+  )
+
+  @sqlserver_supported_functions ~w(
+    count sum min max avg stddev
+    year quarter month day hour minute second weekday
+    sqrt floor ceiling ceil abs round trunc div mod pow ^ * / + - %
+    length lower lcase upper ucase ltrim rtrim left right substring substring_for || concat
+    hex cast coalesce
+  )
+
+  defp supported_functions(:postgresql), do: @postgresql_supported_functions
+  defp supported_functions(:mysql), do: @mysql_supported_functions
+  defp supported_functions(:sqlserver), do: @sqlserver_supported_functions
+  defp supported_functions(_sql_dialect), do: []
 
   defp function_signature(%Expression{function: name, function_args: args}) when is_binary(name), do:
-    "#{name}/#{length(args)}"
-  defp function_signature(_expression), do: nil # cast, bucket, etc.
+    {name, length(args)}
+  defp function_signature(%Expression{function: {:cast, _target}, function_args: [_]}), do: {"cast", 1}
+  defp function_signature(_expression), do: {nil, 0}
+
+  defp supported_function?({name, args}, sql_dialect) do
+    supported_functions = supported_functions(sql_dialect)
+    name in supported_functions or "#{name}/#{args}" in supported_functions
+  end
 end
