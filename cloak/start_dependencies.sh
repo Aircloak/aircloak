f44da54bdfcd36fe1351301e887ed7663d9ffe34
Paweł Obrok <pawel.obrok@gmail.com>
2017-08-10 15:06:31 +0200

    Create the performance DB when starting influx

--- cloak/start_dependencies.sh
+++ cloak/start_dependencies.sh
@@ -9,7 +9,7 @@ if ! named_container_running aircloak_influxdb ; then
   printf "Starting InfluxDB "
   docker run -d --name aircloak_influxdb -p 8086:8086 influxdb:1.3.2 > /dev/null
 
-  until $(curl --output /dev/null --silent --head --fail 'http://localhost:8086/query?q=show+databases'); do
+  until $(curl -X POST --output /dev/null --silent --head --fail 'http://localhost:8086/query?q=create+database+performance'); do
     printf '.'
     sleep 1
   done
