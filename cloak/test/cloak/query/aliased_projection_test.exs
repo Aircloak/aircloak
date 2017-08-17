b6c54ba135be6987eb1df77649a41d9f129a6328
Cristian Berneanu <cristian@aircloak.com>
2017-08-17 16:32:31 +0300

    Fix optimization of projected table with aliased user_id

--- cloak/test/cloak/query/aliased_projection_test.exs
+++ cloak/test/cloak/query/aliased_projection_test.exs
@@ -16,37 +16,54 @@ defmodule Cloak.Query.AliasedProjectionTest do
         user_id_alias: "uid",
       },
     )
+    :ok = Cloak.Test.DB.create_table(
+      "#{@prefix}projected_emulated", "value TEXT",
+      decoders: [
+        %{method: "base64", columns: ["value"]},
+      ],
+      projection: %{
+        table: "#{@prefix}main",
+        foreign_key: "user_id",
+        primary_key: "user_id",
+        user_id_alias: "uid",
+      },
+    )
     :ok
   end
 
   setup do
-    Cloak.Test.DB.clear_table("#{@prefix}projected")
     Cloak.Test.DB.clear_table("#{@prefix}main")
+    Cloak.Test.DB.clear_table("#{@prefix}projected")
+    Cloak.Test.DB.clear_table("#{@prefix}projected_emulated")
     :ok = insert_rows(_user_ids = 1..100, "#{@prefix}main", [], [])
     :ok = insert_rows(_user_ids = 1..100, "#{@prefix}projected", ["value"], ["a"])
     :ok = insert_rows(_user_ids = 1..100, "#{@prefix}projected", ["value"], ["b"])
+    :ok = insert_rows(_user_ids = 1..100, "#{@prefix}projected_emulated", ["value"], [Base.encode64("a")])
+    :ok = insert_rows(_user_ids = 1..100, "#{@prefix}projected_emulated", ["value"], [Base.encode64("b")])
     :ok
   end
 
-  describe "simple queries" do
-    test "count(*)", do:
-      assert_query "select count(*) from #{@prefix}projected",
-        %{rows: [%{occurrences: 1, row: [200]}]}
+  Enum.each(["projected_emulated", "projected"], fn(table) ->
+    describe "simple queries on #{table}" do
+      test "count(*)", do:
+        assert_query "select count(*) from #{@prefix}#{unquote(table)}",
+          %{rows: [%{occurrences: 1, row: [200]}]}
 
-    test "where", do:
-      assert_query "select count(value) from #{@prefix}projected where value = 'a'",
-        %{rows: [%{occurrences: 1, row: [100]}]}
+      test "where", do:
+        assert_query "select count(value) from #{@prefix}#{unquote(table)} where value = 'a'",
+          %{rows: [%{occurrences: 1, row: [100]}]}
 
-    test "order by", do:
-      assert_query "select length(value) as l from #{@prefix}projected order by l desc",
-        %{rows: [%{occurrences: 200, row: [1]}]}
+      test "order by", do:
+        assert_query "select length(value) as l from #{@prefix}#{unquote(table)} order by l desc",
+          %{rows: [%{occurrences: 200, row: [1]}]}
 
-    test "count aliased uid", do:
-      assert_query "select count(distinct uid) from #{@prefix}projected",
-        %{rows: [%{occurrences: 1, row: [100]}]}
+      test "count aliased uid", do:
+        assert_query "select count(distinct uid) from #{@prefix}#{unquote(table)}",
+          %{rows: [%{occurrences: 1, row: [100]}]}
 
-    test "select aliased uid", do:
-      assert_query "select count(*) from (SELECT uid FROM #{@prefix}projected GROUP BY uid) as l",
-        %{rows: [%{occurrences: 1, row: [100]}]}
-  end
+      test "select aliased uid", do:
+        assert_query "select count(*) from (SELECT uid FROM #{@prefix}#{unquote(table)} GROUP BY uid) as l",
+          %{rows: [%{occurrences: 1, row: [100]}]}
+    end
+  end)
 end
