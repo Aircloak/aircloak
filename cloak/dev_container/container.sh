91a87de9b5ad6dd710af8dfb893932dbbfb03a2d
Sasa Juric <sasa.juric@gmail.com>
2017-08-18 17:31:36 +0200

    use separate build folders for dev container
    This avoids the problem of NIF collision. Previously, if e.g. jiffy is built on macos, it won't work properly inside a dev container. With this change, deps and build for dev container are mounted from a different location (dev_container/cache), so there's no collision.

--- cloak/dev_container/container.sh
+++ cloak/dev_container/container.sh
@@ -13,11 +13,26 @@ if [ ! -f cloak/dev_container/cache/.bash_history ]; then
   touch cloak/dev_container/cache/.bash_history
 fi
 
+mounted_from_root="VERSION common/elixir"
+mounted_from_cloak="config datagen include lib perftest priv rel test mix.exs mix.lock Makefile"
+mounted_from_cloak_cache="deps _build .bash_history"
+
+mounts="-v $(pwd)/cloak/dev_container/cache/.bash_history:/root/.bash_history"
+
+for path in $mounted_from_root; do
+  mounts="$mounts -v $(pwd)/$path:/aircloak/$path"
+done
+
+for path in $mounted_from_cloak; do
+  mounts="$mounts -v $(pwd)/cloak/$path:/aircloak/cloak/$path"
+done
+
+for path in $mounted_from_cloak_cache; do
+  mounts="$mounts -v $(pwd)/cloak/dev_container/cache/$path:/aircloak/cloak/$path"
+done
+
 CONTAINER_NAME="cloak_dev"
 DOCKER_IMAGE="aircloak/cloak_dev"
-DOCKER_START_ARGS="
-  -v $(pwd):/aircloak \
-  -v $(pwd)/cloak/dev_container/cache/.bash_history:/root/.bash_history
-"
+DOCKER_START_ARGS="$mounts"
 
 container_ctl $@
