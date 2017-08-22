54cfd31ad8f155522324830eab16810a3e422443
Sasa Juric <sasa.juric@gmail.com>
2017-08-22 15:51:07 +0200

    point localhost in dev container to macos host

--- cloak/dev_container/docker/start.sh
+++ cloak/dev_container/docker/start.sh
@@ -2,8 +2,12 @@
 
 set -eo pipefail
 
+# We're redirecting localhost to point to the macOS docker host. This allows us to use the same
+# dev.json and config.json and still be able to connect to the data sources.
 host_ip=$(getent hosts docker.for.mac.localhost | awk '{ print $1 }')
-echo "$host_ip insights.air-local" >> /etc/hosts
+etc_hosts_contents=$(cat /etc/hosts | sed "s/127\.0\.0\.1\\s*localhost/$host_ip localhost/")
+echo "$etc_hosts_contents" > /etc/hosts
+
 cp /aircloak/cloak/priv/odbc/docker/odbc.ini /etc/
 
 cd /aircloak/cloak
