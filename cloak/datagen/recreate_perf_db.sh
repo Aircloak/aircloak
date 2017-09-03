399cda00fc34344dc7e7350899739c1477c2682a
Sebastian Probst Eide <sebastian@aircloak.com>
2017-09-03 18:22:38 +0200

    Update perf test to use new data generators

---
+++ cloak/datagen/recreate_perf_db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+cd $(dirname $0)
+
+. ./prepare_db.funcs.sh
+
+function main() {
+  conditionally_create_user "performance"
+
+  regenerate_database "performance" "performance"
+
+  cd ../
+	mix gen.test_data perf 10000
+	mix gen.data_source_config perf
+}
+
+export DB_HOST=${DB_HOST:-127.0.0.1}
+export DB_PORT=${DB_PORT:-5432}
+(main)
