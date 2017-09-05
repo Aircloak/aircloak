e17e33bebe46064ef5bcc3dde0d3d21cb964ed7e
Paweł Obrok <pawel.obrok@gmail.com>
2017-09-05 11:26:55 +0200

    Make migration reversible

--- air/priv/repo/migrations/20170904155230_remove_global_id_from_data_sources.exs
+++ air/priv/repo/migrations/20170904155230_remove_global_id_from_data_sources.exs
@@ -1,9 +1,16 @@
 defmodule Air.Repo.Migrations.RemoveGlobalIdFromDataSources do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:data_sources) do
       remove :global_id
     end
+  end
+
+  def down do
+    alter table(:data_sources) do
+      add :global_id, :string
+    end
+    create unique_index(:data_sources, [:global_id])
   end
 end
