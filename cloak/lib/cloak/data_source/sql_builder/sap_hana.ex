396d3c9730914cf8909368aa1dc134f40c55bca4
Sasa Juric <sasa.juric@gmail.com>
2017-09-06 16:08:31 +0200

    ensure higher precision of / and avg in sap hana

--- cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
+++ cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
@@ -24,10 +24,14 @@ defmodule Cloak.DataSource.SqlBuilder.SAPHana do
   def function_sql("quarter", [arg]), do: ["cast(substring(quarter(", arg, "), 7, 1) as integer)"]
   def function_sql("%", args), do: function_sql("mod", args)
   def function_sql("^", args), do: function_sql("power", args)
-  for binary_operator <- ~w(+ - * /) do
+  for binary_operator <- ~w(+ - *) do
     def function_sql(unquote(binary_operator), [arg1, arg2]), do: ["(", arg1, unquote(binary_operator), arg2, ")"]
   end
+  def function_sql("/", [arg1, arg2]), do: ["(to_decimal(", arg1, ") / ", "to_decimal(", arg2, "))"]
   def function_sql("btrim", args), do: function_sql("trim", args)
+  def function_sql("avg", [["DISTINCT " <> _ | _] = arg]), do:
+    ["avg(distinct to_decimal(", arg |> to_string() |> String.replace(~r/DISTINCT /, ""), "))"]
+  def function_sql("avg", [arg]), do: ["avg(to_decimal(", arg, "))"]
   def function_sql(name, args), do: [String.upcase(name), "(", Enum.intersperse(args, ", ") ,")"]
 
   @doc false
