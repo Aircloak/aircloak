9886dabef46c3443200476d73aab8a10d620c04f
Cristian Berneanu <cristian@aircloak.com>
2017-09-18 12:06:44 +0300

    Fix case consistency of type names for SAP HANA

--- cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
+++ cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
@@ -49,7 +49,7 @@ defmodule Cloak.DataSource.SqlBuilder.SAPHana do
   @doc false
   def sql_type(:text), do: "nclob"
   def sql_type(:datetime), do: "timestamp"
-  def sql_type(type) when is_atom(type), do: String.upcase(Atom.to_string(type))
+  def sql_type(type) when is_atom(type), do: Atom.to_string(type)
 
   @doc false
   def unicode_literal(value), do: ["N'", value, ?']
