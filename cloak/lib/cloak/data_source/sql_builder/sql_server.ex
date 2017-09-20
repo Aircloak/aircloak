0bb5e79265ddb27aedc216972dfe70cf26b4509d
Paweł Obrok <pawel.obrok@gmail.com>
2017-09-20 16:16:16 +0200

    Use varchar for casts in SQL Server
    It seems like casting to nvarchar forces the string to be encoded with
    utf16-little. We assume that all arriving strings are encoded using the
    same encoding, defined in the config, and do our own decoding, so this
    causes problems.

--- cloak/lib/cloak/data_source/sql_builder/sql_server.ex
+++ cloak/lib/cloak/data_source/sql_builder/sql_server.ex
@@ -60,8 +60,8 @@ defmodule Cloak.DataSource.SqlBuilder.SQLServer do
   @doc false
   def sql_type(:real), do: "float"
   def sql_type(:boolean), do: "bool"
-  # Due to limitations in the ODBC driver, we can't use nvarchar(max).
-  def sql_type(:text), do: "nvarchar(4000)"
+  # Due to limitations in the ODBC driver, we can't use varchar(max).
+  def sql_type(:text), do: "varchar(4000)"
   def sql_type(type) when is_atom(type), do: Atom.to_string(type)
 
   @doc false
