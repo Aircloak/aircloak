b884e5ce3caaabeb454f07db6fc389b4244e80f9
Paweł Obrok <pawel.obrok@gmail.com>
2017-09-27 15:17:00 +0200

    Handle interval math in SAP HANA

--- cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
+++ cloak/lib/cloak/data_source/sql_builder/sap_hana.ex
@@ -59,4 +59,11 @@ defmodule Cloak.DataSource.SqlBuilder.SAPHana do
     ["CAST(", function_sql("round", [value]), " AS integer)"]
   def cast_sql(value, type), do:
     ["CAST(", value, " AS ", sql_type(type), ")"]
+
+  @doc false
+  def time_arithmetic_expression("+", [date, interval]), do: ["ADD_SECONDS(", date, ", ", interval, ")"]
+  def time_arithmetic_expression("-", [date, interval]), do: ["ADD_SECONDS(", date, ", -(", interval, "))"]
+
+  @doc false
+  def date_subtraction_expression([arg1, arg2]), do: ["SECONDS_BETWEEN(", arg2, ", ", arg1, ")"]
 end
