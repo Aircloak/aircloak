570c6a351b908021238c92a942ba871cabb3657e
Paweł Obrok <pawel.obrok@gmail.com>
2017-09-28 13:49:11 +0200

    Extract SQLServer compliance queries to a module

--- cloak/lib/compliance/data_source/sql_server.ex
+++ cloak/lib/compliance/data_source/sql_server.ex
@@ -7,33 +7,25 @@ defmodule Compliance.DataSource.SQLServer do
   # -------------------------------------------------------------------
 
   use Compliance.DataSource.Connector
+  alias Compliance.DataSource.SQLServer.Queries
 
   @doc false
   def setup(%{parameters: params}) do
     Application.ensure_all_started(:odbc)
     conn = Cloak.DataSource.SQLServer.connect!(params)
-    {:updated, _} = :odbc.param_query(conn, 'SET IMPLICIT_TRANSACTIONS off', [])
+    Enum.each(Queries.setup(), &execute!(conn, &1))
     conn
   end
 
   @doc false
   def create_table(table_name, columns, conn) do
-    execute!(conn, "DROP TABLE IF EXISTS #{table_name}")
-    execute!(conn, "CREATE TABLE #{table_name} (#{columns_sql(columns)})")
+    Enum.each(Queries.create_table(table_name, columns), &execute!(conn, &1))
     conn
   end
 
   @doc false
   def insert_rows(table_name, data, conn) do
-    column_names = column_names(data)
-    rows = rows(data, column_names)
-    escaped_column_names = escaped_column_names(column_names)
-    value_placeholders = List.duplicate("?", length(column_names)) |> Enum.join(", ")
-
-    query = "INSERT INTO #{table_name} (#{Enum.join(escaped_column_names, ", ")}) values (#{value_placeholders})"
-
-    Enum.each(rows, & execute!(conn, query, &1))
-
+    Enum.each(Queries.insert_rows(table_name, data), &execute!(conn, &1))
     conn
   end
 
@@ -53,47 +45,4 @@ defmodule Compliance.DataSource.SQLServer do
       {:error, error} -> raise to_string(error)
     end
   end
-
-  defp column_names(data), do:
-    data
-    |> hd()
-    |> Map.keys()
-    |> Enum.sort()
-
-  defp rows(data, column_names), do:
-    Enum.map(data, fn(entry) ->
-      column_names
-      |> Enum.map(& Map.get(entry, &1))
-      |> Enum.map(& cast_types/1)
-    end)
-
-  defp cast_types(binary) when is_binary(binary), do: {{:sql_varchar, length_null_terminated(binary)}, [binary]}
-  defp cast_types(integer) when is_integer(integer), do: {:sql_integer, [integer]}
-  defp cast_types(float) when is_float(float), do: {:sql_real, [float]}
-  defp cast_types(boolean) when is_boolean(boolean), do: {:sql_bit, [boolean]}
-  defp cast_types(%{calendar: Calendar.ISO} = datetime), do: datetime |> to_string() |> cast_types()
-
-  defp length_null_terminated(binary), do: String.length(binary) + 1
-
-  defp escaped_column_names(column_names), do:
-    column_names
-    |> Enum.map(& Atom.to_string/1)
-    |> Enum.map(& escape_name/1)
-
-  defp columns_sql(columns), do:
-    columns
-    |> Enum.map(& column_sql/1)
-    |> Enum.join(", ")
-
-  defp column_sql({name, type}), do:
-    "#{escape_name(name)} #{sql_type(type)}"
-
-  defp escape_name(name), do:
-    ~s("#{name}")
-
-  defp sql_type(:integer), do: "integer"
-  defp sql_type(:real), do: "real"
-  defp sql_type(:boolean), do: "bit"
-  defp sql_type(:text), do: "text"
-  defp sql_type(:datetime), do: "datetime2"
 end
