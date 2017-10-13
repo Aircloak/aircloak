ec3f00996b79dcb8eaf446c2584896d565e7cc3f
Paweł Obrok <pawel.obrok@gmail.com>
2017-10-13 16:15:05 +0200

    Update noise-dependent test

--- cloak/test/cloak/query/noise_layer_test.exs
+++ cloak/test/cloak/query/noise_layer_test.exs
@@ -108,7 +108,7 @@ defmodule Cloak.Query.NoiseLayerTest do
   end
 
   test "noise layers in hiding the low-count row" do
-    other = 24
+    other = 26
 
     for i <- 1..5, do:
       :ok = insert_rows(_user_ids = [i], "noise_layers", ["number", "other"], [i, other])
