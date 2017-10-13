24688ff373ff3cfc9111f30f663c1686e470bf27
Sebastian Probst Eide <sebastian@aircloak.com>
2017-10-13 15:41:13 +0200

    Explain the particular Poison usage

--- common/elixir/lib/json.ex
+++ common/elixir/lib/json.ex
@@ -10,8 +10,12 @@ defmodule Aircloak.Json do
   @spec safe_decode(String.t) :: {:ok, Map.t} | {:error, String.t}
   def safe_decode(raw_json) do
     try do
+      # Note the usage of decode! (which throws) rather than decode.
+      # The latter doesn't return a usable error message.
       {:ok, Poison.decode!(raw_json)}
     rescue
+      # Different version of Poison throw various exceptions, so we
+      # are throwing a wide net, catching everything and everything.
       e -> {:error, e.message}
     end
   end
