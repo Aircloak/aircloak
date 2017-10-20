5d5feea87ca495a8254bdd854f7f6792190251f4
Sasa Juric <sasa.juric@gmail.com>
2017-10-20 11:19:59 +0200

    update manifests

--- air/config/prod.exs
+++ air/config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :air, :deploy_config_file, "config.json"
 
 config :air, Air.Endpoint,
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
