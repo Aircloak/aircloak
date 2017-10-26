35459ac1c2888933d2963002809c90cb37851216
Sasa Juric <sasa.juric@gmail.com>
2017-10-26 10:32:33 +0200

    move elixir source files

--- central/config/dev.exs
+++ central/config/dev.exs
@@ -20,8 +20,8 @@ config :central, Central.Endpoint,
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
-      ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{lib/central_web/views/.*(ex)$},
+      ~r{lib/central_web/templates/.*(eex)$}
     ]
   ]
 
