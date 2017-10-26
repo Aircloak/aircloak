9f91dec1a31c3175ef0363e2faa82658787aea54
Sasa Juric <sasa.juric@gmail.com>
2017-10-26 14:58:21 +0200

    move elixir source files

--- air/lib/air_web.ex
+++ air/lib/air_web.ex
@@ -69,7 +69,7 @@ defmodule Air.Web do
 
   def view do
     quote do
-      use Phoenix.View, root: "web/templates"
+      use Phoenix.View, root: "lib/air_web/templates"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
