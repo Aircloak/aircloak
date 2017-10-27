41f44bc0668345875ff4b036f9fc2dc274ccc52d
Sebastian Probst Eide <sebastian@aircloak.com>
2017-10-27 18:27:56 +0200

    Force linking instead of removing existing folders

--- cloak/docker/start.sh
+++ cloak/docker/start.sh
@@ -12,9 +12,5 @@ echo "127.0.0.1 insights.air-local" >> /etc/hosts
 
 log "Booting container."
 mkdir -p /aircloak/cloak/lib/cloak-$VERSION/priv/
-
-# By replacing the config folder in cloak app with a symlink to the mounted folder,
-# we can support live reloading of data source definition files.
-rm -rf /aircloak/cloak/lib/cloak-$VERSION/priv/config
-ln -s /runtime_config /aircloak/cloak/lib/cloak-$VERSION/priv/config
+ln -sFf /runtime_config /aircloak/cloak/lib/cloak-$VERSION/priv/config
 exec gosu deployer /aircloak/cloak/bin/cloak foreground
