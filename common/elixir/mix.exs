d8b3d00180612f7cdd4b2ecd713f3d6e23de86dc
Sasa Juric <sasa.juric@gmail.com>
2017-11-13 15:19:33 +0100

    use Elixir/Erlang version specific folders for deps
    This commit ensures that we're using `deps/otp_version/elixir_version` instead of plain deps. This helps avoiding compilation crashes in cases where we're caching the deps folder (for example in Travis).

--- common/elixir/mix.exs
+++ common/elixir/mix.exs
@@ -12,6 +12,7 @@ defmodule Aircloak.ElixirCommon.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
+      deps_path: Path.join(["deps", otp_version(), elixir_version()]),
       aliases: [
         "test.standard": ["test", "eunit", "proper --level simple"],
         "lint": ["credo --strict --ignore #{Enum.join(ignored_credo_checks(Mix.env), ",")}"]
@@ -68,4 +69,13 @@ defmodule Aircloak.ElixirCommon.Mixfile do
     ["ModuleDoc" | ignored_credo_checks(:dev)]
   defp ignored_credo_checks(_), do:
     ["NameRedeclarationBy", "AliasUsage", "PipeChain", "ABCSize", "Nesting"]
+
+  defp otp_version(), do:
+    [:code.root_dir(), "releases", :erlang.system_info(:otp_release), "OTP_VERSION"]
+    |> Path.join()
+    |> File.read!()
+    |> String.strip(?\n)
+
+  defp elixir_version(), do:
+    System.version()
 end
