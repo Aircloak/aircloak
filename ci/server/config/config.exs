cf6ba1d095e072632d7a4e90dc3f43894d64780f
Sasa Juric <sasa.juric@gmail.com>
2017-11-21 12:02:28 +0100

    configure logger

--- ci/server/config/config.exs
+++ ci/server/config/config.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
+config :logger,
+  level: :info,
+  backends: [:console],
+  console: [format: "$time [$level] $metadata$message\n"]
+
 import_config "#{Mix.env}.exs"
 if File.exists?("config/#{Mix.env}.local.exs") do
   import_config "#{Mix.env}.local.exs"
