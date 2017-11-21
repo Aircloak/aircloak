d6a19e1d49a0ce6c6cb3e6525bdcb10631944d2b
Sebastian Probst Eide <sebastian@aircloak.com>
2017-11-21 12:03:48 +0100

    Add some tests on numerical values too

--- cloak/test/compliance/where_clause_filters_test.exs
+++ cloak/test/compliance/where_clause_filters_test.exs
@@ -1,6 +1,108 @@
 defmodule Compliance.WhereClauseFilters.Text do
   use ComplianceCase, async: true, timeout: :timer.minutes(2)
 
+  Enum.each(numerical_columns() |> raw_columns(), fn({column, table, uid}) ->
+    @tag compliance: "#{column} #{table} WHERE-clause equality in subquery"
+    test "input #{column} with a WHERE-clause equality in a sub-query on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT
+          count(*)
+        FROM (
+          SELECT #{unquote(uid)}
+          FROM #{unquote(table)}
+          WHERE #{unquote(column)} = 10
+          GROUP BY 1
+        ) table_alias
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause inequality in subquery"
+    test "on input #{column} with a WHERE-clause inequality in a sub-query on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT
+          count(*)
+        FROM (
+          SELECT #{unquote(uid)}
+          FROM #{unquote(table)}
+          WHERE #{unquote(column)} <> 10
+          GROUP BY 1
+        ) table_alias
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause IN in subquery"
+    test "input #{column} with a WHERE-clause IN in a sub-query on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT
+          count(*)
+        FROM (
+          SELECT #{unquote(uid)}
+          FROM #{unquote(table)}
+          WHERE #{unquote(column)} IN (1, 2, 3)
+          GROUP BY 1
+        ) table_alias
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause NOT IN in subquery"
+    test "input #{column} with a WHERE-clause NOT IN in a sub-query on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT
+          count(*)
+        FROM (
+          SELECT #{unquote(uid)}
+          FROM #{unquote(table)}
+          WHERE #{unquote(column)} NOT IN (1, 2, 3)
+          GROUP BY 1
+        ) table_alias
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause equality"
+    test "input #{column} with a WHERE-clause equality on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT count(*)
+        FROM #{unquote(table)}
+        WHERE #{unquote(column)} = 10
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause inequality"
+    test "on input #{column} with a WHERE-clause inequality on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT count(*)
+        FROM #{unquote(table)}
+        WHERE #{unquote(column)} <> 10
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause IN"
+    test "input #{column} with a WHERE-clause IN on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT count(*)
+        FROM #{unquote(table)}
+        WHERE #{unquote(column)} IN (1, 2, 3)
+      """)
+    end
+
+    @tag compliance: "#{column} #{table} WHERE-clause NOT IN"
+    test "input #{column} with a WHERE-clause NOT IN on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT count(*)
+        FROM #{unquote(table)}
+        WHERE #{unquote(column)} NOT IN (1, 2, 3)
+      """)
+    end
+  end)
+
   Enum.each(text_columns(), fn({column, table, uid}) ->
     @tag compliance: "#{column} #{table} WHERE-clause equality in subquery"
     test "input #{column} with a WHERE-clause equality in a sub-query on #{table}", context do
