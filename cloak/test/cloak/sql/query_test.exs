328abeb7abf779b1d6341f15c6f3021999485523
Cristian Berneanu <cristian@aircloak.com>
2017-11-23 17:05:23 +0200

    Extend direct Query tests

--- cloak/test/cloak/sql/query_test.exs
+++ cloak/test/cloak/sql/query_test.exs
@@ -253,18 +253,14 @@ defmodule Cloak.Sql.QueryTest do
     }
   end
 
-  test "db_columns resolving" do
+  test "db_columns resolving simple column" do
     uid_column = Table.column("uid", :integer)
-    numeric_column = Table.column("numeric", :integer)
     string_column = Table.column("string", :text)
-    table = Table.new("table", "uid", columns: [uid_column, numeric_column, string_column])
+    table = Table.new("table", "uid", columns: [uid_column, string_column])
     query = %Query{
       command: :select,
-      columns: [
-        Expression.column(string_column, table),
-        Expression.function("abs", [Expression.column(numeric_column, table)]),
-      ],
-      column_titles: ["string", "abs"],
+      columns: [Expression.column(string_column, table)],
+      column_titles: ["string"],
       selected_tables: [table],
       from: "table",
     }
@@ -272,11 +268,28 @@ defmodule Cloak.Sql.QueryTest do
     assert [
         %Expression{name: "uid", row_index: 0},
         %Expression{name: "string", row_index: 1},
-        %Expression{name: "numeric", row_index: 2},
       ] = Query.resolve_db_columns(query).db_columns
   end
 
-  test "where clause splitting" do
+  test "db_columns resolving function call" do
+    uid_column = Table.column("uid", :integer)
+    numeric_column = Table.column("numeric", :integer)
+    table = Table.new("table", "uid", columns: [uid_column, numeric_column])
+    query = %Query{
+      command: :select,
+      columns: [Expression.function("abs", [Expression.column(numeric_column, table)])],
+      column_titles: ["abs"],
+      selected_tables: [table],
+      from: "table",
+    }
+
+    assert [
+        %Expression{name: "uid", row_index: 0},
+        %Expression{name: "numeric", row_index: 1},
+      ] = Query.resolve_db_columns(query).db_columns
+  end
+
+  test "offloaded where clauses extraction" do
     uid_column = Table.column("uid", :integer)
     numeric_column = Table.column("numeric", :integer)
     table = Table.new("table", "uid", columns: [uid_column, numeric_column])
@@ -285,5 +298,17 @@ defmodule Cloak.Sql.QueryTest do
 
     assert ^condition = Query.offloaded_where(query)
     assert nil == Query.emulated_where(query)
+  end
+
+  test "emulated where clauses extraction" do
+    uid_column = Table.column("uid", :integer)
+    numeric_column = Table.column("numeric", :integer)
+    decoders = [%{method: "text_to_integer", columns: ["numeric"]}]
+    table = Table.new("table", "uid", columns: [uid_column, numeric_column], decoders: decoders)
+    condition = {:comparison, Expression.column(numeric_column, table), :=, Expression.constant(:integer, 3)}
+    query = %Query{command: :select, where: condition, selected_tables: [table], from: "table"}
+
+    assert ^condition = Query.emulated_where(query)
+    assert nil == Query.offloaded_where(query)
   end
 end
