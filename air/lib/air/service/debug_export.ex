c2eab31b6bfdcd26bc788fb31e5aadff04a454b2
Sebastian Probst Eide <sebastian@aircloak.com>
2017-12-04 16:04:37 +0100

    Adds service for exporting debug information

---
+++ air/lib/air/service/debug_export.ex
@@ -0,0 +1,27 @@
+defmodule Air.Service.DebugExport do
+  @moduledoc "Service for creating an export of queries with artefacts for debugging purposes"
+
+  alias Air.Service.{Query, View}
+  alias Air.Schema.User
+
+
+  # -------------------------------------------------------------------
+  # API functions
+  # -------------------------------------------------------------------
+
+  @doc "Creates a zip-file with data related to an executed query useful for debugging purposes"
+  @spec export(User.t, Query.id) :: {:ok, {charlist, binary}} | {:error, any}
+  def export(user, query_id) do
+    {:ok, query} = Query.get_as_user(user, query_id)
+    query = Air.Repo.preload(query, [:data_source])
+
+    files = [
+      {'query.sql', query.statement},
+      {'result.json', Query.buckets(query, :all) |> Poison.encode!(pretty: true)},
+      {'views.json', View.all(user, query.data_source) |> Poison.encode!(pretty: true)},
+      {'tables.json', query.data_source.tables},
+    ]
+
+    :zip.create('query-dump-#{query_id}.zip', files, [:memory])
+  end
+end
