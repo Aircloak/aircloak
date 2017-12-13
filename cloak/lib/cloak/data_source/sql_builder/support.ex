dcc4bd7a160e1c4aa929b9580aa2c99304e5a4ee
Cristian Berneanu <cristian@aircloak.com>
2017-12-13 13:22:21 +0200

    Fix type issues

--- cloak/lib/cloak/data_source/sql_builder/support.ex
+++ cloak/lib/cloak/data_source/sql_builder/support.ex
@@ -9,13 +9,15 @@ defmodule Cloak.DataSource.SqlBuilder.Support do
   # -------------------------------------------------------------------
 
   @doc "Generates SQL for a function invocation. Provided arguments list must contain SQL fragments."
-  @spec function_sql(Expression.function_name, [iodata], atom) :: iodata
+  @spec function_sql(Expression.function_name | {:cast, atom, atom}, [iodata], atom) :: iodata
   def function_sql({:cast, from_type, to_type}, [arg], sql_dialect_module), do:
     sql_dialect_module.cast_sql(arg, from_type, to_type)
   for name <- ~w(round floor ceil ceiling trunc) do
     def function_sql(unquote(name), [arg], sql_dialect_module) do
-      result = sql_dialect_module.function_sql(synonym(unquote(name)), [arg])
-      function_sql({:cast, :real, :integer}, [result], sql_dialect_module)
+      unquote(name)
+      |> synonym()
+      |> sql_dialect_module.function_sql([arg])
+      |> sql_dialect_module.cast_sql(:real, :integer)
     end
   end
   def function_sql(name, args, sql_dialect_module), do:
