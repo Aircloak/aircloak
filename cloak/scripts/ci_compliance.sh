effd98d1f40f548ba0d98441b8d3a22f3314a2e8
Sasa Juric <sasa.juric@gmail.com>
2017-12-15 14:52:59 +0100

    make compliant with CI version 4
    Ports back the changes from the master branch, to make all branch builds compliant to the latest and greatest CI.

---
+++ cloak/scripts/ci_compliance.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# run from the top-level folder of the project
+ROOT_DIR=$(cd $(dirname ${BASH_SOURCE[0]})/../.. && pwd)
+cd $ROOT_DIR
+
+. docker/docker_helper.sh
+
+trap cleanup EXIT TERM INT
+
+function ignore_cleanup {
+  # NOOP which ignores cleanup while we're already cleaning up.
+  :
+}
+
+function cleanup {
+  local exit_status=$?
+
+  # We'll ignore subsequent exit signals to avoid reentrancy.
+  trap ignore_cleanup EXIT TERM INT
+
+  clean_dangling
+
+  # also stop named db containers so they don't waste dev resources
+  for container in postgres9.4 mongo3.0 mongo3.2 mongo3.4 mysql5.7 sqlserver2017; do
+    docker kill $container > /dev/null || true
+    docker rm $container > /dev/null || true
+  done
+
+  exit $exit_status
+}
+
+function docker_script {
+  ci/scripts/cloak.sh $@
+}
+
+function clean_dangling {
+  printf "performing cleanup of dangling images..."
+
+  for container in $(docker ps --format="{{.Names}}" --filter="name=local_ci_"); do
+    docker kill $container > /dev/null || true
+    docker rm $container > /dev/null || true
+  done
+
+  local dangling_volumes=$(docker volume ls -qf dangling=true)
+  if [ "$dangling_volumes" != "" ]; then
+    docker volume rm $dangling_volumes > /dev/null
+  fi
+
+  for network in $(docker network ls --format="{{.Name}}" --filter="name=local_ci_"); do
+    for container_id in $(
+      docker network inspect $network --format '{{range $key, $value := .Containers}} {{println $key}} {{end}}'
+    ); do
+      docker network disconnect $network $container_id > /dev/null
+    done
+    docker network rm $network > /dev/null || true
+  done
+
+  echo " done"
+}
+
+
+clean_dangling
+
+container_id="local_ci_$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z' | head -c 16; echo '')"
+
+docker_script build_image
+
+docker_script start_container $container_id
+DOCKER_EXEC_ARGS="-t" docker_script prepare_for_compliance $container_id
+
+printf "\ngenerating users...\n"
+DOCKER_EXEC_ARGS="-t" docker_script run_in_container $container_id \
+  MIX_ENV=test mix gen.test_data dockerized_ci ${COMPLIANCE_USERS:-10}
+
+printf "\nyou can invoke compliance tests with \`mix test --only compliance\`\n\n"
+DOCKER_EXEC_ARGS="-t" docker_script run_in_container $container_id "/bin/bash"
