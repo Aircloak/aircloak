4ba529e1f46d8b5abeb0a9e03cf4f6d579348630
Paweł Obrok <pawel.obrok@gmail.com>
2017-12-20 16:34:17 +0100

    Refactor AST structure for query generation

--- cloak/lib/compliance/query_generator.ex
+++ cloak/lib/compliance/query_generator.ex
@@ -11,13 +11,13 @@ defmodule Cloak.Compliance.QueryGenerator do
   @spec generate_ast([table]) :: ast
   def generate_ast(tables) do
     {from_table, from_ast} = generate_from(tables)
-    {:query, nil, [keyword(:select), generate_select(from_table), keyword(:from), from_ast, generate_conditions(from_table)]}
+    {:query, nil, [generate_select(from_table), from_ast, generate_conditions(from_table)]}
   end
 
   @doc "Generates the SQL query string fro the given AST."
   @spec ast_to_sql(ast) :: iolist
-  def ast_to_sql({:table, name, []}), do: name
-  def ast_to_sql({:keyword, keyword, []}), do: [" ", keyword |> to_string() |> String.upcase(), " "]
+  def ast_to_sql({:select, nil, select_list}), do: [" SELECT ", Enum.map(select_list, &ast_to_sql/1)]
+  def ast_to_sql({:from, nil, [{:table, name, []}]}), do: [" FROM ", name]
   def ast_to_sql({:function, name, args}), do: [name, "(", Enum.map(args, &ast_to_sql/1), ")"]
   def ast_to_sql({:star, _, _}), do: "*"
   def ast_to_sql({_, _, items}), do: Enum.map(items, &ast_to_sql/1)
@@ -29,12 +29,10 @@ defmodule Cloak.Compliance.QueryGenerator do
 
   defp generate_from(tables) do
     table = tables |> Map.values() |> Enum.random()
-    {table, {:table, table.name, []}}
+    {table, {:from, nil, [{:table, table.name, []}]}}
   end
-
-  defp keyword(name), do: {:keyword, name, []}
 
   defp generate_conditions(_table), do: {:empty, nil, []}
 
-  defp generate_select(_table), do: {:select_list, nil, [{:function, "COUNT", [{:star, nil, []}]}]}
+  defp generate_select(_table), do: {:select, nil, [{:function, "COUNT", [{:star, nil, []}]}]}
 end
