3c9f7948eeadb1fd3284fbcc1cd8359475f361e8
Paweł Obrok <pawel.obrok@gmail.com>
2017-12-20 17:33:30 +0100

    Generate basic WHERE clauses

--- cloak/lib/compliance/query_generator.ex
+++ cloak/lib/compliance/query_generator.ex
@@ -5,6 +5,7 @@ defmodule Cloak.Compliance.QueryGenerator do
 
   alias Cloak.DataSource.Table
 
+
   # -------------------------------------------------------------------
   # API functions
   # -------------------------------------------------------------------
@@ -18,11 +19,20 @@ defmodule Cloak.Compliance.QueryGenerator do
 
   @doc "Generates the SQL query string fro the given AST."
   @spec ast_to_sql(ast) :: iolist
+  def ast_to_sql({:query, _, items}), do: Enum.map(items, &ast_to_sql/1)
   def ast_to_sql({:select, nil, select_list}), do: [" SELECT ", Enum.map(select_list, &ast_to_sql/1)]
   def ast_to_sql({:from, nil, [{:table, name, []}]}), do: [" FROM ", name]
+  def ast_to_sql({:where, nil, conditions}), do: [" WHERE ", Enum.map(conditions, &ast_to_sql/1)]
+  def ast_to_sql({:=, nil, [lhs, rhs]}), do: [ast_to_sql(lhs), " = ", ast_to_sql(rhs)]
   def ast_to_sql({:function, name, args}), do: [name, "(", Enum.map(args, &ast_to_sql/1), ")"]
+  def ast_to_sql({:column, name, []}), do: name
+  def ast_to_sql({:integer, value, []}), do: to_string(value)
+  def ast_to_sql({:text, value, []}), do: [?', value, ?']
+  def ast_to_sql({:boolean, value, []}), do: to_string(value)
+  def ast_to_sql({:datetime, value, []}), do: [?', value, ?']
+  def ast_to_sql({:real, value, []}), do: to_string(value)
   def ast_to_sql({:star, _, _}), do: "*"
-  def ast_to_sql({_, _, items}), do: Enum.map(items, &ast_to_sql/1)
+  def ast_to_sql({:empty, _, _}), do: ""
 
 
   # -------------------------------------------------------------------
@@ -34,7 +44,35 @@ defmodule Cloak.Compliance.QueryGenerator do
     {table, {:from, nil, [{:table, table.name, []}]}}
   end
 
-  defp generate_conditions(_table), do: {:empty, nil, []}
+  defp generate_conditions(table) do
+    [
+      fn -> {:empty, nil, []} end,
+      fn -> generate_where(table) end,
+    ] |> random_option()
+  end
+
+  defp generate_where(table) do
+    column = Enum.random(table.columns)
+    value = generate_value(column.type)
+    {:where, nil, [{:=, nil, [column_expression(column), value]}]}
+  end
+
+  defp generate_value(:boolean), do: {:boolean, [true, false] |> Enum.random(), []}
+  defp generate_value(:integer), do: {:integer, :rand.uniform(1000), []}
+  defp generate_value(:real), do: {:real, random_float(), []}
+  defp generate_value(:text), do: {:text, random_text(), []}
+  defp generate_value(:datetime), do: {:datetime, "1970-01-01", []}
+
+  defp random_float(), do: (1 - 2 * :rand.uniform()) * :math.pow(10, :rand.uniform(3))
+
+  defp random_text() do
+    len = :rand.uniform(10)
+    1..len |> Enum.map(fn(_) -> Enum.random(?A..?z) end)
+  end
+
+  defp column_expression(%{name: name}), do: {:column, name, []}
 
   defp generate_select(_table), do: {:select, nil, [{:function, "COUNT", [{:star, nil, []}]}]}
+
+  defp random_option(options), do: Enum.random(options).()
 end
--- cloak/test/compliance/fuzz_test.exs
+++ cloak/test/compliance/fuzz_test.exs
@@ -5,7 +5,9 @@ defmodule Cloak.Compliance.FuzzTest do
 
   @tag :fuzz
   test "running a generated query", context = %{data_sources: [%{tables: tables} | _other_sources]} do
-    query = tables |> Map.values() |> QueryGenerator.generate_ast() |> QueryGenerator.ast_to_sql() |> to_string()
-    assert_consistent_and_not_failing(context, query)
+    for _ <- 1..200 do
+      query = tables |> Map.values() |> QueryGenerator.generate_ast() |> QueryGenerator.ast_to_sql() |> to_string()
+      assert_consistent_and_not_failing(context, query)
+    end
   end
 end
