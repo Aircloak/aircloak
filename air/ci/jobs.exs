6b2d628ac7d9cc104b0c977fee1c7acac1838cf6
Sasa Juric <sasa.juric@gmail.com>
2017-12-21 16:33:07 +0100

    parallelize air jobs

--- air/ci/jobs.exs
+++ air/ci/jobs.exs
@@ -2,17 +2,27 @@
   compile: [
     "make deps",
     "MIX_ENV=dev mix compile",
-    "MIX_ENV=test mix compile",
-    "MIX_ENV=prod mix compile",
-    "MIX_HOME=_build mix dialyze --no-analyse --no-compile",
+    parallel: [
+      "MIX_ENV=test mix compile",
+      "MIX_ENV=prod mix compile",
+      "MIX_HOME=_build mix dialyze --no-analyse --no-compile"
+    ],
   ],
   test: [
     "make deps",
-    "make check-warnings",
-    "make docs",
-    "make lint",
-    "MIX_ENV=test mix recreate_db",
-    "make test",
-    "MIX_HOME=_build mix dialyze --no-compile"
+    parallel: [
+      "MIX_ENV=dev mix compile --warnings-as-errors --all-warnings",
+      "MIX_ENV=test mix compile --warnings-as-errors --all-warnings",
+      "MIX_ENV=prod mix compile --warnings-as-errors --all-warnings",
+    ],
+    parallel: [
+      "make docs",
+      "make lint",
+      "MIX_HOME=_build mix dialyze --no-compile",
+      sequence: [
+        "MIX_ENV=test mix recreate_db",
+        "make test",
+      ]
+    ]
   ],
 }
