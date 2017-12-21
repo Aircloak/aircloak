d12fee50f143a53297b1b9b42cb8d4986f70a6a8
Paweł Obrok <pawel.obrok@gmail.com>
2017-12-21 13:37:58 +0100

    Ignore known errors when fuzzing

--- cloak/test/compliance/fuzz_test.exs
+++ cloak/test/compliance/fuzz_test.exs
@@ -2,12 +2,33 @@ defmodule Cloak.Compliance.FuzzTest do
   use ComplianceCase, async: true
 
   alias Cloak.Compliance.QueryGenerator
+  import Cloak.Test.QueryHelpers
 
   @tag :fuzz
   test "running a generated query", context = %{data_sources: [%{tables: tables} | _other_sources]} do
     for _ <- 1..200 do
       query = tables |> Map.values() |> QueryGenerator.generate_ast() |> QueryGenerator.ast_to_sql() |> to_string()
-      assert_consistent_and_not_failing(context, query)
+      assert_consistent_or_failing_nicely(context, query)
+    end
+  end
+
+  defp assert_consistent_or_failing_nicely(context, query) do
+    case assert_query_consistency(query, data_sources: context.data_sources) do
+      %{error: error} ->
+        if nice_error?(error) do
+          error
+        else
+          raise ExUnit.AssertionError,
+            message: "Query execution failed. Query was:\n#{query}.\n\nError:\n#{error}"
+        end
+      result -> result
+    end
+  end
+
+  defp nice_error?(error) do
+    cond do
+      error =~ ~r/`HAVING` clause can not be applied over column/ -> true
+      true -> false
     end
   end
 end
