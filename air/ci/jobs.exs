d5fe40ce56dc3e43a42d9c09110b89d6794f861b
Sasa Juric <sasa.juric@gmail.com>
2017-12-22 10:55:02 +0100

    make sequences explicit
    It's strictly speaking not needed, but I think it's better to state it explicitly.

--- air/ci/jobs.exs
+++ air/ci/jobs.exs
@@ -1,27 +1,31 @@
 %{
   compile: [
-    "make deps",
-    "MIX_ENV=dev mix compile",
-    parallel: [
-      "MIX_ENV=test mix compile",
-      "MIX_ENV=prod mix compile",
-      "MIX_HOME=_build mix dialyze --no-analyse --no-compile"
-    ],
+    sequence: [
+      "make deps",
+      "MIX_ENV=dev mix compile",
+      parallel: [
+        "MIX_ENV=test mix compile",
+        "MIX_ENV=prod mix compile",
+        "MIX_HOME=_build mix dialyze --no-analyse --no-compile"
+      ],
+    ]
   ],
   test: [
-    "make deps",
-    parallel: [
-      "MIX_ENV=dev mix compile --warnings-as-errors --all-warnings",
-      "MIX_ENV=test mix compile --warnings-as-errors --all-warnings",
-      "MIX_ENV=prod mix compile --warnings-as-errors --all-warnings",
-    ],
-    parallel: [
-      "make docs",
-      "make lint",
-      "MIX_HOME=_build mix dialyze --no-compile",
-      sequence: [
-        "MIX_ENV=test mix recreate_db",
-        "make test",
+    sequence: [
+      "make deps",
+      parallel: [
+        "MIX_ENV=dev mix compile --warnings-as-errors --all-warnings",
+        "MIX_ENV=test mix compile --warnings-as-errors --all-warnings",
+        "MIX_ENV=prod mix compile --warnings-as-errors --all-warnings",
+      ],
+      parallel: [
+        "make docs",
+        "make lint",
+        "MIX_HOME=_build mix dialyze --no-compile",
+        sequence: [
+          "MIX_ENV=test mix recreate_db",
+          "make test",
+        ]
       ]
     ]
   ],
--- cloak/ci/jobs.exs
+++ cloak/ci/jobs.exs
@@ -1,35 +1,41 @@
 %{
   compile: [
-    "make deps",
-    parallel: [
-      "MIX_ENV=dev mix compile",
-      "MIX_ENV=test mix compile",
-      "MIX_ENV=prod mix compile",
-      "MIX_HOME=_build mix dialyze --no-analyse --no-compile",
+    sequence: [
+      "make deps",
+      parallel: [
+        "MIX_ENV=dev mix compile",
+        "MIX_ENV=test mix compile",
+        "MIX_ENV=prod mix compile",
+        "MIX_HOME=_build mix dialyze --no-analyse --no-compile",
+      ]
     ]
   ],
 
   test: [
-    "make deps",
-    parallel: [
-      "MIX_ENV=dev mix compile --warnings-as-errors --all-warnings",
-      "MIX_ENV=test mix compile --warnings-as-errors --all-warnings",
-      "MIX_ENV=prod mix compile --warnings-as-errors --all-warnings",
-      "mix lint",
-      "MIX_ENV=test mix lint",
-      "MIX_HOME=_build mix dialyze --no-compile",
-      sequence: [
-        "mongod --fork --logpath /var/log/mongodb.log",
-        # hacky solution for recreating the test database
-        "CLOAK_DATA_SOURCES=postgresql9.4 MIX_ENV=test mix gen.test_data dockerized_ci 1",
-        "CLOAK_DATA_SOURCES=postgresql9.4 mix test --include exclude_in_dev",
+    sequence: [
+      "make deps",
+      parallel: [
+        "MIX_ENV=dev mix compile --warnings-as-errors --all-warnings",
+        "MIX_ENV=test mix compile --warnings-as-errors --all-warnings",
+        "MIX_ENV=prod mix compile --warnings-as-errors --all-warnings",
+        "mix lint",
+        "MIX_ENV=test mix lint",
+        "MIX_HOME=_build mix dialyze --no-compile",
+        sequence: [
+          "mongod --fork --logpath /var/log/mongodb.log",
+          # hacky solution for recreating the test database
+          "CLOAK_DATA_SOURCES=postgresql9.4 MIX_ENV=test mix gen.test_data dockerized_ci 1",
+          "CLOAK_DATA_SOURCES=postgresql9.4 mix test --include exclude_in_dev",
+        ]
       ]
     ]
   ],
 
   compliance: [
-    "make deps",
-    "MIX_ENV=test mix gen.test_data dockerized_ci 100",
-    "mix test --only compliance --max-cases 10",
+    sequence: [
+      "make deps",
+      "MIX_ENV=test mix gen.test_data dockerized_ci 100",
+      "mix test --only compliance --max-cases 10",
+    ]
   ]
 }
