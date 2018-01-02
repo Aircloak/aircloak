fe47bf18bfccf5a8df0b621e1dcdbfdfb170cd0d
Sebastian Probst Eide <sebastian@aircloak.com>
2018-01-02 09:28:59 +0100

    Re-enable test for supported queries now supported

--- cloak/test/supported_queries/teambank_test.exs
+++ cloak/test/supported_queries/teambank_test.exs
@@ -258,44 +258,43 @@ defmodule Cloak.Regressions.TeamBank.Test do
     assert_compiles_successfully(query, data_source_scaffold())
   end
 
-  # Related issue: (#2188)
-  # test "sebastian 1" do
-  #   query = """
-  #   SELECT
-  #     bucket(avg_betrag by 500 align middle) AS income_class,
-  #     bucket(avg_kontostand by 500 align middle) AS kontostand,
-  #     count(distinct inhaberId) AS num_Acc,
-  #     count_noise(distinct inhaberId)
-  #   FROM (
-  #     SELECT
-  #       konto.inhaberId,
-  #       avg(betrag) as avg_betrag,
-  #       avg(kontostand.betrag) as avg_kontostand
-  #     FROM konto INNER JOIN bankzugang
-  #       ON konto.inhaberId = bankzugang.inhaberId AND
-  #         blz NOT IN ('90090042', '47110815') AND
-  #         konto.bankzugangId = bankzugang._id
-  #     INNER JOIN (
-  #       SELECT
-  #         inhaberId,
-  #         kontoId,
-  #         betrag
-  #       FROM umsatz
-  #       WHERE
-  #         UPPER(umsatzeigenschaften.spezifizierung) IN
-  #           ('LOHN', 'GEHALT', 'LOHN/GEHALT', 'BEZÜGE (BEAMTE)', 'BEZÜGE',
-  #           'LOHN_GEHALT', 'BEZUEGE_BEAMTE', 'BEZUEGE') AND
-  #         buchungsDatum BETWEEN '2017-10-29' AND '2017-11-28'
-  #     ) as umsatz ON umsatz.inhaberId = konto.inhaberId AND
-  #       umsatz.kontoId = konto._id
-  #     WHERE konto.letzterUmsatz.buchungsDatum BETWEEN '2017-10-29' AND '2017-11-28'
-  #     GROUP BY konto.inhaberId
-  #   ) average_values
-  #   GROUP BY income_class, kontostand
-  #   ORDER BY income_class, kontostand
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 1" do
+    query = """
+    SELECT
+      bucket(avg_betrag by 500 align middle) AS income_class,
+      bucket(avg_kontostand by 500 align middle) AS kontostand,
+      count(distinct inhaberId) AS num_Acc,
+      count_noise(distinct inhaberId)
+    FROM (
+      SELECT
+        konto.inhaberId,
+        avg(betrag) as avg_betrag,
+        avg(kontostand.betrag) as avg_kontostand
+      FROM konto INNER JOIN bankzugang
+        ON konto.inhaberId = bankzugang.inhaberId AND
+          blz NOT IN ('90090042', '47110815') AND
+          konto.bankzugangId = bankzugang._id
+      INNER JOIN (
+        SELECT
+          inhaberId,
+          kontoId,
+          betrag
+        FROM umsatz
+        WHERE
+          UPPER(umsatzeigenschaften.spezifizierung) IN
+            ('LOHN', 'GEHALT', 'LOHN/GEHALT', 'BEZÜGE (BEAMTE)', 'BEZÜGE',
+            'LOHN_GEHALT', 'BEZUEGE_BEAMTE', 'BEZUEGE') AND
+          buchungsDatum BETWEEN '2017-10-29' AND '2017-11-28'
+      ) as umsatz ON umsatz.inhaberId = konto.inhaberId AND
+        umsatz.kontoId = konto._id
+      WHERE konto.letzterUmsatz.buchungsDatum BETWEEN '2017-10-29' AND '2017-11-28'
+      GROUP BY konto.inhaberId
+    ) average_values
+    GROUP BY income_class, kontostand
+    ORDER BY income_class, kontostand
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
   test "sebastian 2" do
     query = """
@@ -373,59 +372,57 @@ defmodule Cloak.Regressions.TeamBank.Test do
     assert_compiles_successfully(query, data_source_scaffold())
   end
 
-  # Related issue: (#2188)
-  # test "sebastian 6" do
-  #   query = """
-  #   SELECT
-  #     bucket(income by 200 align middle) as income_class,
-  #     count(*),
-  #     count_noise(*)
-  #   FROM (
-  #     SELECT
-  #       income_by_month.inhaberId,
-  #       -- Use median to not have outlier months impact values
-  #       median(monthly_income) as income
-  #     FROM (
-  #       SELECT
-  #         inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY inhaberId, year_month
-  #     ) as income_by_month
-  #     GROUP BY income_by_month.inhaberId
-  #   ) customer_incomes
-  #   GROUP BY income_class
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 6" do
+    query = """
+    SELECT
+      bucket(income by 200 align middle) as income_class,
+      count(*),
+      count_noise(*)
+    FROM (
+      SELECT
+        income_by_month.inhaberId,
+        -- Use median to not have outlier months impact values
+        median(monthly_income) as income
+      FROM (
+        SELECT
+          inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          sum(betrag) as monthly_income
+        FROM umsatz
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY inhaberId, year_month
+      ) as income_by_month
+      GROUP BY income_by_month.inhaberId
+    ) customer_incomes
+    GROUP BY income_class
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
-  # Related issue: (#2188)
-  # test "sebastian 7" do
-  #   query = """
-  #   SELECT
-  #     bucket(number_of_transactions by 10 align middle) as num_transactions,
-  #     count(*),
-  #     count_noise(*)
-  #   FROM (
-  #     SELECT
-  #       inhaberId,
-  #       median(transactions_count) as number_of_transactions
-  #     FROM (
-  #       SELECT
-  #         inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         count(*) as transactions_count
-  #       FROM umsatz
-  #       GROUP BY inhaberId, year_month
-  #     ) as transactions_per_month
-  #     GROUP BY inhaberId
-  #   ) as num_monthly_transactions
-  #   GROUP BY num_transactions
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 7" do
+    query = """
+    SELECT
+      bucket(number_of_transactions by 10 align middle) as num_transactions,
+      count(*),
+      count_noise(*)
+    FROM (
+      SELECT
+        inhaberId,
+        median(transactions_count) as number_of_transactions
+      FROM (
+        SELECT
+          inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          count(*) as transactions_count
+        FROM umsatz
+        GROUP BY inhaberId, year_month
+      ) as transactions_per_month
+      GROUP BY inhaberId
+    ) as num_monthly_transactions
+    GROUP BY num_transactions
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
   test "sebastian 8" do
     query = """
@@ -475,54 +472,52 @@ defmodule Cloak.Regressions.TeamBank.Test do
     assert_compiles_successfully(query, data_source_scaffold())
   end
 
-  # Related issue: (#2188)
-  # test "sebastian 11" do
-  #   query = """
-  #   SELECT
-  #     name,
-  #     bucket(customer_incomes.income by 200) as income_class,
-  #     avg(kontostand.betrag),
-  #     avg_noise(kontostand.betrag),
-  #     max(kontostand.betrag),
-  #     min(kontostand.betrag)
-  #   FROM (
-  #     SELECT
-  #       income_by_month.inhaberId,
-  #       -- Use median to not have outlier months impact values
-  #       median(monthly_income) as income
-  #     FROM (
-  #       SELECT
-  #         inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY inhaberId, year_month
-  #     ) as income_by_month
-  #     GROUP BY income_by_month.inhaberId
-  #   ) customer_incomes INNER JOIN konto ON
-  #     customer_incomes.inhaberId = konto.inhaberId
-  #   WHERE name IS NOT NULL
-  #   GROUP BY name, income_class
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 11" do
+    query = """
+    SELECT
+      name,
+      bucket(customer_incomes.income by 200) as income_class,
+      avg(kontostand.betrag),
+      avg_noise(kontostand.betrag),
+      max(kontostand.betrag),
+      min(kontostand.betrag)
+    FROM (
+      SELECT
+        income_by_month.inhaberId,
+        -- Use median to not have outlier months impact values
+        median(monthly_income) as income
+      FROM (
+        SELECT
+          inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          sum(betrag) as monthly_income
+        FROM umsatz
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY inhaberId, year_month
+      ) as income_by_month
+      GROUP BY income_by_month.inhaberId
+    ) customer_incomes INNER JOIN konto ON
+      customer_incomes.inhaberId = konto.inhaberId
+    WHERE name IS NOT NULL
+    GROUP BY name, income_class
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
-  # Related issue: (#2188)
-  # test "sebastian 12" do
-  #   query = """
-  #   SELECT users.uid
-  #   FROM (
-  #     SELECT inhaberId as uid, sum(betrag) as monthly_income
-  #     FROM umsatz
-  #     WHERE betrag >= 0 and betrag < 100000
-  #     GROUP BY uid
-  #   ) as users
-  #   WHERE users.monthly_income >= 5000 and users.monthly_income < 10000
-  #   GROUP BY 1
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 12" do
+    query = """
+    SELECT users.uid
+    FROM (
+      SELECT inhaberId as uid, sum(betrag) as monthly_income
+      FROM umsatz
+      WHERE betrag >= 0 and betrag < 100000
+      GROUP BY uid
+    ) as users
+    WHERE users.monthly_income >= 5000 and users.monthly_income < 10000
+    GROUP BY 1
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
   test "sebastian 13" do
     query = """
@@ -536,55 +531,53 @@ defmodule Cloak.Regressions.TeamBank.Test do
     assert_compiles_successfully(query, data_source_scaffold())
   end
 
-  # Related issue: (#2188)
-  # test "sebastian 14" do
-  #   query = """
-  #   SELECT
-  #     bucket(income by 200 align middle) as income_class,
-  #     count(*)
-  #   FROM (
-  #     SELECT inhaberId, median(monthly_income) as income FROM (
-  #       SELECT
-  #         inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY inhaberId, year_month
-  #     ) as income_by_month
-  #     GROUP BY inhaberId
-  #   ) as user_incomes
-  #   GROUP BY income_class
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 14" do
+    query = """
+    SELECT
+      bucket(income by 200 align middle) as income_class,
+      count(*)
+    FROM (
+      SELECT inhaberId, median(monthly_income) as income FROM (
+        SELECT
+          inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          sum(betrag) as monthly_income
+        FROM umsatz
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY inhaberId, year_month
+      ) as income_by_month
+      GROUP BY inhaberId
+    ) as user_incomes
+    GROUP BY income_class
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
-  # Related issue: (#2188)
-  # test "sebastian 15" do
-  #   query = """
-  #   SELECT
-  #     institution,
-  #     bucket(income by 200 align middle) as income_class,
-  #     count(*)
-  #   FROM (
-  #     SELECT inhaberId, institution, median(monthly_income) as income FROM (
-  #       SELECT
-  #         umsatz.inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         left(bic, 4) as institution,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz INNER JOIN bankzugang
-  #         ON umsatz.inhaberId = bankzugang.inhaberId
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY umsatz.inhaberId, year_month, institution
-  #     ) as income_by_month
-  #     GROUP BY inhaberId, institution
-  #   ) as user_incomes
-  #   GROUP BY institution, income_class
-  #   ORDER BY institution, income_class
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 15" do
+    query = """
+    SELECT
+      institution,
+      bucket(income by 200 align middle) as income_class,
+      count(*)
+    FROM (
+      SELECT inhaberId, institution, median(monthly_income) as income FROM (
+        SELECT
+          umsatz.inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          left(bic, 4) as institution,
+          sum(betrag) as monthly_income
+        FROM umsatz INNER JOIN bankzugang
+          ON umsatz.inhaberId = bankzugang.inhaberId
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY umsatz.inhaberId, year_month, institution
+      ) as income_by_month
+      GROUP BY inhaberId, institution
+    ) as user_incomes
+    GROUP BY institution, income_class
+    ORDER BY institution, income_class
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
   test "sebastian 16" do
     query = """
@@ -602,73 +595,71 @@ defmodule Cloak.Regressions.TeamBank.Test do
     assert_compiles_successfully(query, data_source_scaffold())
   end
 
-  # Related issue: (#2188)
-  # test "sebastian 17" do
-  #   query = """
-  #   SELECT
-  #     extract_words(verwendungszweck) as shop,
-  #     bucket(income by 200 align middle) as income_class,
-  #     count(*)
-  #   FROM (
-  #     SELECT inhaberId, median(monthly_income) as income
-  #     FROM (
-  #       SELECT
-  #         inhaberId,
-  #         left(cast(buchungsDatum as text), 7) as year_month,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY inhaberId, year_month
-  #     ) as income_by_month
-  #     GROUP BY inhaberId
-  #   ) customer_incomes INNER JOIN umsatz ON
-  #     umsatz.inhaberId = customer_incomes.inhaberId
-  #   GROUP BY income_class, shop
-  #   ORDER BY shop ASC, income_class ASC
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 17" do
+    query = """
+    SELECT
+      extract_words(verwendungszweck) as shop,
+      bucket(income by 200 align middle) as income_class,
+      count(*)
+    FROM (
+      SELECT inhaberId, median(monthly_income) as income
+      FROM (
+        SELECT
+          inhaberId,
+          left(cast(buchungsDatum as text), 7) as year_month,
+          sum(betrag) as monthly_income
+        FROM umsatz
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY inhaberId, year_month
+      ) as income_by_month
+      GROUP BY inhaberId
+    ) customer_incomes INNER JOIN umsatz ON
+      umsatz.inhaberId = customer_incomes.inhaberId
+    GROUP BY income_class, shop
+    ORDER BY shop ASC, income_class ASC
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
-  # Related issue: (#2188)
-  # test "sebastian 18" do
-  #   query = """
-  #   SELECT
-  #     bucket(income by 200 align middle) as income_class,
-  #     avg(monthly_savings)
-  #   FROM (
-  #     SELECT
-  #       expenses_by_month.inhaberId,
-  #       -- Use median to not have outlier months impact values
-  #       median(monthly_income) as income,
-  #       median(monthly_income) + median(monthly_expenses) as monthly_savings
-  #     FROM (
-  #       SELECT
-  #         inhaberId,
-  #         month(buchungsDatum) as month,
-  #         year(buchungsDatum) as year,
-  #         sum(betrag) as monthly_income
-  #       FROM umsatz
-  #       WHERE betrag >= 0 and betrag < 1000000000
-  #       GROUP BY inhaberId, month, year
-  #     ) as income_by_month INNER JOIN (
-  #       SELECT
-  #         inhaberId,
-  #         month(buchungsDatum) as month,
-  #         year(buchungsDatum) as year,
-  #         sum(betrag) as monthly_expenses
-  #       FROM umsatz
-  #       WHERE betrag >= -1000000000 and betrag < 0
-  #       GROUP BY inhaberId, month, year
-  #     ) as expenses_by_month ON
-  #       income_by_month.inhaberId = expenses_by_month.inhaberId and
-  #       income_by_month.year = expenses_by_month.year and
-  #       income_by_month.month = expenses_by_month.month
-  #     GROUP BY expenses_by_month.inhaberId
-  #   ) customer_incomes
-  #   GROUP BY income_class
-  #   """
-  #   assert_compiles_successfully(query, data_source_scaffold())
-  # end
+  test "sebastian 18" do
+    query = """
+    SELECT
+      bucket(income by 200 align middle) as income_class,
+      avg(monthly_savings)
+    FROM (
+      SELECT
+        expenses_by_month.inhaberId,
+        -- Use median to not have outlier months impact values
+        median(monthly_income) as income,
+        median(monthly_income) + median(monthly_expenses) as monthly_savings
+      FROM (
+        SELECT
+          inhaberId,
+          month(buchungsDatum) as month,
+          year(buchungsDatum) as year,
+          sum(betrag) as monthly_income
+        FROM umsatz
+        WHERE betrag >= 0 and betrag < 1000000000
+        GROUP BY inhaberId, month, year
+      ) as income_by_month INNER JOIN (
+        SELECT
+          inhaberId,
+          month(buchungsDatum) as month,
+          year(buchungsDatum) as year,
+          sum(betrag) as monthly_expenses
+        FROM umsatz
+        WHERE betrag >= -1000000000 and betrag < 0
+        GROUP BY inhaberId, month, year
+      ) as expenses_by_month ON
+        income_by_month.inhaberId = expenses_by_month.inhaberId and
+        income_by_month.year = expenses_by_month.year and
+        income_by_month.month = expenses_by_month.month
+      GROUP BY expenses_by_month.inhaberId
+    ) customer_incomes
+    GROUP BY income_class
+    """
+    assert_compiles_successfully(query, data_source_scaffold())
+  end
 
   test "sebastian 19" do
     query = """
