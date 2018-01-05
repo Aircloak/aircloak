df8b600fde5e3d4b6095a356a8eca24c727b9f4b
Paweł Obrok <pawel.obrok@gmail.com>
2018-01-05 15:01:22 +0100

    Add tests for parsing dates over the year 9999

--- cloak/test/cloak/time_test.exs
+++ cloak/test/cloak/time_test.exs
@@ -9,6 +9,11 @@ defmodule Cloak.Time.Test do
     assert {:error, :pre_gregorian_calendar} = Cloak.Time.parse_date("1582-12-31")
   end
 
+  test "9999-12-31 is the maximum parseable date" do
+    assert {:ok, ~D[9999-12-31]} = Cloak.Time.parse_date("9999-12-31")
+    assert {:error, :invalid_format} = Cloak.Time.parse_date("10000-01-01")
+  end
+
   test "parsing an invalid datetime", do:
     assert {:error, :invalid_format} = Cloak.Time.parse_datetime("invalid")
 
@@ -20,6 +25,15 @@ defmodule Cloak.Time.Test do
 
   test "1583-01-01 is the minimum parseable datetime" do
     assert {:ok, ~N[1583-01-01 00:00:00.000000]} = Cloak.Time.parse_datetime("1583-01-01 00:00:00")
+    assert {:ok, ~N[1583-01-01 00:00:00.000000]} = Cloak.Time.parse_datetime("1583-01-01")
     assert {:error, :pre_gregorian_calendar} = Cloak.Time.parse_datetime("1582-12-31 23:59:59")
+    assert {:error, :pre_gregorian_calendar} = Cloak.Time.parse_datetime("1582-12-31")
+  end
+
+  test "9999-12-31 is the maximum parseable datetime" do
+    assert {:ok, ~N[9999-12-31 23:59:59.999999]} = Cloak.Time.parse_datetime("9999-12-31 23:59:59.999999")
+    assert {:ok, ~N[9999-12-31 00:00:00.000000]} = Cloak.Time.parse_datetime("9999-12-31")
+    assert {:error, :invalid_format} = Cloak.Time.parse_datetime("10000-01-01 00:00:00")
+    assert {:error, :invalid_format} = Cloak.Time.parse_datetime("10000-01-01")
   end
 end
