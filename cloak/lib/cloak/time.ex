055184579e98263e0a43500957c2f4986fcd3c01
Paweł Obrok <pawel.obrok@gmail.com>
2018-01-05 15:08:39 +0100

    Add docs and specs

--- cloak/lib/cloak/time.ex
+++ cloak/lib/cloak/time.ex
@@ -6,7 +6,21 @@ defmodule Cloak.Time do
   # API functions
   # -------------------------------------------------------------------
 
+  @doc """
+  Returns the minimum legal datetime. It is set to 1583-01-01, as that is the first normal year of the gregorian
+  calendar. While the calendar was introduced a year earlier, the year 1582 had some weird features, like 10 days
+  missing, to synchronize the calendar with the sun. Dates prior to this are representable with the native Elixir
+  date and datetime datatype (down to 0000-01-01), but are not guaranteed to work according to ISO-8601. For example
+  the date 0000-01-01 is invalid in postgres.
+  """
+  @spec datetime_lower_bound() :: NaiveDateTime.t
   def datetime_lower_bound(), do: ~N[1583-01-01 00:00:00.000000]
+
+  @doc """
+  Returns the maximum legal datetime. It is set to 9999-12-31. Dates above this are broken in Elixir (for example they
+  inspect incorrectly), and are not guaranteed to work according to ISO-8601.
+  """
+  @spec datetime_upper_bound() :: NaiveDateTime.t
   def datetime_upper_bound(), do: ~N[9999-12-31 23:59:59.999999]
 
   @doc "Parses string as an ISO8601 time."
