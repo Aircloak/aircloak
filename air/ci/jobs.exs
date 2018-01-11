9f01441cb3499575b4b00a593aea59d5cdf5e870
Sasa Juric <sasa.juric@gmail.com>
2018-01-11 09:13:42 +0100

    use plain tuples instead of helper functions

--- air/ci/jobs.exs
+++ air/ci/jobs.exs
@@ -1,34 +1,36 @@
-parallel = fn(commands) -> {:parallel, commands} end
-sequence = fn(commands) -> {:sequence, commands} end
-
 test = fn
   :test ->
-    sequence.([
+    {:sequence, [
       "MIX_ENV=test ./check_warnings.sh",
-      parallel.(["MIX_ENV=test mix lint", sequence.(["MIX_ENV=test mix recreate_db", "mix test"])])
-    ])
+      {:parallel, ["MIX_ENV=test mix lint", {:sequence, ["MIX_ENV=test mix recreate_db", "mix test"]}]}
+    ]}
 
   :dev ->
-    sequence.([
+    {:sequence, [
       "MIX_ENV=dev ./check_warnings.sh",
-      parallel.(["make docs", "make eslint", "make flow", "mix lint", "MIX_HOME=_build make dialyze"])
-    ])
+      {:parallel, ["make docs", "make eslint", "make flow", "mix lint", "MIX_HOME=_build make dialyze"]}
+    ]}
 
   :prod ->
-    sequence.(["MIX_ENV=prod ./check_warnings.sh"])
+    {:sequence, ["MIX_ENV=prod ./check_warnings.sh"]}
 end
 
 # jobs map
 %{
   compile:
-    sequence.(["make deps", "MIX_ENV=dev mix compile",
-      parallel.([
+    {:sequence, [
+      "make deps",
+      "MIX_ENV=dev mix compile",
+      {:parallel, [
         "MIX_ENV=test mix compile",
         "MIX_ENV=prod mix compile",
         "MIX_HOME=_build mix dialyze --no-analyse"
-      ])
-    ]),
+      ]}
+    ]},
 
   test:
-    sequence.(["make deps", parallel.([test.(:test), test.(:dev), test.(:prod)])]),
+    {:sequence, [
+      "make deps",
+      {:parallel, [test.(:test), test.(:dev), test.(:prod)]}
+    ]},
 }
--- integration_tests/ci/jobs.exs
+++ integration_tests/ci/jobs.exs
@@ -1,19 +1,15 @@
-parallel = fn(commands) -> {:parallel, commands} end
-sequence = fn(commands) -> {:sequence, commands} end
-
-# jobs map
 %{
   compile:
-    sequence.(["mix deps.get", "MIX_ENV=test mix compile"]),
+    {:sequence, ["mix deps.get", "MIX_ENV=test mix compile"]},
 
   test:
-    sequence.([
+    {:sequence, [
       "MIX_ENV=test mix await_databases",
-      parallel.([
+      {:parallel, [
         ~s/MIX_ENV=test mix cloak.create_db dockerized_integration/,
         ~s/MIX_ENV=test mix do run --no-start -e Air.Repo.configure, ecto.create -r Air.Repo, ecto.migrate -r Air.Repo/,
         ~s/MIX_ENV=test mix do run --no-start -e Central.Repo.configure, ecto.create -r Central.Repo, ecto.migrate -r Central.Repo/,
-      ]),
+      ]},
       "mix test"
-    ]),
+    ]},
 }
