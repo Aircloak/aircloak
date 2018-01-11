a4efedfc4d709647769535e3be3f45447c0a90b9
Sasa Juric <sasa.juric@gmail.com>
2018-01-11 09:06:08 +0100

    fix a typo

--- cloak/dev/mix/tasks/create_db.ex
+++ cloak/dev/mix/tasks/create_db.ex
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.Cloak.CreateDb do
-  @shortdoc "Creates databasese from the given json configuration."
-  @moduledoc "Creates databasese from the given json configuration."
+  @shortdoc "Creates databases from the given json configuration."
+  @moduledoc "Creates databases from the given json configuration."
 
   # Mix.Task behaviour is not in PLT since Mix is not a runtime dep, so we disable the warning
   @dialyzer :no_undefined_callbacks
