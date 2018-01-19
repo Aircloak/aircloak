9662abeb3614496ff463ecc92e0e2c03cce5de9a
Sasa Juric <sasa.juric@gmail.com>
2018-01-19 16:07:32 +0100

    simulate git clone in tests

--- ci/test/support/test_exec.ex
+++ ci/test/support/test_exec.ex
@@ -1,12 +1,27 @@
 defmodule AircloakCI.TestExec do
   @moduledoc false
 
-  def run_link(_cmd, _opts) do
+  def run_link(cmd, _opts) do
+    case Regex.named_captures(~r[^git clone git@github.com:.* (?<target>\.test_data/.*)$], to_string(cmd)) do
+      nil -> :ok
+
+      %{"target" => target_folder} ->
+        AircloakCI.LocalProject.for_local("..")
+        |> AircloakCI.LocalProject.components()
+        |> Enum.each(&transfer_component(&1, target_folder))
+    end
+
     {:ok, pid} = Task.start_link(fn -> :ok end)
     {:ok, pid, nil}
   end
 
   def stop(_pid) do
     :ok
+  end
+
+  defp transfer_component(component, target_folder) do
+    target_path = Path.expand(Path.join(target_folder, "#{component}/ci"))
+    File.mkdir_p!(target_path)
+    File.cp_r!("../#{component}/ci", "#{target_path}")
   end
 end
