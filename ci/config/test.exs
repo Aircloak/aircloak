77253bf4b6f37613fcbf4ec8f5ac5400a657f4f0
Sasa Juric <sasa.juric@gmail.com>
2018-01-19 17:48:48 +0100

    reduce the number of components in tests

--- ci/config/test.exs
+++ ci/config/test.exs
@@ -7,4 +7,8 @@ config :aircloak_ci,
   github_api: AircloakCI.TestGithubAPI,
   poll_github: false,
   write_to_github: true,
-  cmd_runner: [console_out: false]
+  cmd_runner: [console_out: false],
+  # Note: we're using a reduced set of components, because a single build for all components takes 0.5 seconds.
+  # Since all the commands are mocked, it should be analyzed where the time is spent, and optimize the solution.
+  # However, since this is not an issue in production at the moment, a simple solution has been chosen.
+  components_filter: {:only, ["air", "cloak"]}
