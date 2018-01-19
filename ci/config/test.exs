32dbd893d648a77b4f536d643e509cd0fb1dedab
Sasa Juric <sasa.juric@gmail.com>
2018-01-19 14:30:49 +0100

    stop simulating jobs and commands in tests

--- ci/config/test.exs
+++ ci/config/test.exs
@@ -4,6 +4,4 @@ config :aircloak_ci,
   exec_mod: AircloakCI.TestExec,
   poll_github: false,
   write_to_github: false,
-  simulate_commands: true,
-  simulated_jobs: [:compile, :compliance, :test],
   cmd_runner: [console_out: false]
