bcf10efada8f32806ac992eb5a3d15077b8e2112
Sasa Juric <sasa.juric@gmail.com>
2018-01-25 13:11:05 +0100

    require user confirmation on exit

--- tools/batch_queries/batch_queries.go
+++ tools/batch_queries/batch_queries.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
@@ -40,15 +41,27 @@ type row struct {
 	Occurrences int           `json:"occurrences"`
 }
 
-var configuration = readConfig()
+var configuration = config{}
 var resultsPath = fmt.Sprintf("results/%s", time.Now().Format("20060102150405"))
 
 func main() {
+	defer func() {
+		r := recover()
+		if r != nil && r != "stop" {
+			fmt.Printf("Error: %s", r)
+		}
+
+		fmt.Print("\nPress Enter to finish")
+		bufio.NewReader(os.Stdin).ReadString('\n')
+	}()
+
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	configuration = readConfig()
 
 	err := os.MkdirAll(resultsPath, os.ModePerm)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	queries := readQueries()
@@ -62,13 +75,13 @@ func main() {
 func readConfig() config {
 	raw, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	var decoded config
 	err = json.Unmarshal(raw, &decoded)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	return decoded
@@ -77,7 +90,7 @@ func readConfig() config {
 func readQueries() []string {
 	buffer, err := ioutil.ReadFile("queries.sql")
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	return regexp.MustCompile(`(?m)^\s*$`).Split(string(buffer), -1)
@@ -132,7 +145,7 @@ func jsonRequest(method string, path string, payload interface{}, out interface{
 	if payload != nil {
 		err := json.NewEncoder(buffer).Encode(payload)
 		if err != nil {
-			log.Fatal(err)
+			reportError(err)
 		}
 	}
 
@@ -140,7 +153,7 @@ func jsonRequest(method string, path string, payload interface{}, out interface{
 	client := &http.Client{}
 	req, err := http.NewRequest(method, fullPath, buffer)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	req.Header.Add("auth-token", configuration.APIToken)
@@ -148,23 +161,23 @@ func jsonRequest(method string, path string, payload interface{}, out interface{
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == 401 {
-		log.Fatal(fmt.Sprintf("Unauthorized request %s %s. Check if API token is valid.", method, fullPath))
+		reportError(fmt.Sprintf("Unauthorized request %s %s. Check if API token is valid.", method, fullPath))
 	}
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	err = json.Unmarshal(bodyBytes, out)
 	if err != nil {
-		log.Fatal(string(bodyBytes))
+		reportError(string(bodyBytes))
 	}
 }
 
@@ -179,7 +192,7 @@ func storeResult(index int, query string, queryResult queryResult) {
 func storeSuccess(index int, queryResult queryResult) {
 	file, err := os.Create(fmt.Sprintf("%s/query_%d.csv", resultsPath, index+1))
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 	defer file.Close()
 
@@ -188,7 +201,7 @@ func storeSuccess(index int, queryResult queryResult) {
 
 	err = writer.Write(queryResult.Query.Columns)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 
 	for _, row := range queryResult.Query.Rows {
@@ -200,7 +213,7 @@ func storeSuccess(index int, queryResult queryResult) {
 		for i := 0; i < row.Occurrences; i++ {
 			err = writer.Write(strings)
 			if err != nil {
-				log.Fatal(err)
+				reportError(err)
 			}
 		}
 	}
@@ -213,9 +226,14 @@ func storeError(query string, queryResult queryResult) {
 		0644,
 	)
 	if err != nil {
-		log.Fatal(err)
+		reportError(err)
 	}
 	defer file.Close()
 
 	file.Write([]byte(fmt.Sprintf("----\nError runninq query\n%s\n%s\n----\n\n", query, *queryResult.Query.Error)))
+}
+
+func reportError(v ...interface{}) {
+	log.Output(2, fmt.Sprint(v...))
+	panic("stop")
 }
