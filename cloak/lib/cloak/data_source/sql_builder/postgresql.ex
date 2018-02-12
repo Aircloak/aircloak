e1a1050688dcc425f38b8bb2190d99f5d38db1f0
Sasa Juric <sasa.juric@gmail.com>
2018-02-12 12:55:08 +0100

    use bigint when casting to integers
    This reduces the chance of overflow errors happening when large float is casted into an integer.

--- cloak/lib/cloak/data_source/sql_builder/postgresql.ex
+++ cloak/lib/cloak/data_source/sql_builder/postgresql.ex
@@ -42,10 +42,12 @@ defmodule Cloak.DataSource.SqlBuilder.PostgreSQL do
   def limit_sql(limit, offset), do: [" LIMIT ", to_string(limit), " OFFSET ", to_string(offset)]
 
   @impl Dialect
+  def cast_sql(value, :boolean, :integer), do:
+    cast_sql(value, :boolean, :int32)
   def cast_sql(value, :boolean, :real), do:
     value |> cast_sql(:boolean, :integer) |> cast_sql(:integer, :real)
   def cast_sql(value, :real, :boolean), do:
-    value |> cast_sql(:real, :integer) |> cast_sql(:integer, :boolean)
+    value |> cast_sql(:real, :int32) |> cast_sql(:integer, :boolean)
   def cast_sql(value, _, type), do:
     ["CAST(", value, " AS ", sql_type(type), ")"]
 
@@ -62,5 +64,7 @@ defmodule Cloak.DataSource.SqlBuilder.PostgreSQL do
 
   defp sql_type(:real), do: "float"
   defp sql_type(:boolean), do: "bool"
+  defp sql_type(:integer), do: "bigint"
+  defp sql_type(:int32), do: "integer"
   defp sql_type(type) when is_atom(type), do: Atom.to_string(type)
 end
