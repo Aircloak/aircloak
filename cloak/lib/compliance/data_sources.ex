adb7a838dcc855bee8d4a82687a2dd83d843865c
Sasa Juric <sasa.juric@gmail.com>
2018-02-15 10:37:34 +0100

    support concurrent insertions in gen.test_data

--- cloak/lib/compliance/data_sources.ex
+++ cloak/lib/compliance/data_sources.ex
@@ -28,22 +28,27 @@ defmodule Compliance.DataSources do
     |> Cloak.DataSource.config_to_datasources()
 
   @doc "Creates tables for a normal and a encoded dataset and inserts data into them."
-  @spec setup([DataSource.t], Map.t, non_neg_integer) :: :ok
-  def setup(data_sources, data, num_users) do
-    insert_servers = Enum.map(data_sources, &start_insert_server!/1)
+  @spec setup([DataSource.t], Map.t, non_neg_integer, pos_integer) :: :ok
+  def setup(data_sources, data, num_users, concurrency) do
+    data_sources
+    |> Enum.map(&Task.async(fn -> setup_data_source(&1) end))
+    |> Enum.each(&Task.await(&1, :timer.minutes(1)))
 
-    chunk_size = 1000
-    num_chunks = Float.ceil(num_users/chunk_size)
+    insert_servers = Enum.map(data_sources, &start_insert_servers!(&1, concurrency))
+
+    chunk_size = 100
+    num_steps = Float.ceil(num_users / (chunk_size * concurrency))
 
     data
     |> Stream.chunk_every(chunk_size)
-    |> Stream.map(&Enum.unzip(&1))
-    |> Stream.map(fn(chunk) -> Enum.each(insert_servers, &send_chunk(&1, chunk)) end)
+    |> Stream.map(&Enum.unzip/1)
+    |> Stream.chunk_every(concurrency)
+    |> Stream.map(fn(chunks) -> Enum.each(insert_servers, &insert_chunks(&1, chunks)) end)
     |> Stream.with_index()
-    |> Stream.map(fn({_, chunk_index}) -> IO.write("inserted users #{round((chunk_index / num_chunks) * 100)}%\r") end)
+    |> Stream.map(fn({_, chunk_index}) -> IO.write("inserted users #{round((chunk_index / num_steps) * 100)}%\r") end)
     |> Stream.run()
 
-    Enum.each(insert_servers, &stop_insert_server/1)
+    Enum.each(insert_servers, &stop_insert_servers/1)
     IO.puts("\rinserted #{num_users} users                                   ")
   end
 
@@ -71,8 +76,41 @@ defmodule Compliance.DataSources do
 
 
   # -------------------------------------------------------------------
-  # Internal functions - Creating tables
+  # Data source setup
   # -------------------------------------------------------------------
+
+  defp setup_data_source(data_source) do
+    handler = handler_for_data_source(data_source)
+    handler.setup(data_source)
+
+    conn = handler.connect(data_source)
+
+    plain_definitions = table_definitions(&TableDefinitions.plain/1, data_source)
+    create_tables(handler, conn, plain_definitions, @plain_name_postfix)
+
+    encoded_definitions = table_definitions(&TableDefinitions.encoded/1, data_source)
+    create_tables(handler, conn, encoded_definitions, @encoded_name_postfix)
+
+    handler.terminate(conn)
+  end
+
+  defp create_tables(handler, conn, definitions, table_postfix) do
+    Enum.each(
+      definitions,
+      fn({name, %{columns: columns}}) ->
+        handler.create_table("#{name}#{table_postfix}", columns, conn)
+      end
+    )
+  end
+
+
+  # -------------------------------------------------------------------
+  # Insert servers
+  # -------------------------------------------------------------------
+
+  defp start_insert_servers!(data_source, concurrency) do
+    Enum.map(1..concurrency, fn(_) -> start_insert_server!(data_source) end)
+  end
 
   defp start_insert_server!(data_source) do
     {:ok, insert_server} = Agent.start_link(fn -> nil end)
@@ -81,14 +119,9 @@ defmodule Compliance.DataSources do
       insert_server,
       fn(nil) ->
         handler = handler_for_data_source(data_source)
-        handler.setup(data_source)
         conn = handler.connect(data_source)
-
         plain_definitions = table_definitions(&TableDefinitions.plain/1, data_source)
-        create_tables(handler, conn, plain_definitions, @plain_name_postfix)
-
         encoded_definitions = table_definitions(&TableDefinitions.encoded/1, data_source)
-        create_tables(handler, conn, encoded_definitions, @encoded_name_postfix)
 
         %{
           data_source: data_source,
@@ -103,20 +136,19 @@ defmodule Compliance.DataSources do
     insert_server
   end
 
-  defp create_tables(handler, conn, definitions, table_postfix) do
-    Enum.each(
-      definitions,
-      fn({name, %{columns: columns}}) ->
-        handler.create_table("#{name}#{table_postfix}", columns, conn)
-      end
-    )
-  end
+  defp stop_insert_servers(insert_servers), do: Enum.each(insert_servers, &stop_insert_server/1)
 
   defp stop_insert_server(insert_server) do
     Agent.get(insert_server, &(&1.handler.terminate(&1.conn)), :timer.minutes(5))
   end
 
-  defp send_chunk(insert_server, {plain_data, encoded_data}) do
+  defp insert_chunks(insert_servers, chunks) do
+    chunks
+    |> Stream.zip(insert_servers)
+    |> Enum.each(fn({users, insert_server}) -> insert_users(insert_server, users) end)
+  end
+
+  defp insert_users(insert_server, {plain_data, encoded_data}) do
     # The get followed by cast ensures simple buffering. We don't immediately wait for the outcome of the insertion,
     # which allows us to prepare the next chunk while the insertion is running. Then with this dummy sync lookup,
     # we make sure we don't send the next chunk until the current one has been processed.
