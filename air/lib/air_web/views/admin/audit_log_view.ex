869c598708a96831ab08e77103834feff73aef49
Paweł Obrok <pawel.obrok@gmail.com>
2018-02-28 13:03:21 +0100

    Submit audit log time filter

--- air/lib/air_web/views/admin/audit_log_view.ex
+++ air/lib/air_web/views/admin/audit_log_view.ex
@@ -43,4 +43,11 @@ defmodule AirWeb.Admin.AuditLogView do
 
   defp to(%Plug.Conn{query_params: query_params}), do:
     Map.get(query_params, "to", Timex.now() |> Timex.format!("{ISOdate} {ISOtime}"))
+
+  defp serialize_params(%Plug.Conn{query_params: query_params}) do
+    Enum.flat_map(query_params, fn
+      {name, values} when is_list(values) -> Enum.map(values, &{name <> "[]", &1})
+      other -> [other]
+    end)
+  end
 end
