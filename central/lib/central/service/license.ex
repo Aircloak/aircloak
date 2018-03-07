302e4ca09c6ea874b27ceb87056d6f327a075bb8
Paweł Obrok <pawel.obrok@gmail.com>
2018-03-07 16:03:19 +0100

    Add docs and specs

--- central/lib/central/service/license.ex
+++ central/lib/central/service/license.ex
@@ -1,8 +1,17 @@
 defmodule Central.Service.License do
-  alias Central.{Repo, Schemas.License}
+  @moduledoc "Service module for working with licenses."
+
+  alias Central.{Repo, Schemas.License, Schemas.Customer}
   alias Ecto.Changeset
   import Ecto.Query
 
+
+  # -------------------------------------------------------------------
+  # API
+  # -------------------------------------------------------------------
+
+  @doc "Creates a new licenses with the given params."
+  @spec create(Customer.t, map) :: {:ok, License.t} | {:error, Changeset.t}
   def create(customer, params), do:
     customer
     |> Ecto.build_assoc(:licenses)
@@ -10,18 +19,26 @@ defmodule Central.Service.License do
     |> License.changeset(params)
     |> Repo.insert()
 
+  @doc "Updates the given licenses with the given params."
+  @spec update(License.t, map) :: {:ok, License.t} | {:error, Changeset.t}
   def update(license, params), do:
     license
     |> License.changeset(params)
     |> Repo.update()
 
+  @doc "Returns all licenses for the given customer."
+  @spec for_customer(Customer.t) :: [License.t]
   def for_customer(customer), do:
     License
     |> for_customer_id(customer.id)
     |> Repo.all()
 
+  @doc "Returns a changeset representing no changes to the given license."
+  @spec empty_changeset(License.t) :: Changeset.t
   def empty_changeset(license \\ %License{}), do: License.changeset(license)
 
+  @doc "Returns an encrypted representation of this license."
+  @spec export(License.t) :: String.t
   def export(license), do:
     %{
       id: license.id,
@@ -31,6 +48,8 @@ defmodule Central.Service.License do
     |> Poison.encode!()
     |> encrypt!()
 
+  @doc "Returns the license with the given id for the given customer."
+  @spec get(Customer.t, any) :: {:ok, License.t} | :not_found
   def get(customer, id) do
     License
     |> for_customer_id(customer.id)
@@ -41,15 +60,34 @@ defmodule Central.Service.License do
     end
   end
 
+  @doc """
+  Returns the time when the given license expires. Note that for auto_renew licenses this is merely the time
+  before which it will have to be renewed.
+  """
+  @spec expires_at(License.t) :: Timex.Types.valid_datetime() | Timex.AmbiguousDateTime.t() | {:error, term()}
   def expires_at(license), do:
     license |> base_time() |> Timex.shift(days: license.length_in_days)
 
+  @doc "Revokes the given license. Revoked licenses keep all their attributes but are treated as not-auto-renew."
+  @spec revoke(License.t) :: {:ok, License.t} | {:error, Changeset.t}
   def revoke(license), do: __MODULE__.update(license, %{revoked: true})
 
+  @doc "Restores the given revoked license."
+  @spec restore(License.t) :: {:ok, License.t} | {:error, Changeset.t}
   def restore(license), do: __MODULE__.update(license, %{revoked: false})
+
+
+  # -------------------------------------------------------------------
+  # Scopes
+  # -------------------------------------------------------------------
 
   defp for_customer_id(query, customer_id), do:
     where(query, [q], q.customer_id == ^customer_id)
+
+
+  # -------------------------------------------------------------------
+  # Exporting
+  # -------------------------------------------------------------------
 
   defp base_time(%{revoked: true, inserted_at: inserted_at}), do: inserted_at
   defp base_time(%{auto_renew: true}), do: Timex.now()
