6c8ee2df753dbf4c85a10a724e66638a7ee2230a
Paweł Obrok <pawel.obrok@gmail.com>
2018-03-09 14:25:06 +0100

    Accept uploaded license files

--- air/lib/air/service/license.ex
+++ air/lib/air/service/license.ex
@@ -1,3 +1,5 @@
 defmodule Air.Service.License do
   def valid?(), do: false
+
+  def load(text), do: :error
 end
--- air/lib/air_web/controllers/admin/license_controller.ex
+++ air/lib/air_web/controllers/admin/license_controller.ex
@@ -2,6 +2,8 @@ defmodule AirWeb.Admin.LicenseController do
   @moduledoc false
   use Air.Web, :admin_controller
 
+  alias Air.Service.License
+
 
   # -------------------------------------------------------------------
   # AirWeb.VerifyPermissions callback
@@ -23,11 +25,33 @@ defmodule AirWeb.Admin.LicenseController do
     render(conn, "edit.html")
   end
 
+  def update(conn, %{"license" => %{"text" => %Plug.Upload{path: path}}}) do
+    with {:ok, text} <- File.read(path) do
+      case License.load(text) do
+        :ok -> redirect_to_edit(conn, :info, "License uploaded")
+        :error -> redirect_to_edit(conn, :error, "Could not verify license file")
+      end
+    else
+      _ -> redirect_to_edit(conn, :error, "Unknown error. Please try again.")
+    end
+  end
+
   def invalid(conn, _params) do
     if Air.Schemas.User.admin?(conn.assigns.current_user) do
       redirect(conn, to: admin_license_path(conn, :edit))
     else
       render(conn, "invalid.html", layout: false)
     end
+  end
+
+
+  # -------------------------------------------------------------------
+  # Actions
+  # -------------------------------------------------------------------
+
+  def redirect_to_edit(conn, flash_level, flash) do
+    conn
+    |> put_flash(flash_level, flash)
+    |> redirect(to: admin_license_path(conn, :edit))
   end
 end
