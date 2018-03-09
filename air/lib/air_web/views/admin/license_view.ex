e996b684f4d41deb3dbc0146205197461cadfa24
Paweł Obrok <pawel.obrok@gmail.com>
2018-03-09 12:43:52 +0100

    Display info on license page when license invalid

--- air/lib/air_web/views/admin/license_view.ex
+++ air/lib/air_web/views/admin/license_view.ex
@@ -1,4 +1,10 @@
 defmodule AirWeb.Admin.LicenseView do
   @moduledoc false
   use Air.Web, :view
+
+  alias Air.Service.License
+
+  def license_valid?(), do: License.valid?()
+
+  def license_expiry(), do: Liense.expiry()
 end
