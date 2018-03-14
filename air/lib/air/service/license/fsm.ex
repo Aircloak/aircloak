8b6e8ff59bc938335dfc19a7ea4665008368261c
Paweł Obrok <pawel.obrok@gmail.com>
2018-03-14 12:33:35 +0100

    Add docs and specs

--- air/lib/air/service/license/fsm.ex
+++ air/lib/air/service/license/fsm.ex
@@ -1,6 +1,17 @@
 defmodule Air.Service.License.FSM do
+  @moduledoc """
+  Represents the license state of the system. It starts out with no license (system setup). Later license texts can be
+  submitted to update that to a valid license.
+  """
+
+  @type t :: :no_license | Aircloak.License.t
+
+  @doc "Returns the initial state representing no license."
+  @spec initial() :: t
   def initial(), do: :no_license
 
+  @doc "Tries to load the given license text using the given public key for decryption."
+  @spec load(t, ExPublicKey.RSAPublicKey.t, String.t) :: t
   def load(state, public_key, encrypted_license) do
     case Aircloak.License.decrypt(public_key, encrypted_license) do
       {:ok, license} -> {:ok, Map.put(license, :text, encrypted_license)}
@@ -8,11 +19,17 @@ defmodule Air.Service.License.FSM do
     end
   end
 
+  @doc "Returns true if the given state represents a loaded license (either valid or expired), false otherwise."
+  @spec present?(t) :: boolean
   def present?(:no_license), do: false
   def present?(_), do: true
 
+  @doc "Returns true if the given state represents a valid license, false otherwise."
+  @spec valid?(t) :: boolean
   def valid?(state), do: Timex.diff(expiry(state), Timex.now()) > 0
 
+  @doc "Returns the expiry time for the given state."
+  @spec expiry(t) :: DateTime.t
   def expiry(:no_license), do: Timex.now() |> Timex.shift(years: -1)
   def expiry(state), do: state.expires_at
 
@@ -22,6 +39,8 @@ defmodule Air.Service.License.FSM do
   def license_id(:no_license), do: nil
   def license_id(state), do: state.license_id
 
+  @doc "Returns the original text of the license before decryption."
+  @spec text(t) :: String.t
   def text(:no_license), do: ""
   def text(state), do: state.text
 end
