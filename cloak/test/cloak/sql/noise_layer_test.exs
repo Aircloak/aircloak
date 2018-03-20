a5dab8f39a660f1a71af45628ddf41f585394ad9
Cristian Berneanu <cristian@aircloak.com>
2018-03-20 12:18:47 +0200

    More tests for the NoiseLayer module

--- cloak/test/cloak/sql/noise_layer_test.exs
+++ cloak/test/cloak/sql/noise_layer_test.exs
@@ -3,30 +3,82 @@ defmodule Cloak.Sql.NoiseLayer.Test do
 
   alias Cloak.Sql.{NoiseLayer, Expression}
 
+  defp sum(accumulator), do:
+    Enum.map(accumulator, &NoiseLayer.sum(&1, "salt"))
+
+  defp new_accumulator(layers, rows) do
+    processed_layers = NoiseLayer.pre_process_layers(layers)
+    Enum.reduce(rows, NoiseLayer.new_accumulator(layers), &NoiseLayer.accumulate(processed_layers, &2, &1))
+  end
+
   test "accumulate changes results of evaluating expressions" do
     layers = [
       NoiseLayer.new("alice", [%Expression{row_index: 1}, %Expression{row_index: 2}]),
       NoiseLayer.new("bob", [%Expression{row_index: 0}])
     ]
-    processed_layers = NoiseLayer.pre_process_layers(layers)
 
-    accumulator1 = NoiseLayer.new_accumulator(layers)
-    accumulator2 = NoiseLayer.accumulate(processed_layers, NoiseLayer.new_accumulator(layers), ["value1", true, 3.4])
+    accumulator1 = new_accumulator(layers, [])
+    accumulator2 = new_accumulator(layers, [["value1", true, 3.4]])
 
-    assert Enum.map(accumulator1, &NoiseLayer.sum(&1, "salt")) != Enum.map(accumulator2, &NoiseLayer.sum(&1, "salt"))
+    assert sum(accumulator1) != sum(accumulator2)
   end
 
-  test "merging of accumulators" do
+  test "acc() + acc(data) = acc(data)" do
     layers = [
       NoiseLayer.new("alice", [%Expression{row_index: 1}, %Expression{row_index: 2}]),
       NoiseLayer.new("bob", [%Expression{row_index: 0}])
     ]
-    processed_layers = NoiseLayer.pre_process_layers(layers)
 
-    accumulator1 = NoiseLayer.new_accumulator(layers)
-    accumulator2 = NoiseLayer.accumulate(processed_layers, NoiseLayer.new_accumulator(layers), ["value1", true, 3.4])
+    accumulator1 = new_accumulator(layers, [])
+    accumulator2 = new_accumulator(layers, [["value1", true, 3.4]])
     accumulator3 = NoiseLayer.merge_accumulators(accumulator1, accumulator2)
 
-    assert Enum.map(accumulator3, &NoiseLayer.sum(&1, "salt")) == Enum.map(accumulator2, &NoiseLayer.sum(&1, "salt"))
+    assert sum(accumulator3) == sum(accumulator2)
+  end
+
+  test "acc(data1) + acc(data2) = acc(data1 + data2)" do
+    layers = [NoiseLayer.new("xxx", [%Expression{row_index: 0}, %Expression{row_index: 2}])]
+
+    accumulator1 = new_accumulator(layers, [["value1", false, 0]])
+    accumulator2 = new_accumulator(layers, [["value2", true, 3.4]])
+    accumulator3 = new_accumulator(layers, [["value1", false, 0], ["value2", true, 3.4]])
+
+    assert sum(NoiseLayer.merge_accumulators(accumulator1, accumulator2)) == sum(accumulator3)
+  end
+
+  test "acc(data1 + data2) = acc(data2 + data1)" do
+    layers = [NoiseLayer.new("xxx", [%Expression{row_index: 0}, %Expression{row_index: 2}])]
+
+    accumulator1 = new_accumulator(layers, [["value1", false, 0], ["value2", true, 3.4]])
+    accumulator2 = new_accumulator(layers, [["value2", true, 3.4], ["value1", false, 0]])
+
+    assert sum(accumulator1) == sum(accumulator2)
+  end
+
+  test "acc(data1) != acc(data2)" do
+    layers = [NoiseLayer.new("xxx", [%Expression{row_index: 0}, %Expression{row_index: 2}])]
+
+    accumulator1 = new_accumulator(layers, [["value1", true, 3.4]])
+    accumulator2 = new_accumulator(layers, [["value2", true, 3.4]])
+
+    assert sum(accumulator1) != sum(accumulator2)
+  end
+
+  test "acc(data) == acc(data)" do
+    layers = [NoiseLayer.new("xxx", [%Expression{row_index: 0}, %Expression{row_index: 2}])]
+
+    accumulator1 = new_accumulator(layers, [["value", true, 3.4]])
+    accumulator2 = new_accumulator(layers, [["value", false, 3.4]])
+
+    assert sum(accumulator1) == sum(accumulator2)
+  end
+
+  test "acc(data1 + data1) = acc(data1)" do
+    layers = [NoiseLayer.new("xxx", [%Expression{row_index: 0}, %Expression{row_index: 2}])]
+
+    accumulator1 = new_accumulator(layers, [["value1", false, 0], ["value1", true, 0]])
+    accumulator2 = new_accumulator(layers, [["value1", false, 0]])
+
+    assert sum(accumulator1) == sum(accumulator2)
   end
 end
