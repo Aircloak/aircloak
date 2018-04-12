56793f6a7a9e2e9c14a206134528bb1b91c84f87
Sebastian Probst Eide <sebastian@aircloak.com>
2018-04-12 16:05:58 +0200

    Add support for updating privacy policy

--- air/lib/air/service/privacy_policy.ex
+++ air/lib/air/service/privacy_policy.ex
@@ -10,4 +10,14 @@ defmodule Air.Service.PrivacyPolicy do
   @doc "Returns true if a privacy policy has been set"
   @spec exists?() :: boolean
   def exists?(), do: Repo.aggregate(PrivacyPolicy, :count, :id) > 0
+
+  @doc "Records the rivacy policy. Should be used to create as well as edit the privacy policy as each edit consists of creating a new version"
+  @spec set(String.t()) :: :ok
+  def set(content) do
+    %Air.Schemas.PrivacyPolicy{}
+    |> Air.Schemas.PrivacyPolicy.changeset(%{content: content})
+    |> Repo.insert!()
+
+    :ok
+  end
 end
--- air/test/air/service/privacy_policy_test.exs
+++ air/test/air/service/privacy_policy_test.exs
@@ -12,4 +12,25 @@ defmodule Air.Service.PrivacyPolicyTest do
       assert PrivacyPolicy.exists?()
     end
   end
+
+  describe "set" do
+    test "creates when none existed" do
+      assert privacy_policy_count() == 0
+      PrivacyPolicy.set("content")
+      assert privacy_policy_count() == 1
+    end
+
+    test "altering content results in creation of new policy" do
+      create_privacy_policy!()
+      count_before = privacy_policy_count()
+      PrivacyPolicy.set("new content")
+      assert privacy_policy_count() == count_before + 1
+    end
+  end
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp privacy_policy_count(), do: Repo.aggregate(Air.Schemas.PrivacyPolicy, :count, :id)
 end
