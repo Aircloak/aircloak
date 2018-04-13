ec46ab4f59e522c7ce09561be6c4b757595be0ff
Sebastian Probst Eide <sebastian@aircloak.com>
2018-04-13 16:46:20 +0200

    Show information about changes if they have been provided

--- air/lib/air_web/views/privacy_policy_view.ex
+++ air/lib/air_web/views/privacy_policy_view.ex
@@ -17,6 +17,20 @@ defmodule AirWeb.PrivacyPolicyView do
     end
   end
 
+  defp has_change_info?() do
+    case PrivacyPolicy.get() do
+      {:error, :no_privacy_policy_created} -> false
+      {:ok, privacy_policy} -> privacy_policy.changes != ""
+    end
+  end
+
+  defp changes_as_html() do
+    case PrivacyPolicy.get() do
+      {:error, :no_privacy_policy_created} -> ""
+      {:ok, privacy_policy} -> Earmark.as_html!(privacy_policy.changes)
+    end
+  end
+
   defp has_opted_in?(conn), do: User.privacy_policy_status(conn.assigns.current_user) == :ok
 
   defp never_accepted_or_rejected?(conn), do: conn.assigns.current_user.accepted_privacy_policy_id == nil
