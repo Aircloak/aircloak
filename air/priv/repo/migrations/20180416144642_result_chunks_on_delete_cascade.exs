00460fe372712126f55a6fb16bdfd7a37b7af354
Sebastian Probst Eide <sebastian@aircloak.com>
2018-04-20 09:51:18 +0200

    Add missing down migrations
    I couldn't run tests without them, as `make test` performs a database
    recreate which fails without them. Not sure how this passed CI!

--- air/priv/repo/migrations/20180416144642_result_chunks_on_delete_cascade.exs
+++ air/priv/repo/migrations/20180416144642_result_chunks_on_delete_cascade.exs
@@ -8,4 +8,12 @@ defmodule Air.Repo.Migrations.ResultChunksOnDeleteCascade do
       modify(:query_id, references(:queries, type: :uuid, on_delete: :delete_all))
     end
   end
+
+  def down do
+    execute("ALTER TABLE result_chunks DROP CONSTRAINT result_chunks_query_id_fkey")
+
+    alter table(:result_chunks) do
+      modify(:query_id, references(:queries, type: :uuid))
+    end
+  end
 end
