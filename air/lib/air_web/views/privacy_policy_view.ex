48ae37d8ee87a3f8abb52da58d2115cab31c50c7
Sebastian Probst Eide <sebastian@aircloak.com>
2018-04-20 11:20:01 +0200

    Improve markdown rendering options

--- air/lib/air_web/views/privacy_policy_view.ex
+++ air/lib/air_web/views/privacy_policy_view.ex
@@ -4,19 +4,13 @@ defmodule AirWeb.PrivacyPolicyView do
 
   alias Air.Service.User
 
-  def content_as_html(privacy_policy),
-    do:
-      privacy_policy.content
-      |> Earmark.as_html!()
-      |> Aircloak.HTMLScrubber.scrub()
+  def content_as_html(privacy_policy), do: md_to_safe_html(privacy_policy.content)
 
   def has_change_info?(privacy_policy), do: not is_nil(privacy_policy.changes)
 
   def changes_as_html(privacy_policy) do
     if has_change_info?(privacy_policy) do
-      privacy_policy.changes
-      |> Earmark.as_html!()
-      |> Aircloak.HTMLScrubber.scrub()
+      md_to_safe_html(privacy_policy.changes)
     else
       nil
     end
@@ -25,4 +19,10 @@ defmodule AirWeb.PrivacyPolicyView do
   defp has_opted_in?(conn), do: User.privacy_policy_status(conn.assigns.current_user) == :ok
 
   defp never_accepted_or_rejected?(conn), do: conn.assigns.current_user.accepted_privacy_policy_id == nil
+
+  defp md_to_safe_html(text) do
+    text
+    |> Earmark.as_html!(%Earmark.Options{gfm: true, breaks: true, smartypants: true})
+    |> Aircloak.HTMLScrubber.scrub()
+  end
 end
