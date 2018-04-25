181f62842743413e8816abf0435499e49a3882ef
Sasa Juric <sasa.juric@gmail.com>
2018-04-25 17:03:29 +0200

    add cloak container initialization

--- system_test/system.sh
+++ system_test/system.sh
@@ -12,8 +12,12 @@ NETWORK_NAME="system_test"
 
 function start_system {
   docker network create --driver bridge $NETWORK_NAME > /dev/null
+
   start_air_db
   start_air_container
+  start_cloak_dbs
+  start_cloak_container
+
   remove_old_git_head_image_tags "aircloak"
 }
 
@@ -40,7 +44,7 @@ function start_air_container {
     aircloak/air:latest
 
   echo "waiting for air to start ..."
-  container_script air wait_for_service localhost:8080
+  wait_for_app air
 
   echo "configuring air"
   erlang_eval air "
@@ -53,6 +57,34 @@ function start_air_container {
 
     {ok, License} = file:read_file(code:priv_dir(air) ++ \"/config/license.lic\"),
     ok = 'Elixir.Air.Service.License':load(License)
+  "
+}
+
+function start_cloak_dbs {
+  start_system_container cloak_postgres \
+    --tmpfs=/ramdisk:rw,size=1G -e PGDATA=/ramdisk \
+    postgres:9.4 > /dev/null
+}
+
+function start_cloak_container {
+  if [ "$SKIP_BUILD" != "true" ]; then PREVENT_OLD_IMAGE_REMOVAL=true cloak/build-image.sh; fi
+
+  # note: we're making a temp copy of config, because we're recreating config files after generating compliance data
+  mkdir -p $(pwd)/system_test/tmp/
+  if [ -d $(pwd)/system_test/tmp/cloak_config ]; then rm -rf $(pwd)/system_test/tmp/cloak_config; fi
+  cp -rp $(pwd)/system_test/cloak_config $(pwd)/system_test/tmp/
+
+  start_system_container cloak \
+    -v $(pwd)/system_test/tmp/cloak_config:/runtime_config \
+    aircloak/cloak:latest
+
+  echo "waiting for cloak to start ..."
+  wait_for_app cloak
+
+  echo "populating database ..."
+  erlang_eval cloak "
+    'Elixir.Compliance':initialize(<<\"config\">>, 10, 1),
+    'Elixir.Compliance':regenerate_config_from_db(<<\"config\">>)
   "
 }
 
@@ -77,6 +109,18 @@ function erlang_eval {
   docker exec "system_test_$container" /aircloak/$container/bin/$container eval $@
 }
 
+function wait_for_app {
+  local app=$1
+  while [ "$(docker exec system_test_$app /aircloak/$app/bin/$app ping)" != "pong" ]; do sleep 1; done
+  until $(app_running $app); do sleep 1; done
+}
+
+function app_running {
+  local app=$1
+  local running=$(erlang_eval $app "[App || {App, _, _} <- application:which_applications(), App =:= $app]")
+  if [ "$running" == "[]" ]; then return 1; else return 0; fi
+}
+
 case "$1" in
   start)
     start_system
@@ -84,6 +128,11 @@ case "$1" in
 
   stop)
     stop_system
+    ;;
+
+  restart)
+    stop_system
+    start_system
     ;;
 
   *)
