ab9314899f06b1090e37958bba831ceb8476b40b
Paweł Obrok <pawel.obrok@gmail.com>
2018-04-26 15:36:39 +0200

    Load auto renewal information from license

--- common/elixir/lib/license.ex
+++ common/elixir/lib/license.ex
@@ -24,10 +24,16 @@ defmodule Aircloak.License do
     |> Enum.find(:error, & &1)
   end
 
-  defp unpack(%{"customer_id" => customer_id, "id" => license_id, "expires_at" => expires_at}) do
+  defp unpack(data = %{"customer_id" => customer_id, "id" => license_id, "expires_at" => expires_at}) do
     case Timex.parse(expires_at, "{ISO:Basic}") do
       {:ok, expires_at} ->
-        {:ok, %{customer_id: customer_id, license_id: license_id, expires_at: expires_at}}
+        {:ok,
+         %{
+           customer_id: customer_id,
+           license_id: license_id,
+           expires_at: expires_at,
+           auto_renew: data["auto_renew"] || false
+         }}
 
       _ ->
         :error
