a06d1711398ed168e3f941f9ef6c02ad56a2ab3e
Sasa Juric <sasa.juric@gmail.com>
2018-05-04 14:44:21 +0200

    support native functions

--- cloak/lib/cloak/data_source/sql_builder/sap_iq.ex
+++ cloak/lib/cloak/data_source/sql_builder/sap_iq.ex
@@ -9,9 +9,29 @@ defmodule Cloak.DataSource.SqlBuilder.SAPIQ do
   alias Cloak.DataSource.SqlBuilder.Dialect
 
   @impl Dialect
-  def supported_functions(), do: ~w()
+  def supported_functions(), do: ~w(
+      count sum min max avg stddev count_distinct sum_distinct min_distinct max_distinct avg_distinct
+      year quarter month day hour minute second weekday
+      sqrt floor ceil abs round trunc mod div ^ % * / + -
+      length lower upper btrim/1 ltrim/1 rtrim/1 left right substring concat
+      cast coalesce
+    )
 
   @impl Dialect
+  def function_sql("stddev", [arg]), do: ["STDDEV_SAMP(", arg, ")"]
+  def function_sql("weekday", [arg]), do: ["DOW(", arg, ")"]
+  def function_sql("%", args), do: function_sql("mod", args)
+  def function_sql("^", args), do: function_sql("power", args)
+  def function_sql("btrim", [arg1]), do: function_sql("ltrim", [function_sql("rtrim", [arg1])])
+  def function_sql("concat", [arg1, arg2]), do: function_sql("+", [arg1, arg2])
+
+  def function_sql("/", [arg1, arg2]),
+    do: ["(", cast_sql(arg1, nil, :decimal), " / ", cast_sql(arg2, nil, :decimal), ")"]
+
+  for binary_operator <- ~w(+ - *) do
+    def function_sql(unquote(binary_operator), [arg1, arg2]), do: ["(", arg1, unquote(binary_operator), arg2, ")"]
+  end
+
   def function_sql(name, args), do: [String.upcase(name), "(", Enum.intersperse(args, ", "), ")"]
 
   @impl Dialect
