182fa5a3bd8ada36389221f2e06bb093ba81a8fd
Sasa Juric <sasa.juric@gmail.com>
2018-05-07 16:16:39 +0200

    mount sap odbc drivers to aircloak cloaks

--- cloak/production.sh
+++ cloak/production.sh
@@ -6,4 +6,5 @@ cd $(dirname $0)
 . ../docker/production_helper.sh
 
 RUNTIME_CONFIG_PATH="/opt/share/cloak_runtime_configs/$DEPLOYMENT_NAME/"
+DOCKER_ARGS="-v /opt/share/cloak_odbc_drivers/:/odbc_drivers"
 run_production_command cloak cloak "${DEPLOYMENT_NAME}_cloak" $@
