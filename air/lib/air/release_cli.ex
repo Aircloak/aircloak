d74d768775a650e18e2f1b32f6c38ff9a71e6b71
Paweł Obrok <pawel.obrok@gmail.com>
2018-05-08 15:04:53 +0200

    Add a CLI interface for resetting password

---
+++ air/lib/air/release_cli.ex
@@ -0,0 +1,22 @@
+defmodule ReleaseCLI do
+  @moduledoc "Contains functions exposed to the system administrator via CLI commands."
+
+  alias Air.{Repo, Schemas, Service}
+
+  @doc "Prints a password reset token for the user with the given email."
+  @spec reset_password(String.t()) :: :ok
+  def reset_password(email) do
+    Schemas.User
+    |> Repo.get_by(email: email)
+    |> case do
+      nil ->
+        IO.puts("A user with that email does not exist.")
+
+      user ->
+        token = Service.User.reset_password_token(user)
+        IO.puts("Use the following token in the `Forgot password` form:\n\n#{token}\n")
+    end
+
+    :ok
+  end
+end
