3bd6754500b0ad140d8a97e0ad10fdb95f29f63a
Sasa Juric <sasa.juric@gmail.com>
2018-05-14 15:41:10 +0200

    exclude stddev compliance for sapiq

--- cloak/test/compliance/aggregate_functions_test.exs
+++ cloak/test/compliance/aggregate_functions_test.exs
@@ -25,7 +25,7 @@ Enum.each(
   fn {aggregate, allowed_in_subquery} ->
     defmodule Module.concat([Compliance.AggregateFunctions, String.to_atom(aggregate), Test]) do
       use ComplianceCase, async: true
-      alias Cloak.DataSource.{MongoDB, MySQL, SQLServer, SQLServerRODBC}
+      alias Cloak.DataSource.{MongoDB, MySQL, SQLServer, SQLServerRODBC, SAPIQ}
 
       @moduletag :"#{aggregate}"
       @integer_columns for {column, _table, _user_id} <- integer_columns(), do: column
@@ -40,6 +40,7 @@ Enum.each(
             |> disable_for(SQLServer, match?("stddev(" <> _, unquote(aggregate)))
             |> disable_for(SQLServerRODBC, match?("stddev(" <> _, unquote(aggregate)))
             |> disable_for(MySQL, match?("stddev(" <> _, unquote(aggregate)))
+            |> disable_for(SAPIQ, match?("stddev(" <> _, unquote(aggregate)))
             |> assert_consistent_and_not_failing("""
               SELECT
                 aggregate
