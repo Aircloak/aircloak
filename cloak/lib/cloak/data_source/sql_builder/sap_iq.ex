de699adbd47d5d941d0d10f44dde74e4697478a6
Sasa Juric <sasa.juric@gmail.com>
2018-05-14 14:57:14 +0200

    fix trunc for sapiq

--- cloak/lib/cloak/data_source/sql_builder/sap_iq.ex
+++ cloak/lib/cloak/data_source/sql_builder/sap_iq.ex
@@ -26,6 +26,8 @@ defmodule Cloak.DataSource.SqlBuilder.SAPIQ do
   def function_sql("^", args), do: function_sql("power", args)
   def function_sql("btrim", [arg1]), do: function_sql("ltrim", [function_sql("rtrim", [arg1])])
   def function_sql("concat", [arg1, arg2]), do: function_sql("+", [arg1, arg2])
+  def function_sql("trunc", [arg]), do: ["TRUNCNUM(", arg, ", 0)"]
+  def function_sql("trunc", [arg1, arg2]), do: ["TRUNCNUM(", arg1, ", ", arg2, ")"]
 
   def function_sql("/", [arg1, arg2]),
     do: ["(", cast_sql(arg1, nil, :decimal), " / ", cast_sql(arg2, nil, :decimal), ")"]
