b6e905b0489390821eb5d86716bf3797f3094cb3
Sasa Juric <sasa.juric@gmail.com>
2018-05-17 09:30:08 +0200

    revert sapiq to erlang odbc

--- cloak/lib/cloak/data_source/sap_iq.ex
+++ cloak/lib/cloak/data_source/sap_iq.ex
@@ -4,9 +4,8 @@ defmodule Cloak.DataSource.SAPIQ do
   For more information, see `DataSource`.
   """
 
+  alias Cloak.DataSource.ODBC
   use Cloak.DataSource.Driver.SQL
-  alias Cloak.DataSource
-  alias Cloak.DataSource.{RODBC, Table}
 
   # -------------------------------------------------------------------
   # API functions
@@ -34,47 +33,26 @@ defmodule Cloak.DataSource.SAPIQ do
   def sql_dialect_module(_), do: Cloak.DataSource.SqlBuilder.SAPIQ
 
   @impl Driver
-  def connect!(parameters) do
-    unless File.exists?(driver_path()), do: DataSource.raise_error("ODBC driver for SAP IQ is not mounted.")
-    RODBC.connect!(parameters, &conn_params/1)
-  end
+  def connect!(parameters), do: ODBC.connect!(parameters, &conn_params/1)
 
   @impl Driver
-  defdelegate disconnect(connection), to: RODBC
+  defdelegate disconnect(connection), to: ODBC
 
   @impl Driver
-  def load_tables(connection, table) do
-    statement = "SELECT cname, coltype FROM  sys.syscolumns WHERE tname='#{table.db_name}' ORDER BY colno DESC"
-
-    row_mapper = fn [name, type_name] -> Table.column(name, parse_type(type_name)) end
-
-    case RODBC.Driver.execute(connection, statement) do
-      :ok ->
-        case RODBC.Driver.fetch_all(connection, row_mapper) do
-          {:ok, []} -> DataSource.raise_error("Table `#{table.db_name}` does not exist")
-          {:ok, columns} -> [%{table | columns: Enum.to_list(columns), db_name: ~s/"#{table.db_name}"/}]
-          {:error, reason} -> DataSource.raise_error("`#{to_string(reason)}`")
-        end
-
-      {:error, reason} ->
-        DataSource.raise_error("`#{to_string(reason)}`")
-    end
-  end
+  def load_tables(connection, table), do: ODBC.load_tables(connection, update_in(table.db_name, &~s/"#{&1}"/))
 
   @impl Driver
-  defdelegate select(connection, sql_query, result_processor), to: RODBC
+  defdelegate select(connection, sql_query, result_processor), to: ODBC
 
   @impl Driver
-  defdelegate driver_info(connection), to: RODBC
+  defdelegate driver_info(connection), to: ODBC
 
   @impl Driver
-  defdelegate supports_connection_sharing?(), to: RODBC
+  defdelegate supports_connection_sharing?(), to: ODBC
 
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
-
-  defp driver_path(), do: Path.join(~w(#{Application.app_dir(:cloak)} priv odbc drivers sapiq lib64 libdbodbc.so))
 
   defp conn_params(normalized_parameters) do
     %{
@@ -85,26 +63,4 @@ defmodule Cloak.DataSource.SAPIQ do
       DSN: "SAPIQ"
     }
   end
-
-  defp parse_type("varchar"), do: :text
-  defp parse_type("nvarchar"), do: :text
-  defp parse_type("text"), do: :text
-  defp parse_type("varbinary"), do: :text
-  defp parse_type("binary"), do: :text
-  defp parse_type("bit"), do: :boolean
-  defp parse_type("bigint"), do: :integer
-  defp parse_type("integer"), do: :integer
-  defp parse_type("smallint"), do: :integer
-  defp parse_type("tinyint"), do: :integer
-  defp parse_type("float"), do: :real
-  defp parse_type("double"), do: :real
-  defp parse_type("numeric"), do: :real
-  defp parse_type("decimal"), do: :real
-  defp parse_type("real"), do: :real
-  defp parse_type("time"), do: :time
-  defp parse_type("date"), do: :date
-  defp parse_type("smalldatetime"), do: :datetime
-  defp parse_type("datetime"), do: :datetime
-  defp parse_type("timestamp"), do: :datetime
-  defp parse_type(type), do: {:unsupported, type}
 end
