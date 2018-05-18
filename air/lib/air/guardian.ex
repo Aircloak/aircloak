afc9406bae1bdf762e800da80fb1297332602a96
Paweł Obrok <pawel.obrok@gmail.com>
2018-05-18 17:20:46 +0200

    Tweak docs

--- air/lib/air/guardian.ex
+++ air/lib/air/guardian.ex
@@ -1,13 +1,15 @@
 defmodule Air.Guardian do
-  @moduledoc "The API module for Guardian."
+  @moduledoc "Implementation module for Guardian."
   use Guardian, otp_app: :air
 
   import Ecto.Query, only: [from: 2]
   alias Air.{Repo, Schemas.User}
 
+  @doc false
   def subject_for_token(user = %User{}, _claims), do: {:ok, "User:#{user.id}"}
   def subject_for_token(_, _), do: {:error, "Unknown resource type"}
 
+  @doc false
   def resource_from_claims(%{"sub" => "User:" <> id}) do
     case Repo.one(from(user in User, where: user.id == ^id, preload: [:groups])) do
       nil -> {:error, "User doesn't exist"}
