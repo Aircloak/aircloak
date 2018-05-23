59d5fdbd3385a8032b10fd0cdfdb4bff60da5098
Sasa Juric <sasa.juric@gmail.com>
2018-05-23 12:14:14 +0200

    move seeding to dedicated module
    This will allow us to run the same seeding code from the test system.

---
+++ air/lib/air/repo/seeder.ex
@@ -0,0 +1,52 @@
+defmodule Air.Repo.Seeder do
+  @moduledoc "Used in development and tests to seed the database."
+
+  alias Air.Service.User
+
+  # -------------------------------------------------------------------
+  # API
+  # -------------------------------------------------------------------
+
+  @doc "Seeds the database with the development data."
+  @spec seed() :: :ok
+  def seed() do
+    create_admin!()
+    create_plain_user!()
+
+    :ok
+  end
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp admin_group() do
+    case User.admin_groups() do
+      [] -> User.create_group!(%{name: "admin", admin: true})
+      [group | _] -> group
+    end
+  end
+
+  defp create_admin!() do
+    {:ok, admin} =
+      %{
+        email: "admin@aircloak.com",
+        name: "Aircloak test administrator",
+        groups: [admin_group().id]
+      }
+      |> User.create!()
+      |> User.reset_password_token()
+      |> User.reset_password(%{password: "1234", password_confirmation: "1234"})
+
+    admin
+  end
+
+  defp create_plain_user!() do
+    User.create!(%{
+      email: "user@aircloak.com",
+      password: "1234",
+      password_confirmation: "1234",
+      name: "Test client regular user"
+    })
+  end
+end
