2c5a0767076c6607d3705219e528cd5147a95704
Paweł Obrok <pawel.obrok@gmail.com>
2018-05-29 17:06:53 +0200

    Update httpoison in BOM
    It conflicts with appsignal, and there shouldn't be very negative
    effects from updating 0.10 -> 0.11.

--- bom/mix.exs
+++ bom/mix.exs
@@ -36,7 +36,7 @@ defmodule Bom.Mixfile do
       {:poison, "~> 2.2.0", override: true},
       {:aircloak_common, path: "../common/elixir"},
       {:hackney, "1.6.3"},
-      {:httpoison, "0.10.0"}
+      {:httpoison, "0.11.0"}
     ]
   end
 
