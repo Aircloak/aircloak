9bbc0dbb3210c4d48ee572a20fcdd3023b9385a2
Sasa Juric <sasa.juric@gmail.com>
2018-06-05 08:53:16 +0200

    test cache prioritizing

--- cloak/test/cloak/data_source/isolators/cache_test.exs
+++ cloak/test/cloak/data_source/isolators/cache_test.exs
@@ -23,18 +23,39 @@ defmodule Cloak.DataSource.Isolators.Cache.Test do
     end)
   end
 
-  defp new_cache_provider(column_names) do
+  test "prioritizing on demand" do
+    known_columns = ~w(col1 col2 col3)
+
+    provider =
+      new_cache_provider(
+        known_columns,
+        compute_isolation_fun:
+          compute_isolation_fun(%{
+            "col1" => fn -> Process.sleep(200) end,
+            "col2" => fn -> Process.sleep(:infinity) end
+          })
+      )
+
+    {:ok, cache} = Cache.start_link(provider.cache_opts)
+    assert Cache.isolates_users?(cache, provider.data_source, provider.table_name, "col3") == {:isolated, "col3"}
+  end
+
+  defp new_cache_provider(column_names, opts \\ []) do
     data_source = %{name: inspect(make_ref())}
     table_name = inspect(make_ref())
     columns = Enum.map(column_names, &{data_source.name, table_name, &1})
 
     {:ok, provider} = Agent.start_link(fn -> columns end)
 
-    cache_opts = [
-      columns_provider: fn -> Agent.get(provider, & &1) end,
-      compute_isolation_fun: fn {_data_source_name, _table_name, column_name} -> {:isolated, column_name} end,
-      registered?: false
-    ]
+    cache_opts =
+      Keyword.merge(
+        [
+          columns_provider: columns_provider(provider),
+          compute_isolation_fun: compute_isolation_fun(),
+          registered?: false
+        ],
+        opts
+      )
 
     %{
       provider: provider,
@@ -42,5 +63,14 @@ defmodule Cloak.DataSource.Isolators.Cache.Test do
       data_source: data_source,
       table_name: table_name
     }
+  end
+
+  defp columns_provider(provider), do: fn -> Agent.get(provider, & &1) end
+
+  defp compute_isolation_fun(map \\ %{}) do
+    fn {_data_source_name, _table_name, column_name} ->
+      with {:ok, fun} <- Map.fetch(map, column_name), do: fun.()
+      {:isolated, column_name}
+    end
   end
 end
