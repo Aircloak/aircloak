505b0ee8dc6d7cc639a33ef226645656811e6002
Sebastian Probst Eide <sebastian@aircloak.com>
2018-06-07 19:23:09 +0200

    Make login screen happier
    I have been wanting something like this for a long time.
    It's so lovely how the microsoft office login screen and the
    bing search engine have these large nice background images.
    
    Finally I took some time out of my schedule to implement
    it, if nothing else, to satisfy my own little desire for
    some pretty pictures :)

--- air/lib/air_web/views/layout_view.ex
+++ air/lib/air_web/views/layout_view.ex
@@ -40,4 +40,49 @@ defmodule AirWeb.LayoutView do
       link, acc -> [acc, ", ", link]
     end)
   end
+
+  @background_images [
+    %{
+      photographer: "Kalen Emsley",
+      credit_url:
+        "https://unsplash.com/photos/_LuLiJc1cdo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Kinney Lake, Canada"
+    },
+    %{
+      photographer: "Victoria Chen",
+      credit_url:
+        "https://unsplash.com/photos/N6nnIx4C-Fo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Omaha, New Zealand"
+    },
+    %{
+      photographer: "Riccardo Chiarini",
+      credit_url:
+        "https://unsplash.com/photos/2VDa8bnLM8c?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Lake of Carezza, Carezza, Nova Levante, Italy"
+    },
+    %{
+      photographer: "Ivana Cajina",
+      credit_url:
+        "https://unsplash.com/photos/Tp6L7SrCgxU?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Road"
+    },
+    %{
+      photographer: "Aaron Burden",
+      credit_url:
+        "https://unsplash.com/photos/Glh5hc9KDaw?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Winter"
+    },
+    %{
+      photographer: "Ishan @seefromthesky",
+      credit_url:
+        "https://unsplash.com/photos/BMJWpck6eQA?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+      motive: "Vaavu Atoll, Maldives"
+    }
+  ]
+
+  defp background_image_num(), do: rem(NaiveDateTime.utc_now().day, 6)
+
+  defp background_img_info(), do: Enum.at(@background_images, background_image_num())
+
+  defp background_img_class(), do: "img#{background_image_num() + 1}"
 end
