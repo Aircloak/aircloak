e80b2720bcb070688a6663cab884ad40228a221f
Paweł Obrok <pawel.obrok@gmail.com>
2018-07-09 13:07:51 +0200

    Remove unused functions

--- air/lib/air_web/views/admin/audit_log_view.ex
+++ air/lib/air_web/views/admin/audit_log_view.ex
@@ -2,31 +2,6 @@ defmodule AirWeb.Admin.AuditLogView do
   @moduledoc false
   use Air.Web, :view
 
-  defp selected?(%Plug.Conn{query_params: query_params}, name, param), do: selected?(query_params, name, param)
-
-  defp selected?(query_params, name, param) do
-    Enum.any?(query_params[to_string(name)] || [], &(&1 == to_string(param)))
-  end
-
-  defp toggle_item(%Plug.Conn{query_params: query_params}, name, param) do
-    normalized_name = to_string(name)
-    normalized_param = to_string(param)
-    values_for_name = query_params[normalized_name] || []
-
-    values_for_name =
-      if selected?(query_params, normalized_name, normalized_param) do
-        values_for_name -- [normalized_param]
-      else
-        [normalized_param | values_for_name]
-      end
-
-    Map.put(query_params, normalized_name, values_for_name)
-  end
-
-  defp glyph(true), do: {:safe, "<span class='glyphicon glyphicon-remove' aria-hidden='true'></span> "}
-
-  defp glyph(false), do: {:safe, ""}
-
   defp audit_logs_to_json(audit_logs) do
     audit_logs
     |> Enum.map(fn audit_log ->
