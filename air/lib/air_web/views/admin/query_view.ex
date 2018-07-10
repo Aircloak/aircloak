8572c94b54a4a49a2b23441b9cfd03bb5d405dea
Paweł Obrok <pawel.obrok@gmail.com>
2018-07-10 15:42:18 +0200

    Remove dead code

--- air/lib/air_web/views/admin/query_view.ex
+++ air/lib/air_web/views/admin/query_view.ex
@@ -1,11 +1,4 @@
 defmodule AirWeb.Admin.QueryView do
   @moduledoc false
   use Air.Web, :view
-
-  defp time_ago(entry), do: Air.Utils.DateTime.time_ago(entry.inserted_at)
-
-  defp absolute_time(entry), do: Timex.format!(entry.inserted_at, "{ISOdate} {h24}:{m}:{s}")
-
-  defp data_source_name(%{data_source: nil}), do: ""
-  defp data_source_name(%{data_source: data_source}), do: data_source.name
 end
