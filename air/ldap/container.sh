3f487664355da3f2c03c816464483303aeaa2bee
Paweł Obrok <pawel.obrok@gmail.com>
2018-08-13 15:20:44 +0200

    Start separate LDAP for test and dev

--- air/ldap/container.sh
+++ air/ldap/container.sh
@@ -5,13 +5,22 @@ set -eo pipefail
 cd $(dirname $0)
 . ../../docker/docker_helper.sh
 
+if [ "$LDAP_ENV" == "test" ]; then
+  DOCKER_START_ARGS="
+    -e LDAP_TLS_VERIFY_CLIENT=allow
+    -p 389:389
+    -p 636:636
+  "
+elif [ "$LDAP_ENV" == "dev" ]; then
+  DOCKER_START_ARGS="
+    -e LDAP_TLS_VERIFY_CLIENT=allow
+    -p 1389:389
+    -p 1636:636
+  "
+fi
+
 DOCKER_IMAGE="aircloak/ldap"
 DOCKER_IMAGE_VERSION="latest"
-DOCKER_START_ARGS="
-  -e LDAP_TLS_VERIFY_CLIENT=allow
-  -p 389:389
-  -p 636:636
-"
-CONTAINER_NAME="aircloak_ldap"
+CONTAINER_NAME="aircloak_ldap_${LDAP_ENV}"
 
 container_ctl $@
