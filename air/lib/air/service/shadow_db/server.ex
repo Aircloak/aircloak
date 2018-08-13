6edd0ebe95e170bcf605c0bd4c46582d83e39675
Sasa Juric <sasa.juric@gmail.com>
2018-08-13 16:44:09 +0200

    stop dropping shadow databases
    Dropping databases won't work if there are other connections open, so instead, we're just recreating tables. In addition, we'll only recreate tables which have in fact changed. This will reduce the number of dropped tables, and in turn reduce the chance of a datasource update breaking currently running queries.

--- air/lib/air/service/shadow_db/server.ex
+++ air/lib/air/service/shadow_db/server.ex
@@ -20,7 +20,7 @@ defmodule Air.Service.ShadowDb.Server do
   def init(data_source_name) do
     Process.flag(:trap_exit, true)
     update_definition(self())
-    {:ok, %{data_source_name: data_source_name, tables: nil}}
+    {:ok, %{data_source_name: data_source_name, tables: []}}
   end
 
   @impl GenServer
@@ -33,7 +33,7 @@ defmodule Air.Service.ShadowDb.Server do
         tables = Air.Schemas.DataSource.tables(data_source)
 
         if Application.get_env(:air, :shadow_db?, true) and state.tables != tables,
-          do: update_shadow_db(state.data_source_name, tables)
+          do: update_shadow_db(state, tables)
 
         {:noreply, %{state | tables: tables}}
     end
@@ -43,30 +43,39 @@ defmodule Air.Service.ShadowDb.Server do
   # Internal functions
   # -------------------------------------------------------------------
 
-  defp update_shadow_db(data_source_name, tables) do
-    Logger.info("data source definition changed for #{data_source_name}, updating shadow database")
-    recreate_db!(data_source_name)
-    create_tables!(data_source_name, tables)
+  defp update_shadow_db(state, tables) do
+    Logger.info("data source definition changed for #{state.data_source_name}, updating shadow database")
+    ensure_db!(state.data_source_name)
+    create_tables!(state, tables)
   end
 
-  defp recreate_db!(data_source_name) do
+  defp ensure_db!(data_source_name) do
     {:ok, conn} = Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", database: "postgres")
 
     try do
-      Postgrex.query!(conn, ~s/DROP DATABASE IF EXISTS "#{db_name(data_source_name)}"/, [])
-      Postgrex.query!(conn, ~s/CREATE DATABASE "#{db_name(data_source_name)}"/, [])
+      if match?(
+           %Postgrex.Result{rows: [[0]]},
+           Postgrex.query!(conn, "SELECT count(*) FROM pg_database where datname = $1", [db_name(data_source_name)])
+         ) do
+        Postgrex.query!(conn, ~s/CREATE DATABASE "#{sanitize_name(db_name(data_source_name))}"/, [])
+      end
     after
       close_conn(conn)
     end
   end
 
-  defp create_tables!(data_source_name, tables) do
-    {:ok, conn} = Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", database: db_name(data_source_name))
+  defp create_tables!(state, tables) do
+    {:ok, conn} =
+      Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", database: db_name(state.data_source_name))
+
+    tables = Enum.reject(tables, &Enum.empty?(Map.get(&1, "columns", [])))
+
+    delete_obsolete_tables(conn, tables)
 
     try do
       tables
-      |> Stream.reject(&Enum.empty?(Map.get(&1, "columns", [])))
-      |> Stream.map(&{&1, Postgrex.query(conn, table_sql(&1), [])})
+      |> changed_tables(state)
+      |> Stream.map(&{&1, update_table_definition(conn, &1)})
       |> Stream.filter(&match?({_table, {:error, _}}, &1))
       |> Enum.each(&report_error/1)
     after
@@ -74,11 +83,36 @@ defmodule Air.Service.ShadowDb.Server do
     end
   end
 
+  defp delete_obsolete_tables(conn, tables) do
+    known_tables = Enum.map(tables, &Map.fetch!(&1, "id"))
+
+    conn
+    |> Postgrex.query!("SELECT table_name FROM information_schema.tables where table_schema=$1", ["public"])
+    |> Map.fetch!(:rows)
+    |> Stream.map(fn [table_name] -> table_name end)
+    |> Stream.reject(&Enum.member?(known_tables, &1))
+    |> Enum.each(&Postgrex.query(conn, ~s/DROP TABLE "#{sanitize_name(&1)}"/, []))
+  end
+
+  defp changed_tables(tables, state) do
+    tables
+    |> Stream.map(&{&1, Enum.find(state.tables, fn table -> Map.fetch!(table, "id") == Map.fetch!(&1, "id") end)})
+    |> Stream.reject(fn {table_def, prev_table_def} -> table_def == prev_table_def end)
+    |> Enum.map(fn {table_def, _prev_table_def} -> table_def end)
+  end
+
+  defp update_table_definition(conn, table) do
+    Postgrex.query(conn, ~s/DROP TABLE "#{sanitize_name(Map.fetch!(table, "id"))}"/, [])
+
+    Postgrex.query(
+      conn,
+      ~s/CREATE TABLE "#{sanitize_name(Map.fetch!(table, "id"))}" (#{columns_sql(Map.fetch!(table, "columns"))})/,
+      []
+    )
+  end
+
   defp report_error({table, {:error, error}}),
     do: Logger.error("error creating shadow table #{Map.fetch!(table, "id")}: #{inspect(error)}")
-
-  defp table_sql(table),
-    do: ~s/CREATE TABLE "#{sanitize_name(Map.fetch!(table, "id"))}" (#{columns_sql(Map.fetch!(table, "columns"))})/
 
   defp columns_sql(columns), do: columns |> Enum.map(&column_sql/1) |> Enum.join(", ")
 
@@ -94,7 +128,7 @@ defmodule Air.Service.ShadowDb.Server do
   defp type_sql("datetime"), do: "timestamp without time zone"
   defp type_sql("unknown"), do: "text"
 
-  def db_name(data_source_name), do: "aircloak_shadow_#{sanitize_name(data_source_name)}"
+  defp db_name(data_source_name), do: "aircloak_shadow_#{data_source_name}"
 
   defp sanitize_name(name), do: Regex.replace(~r/"/, name, ~s/""/)
 
