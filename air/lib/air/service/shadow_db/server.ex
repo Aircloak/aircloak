6840da273b5c8fe2bb69f80291e7a3c61ca8cc4c
Sasa Juric <sasa.juric@gmail.com>
2018-08-14 10:00:06 +0200

    normalize tables map for shadow db

--- air/lib/air/service/shadow_db/server.ex
+++ air/lib/air/service/shadow_db/server.ex
@@ -25,13 +25,11 @@ defmodule Air.Service.ShadowDb.Server do
 
   @impl GenServer
   def handle_cast(:update_definition, state) do
-    case Air.Service.DataSource.by_name(state.data_source_name) do
-      nil ->
+    case data_source_tables(state.data_source_name) do
+      [] ->
         {:stop, :normal, state}
 
-      data_source ->
-        tables = Air.Schemas.DataSource.tables(data_source)
-
+      tables ->
         if Application.get_env(:air, :shadow_db?, true) and state.tables != tables,
           do: update_shadow_db(state, tables)
 
@@ -42,6 +40,28 @@ defmodule Air.Service.ShadowDb.Server do
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
+
+  defp data_source_tables(data_source_name) do
+    case Air.Service.DataSource.by_name(data_source_name) do
+      nil -> []
+      data_source -> data_source |> Air.Schemas.DataSource.tables() |> normalize_tables()
+    end
+  end
+
+  defp normalize_tables(tables) do
+    tables
+    |> Stream.map(&normalize_table/1)
+    |> Enum.reject(&Enum.empty?(&1.columns))
+  end
+
+  defp normalize_table(table) do
+    %{
+      id: Map.fetch!(table, "id"),
+      columns: table |> Map.get("columns", []) |> Enum.map(&normalize_column/1)
+    }
+  end
+
+  defp normalize_column(%{"name" => name, "type" => type}), do: %{name: name, type: type}
 
   defp update_shadow_db(state, tables) do
     Logger.info("data source definition changed for #{state.data_source_name}, updating shadow database")
@@ -68,8 +88,6 @@ defmodule Air.Service.ShadowDb.Server do
     {:ok, conn} =
       Postgrex.start_link(hostname: "127.0.0.1", username: "postgres", database: db_name(state.data_source_name))
 
-    tables = Enum.reject(tables, &Enum.empty?(Map.get(&1, "columns", [])))
-
     try do
       delete_obsolete_tables(conn, tables)
 
@@ -84,7 +102,7 @@ defmodule Air.Service.ShadowDb.Server do
   end
 
   defp delete_obsolete_tables(conn, tables) do
-    known_tables = Enum.map(tables, &Map.fetch!(&1, "id"))
+    known_tables = Enum.map(tables, & &1.id)
 
     conn
     |> Postgrex.query!("SELECT table_name FROM information_schema.tables where table_schema=$1", ["public"])
@@ -96,28 +114,27 @@ defmodule Air.Service.ShadowDb.Server do
 
   defp changed_tables(tables, state) do
     tables
-    |> Stream.map(&{&1, Enum.find(state.tables, fn table -> Map.fetch!(table, "id") == Map.fetch!(&1, "id") end)})
+    |> Stream.map(&{&1, Enum.find(state.tables, fn table -> table.id == &1.id end)})
     |> Stream.reject(fn {table_def, prev_table_def} -> table_def == prev_table_def end)
     |> Enum.map(fn {table_def, _prev_table_def} -> table_def end)
   end
 
   defp update_table_definition(conn, table) do
-    Postgrex.query(conn, ~s/DROP TABLE "#{sanitize_name(Map.fetch!(table, "id"))}"/, [])
+    Postgrex.query(conn, ~s/DROP TABLE "#{sanitize_name(table.id)}"/, [])
 
     Postgrex.query(
       conn,
-      ~s/CREATE TABLE "#{sanitize_name(Map.fetch!(table, "id"))}" (#{columns_sql(Map.fetch!(table, "columns"))})/,
+      ~s/CREATE TABLE "#{sanitize_name(table.id)}" (#{columns_sql(table.columns)})/,
       []
     )
   end
 
   defp report_error({table, {:error, error}}),
-    do: Logger.error("error creating shadow table #{Map.fetch!(table, "id")}: #{inspect(error)}")
+    do: Logger.error("error creating shadow table #{table.id}: #{inspect(error)}")
 
   defp columns_sql(columns), do: columns |> Enum.map(&column_sql/1) |> Enum.join(", ")
 
-  defp column_sql(column),
-    do: ~s/"#{sanitize_name(Map.fetch!(column, "name"))}" #{type_sql(Map.fetch!(column, "type"))}/
+  defp column_sql(column), do: ~s/"#{sanitize_name(column.name)}" #{type_sql(column.type)}/
 
   defp type_sql("boolean"), do: "boolean"
   defp type_sql("real"), do: "real"
