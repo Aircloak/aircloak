04aaa991086ca5a05d2f6e6724df0852229d84c0
Paweł Obrok <pawel.obrok@gmail.com>
2018-08-22 17:51:17 +0200

    Use make test instead of mix test on CI
    It runs a bunch more tests, like testing JS and migrations. In fact the
    JS tests seem broken ATM, because they weren't running on CI.

--- air/ci/jobs.exs
+++ air/ci/jobs.exs
@@ -1,23 +1,26 @@
 test = fn
   :test ->
-    {:sequence, [
-      "MIX_ENV=test ./check_warnings.sh",
-      {:parallel, ["MIX_ENV=test mix lint", {:sequence, ["MIX_ENV=test mix recreate_db", "mix test"]}]}
-    ]}
+    {:sequence,
+     [
+       "MIX_ENV=test ./check_warnings.sh",
+       {:parallel, ["MIX_ENV=test mix lint", "make test"]}
+     ]}
 
   :dev ->
-    {:sequence, [
-      "MIX_ENV=dev ./check_warnings.sh",
-      {:parallel, [
-        "make docs",
-        "make eslint",
-        "make flow",
-        "mix lint",
-        "make check-format",
-        "mix bom --elixir deps --node assets/node_modules /tmp",
-        "MIX_HOME=_build make dialyze",
-      ]}
-    ]}
+    {:sequence,
+     [
+       "MIX_ENV=dev ./check_warnings.sh",
+       {:parallel,
+        [
+          "make docs",
+          "make eslint",
+          "make flow",
+          "mix lint",
+          "make check-format",
+          "mix bom --elixir deps --node assets/node_modules /tmp",
+          "MIX_HOME=_build make dialyze"
+        ]}
+     ]}
 
   :prod ->
     {:sequence, ["MIX_ENV=prod ./check_warnings.sh"]}
@@ -26,21 +29,24 @@ end
 # jobs map
 %{
   compile:
-    {:sequence, [
-      "make deps",
-      "mix compile",
-      {:parallel, [
-        {:sequence, [
-          "MIX_ENV=test mix compile",
-          "MIX_ENV=prod mix compile",
-        ]},
-        "MIX_HOME=_build mix dialyze --no-analyse",
-      ]}
-    ]},
-
+    {:sequence,
+     [
+       "make deps",
+       "mix compile",
+       {:parallel,
+        [
+          {:sequence,
+           [
+             "MIX_ENV=test mix compile",
+             "MIX_ENV=prod mix compile"
+           ]},
+          "MIX_HOME=_build mix dialyze --no-analyse"
+        ]}
+     ]},
   test:
-    {:sequence, [
-      "make deps",
-      {:parallel, [test.(:test), test.(:dev), test.(:prod)]}
-    ]},
+    {:sequence,
+     [
+       "make deps",
+       {:parallel, [test.(:test), test.(:dev), test.(:prod)]}
+     ]}
 }
