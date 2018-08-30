98e892ad59f249a978a24be4abdee39adfa2c933
Sebastian Probst Eide <sebastian@aircloak.com>
2018-08-30 12:38:40 +0200

    Extract PRIV_DIR to variable in air start script

--- air/docker/start.sh
+++ air/docker/start.sh
@@ -13,6 +13,8 @@ su postgres -c "mkdir -p /var/run/postgresql/9.6-main.pg_stat_tmp"
 su postgres -c \
   "/usr/lib/postgresql/9.6/bin/pg_ctl -D /etc/postgresql/9.6/main -l /var/log/postgresql/postgresql.log start"
 
-mkdir -p /aircloak/air/lib/air-$VERSION/priv/config/
-cp -rp /runtime_config/* /aircloak/air/lib/air-$VERSION/priv/config/
+PRIV_DIR=/aircloak/air/lib/air-$VERSION/priv
+
+mkdir -p ${PRIV_DIR}/config
+cp -rp /runtime_config/* ${PRIV_DIR}/config/
 exec gosu deployer /aircloak/air/bin/air foreground
