335ba26c47e989170944b5d5056335ddfc8648e8
Sasa Juric <sasa.juric@gmail.com>
2018-08-30 08:33:32 +0200

    alias standard release task
    This change removes the custom `site_release` task, and aliases the standard `release` task, so we can build the site properly with just `mix release`.

--- air/lib/mix/tasks/release_assets.ex
+++ air/lib/mix/tasks/release_assets.ex
@@ -1,25 +1,15 @@
-defmodule Mix.Tasks.SiteRelease do
-  @shortdoc "Builds the full release, including static digests"
-  @moduledoc """
-  Builds the full release, including static digests.
-
-  Usage:
-
-  ```
-  mix site_release
-  ```
-  """
+defmodule Mix.Tasks.ReleaseAssets do
+  @shortdoc "Builds the assets for release."
+  @moduledoc "Builds the assets for release."
   use Mix.Task
 
   # Mix.Task behaviour is not in PLT since Mix is not a runtime dep, so we disable the warning
   @dialyzer :no_undefined_callbacks
 
   @impl Mix.Task
-  def run(args) do
+  def run(_args) do
     {:ok, cwd} = File.cwd()
     brunch_bin = Path.join([cwd, "assets/node_modules/brunch/bin/brunch"])
     {_, 0} = System.cmd(brunch_bin, ["build", "--production"], cd: "assets")
-    :ok = Mix.Task.run("phx.digest")
-    :ok = Mix.Task.run("release", args)
   end
 end
