9043329432c52f775a45ef1f3d84fde833de9461
Sasa Juric <sasa.juric@gmail.com>
2018-09-03 16:44:35 +0200

    support fine-grained release image build
    We'll use this feature from the CI server to separate docker build phases (which need to be executed exclusively) from the docker run phase (which can run concurrently to other docker builds and runs).

--- air/build-image.sh
+++ air/build-image.sh
@@ -2,81 +2,118 @@
 
 set -eo pipefail
 
-# build from the top-level folder of the project
 ROOT_DIR=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
-cd $ROOT_DIR
+. $ROOT_DIR/docker/docker_helper.sh
 
-. docker/docker_helper.sh
+function system_version {
+  cat $ROOT_DIR/VERSION
+}
 
-current_version=$(cat VERSION)
+function build_builder_image {
+  cd $ROOT_DIR
 
-if [ "$BUILD_BASE" != "false" ]; then PREVENT_OLD_IMAGE_REMOVAL=true common/docker/phoenix/build-image.sh; fi
+  if [ "$BUILD_BASE" != "false" ]; then PREVENT_OLD_IMAGE_REMOVAL=true common/docker/phoenix/build-image.sh; fi
 
-# build the builder image
-SYSTEM_VERSION=$current_version \
-  PREVENT_OLD_IMAGE_REMOVAL=true build_aircloak_image air_build air/builder.dockerfile air/.dockerignore-builder
+  SYSTEM_VERSION=$(system_version) \
+    PREVENT_OLD_IMAGE_REMOVAL=true build_aircloak_image air_build air/builder.dockerfile air/.dockerignore-builder
+}
 
-# build the release
-docker run --rm -i \
-  -v $(pwd)/VERSION:/aircloak/VERSION \
-  -v $(pwd)/common:/aircloak/common \
-  -v $(pwd)/air:/aircloak/air \
-  -v $(pwd)/bom:/aircloak/bom \
-  -v $(pwd)/cloak:/aircloak/cloak \
-  -v $(pwd)/$(air_cache_folder)/_build:/aircloak/air/_build \
-  -v $(pwd)/$(air_cache_folder)/deps:/aircloak/air/deps \
-  -v $(pwd)/$(air_cache_folder)/assets/node_modules:/aircloak/air/assets/node_modules \
-  -v $(pwd)/$(air_cache_folder)/docs/node_modules:/aircloak/air/docs/node_modules \
-  -v $(pwd)/$(air_cache_folder)/.gitbook:/root/.gitbook \
-  -v $(pwd)/$(air_cache_folder)/cloak/deps:/aircloak/cloak/deps \
-  -v $(pwd)/$(air_cache_folder)/bom/deps:/aircloak/bom/deps \
-  -v $(pwd)/$(air_cache_folder)/bom/_build:/aircloak/bom/_build \
-  $(aircloak_image_name air_build):$(git_head_image_tag) \
-  /bin/bash -c '
-    set -eo pipefail
-    . ~/.asdf/asdf.sh
+function build_release {
+  cd $ROOT_DIR
 
-    cd /aircloak/air
-    ./fetch_deps.sh --only prod
+  docker run --rm -i \
+    -v $(pwd)/VERSION:/aircloak/VERSION \
+    -v $(pwd)/common:/aircloak/common \
+    -v $(pwd)/air:/aircloak/air \
+    -v $(pwd)/bom:/aircloak/bom \
+    -v $(pwd)/cloak:/aircloak/cloak \
+    -v $(pwd)/$(air_cache_folder)/_build:/aircloak/air/_build \
+    -v $(pwd)/$(air_cache_folder)/deps:/aircloak/air/deps \
+    -v $(pwd)/$(air_cache_folder)/assets/node_modules:/aircloak/air/assets/node_modules \
+    -v $(pwd)/$(air_cache_folder)/docs/node_modules:/aircloak/air/docs/node_modules \
+    -v $(pwd)/$(air_cache_folder)/.gitbook:/root/.gitbook \
+    -v $(pwd)/$(air_cache_folder)/cloak/deps:/aircloak/cloak/deps \
+    -v $(pwd)/$(air_cache_folder)/bom/deps:/aircloak/bom/deps \
+    -v $(pwd)/$(air_cache_folder)/bom/_build:/aircloak/bom/_build \
+    $(aircloak_image_name air_build):$(git_head_image_tag) \
+    /bin/bash -c '
+      set -eo pipefail
+      . ~/.asdf/asdf.sh
 
-    cd assets
-    yarn install
-    cd ..
+      cd /aircloak/air
+      ./fetch_deps.sh --only prod
 
-    cd docs
-    yarn install
-    gitbook_version=$(cat /aircloak/air/docs/node_modules/gitbook/package.json | jq -r ".version")
-    if [ ! -d /root/.gitbook/versions/$gitbook_version ]; then yarn run gitbook fetch $gitbook_version; fi
-    cd ..
+      cd assets
+      yarn install
+      cd ..
 
-    cd /aircloak/cloak
-    ./fetch_deps.sh --only prod
+      cd docs
+      yarn install
+      gitbook_version=$(cat /aircloak/air/docs/node_modules/gitbook/package.json | jq -r ".version")
+      if [ ! -d /root/.gitbook/versions/$gitbook_version ]; then yarn run gitbook fetch $gitbook_version; fi
+      cd ..
 
-    cd /aircloak/bom
-    ./fetch_deps.sh
+      cd /aircloak/cloak
+      ./fetch_deps.sh --only prod
 
-    mix bom \
-      --elixir /aircloak/air/deps \
-      --elixir /aircloak/cloak/deps \
-      --rust /aircloak/cloak/src/rodbc \
-      --node /aircloak/air/assets/node_modules \
-      /aircloak/air/priv
+      cd /aircloak/bom
+      ./fetch_deps.sh
 
-    cd /aircloak/air
-    make release
-  '
+      mix bom \
+        --elixir /aircloak/air/deps \
+        --elixir /aircloak/cloak/deps \
+        --rust /aircloak/cloak/src/rodbc \
+        --node /aircloak/air/assets/node_modules \
+        /aircloak/air/priv
 
-# copy the generated release to the artifacts folder
-mkdir -p air/artifacts/rel
-rm -rf air/artifacts/rel/*
-cp -rp ./$(air_cache_folder)/_build/prod/rel/air/releases/$current_version/air.tar.gz air/artifacts/rel
-cd air/artifacts/rel
-tar -xzf air.tar.gz
-rm air.tar.gz
+      cd /aircloak/air
+      make release
+    '
+}
 
-# build the release image
-cd $ROOT_DIR
-SYSTEM_VERSION=$current_version \
-  PREVENT_OLD_IMAGE_REMOVAL=true build_aircloak_image air air/release.dockerfile air/.dockerignore-release
+function build_release_image {
+  # copy the generated release to the artifacts folder
+  cd $ROOT_DIR
+  mkdir -p air/artifacts/rel
+  rm -rf air/artifacts/rel/*
+  cp -rp ./$(air_cache_folder)/_build/prod/rel/air/releases/$(system_version)/air.tar.gz air/artifacts/rel
+  cd air/artifacts/rel
+  tar -xzf air.tar.gz
+  rm air.tar.gz
 
-remove_old_git_head_image_tags "aircloak" > /dev/null 2>&1
+  # build the release image
+  cd $ROOT_DIR
+  SYSTEM_VERSION=$(system_version) \
+    PREVENT_OLD_IMAGE_REMOVAL=true build_aircloak_image air air/release.dockerfile air/.dockerignore-release
+
+  remove_old_git_head_image_tags "aircloak" > /dev/null 2>&1
+}
+
+function build_all {
+  build_builder_image
+  build_release
+  build_release_image
+}
+
+case ${1:-all} in
+	all)
+    build_all
+    ;;
+
+  builder_image)
+    build_builder_image
+    ;;
+
+  release)
+    build_release
+    ;;
+
+  release_image)
+    build_release_image
+    ;;
+
+  *)
+    echo $0": unrecognized option:" $1
+    exit 1
+	  ;;
+esac
--- cloak/build-image.sh
+++ cloak/build-image.sh
@@ -1,50 +1,84 @@
 #!/bin/bash
-
 set -eo pipefail
 
-# build from the top-level folder of the project
 ROOT_DIR=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+. $ROOT_DIR/docker/docker_helper.sh
 
-cd $ROOT_DIR/cloak
-make odbc_drivers
+function system_version {
+  cat $ROOT_DIR/VERSION
+}
 
-cd $ROOT_DIR
+function build_builder_image {
+  cd $ROOT_DIR
+  if [ "$BUILD_BASE" != "false" ]; then PREVENT_OLD_IMAGE_REMOVAL=true common/docker/rust/build-image.sh; fi
+}
 
-. docker/docker_helper.sh
+function build_release {
+  cd $ROOT_DIR/cloak
+  make odbc_drivers
+  cd $ROOT_DIR
 
-current_version=$(cat VERSION)
+  # build the release
+  docker run --rm -i \
+    -v $(pwd)/VERSION:/aircloak/VERSION \
+    -v $(pwd)/common:/aircloak/common \
+    -v $(pwd)/cloak:/aircloak/cloak \
+    -v $(pwd)/$(cloak_cache_folder)/deps:/aircloak/cloak/deps \
+    -v $(pwd)/$(cloak_cache_folder)/_build:/aircloak/cloak/_build \
+    -v $(pwd)/$(cloak_cache_folder)/.cargo:/root/.cargo \
+    $(aircloak_image_name rust):$(git_head_image_tag) \
+    /bin/bash -c '
+      set -eo pipefail
+      . ~/.asdf/asdf.sh
+      cd /aircloak/cloak
+      MIX_ENV=prod ./fetch_deps.sh --only prod
+      make release
+    '
+}
 
-# build the builder image
-if [ "$BUILD_BASE" != "false" ]; then PREVENT_OLD_IMAGE_REMOVAL=true common/docker/rust/build-image.sh; fi
+function build_release_image {
+  # copy the generated release to the artifacts folder
+  cd $ROOT_DIR
+  mkdir -p cloak/artifacts/rel
+  rm -rf cloak/artifacts/rel/*
+  cp -rp ./$(cloak_cache_folder)/_build/prod/rel/cloak/releases/$(system_version)/cloak.tar.gz cloak/artifacts/rel
+  cd cloak/artifacts/rel
+  tar -xzf cloak.tar.gz
+  rm cloak.tar.gz
 
-# build the release
-docker run --rm -i \
-  -v $(pwd)/VERSION:/aircloak/VERSION \
-  -v $(pwd)/common:/aircloak/common \
-  -v $(pwd)/cloak:/aircloak/cloak \
-  -v $(pwd)/$(cloak_cache_folder)/deps:/aircloak/cloak/deps \
-  -v $(pwd)/$(cloak_cache_folder)/_build:/aircloak/cloak/_build \
-  -v $(pwd)/$(cloak_cache_folder)/.cargo:/root/.cargo \
-  $(aircloak_image_name rust):$(git_head_image_tag) \
-  /bin/bash -c '
-    set -eo pipefail
-    . ~/.asdf/asdf.sh
-    cd /aircloak/cloak
-    MIX_ENV=prod ./fetch_deps.sh --only prod
-    make release
-  '
+  # build the release image
+  cd $ROOT_DIR
+  SYSTEM_VERSION=$(system_version) PREVENT_OLD_IMAGE_REMOVAL=true \
+    build_aircloak_image cloak cloak/docker/release.dockerfile cloak/docker/.dockerignore-release
 
-# copy the generated release to the artifacts folder
-mkdir -p cloak/artifacts/rel
-rm -rf cloak/artifacts/rel/*
-cp -rp ./$(cloak_cache_folder)/_build/prod/rel/cloak/releases/$current_version/cloak.tar.gz cloak/artifacts/rel
-cd cloak/artifacts/rel
-tar -xzf cloak.tar.gz
-rm cloak.tar.gz
+  remove_old_git_head_image_tags "aircloak"
+}
 
-# build the release image
-cd $ROOT_DIR
-SYSTEM_VERSION=$current_version PREVENT_OLD_IMAGE_REMOVAL=true \
-  build_aircloak_image cloak cloak/docker/release.dockerfile cloak/docker/.dockerignore-release
+function build_all {
+  build_builder_image
+  build_release
+  build_release_image
+}
 
-remove_old_git_head_image_tags "aircloak"
+case ${1:-all} in
+	all)
+    build_all
+    ;;
+
+  builder_image)
+    build_builder_image
+    ;;
+
+  release)
+    build_release
+    ;;
+
+  release_image)
+    build_release_image
+    ;;
+
+  *)
+    echo $0": unrecognized option:" $1
+    exit 1
+	  ;;
+esac
