e76bc226b9295297b629a541811437d20afa349e
Sasa Juric <sasa.juric@gmail.com>
2018-09-06 10:04:20 +0200

    format code

--- bom/config/.credo.exs
+++ bom/config/.credo.exs
@@ -4,35 +4,39 @@
       name: "default",
       files: %{
         included:
-          case Mix.env do
+          case Mix.env() do
             :dev -> ["lib/", "src/"]
             :test -> ["test/"]
           end,
         excluded: []
       },
-      checks: [
-        {Credo.Check.Consistency.ExceptionNames},
-        {Credo.Check.Consistency.SpaceInParentheses},
-        {Credo.Check.Consistency.SpaceAroundOperators},
-        {Credo.Check.Consistency.TabsOrSpaces},
-        {Credo.Check.Readability.PredicateFunctionNames},
-        {Credo.Check.Readability.TrailingBlankLine},
-        {Credo.Check.Readability.TrailingWhiteSpace},
-        {Credo.Check.Readability.MaxLineLength, max_length: 120},
-        {Credo.Check.Readability.VariableNames},
-        {Credo.Check.Warning.IExPry},
-        {Credo.Check.Warning.IoInspect},
-        {Credo.Check.Readability.RedundantBlankLines, max_blank_lines: 2},
-        {Credo.Check.Readability.ParenthesesOnZeroArityDefs, false},
-        {Credo.Check.Consistency.ParameterPatternMatching, false},
-        {Credo.Check.Readability.Semicolons, false},
-      ] ++ case Mix.env do
-        :dev ->
-          [
-            {Credo.Check.Readability.ModuleDoc}
-          ]
-        :test -> []
-      end
+      checks:
+        [
+          {Credo.Check.Consistency.ExceptionNames},
+          {Credo.Check.Consistency.SpaceInParentheses},
+          {Credo.Check.Consistency.SpaceAroundOperators},
+          {Credo.Check.Consistency.TabsOrSpaces},
+          {Credo.Check.Readability.PredicateFunctionNames},
+          {Credo.Check.Readability.TrailingBlankLine},
+          {Credo.Check.Readability.TrailingWhiteSpace},
+          {Credo.Check.Readability.MaxLineLength, max_length: 120},
+          {Credo.Check.Readability.VariableNames},
+          {Credo.Check.Warning.IExPry},
+          {Credo.Check.Warning.IoInspect},
+          {Credo.Check.Readability.RedundantBlankLines, max_blank_lines: 2},
+          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, false},
+          {Credo.Check.Consistency.ParameterPatternMatching, false},
+          {Credo.Check.Readability.Semicolons, false}
+        ] ++
+          case Mix.env() do
+            :dev ->
+              [
+                {Credo.Check.Readability.ModuleDoc}
+              ]
+
+            :test ->
+              []
+          end
     }
   ]
 }
