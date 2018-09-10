93ed2f02db0db8a3101aec366e2fc0b5c52a0f6d
Cristian Berneanu <cristian@aircloak.com>
2018-09-10 13:35:52 +0300

    Use common method for loading tables for RODBC drivers

--- cloak/lib/cloak/data_source/sap_hana_rodbc.ex
+++ cloak/lib/cloak/data_source/sap_hana_rodbc.ex
@@ -4,7 +4,7 @@ defmodule Cloak.DataSource.SAPHanaRODBC do
   For more information, see `DataSource`.
   """
 
-  alias Cloak.DataSource.{Table, RODBC}
+  alias Cloak.DataSource.RODBC
   alias Cloak.DataSource
 
   use Cloak.DataSource.Driver.SQL
@@ -28,31 +28,7 @@ defmodule Cloak.DataSource.SAPHanaRODBC do
   defdelegate disconnect(connection), to: RODBC
 
   @impl Driver
-  def load_tables(connection, table) do
-    statement =
-      "SELECT column_name, lower(data_type_name) FROM table_columns " <>
-        "WHERE table_name = '#{table.db_name}' AND schema_name in (SELECT CURRENT_SCHEMA FROM DUMMY) " <>
-        "ORDER BY position DESC"
-
-    row_mapper = fn [name, type_name] -> Table.column(name, parse_type(type_name)) end
-
-    case RODBC.Driver.execute(connection, statement) do
-      :ok ->
-        case RODBC.Driver.fetch_all(connection, row_mapper) do
-          {:ok, []} ->
-            DataSource.raise_error("Table `#{table.db_name}` does not exist")
-
-          {:ok, columns} ->
-            [%{table | columns: Enum.to_list(columns), db_name: ~s/"#{table.db_name}"/}]
-
-          {:error, reason} ->
-            DataSource.raise_error("`#{to_string(reason)}`")
-        end
-
-      {:error, reason} ->
-        DataSource.raise_error("`#{to_string(reason)}`")
-    end
-  end
+  def load_tables(connection, table), do: RODBC.load_tables(connection, update_in(table.db_name, &"\"#{&1}\""))
 
   @impl Driver
   defdelegate select(connection, sql_query, result_processor), to: RODBC
@@ -80,28 +56,4 @@ defmodule Cloak.DataSource.SAPHanaRODBC do
 
   defp schema_option(nil), do: %{}
   defp schema_option(schema), do: %{cs: ~s/"#{schema}"/}
-
-  defp parse_type("varchar"), do: :text
-  defp parse_type("nvarchar"), do: :text
-  defp parse_type("nclob"), do: :text
-  defp parse_type("clob"), do: :text
-  defp parse_type("blob"), do: :text
-  defp parse_type("varbinary"), do: :text
-  defp parse_type("binary"), do: :text
-  defp parse_type("alphanumeric"), do: :text
-  defp parse_type("boolean"), do: :boolean
-  defp parse_type("bigint"), do: :integer
-  defp parse_type("integer"), do: :integer
-  defp parse_type("smallint"), do: :integer
-  defp parse_type("tinyint"), do: :integer
-  defp parse_type("real"), do: :real
-  defp parse_type("double"), do: :real
-  defp parse_type("numeric"), do: :real
-  defp parse_type("decimal" <> _), do: :real
-  defp parse_type("smalldecimal"), do: :real
-  defp parse_type("time"), do: :time
-  defp parse_type("date"), do: :date
-  defp parse_type("seconddate"), do: :datetime
-  defp parse_type("timestamp"), do: :datetime
-  defp parse_type(type), do: {:unsupported, type}
 end
--- cloak/lib/cloak/data_source/sap_iq_rodbc.ex
+++ cloak/lib/cloak/data_source/sap_iq_rodbc.ex
@@ -6,7 +6,7 @@ defmodule Cloak.DataSource.SAPIQRODBC do
 
   use Cloak.DataSource.Driver.SQL
   alias Cloak.DataSource
-  alias Cloak.DataSource.{RODBC, Table}
+  alias Cloak.DataSource.RODBC
 
   # -------------------------------------------------------------------
   # API functions
@@ -43,23 +43,7 @@ defmodule Cloak.DataSource.SAPIQRODBC do
   defdelegate disconnect(connection), to: RODBC
 
   @impl Driver
-  def load_tables(connection, table) do
-    statement = "SELECT cname, coltype FROM  sys.syscolumns WHERE tname='#{table.db_name}' ORDER BY colno DESC"
-
-    row_mapper = fn [name, type_name] -> Table.column(name, parse_type(type_name)) end
-
-    case RODBC.Driver.execute(connection, statement) do
-      :ok ->
-        case RODBC.Driver.fetch_all(connection, row_mapper) do
-          {:ok, []} -> DataSource.raise_error("Table `#{table.db_name}` does not exist")
-          {:ok, columns} -> [%{table | columns: Enum.to_list(columns), db_name: ~s/"#{table.db_name}"/}]
-          {:error, reason} -> DataSource.raise_error("`#{to_string(reason)}`")
-        end
-
-      {:error, reason} ->
-        DataSource.raise_error("`#{to_string(reason)}`")
-    end
-  end
+  def load_tables(connection, table), do: RODBC.load_tables(connection, update_in(table.db_name, &"\"#{&1}\""))
 
   @impl Driver
   defdelegate select(connection, sql_query, result_processor), to: RODBC
@@ -88,26 +72,4 @@ defmodule Cloak.DataSource.SAPIQRODBC do
       DSN: "SAPIQ"
     }
   end
-
-  defp parse_type("varchar"), do: :text
-  defp parse_type("nvarchar"), do: :text
-  defp parse_type("text"), do: :text
-  defp parse_type("varbinary"), do: :text
-  defp parse_type("binary"), do: :text
-  defp parse_type("bit"), do: :boolean
-  defp parse_type("bigint"), do: :integer
-  defp parse_type("integer"), do: :integer
-  defp parse_type("smallint"), do: :integer
-  defp parse_type("tinyint"), do: :integer
-  defp parse_type("float"), do: :real
-  defp parse_type("double"), do: :real
-  defp parse_type("numeric"), do: :real
-  defp parse_type("decimal"), do: :real
-  defp parse_type("real"), do: :real
-  defp parse_type("time"), do: :time
-  defp parse_type("date"), do: :date
-  defp parse_type("smalldatetime"), do: :datetime
-  defp parse_type("datetime"), do: :datetime
-  defp parse_type("timestamp"), do: :datetime
-  defp parse_type(type), do: {:unsupported, type}
 end
