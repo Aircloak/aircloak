02f86871fd1717025fc4da9f908991b512f1c36a
Paweł Obrok <pawel.obrok@gmail.com>
2018-09-11 16:17:32 +0200

    Add docs and specs

--- cloak/lib/compliance/query_generator/generation.ex
+++ cloak/lib/compliance/query_generator/generation.ex
@@ -23,6 +23,11 @@ defmodule Cloak.Compliance.QueryGenerator.Generation do
     end
   end
 
+  @doc """
+  Take values from the generator until one not matching the predicate is found. Raises an error if no such value is
+  found after `attempts` attempts.
+  """
+  @spec reject((() -> x), (x -> boolean), non_neg_integer) :: x when x: var
   def reject(generator, predicate, attempts \\ 100)
   def reject(_generator, _predicate, 0), do: raise("Filter too narrow")
 
@@ -36,10 +41,17 @@ defmodule Cloak.Compliance.QueryGenerator.Generation do
     end
   end
 
+  @doc "Same as `many1/2` but the list can have 0 elements as well."
+  @spec many(non_neg_integer, (non_neg_integer -> x)) :: [x] when x: var
   def many(complexity, generator) do
     if boolean(), do: many1(div(complexity, 2), generator), else: []
   end
 
+  @doc """
+  Generate a list of 1 or more values taken from the generator. The list will be longer if the complexity is larger. The
+  generator will be passed a reduced complexity. The reduction is proportional to the length of the generated list.
+  """
+  @spec many1(non_neg_integer, (non_neg_integer -> x)) :: [x] when x: var
   def many1(complexity, generator) do
     length = log_integer(complexity)
 
@@ -48,12 +60,12 @@ defmodule Cloak.Compliance.QueryGenerator.Generation do
     end
   end
 
-  def log_integer(complexity) do
-    complexity |> max(1) |> :math.log2() |> trunc() |> uniform()
-  end
-
+  @doc "Generate a random boolean."
+  @spec boolean() :: boolean
   def boolean(), do: Enum.random([true, false])
 
+  @doc "Generate a random integer. The integer will be drawn from the range of 1..max. It will be 1 if max is 0."
+  @spec uniform(non_neg_integer) :: pos_integer
   def uniform(max) when max < 1, do: 1
   def uniform(max), do: :rand.uniform(max)
 
@@ -70,4 +82,8 @@ defmodule Cloak.Compliance.QueryGenerator.Generation do
 
   defp pick_option([{frequency, option} | _], number) when number < frequency, do: option.()
   defp pick_option([{frequency, _} | options], number), do: pick_option(options, number - frequency)
+
+  defp log_integer(complexity) do
+    complexity |> max(1) |> :math.log2() |> trunc() |> uniform()
+  end
 end
