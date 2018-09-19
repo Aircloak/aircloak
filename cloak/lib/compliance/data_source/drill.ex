54b6e55a85082894df1c292a03d72f2d3951c038
Paweł Obrok <pawel.obrok@gmail.com>
2018-09-19 16:13:08 +0200

    Cast datetimes in drill compliance

--- cloak/lib/compliance/data_source/drill.ex
+++ cloak/lib/compliance/data_source/drill.ex
@@ -7,23 +7,25 @@ defmodule Compliance.DataSource.Drill do
 
   use Compliance.DataSource.Connector
   alias Compliance.DataSource.Connector
+  alias Cloak.DataSource.Drill
 
   @impl Connector
   def setup(options = %{parameters: params}) do
     Application.ensure_all_started(:httpoison)
+    Application.ensure_all_started(:odbc)
 
     port = Map.get(params, :management_port, 8047)
     Connector.await_port(params.hostname, port)
     configure_storage_plugin(params.hostname, port, params.workspace, params.drill_data_dir)
 
-    params
+    options
   end
 
   @impl Connector
-  def connect(state), do: state
+  def connect(state), do: Map.put(state, :conn, Drill.connect!(state.parameters))
 
   @impl Connector
-  def create_table(table_name, columns, state), do: state
+  def create_table(_table_name, _columns, state), do: state
 
   @impl Connector
   def after_tables_created(state), do: state
@@ -32,18 +34,52 @@ defmodule Compliance.DataSource.Drill do
   def insert_rows(table_name, data, state) do
     jsonized =
       data
+      |> Enum.map(&format_datetimes/1)
       |> Enum.map(&Poison.encode!/1)
       |> Enum.join("\n")
 
     state.parameters.data_dir
-    |> Path.join(table_name)
+    |> Path.join("#{table_name}_raw")
     |> File.write!(jsonized)
+
+    create_view(table_name, data, state.conn)
 
     state
   end
 
   @impl Connector
-  def terminate(state), do: :ok
+  def terminate(state), do: :odbc.disconnect(state.conn)
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp format_datetimes(data) do
+    update_in(data, [Lens.map_values() |> Lens.filter(&match?(%NaiveDateTime{}, &1))], &to_string/1)
+  end
+
+  defp create_view(table_name, [item | _data], conn) do
+    run_query("DROP VIEW IF EXISTS #{table_name}", conn)
+    run_query("CREATE VIEW #{table_name} AS SELECT #{view_select_list(item)} FROM #{table_name}_raw", conn)
+  end
+
+  defp view_select_list(item) do
+    item
+    |> Enum.map(fn
+      {field, %Date{}} -> "CAST(`#{field}` AS date) AS `#{field}`"
+      {field, %Time{}} -> "CAST(`#{field}` AS time) AS `#{field}`"
+      {field, %NaiveDateTime{}} -> "CAST(`#{field}` AS timestamp) AS `#{field}`"
+      {field, _} -> "`#{field}`"
+    end)
+    |> Enum.join(", ")
+  end
+
+  defp run_query(query, conn) do
+    case :odbc.sql_query(conn, to_charlist(query)) do
+      {:error, reason} -> raise to_string(reason)
+      _ -> :ok
+    end
+  end
 
   defp configure_storage_plugin(drill_hostname, drill_port, workspace, drill_data_dir) do
     config =
