2772264abe35f15914192182e56f8b26384781ec
Paweł Obrok <pawel.obrok@gmail.com>
2018-09-26 18:46:24 +0200

    Implement shadow lookup for simple expressions

---
+++ cloak/lib/cloak/data_source/shadows/lookup.ex
@@ -0,0 +1,18 @@
+defmodule Cloak.DataSource.Shadows.Lookup do
+  alias Cloak.Sql.{Query, Expression}
+
+  def any?(expression, value, shadow) do
+    Enum.any?(shadow, fn candidate ->
+      evaluate(expression, candidate) == value
+    end)
+  end
+
+  defp evaluate(expression, candidate) do
+    expression
+    |> put_in(
+      [Query.Lenses.leaf_expressions() |> Lens.reject(& &1.constant?)],
+      Expression.constant(:ignored, candidate)
+    )
+    |> Expression.const_value()
+  end
+end
