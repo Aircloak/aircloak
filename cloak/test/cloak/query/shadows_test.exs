7324d2cd3fa16892337c67cd0559380cda4ea45f
Paweł Obrok <pawel.obrok@gmail.com>
2018-10-04 13:53:27 +0200

    Refactor shadows_test

--- cloak/test/cloak/query/shadows_test.exs
+++ cloak/test/cloak/query/shadows_test.exs
@@ -24,9 +24,7 @@ defmodule Cloak.Query.Shadows.Test do
       :ok = insert_rows(_user_ids = 1..1, "query_shadows", ["value"], [1])
       :ok = insert_rows(_user_ids = 1..1, "query_shadows", ["value"], [2])
 
-      assert_query("SELECT COUNT(*) FROM query_shadows WHERE value <> 1 AND value <> 2 AND value <> 10", %{
-        rows: [%{row: [0]}]
-      })
+      assert_allowed("SELECT COUNT(*) FROM query_shadows WHERE value <> 1 AND value <> 2 AND value <> 10")
     end
 
     test "allows any number of <> conditions with popular values" do
@@ -34,16 +32,11 @@ defmodule Cloak.Query.Shadows.Test do
       :ok = insert_rows(_user_ids = 1..20, "query_shadows", ["value"], [20])
       :ok = insert_rows(_user_ids = 1..20, "query_shadows", ["value"], [30])
 
-      assert_query(
-        "SELECT COUNT(*) FROM query_shadows WHERE value <> 10 AND value <> 20 AND value <> 30",
-        %{rows: [%{row: [0]}]}
-      )
+      assert_allowed("SELECT COUNT(*) FROM query_shadows WHERE value <> 10 AND value <> 20 AND value <> 30")
     end
 
     test "forbids more than 2 <> conditions with rare values" do
-      assert_query("SELECT COUNT(*) FROM query_shadows WHERE value NOT IN (1, 2, 3)", %{error: error})
-
-      assert error =~ ~r/At most 2 negative conditions/
+      assert_forbidden("SELECT COUNT(*) FROM query_shadows WHERE value NOT IN (1, 2, 3)")
     end
   end
 
@@ -55,35 +48,24 @@ defmodule Cloak.Query.Shadows.Test do
     end
 
     test "allows up to 2 conditions with rare values" do
-      assert_query(
-        """
-          SELECT COUNT(*) FROM query_shadows
-          WHERE string NOT LIKE '%a%' AND string NOT LIKE '_b_' AND string NOT LIKE 'cc_'
-        """,
-        %{rows: [%{row: [0]}]}
-      )
+      assert_allowed("""
+        SELECT COUNT(*) FROM query_shadows
+        WHERE string NOT LIKE '%a%' AND string NOT LIKE '_b_' AND string NOT LIKE 'cc_'
+      """)
     end
 
     test "allows any number of conditions with popular values" do
-      assert_query(
-        """
-          SELECT COUNT(*) FROM query_shadows
-          WHERE string NOT LIKE '%a%' AND string NOT LIKE '_a_' AND string NOT LIKE 'aa_'
-        """,
-        %{rows: [%{row: [0]}]}
-      )
+      assert_allowed("""
+        SELECT COUNT(*) FROM query_shadows
+        WHERE string NOT LIKE '%a%' AND string NOT LIKE '_a_' AND string NOT LIKE 'aa_'
+      """)
     end
 
     test "forbids more than 2 conditions with rare values" do
-      assert_query(
-        """
-          SELECT COUNT(*) FROM query_shadows
-          WHERE string NOT LIKE '%b%' AND string NOT LIKE '_A_' AND string NOT LIKE 'cc_'
-        """,
-        %{error: error}
-      )
-
-      assert error =~ ~r/At most 2 negative conditions/
+      assert_forbidden("""
+        SELECT COUNT(*) FROM query_shadows
+        WHERE string NOT LIKE '%b%' AND string NOT LIKE '_A_' AND string NOT LIKE 'cc_'
+      """)
     end
   end
 
@@ -93,37 +75,34 @@ defmodule Cloak.Query.Shadows.Test do
       :ok = insert_rows(_user_ids = 1..1, "query_shadows", ["string"], ["bbb"])
       :ok = insert_rows(_user_ids = 1..1, "query_shadows", ["string"], ["ccc"])
 
-      assert_query(
-        """
-          SELECT COUNT(*) FROM query_shadows
-          WHERE string NOT ILIKE 'aAa' AND string NOT ILIKE 'AaA' AND string NOT ILIKE 'AAA'
-        """,
-        %{rows: [%{row: [0]}]}
-      )
+      assert_allowed("""
+        SELECT COUNT(*) FROM query_shadows
+        WHERE string NOT ILIKE 'aAa' AND string NOT ILIKE 'AaA' AND string NOT ILIKE 'AAA'
+      """)
     end
   end
 
   test "condition on column from subquery" do
-    assert_query(
-      """
-        SELECT COUNT(*) FROM (
-          SELECT user_id, value FROM query_shadows
-        ) foo
-        WHERE value NOT IN (1, 2, 3)
-      """,
-      %{error: error}
-    )
-
-    assert error =~ ~r/At most 2 negative conditions/
+    assert_forbidden("""
+      SELECT COUNT(*) FROM (
+        SELECT user_id, value FROM query_shadows
+      ) foo
+      WHERE value NOT IN (1, 2, 3)
+    """)
   end
 
   test "condition on expression" do
-    assert_query("SELECT COUNT(*) FROM query_shadows WHERE lower(string) NOT IN ('1', '2', '3')", %{error: error})
-
-    assert error =~ ~r/At most 2 negative conditions/
+    assert_forbidden("SELECT COUNT(*) FROM query_shadows WHERE lower(string) NOT IN ('1', '2', '3')")
   end
 
   test "conditions with constants are always safe" do
-    assert_query("SELECT COUNT(*) FROM query_shadows WHERE '1' NOT IN ('1', '2', '3')", %{rows: [%{row: [0]}]})
+    assert_allowed("SELECT COUNT(*) FROM query_shadows WHERE '1' NOT IN ('1', '2', '3')")
+  end
+
+  defp assert_allowed(query), do: assert_query(query, %{rows: [%{row: _} | _]})
+
+  defp assert_forbidden(query) do
+    assert_query(query, %{error: error})
+    assert error =~ ~r/At most 2 negative conditions/
   end
 end
