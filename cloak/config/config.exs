47064977dcfb6aff0d449448a432200b9f618d0e
Paweł Obrok <pawel.obrok@gmail.com>
2018-10-15 13:22:37 +0200

    Store up to 10k values in shadow tables
    If we store more of them, we minimize the chance that a query will be
    rejected when it could in fact be allowed. 10k is still only 40kB or so
    per column of integers that needs to be stored.

--- cloak/config/config.exs
+++ cloak/config/config.exs
@@ -60,7 +60,7 @@ config :cloak, :shadow_tables,
   max_rare_negative_conditions: 2,
 
   # Number of popular values to keep in a shadow table
-  size: 100,
+  size: 10_000,
 
   # Minimum users with a particular value needed to include it in a shadow table
   min_users: 10
