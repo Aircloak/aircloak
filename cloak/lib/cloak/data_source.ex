570c1b9a8545048c13091b56135f753758aeae14
Sasa Juric <sasa.juric@gmail.com>
2018-10-16 11:57:27 +0200

    fix startup order
    Starting the pool first allows other processes to use it even during the initialization of the datasource subtree.

--- cloak/lib/cloak/data_source.ex
+++ cloak/lib/cloak/data_source.ex
@@ -501,8 +501,8 @@ defmodule Cloak.DataSource do
         supervisor(
           [
             registry(:duplicate, __MODULE__.ChangeListenersRegistry),
-            gen_server(__MODULE__, load_data_source_configs(), name: __MODULE__),
             Cloak.DataSource.Connection.Pool,
+            gen_server(__MODULE__, load_data_source_configs(), name: __MODULE__),
             Supervisor.child_spec(Cloak.DataSource.Isolators, id: :isolators),
             Supervisor.child_spec(Cloak.DataSource.Shadows, id: :shadows)
           ],
