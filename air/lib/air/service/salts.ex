ec12c27ef5a0807505dfc5854f128ced35ad685b
Paweł Obrok <pawel.obrok@gmail.com>
2018-10-19 19:15:09 +0200

    Add docs and specs

--- air/lib/air/service/salts.ex
+++ air/lib/air/service/salts.ex
@@ -1,4 +1,10 @@
 defmodule Air.Service.Salts do
+  @moduledoc """
+  This module manages lazily-generated salt values to be used for various cryptographic needs. The values are stored in
+  the database and cached in-memory. Thanks to generating these in a deployed instance we avoid keeping the values in
+  our codebase and/or requiring additional configuration options. It also makes it extremely easy to add a new salt.
+  """
+
   @known_names ~w[api_token password_reset session_signing session_encryption]a
   @salt_size 64
 
@@ -7,6 +13,15 @@ defmodule Air.Service.Salts do
   import Ecto.Query
   import Aircloak, only: [in_env: 1]
 
+  # -------------------------------------------------------------------
+  # API functions
+  # -------------------------------------------------------------------
+
+  @doc """
+  Returns the named salt. The name is checked against a list of registered names. This is done in order to avoid typos
+  in matching calls such as `Phoenix.Token.sign`/`Phoenix.Token.verify`.
+  """
+  @spec get(Agent.agent(), atom()) :: String.t()
   def get(server \\ __MODULE__, name) do
     unless name in @known_names do
       raise "Unknown salt"
@@ -15,8 +30,9 @@ defmodule Air.Service.Salts do
     in_env(test: get_test(server, name), else: do_get(server, name))
   end
 
-  defp get_test(__MODULE__, name), do: to_string(name)
-  defp get_test(server, name), do: do_get(server, name)
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
 
   defp do_get(server, name) do
     Agent.get_and_update(server, &fetch_from_cache(&1, name))
@@ -50,6 +66,15 @@ defmodule Air.Service.Salts do
   end
 
   defp random_string(), do: :crypto.strong_rand_bytes(@salt_size) |> Base.encode64()
+
+  if Mix.env() == :test do
+    defp get_test(__MODULE__, name), do: to_string(name)
+    defp get_test(server, name), do: do_get(server, name)
+  end
+
+  # -------------------------------------------------------------------
+  # Supervision tree
+  # -------------------------------------------------------------------
 
   def child_spec(_arg), do: Aircloak.ChildSpec.agent(fn -> %{} end, name: __MODULE__)
 end
