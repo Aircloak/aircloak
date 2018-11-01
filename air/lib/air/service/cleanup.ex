94c6e45323d1f42b193729ea19ceecd0cdc74f65
Sasa Juric <sasa.juric@gmail.com>
2018-11-01 10:46:23 +0100

    use started_on_cloak in cleanup

--- air/lib/air/service/cleanup.ex
+++ air/lib/air/service/cleanup.ex
@@ -22,13 +22,11 @@ defmodule Air.Service.Cleanup do
   @spec cleanup_dead_queries() :: :ok
   def cleanup_dead_queries() do
     # We need to first fetch pending queries on air, to avoid possible race conditions.
-    not_finished_as_seen_by_air = Air.Service.Query.not_finished()
-    awaiting_start = Air.Service.Query.awaiting_start() |> Enum.map(& &1.id) |> MapSet.new()
+    started_on_cloak_as_seen_by_air = Air.Service.Query.started_on_cloak()
     currently_running_on_connected_cloaks = MapSet.new(Air.Service.Cloak.running_queries())
 
-    not_finished_as_seen_by_air
+    started_on_cloak_as_seen_by_air
     |> Enum.reject(&MapSet.member?(currently_running_on_connected_cloaks, &1.id))
-    |> Enum.reject(&MapSet.member?(awaiting_start, &1.id))
     |> Enum.each(&Air.Service.Query.Lifecycle.query_died(&1.id))
 
     :ok
