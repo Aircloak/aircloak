aa9e3615330e3998729e0b4ef8a9215df09492f1
Sebastian Probst Eide <sebastian@aircloak.com>
2018-11-15 09:18:24 +0100

    Update to latest pre-16 version of react and update CSS transition group
    The version we were using of CSS transition group was deprecated.
    Now we are up to date with the latest version of this dep.
    
    React is now pretty much exactly one year old. It would be good to
    get up to version 16.X at some point too, but we have other dependencies
    which are causing problems

--- air/assets/js/activity_monitor/queries.js
+++ air/assets/js/activity_monitor/queries.js
@@ -1,7 +1,8 @@
 // @flow
 
 import React from "react";
-import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import {QueryView} from "./query";
 import type {Query} from "./query";
@@ -9,15 +10,27 @@ import type {Query} from "./query";
 const renderQueries = (queries: Query[]) => {
   if (queries.length > 0) {
     return queries.map((query) =>
-      <QueryView key={query.id} query={query} />
+      <CSSTransition
+        key={query.id}
+        classNames="activity-monitor-query"
+        timeout={{enter: 500, exit: 300}}
+      >
+        <QueryView query={query} />
+      </CSSTransition>
     );
   } else {
     return (
-      <tr>
-        <td colSpan="5">
-          Currently there are no queries running.
-        </td>
-      </tr>
+      <CSSTransition
+        key="no-queries"
+        classNames="activity-monitor-query"
+        timeout={{enter: 500, exit: 300}}
+      >
+        <tr>
+          <td colSpan="5">
+            Currently there are no queries running.
+          </td>
+        </tr>
+      </CSSTransition>
     );
   }
 };
@@ -37,13 +50,8 @@ export const QueriesView = (props: {queries: Query[]}) =>
           <th></th>
         </tr>
       </thead>
-      <ReactCSSTransitionGroup
-        component="tbody"
-        transitionName="activity-monitor-queries"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={300}
-      >
+      <TransitionGroup component="tbody">
         {renderQueries(props.queries)}
-      </ReactCSSTransitionGroup>
+      </TransitionGroup>
     </table>
   </div>;
