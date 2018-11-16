155e380ea18c6633769e5921669e996f2060cf02
Paweł Obrok <pawel.obrok@gmail.com>
2018-11-16 16:53:31 +0100

    Handle casting numbers to text in oracle

--- cloak/lib/cloak/data_source/sql_builder/oracle.ex
+++ cloak/lib/cloak/data_source/sql_builder/oracle.ex
@@ -32,6 +32,8 @@ defmodule Cloak.DataSource.SqlBuilder.Oracle do
   def cast_sql(value, :real, :boolean),
     do: ["(CASE WHEN ", value, " IS NULL THEN NULL WHEN ", value, " = 0.0 THEN 0 ELSE 1 END)"]
 
+  def cast_sql(value, number, :text) when number in [:integer, :real], do: ["TO_CHAR(", value, ?)]
+
   def cast_sql(value, _, type), do: ["CAST(", value, " AS ", sql_type(type), ")"]
 
   @impl Dialect
