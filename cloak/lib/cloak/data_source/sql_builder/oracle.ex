b7c3487da1f6c95e6a56e3ca3d74ff9985a8b327
Paweł Obrok <pawel.obrok@gmail.com>
2018-11-16 16:21:48 +0100

    Handle 'booleans' in oracle

--- cloak/lib/cloak/data_source/sql_builder/oracle.ex
+++ cloak/lib/cloak/data_source/sql_builder/oracle.ex
@@ -26,6 +26,12 @@ defmodule Cloak.DataSource.SqlBuilder.Oracle do
   def function_sql(name, args), do: [String.upcase(name), "(", Enum.intersperse(args, ", "), ")"]
 
   @impl Dialect
+  def cast_sql(value, :integer, :boolean),
+    do: ["(CASE WHEN ", value, " IS NULL THEN NULL WHEN ", value, " = 0 THEN 0 ELSE 1 END)"]
+
+  def cast_sql(value, :real, :boolean),
+    do: ["(CASE WHEN ", value, " IS NULL THEN NULL WHEN ", value, " = 0.0 THEN 0 ELSE 1 END)"]
+
   def cast_sql(value, _, type), do: ["CAST(", value, " AS ", sql_type(type), ")"]
 
   @impl Dialect
@@ -37,12 +43,16 @@ defmodule Cloak.DataSource.SqlBuilder.Oracle do
   @impl Dialect
   def limit_sql(_, _), do: []
 
+  @impl Dialect
+  def boolean_literal(true), do: ?1
+  def boolean_literal(false), do: ?0
+
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
 
   defp sql_type(:real), do: "BINARY_FLOAT"
-  defp sql_type(:boolean), do: "NUMBER(1,0)"
+  defp sql_type(:boolean), do: "NUMBER(1)"
   defp sql_type(:datetime), do: "TIMESTAMP"
   defp sql_type(:integer), do: "NUMBER"
   defp sql_type(:date), do: "DATE"
