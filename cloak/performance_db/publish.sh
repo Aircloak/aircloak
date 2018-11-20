7dc4860c634ab2ccd7113883ca572b5acb0bda9c
Sebastian Probst Eide <sebastian@aircloak.com>
2018-11-20 12:56:54 +0100

    Use Postgres 11 for performance DB
    Updates the version of Postgres and adds configuration tuning
    geared at our diffix0 server. This also bumps the container VERSION
    number.
    
    Unfortunately I can't build this container at MPI due to package mirror
    discrepancies. It does however build flawlessly locally. Therefore
    the publishing script is also updated to build locally and then publish.

--- cloak/performance_db/publish.sh
+++ cloak/performance_db/publish.sh
@@ -3,6 +3,23 @@
 set -eo pipefail
 
 cd $(dirname $0)
-. ../../docker/production_helper.sh
 
-run_production_command cloak/performance_db performance_db $1 publish
+version=$(cat VERSION)
+quay="quay.io/aircloak/performance_db"
+
+echo "NOTE: This will build the container locally and publish the result to quay as version:"
+echo "      $quay:$version and $quay:latest"
+echo
+
+read -p "Continue? (y|N)  " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  ./build-image.sh
+
+  docker tag aircloak/performance_db:latest "$quay:latest"
+  docker push "$quay:latest"
+
+  docker tag aircloak/performance_db:latest "$quay:$version"
+  docker push "$quay:$version"
+fi
