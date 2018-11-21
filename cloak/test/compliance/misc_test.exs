1775032f17ec908be098a42f09731f6148bb9c27
Paweł Obrok <pawel.obrok@gmail.com>
2018-11-21 14:04:16 +0100

    Increas delta in SAMPLE_USERS test

--- cloak/test/compliance/misc_test.exs
+++ cloak/test/compliance/misc_test.exs
@@ -7,7 +7,7 @@ defmodule Compliance.MiscTest do
     @tag compliance: "sample users from #{table} in query"
     test "sample users from #{table} test in query", context do
       context
-      |> Map.put(:delta, 0.25)
+      |> Map.put(:delta, 0.4)
       |> assert_consistent_and_not_failing("""
         SELECT COUNT(*), COUNT(DISTINCT #{unquote(uid)}) FROM #{unquote(table)} SAMPLE_USERS 25%
       """)
@@ -16,7 +16,7 @@ defmodule Compliance.MiscTest do
     @tag compliance: "sample users from #{table} in subquery"
     test "sample users from #{table} test in subquery", context do
       context
-      |> Map.put(:delta, 0.25)
+      |> Map.put(:delta, 0.4)
       |> assert_consistent_and_not_failing("""
         SELECT COUNT(*) FROM (SELECT DISTINCT #{unquote(uid)} FROM #{unquote(table)} SAMPLE_USERS 25%) AS t
       """)
