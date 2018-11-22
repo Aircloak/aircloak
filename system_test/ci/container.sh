f241528b84361ac79759a8ed1d02874fe7bb1f68
Sebastian Probst Eide <sebastian@aircloak.com>
2018-11-22 19:26:27 +0100

    Fix broken name
    The system test is invoking a system test not a nightly command.
    I believe this change is correct? At least without it make dev-container
    is broken, and I can't see how nightly would work either.

--- system_test/ci/container.sh
+++ system_test/ci/container.sh
@@ -8,7 +8,7 @@ cd $ROOT_DIR
 
 . docker/ci_helper.sh system_test
 
-function prepare_for_nightly {
+function prepare_for_system_test {
   local container_name="$1"
 
   # starting databases first, so they have the time to boot and initialize before client containers are started
@@ -182,8 +182,8 @@ case "$1" in
     handle_build_image
     ;;
 
-  prepare_for_nightly)
-    prepare_for_nightly $2
+  prepare_for_system_test)
+    prepare_for_system_test $2
     ;;
 
   *)
