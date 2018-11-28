d476c5ddb430854e47ce58ad20860fad5b863219
Cristian Berneanu <cristian@aircloak.com>
2018-11-28 17:45:47 +0200

    Disable offloading of stddev(distinct) from Oracle
    `stddev_samp(distinct)` is invalid and `stddev(distinct)` returns
    0 for one input, instead of `null`.

--- cloak/lib/cloak/data_source/sql_builder/oracle.ex
+++ cloak/lib/cloak/data_source/sql_builder/oracle.ex
@@ -13,7 +13,7 @@ defmodule Cloak.DataSource.SqlBuilder.Oracle do
 
   @impl Dialect
   def supported_functions(), do: ~w(
-      count sum min max avg stddev count_distinct sum_distinct min_distinct max_distinct avg_distinct stddev_distinct
+      count sum min max avg stddev count_distinct sum_distinct min_distinct max_distinct avg_distinct
       year quarter month day hour minute second weekday date_trunc
       sqrt floor ceil abs round trunc div mod ^ % * / + -
       length lower upper btrim ltrim rtrim left right substring concat
