93dee1593fca87ec01177bf3711693a7962a463a
Sebastian Probst Eide <sebastian@aircloak.com>
2018-11-29 12:47:59 +0100

    Restore original publish script

--- cloak/performance_db/publish.sh
+++ cloak/performance_db/publish.sh
@@ -3,23 +3,6 @@
 set -eo pipefail
 
 cd $(dirname $0)
+. ../../docker/production_helper.sh
 
-version=$(cat VERSION)
-quay="quay.io/aircloak/performance_db"
-
-echo "NOTE: This will build the container locally and publish the result to quay as version:"
-echo "      $quay:$version and $quay:latest"
-echo
-
-read -p "Continue? (y|N)  " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  ./build-image.sh
-
-  docker tag aircloak/performance_db:latest "$quay:latest"
-  docker push "$quay:latest"
-
-  docker tag aircloak/performance_db:latest "$quay:$version"
-  docker push "$quay:$version"
-fi
+run_production_command cloak/performance_db performance_db $1 publish
