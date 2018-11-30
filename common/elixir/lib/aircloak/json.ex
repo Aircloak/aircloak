f9b0a1b5d2627feffe8594b22d865a3beeba96ed
Sebastian Probst Eide <sebastian@aircloak.com>
2018-11-30 10:43:45 +0100

    Clean up JSON module for new usage

--- common/elixir/lib/aircloak/json.ex
+++ common/elixir/lib/aircloak/json.ex
@@ -5,16 +5,28 @@ defmodule Aircloak.Json do
   # API functions
   # -------------------------------------------------------------------
 
-  @doc "Decodes a JSON file, capturing a wide wariety of potential errors."
-  @spec safe_decode!(String.t()) :: Map.t()
-  def safe_decode!(raw_json) do
-    {:ok, result} = safe_decode(raw_json)
-    result
-  end
+  @doc """
+  Decodes a string with data encoded as JSON into a map, capturing a wide wariety of potential errors
+  and producing a descriptive error message.
 
-  @doc "Decodes a JSON file, capturing a wide wariety of potential errors."
-  @spec safe_decode(String.t()) :: {:ok, Map.t()} | {:error, String.t()}
-  def safe_decode(raw_json) do
+  This implementation uses Poison for decoding rather than Jason. Jason is more restrictive in its
+  adherence to the JSON spec, and therefore does not allow multiline strings which are required to
+  allow queries to be defined inline in data source definitions:
+
+    "accounts": {
+      "query": "
+        SELECT uid, name, balance
+        FROM transactions
+        ORDER BY date
+        LIMIT 1
+      ",
+      "user_id": "uid"
+    }
+
+  And furthermore Jason outputs error messages which are impossible to parse.
+  """
+  @spec permissive_decode(String.t()) :: {:ok, Map.t()} | {:error, String.t()}
+  def permissive_decode(raw_json) do
     # We're using decode! instead of decode since the latter doesn't return a usable error message
     {:ok, Poison.decode!(raw_json)}
   rescue
