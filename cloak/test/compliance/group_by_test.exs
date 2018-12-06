0eaa02d9b4b659d78eb17804e321288cb1d75d3e
Cristian Berneanu <cristian@aircloak.com>
2018-12-06 13:35:43 +0200

    Disable grouping tests for SQL Server and MySQL

--- cloak/test/compliance/group_by_test.exs
+++ cloak/test/compliance/group_by_test.exs
@@ -5,6 +5,8 @@ defmodule Compliance.GroupByTest do
     @tag compliance: "#{column} #{table} group by"
     test "group by #{column} in query on #{table}", context do
       context
+      |> disable_for(Cloak.DataSource.SQLServer, unquote(column) == "name")
+      |> disable_for(Cloak.DataSource.MySQL, unquote(column) == "name")
       |> assert_consistent_and_not_failing("""
         SELECT #{unquote(column)}, COUNT(DISTINCT #{unquote(uid)})
         FROM #{unquote(table)} GROUP BY 1 ORDER BY 1
