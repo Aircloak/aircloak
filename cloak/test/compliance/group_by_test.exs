cea14b51d504ae6d7306c5b337ac5a0ba4e47f7e
Cristian Berneanu <cristian@aircloak.com>
2018-12-06 13:08:58 +0200

    Rename compliance aggregation tests to group by tests

---
+++ cloak/test/compliance/group_by_test.exs
@@ -0,0 +1,14 @@
+defmodule Compliance.GroupByTest do
+  use ComplianceCase, async: true
+
+  Enum.each(all_columns(), fn {column, table, uid} ->
+    @tag compliance: "#{column} #{table} group by"
+    test "group by #{column} in query on #{table}", context do
+      context
+      |> assert_consistent_and_not_failing("""
+        SELECT #{unquote(column)}, COUNT(DISTINCT #{unquote(uid)})
+        FROM #{unquote(table)} GROUP BY 1 ORDER BY 1
+      """)
+    end
+  end)
+end
