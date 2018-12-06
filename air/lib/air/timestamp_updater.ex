db744fe08b452ade97cde4b29afcd3f39ceb28b4
Paweł Obrok <pawel.obrok@gmail.com>
2018-12-06 15:03:15 +0100

    Fix Ecto.Sandbox crashes

--- air/lib/air/timestamp_updater.ex
+++ air/lib/air/timestamp_updater.ex
@@ -11,15 +11,22 @@ defmodule Air.TimestampUpdater do
   @spec start_link() :: Supervisor.on_start()
   def start_link(), do: Task.Supervisor.start_link(name: __MODULE__)
 
-  @doc "Starts a process which touches the model."
+  @doc """
+  Starts a process which touches the model. Does nothing in test, as starting an async task that uses the DB plays badly
+  with the Ecto Sandbox.
+  """
   @spec start_toucher(Ecto.Model.t()) :: :ok
-  def start_toucher(model) do
-    Task.Supervisor.start_child(__MODULE__, fn ->
-      model
-      |> Ecto.Changeset.change(%{updated_at: NaiveDateTime.utc_now()})
-      |> Air.Repo.update()
-    end)
+  if Mix.env() == :test do
+    def start_toucher(_model), do: :ok
+  else
+    def start_toucher(model) do
+      Task.Supervisor.start_child(__MODULE__, fn ->
+        model
+        |> Ecto.Changeset.change(%{updated_at: NaiveDateTime.utc_now()})
+        |> Air.Repo.update()
+      end)
 
-    :ok
+      :ok
+    end
   end
 end
