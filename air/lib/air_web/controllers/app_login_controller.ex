c3e689d0081fc33e19dcb20b95e3442b205463ec
Paweł Obrok <pawel.obrok@gmail.com>
2018-12-06 11:56:22 +0100

    Display existing app logins in frontend

--- air/lib/air_web/controllers/app_login_controller.ex
+++ air/lib/air_web/controllers/app_login_controller.ex
@@ -19,13 +19,13 @@ defmodule AirWeb.AppLoginController do
   # -------------------------------------------------------------------
 
   def index(conn, _params) do
-    render(conn, "index.html", api_tokens: [], changeset: User.app_login_changeset())
+    render(conn, "index.html", app_logins: app_logins(conn), changeset: User.app_login_changeset())
   end
 
   def create(conn, %{"login" => params}) do
     case User.create_app_login(conn.assigns.current_user, params) do
       {:error, changeset} ->
-        render(conn, "index.html", api_tokens: [], changeset: changeset)
+        render(conn, "index.html", app_logins: app_logins(conn), changeset: changeset)
 
       {:ok, login, password} ->
         audit_log(conn, "Created app login")
@@ -36,4 +36,10 @@ defmodule AirWeb.AppLoginController do
         |> redirect(to: app_login_path(conn, :index))
     end
   end
+
+  # -------------------------------------------------------------------
+  # Actions
+  # -------------------------------------------------------------------
+
+  defp app_logins(conn), do: Enum.reject(conn.assigns.current_user.logins, &(&1.login_type == :main))
 end
