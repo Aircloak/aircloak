e0f885fa137d386bb0f7124110b9fe80555e2ad6
Paweł Obrok <pawel.obrok@gmail.com>
2018-12-12 12:20:54 +0100

    Allow setting random seed in gen.test_data

--- cloak/lib/mix/tasks/gen/test_data.ex
+++ cloak/lib/mix/tasks/gen/test_data.ex
@@ -9,8 +9,10 @@ defmodule Mix.Tasks.Gen.TestData do
     - the name of the data source config file to be used
     - the number of users to be generated
 
-    Optionally, you can also provide the `--concurrency positive_integer` option to specify the number of concurrent
-    insert processes used in each data source. The default value for this parameter is the number of online schedulers.
+    Additionally you may specify the following options:
+
+    --concurrency - specifies the number of concurrent insertion processes. Defaults to number of schedulers.
+    --seed - specifies the random seed to use when generating the data. The seed used is printed for future reference.
 
     The config file is expected to be found in `priv/config`
 
@@ -32,12 +34,23 @@ defmodule Mix.Tasks.Gen.TestData do
       # this ensures that protocols are consolidated, which improves the overall running time
       Mix.Task.run("app.start", ["--no-start"])
 
-      case OptionParser.parse!(args, switches: [concurrency: :integer]) do
+      case OptionParser.parse!(args, switches: [seed: :string, concurrency: :integer]) do
         {options, [config_name, num_users]} ->
+          seed =
+            case Keyword.fetch(options, :seed) do
+              {:ok, seed} ->
+                seed
+
+              :error ->
+                :rand.seed(:exrop)
+                :rand.export_seed() |> :erlang.term_to_binary() |> Base.encode64()
+            end
+
           concurrency = Keyword.get(options, :concurrency, System.schedulers_online())
           num_users = String.to_integer(num_users)
-          IO.puts("Generating data for #{num_users} users.")
+          IO.puts("Generating data for #{num_users} users. Using seed #{inspect(seed)}.")
 
+          seed |> Base.decode64!() |> :erlang.binary_to_term() |> :rand.seed()
           Compliance.initialize(config_name, num_users, concurrency)
 
         _ ->
