c66db99e039de921b2c13e27a407316118167fb3
Paweł Obrok <pawel.obrok@gmail.com>
2019-01-08 16:16:52 +0100

    Populate table on analysis page

--- air/lib/air_web/controllers/admin/analysis_controller.ex
+++ air/lib/air_web/controllers/admin/analysis_controller.ex
@@ -16,6 +16,38 @@ defmodule AirWeb.Admin.AnalysisController do
   # -------------------------------------------------------------------
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", groups: tables())
   end
+
+  # -------------------------------------------------------------------
+  # Internal functions
+  # -------------------------------------------------------------------
+
+  defp tables() do
+    for data_source <- Air.Service.DataSource.all(),
+        table <- Air.Schemas.DataSource.tables(data_source) do
+      %{
+        name: "#{data_source.name}/#{table["id"]}",
+        columns: length(table["columns"]),
+        isolators_computed: Enum.count(table["columns"], &isolators_computed?/1),
+        isolators_failed: Enum.count(table["columns"], &isolators_failed?/1),
+        rare_values_computed: Enum.count(table["columns"], &shadow_computed?/1),
+        rare_values_failed: Enum.count(table["columns"], &shadow_failed?/1),
+        analyzed: Enum.count(table["columns"], &analyzed?/1),
+        analysis_failed: Enum.count(table["columns"], &analysis_failed?/1)
+      }
+    end
+  end
+
+  defp analyzed?(column), do: isolators_computed?(column) and shadow_computed?(column)
+
+  defp analysis_failed?(column), do: isolators_failed?(column) or shadow_failed?(column)
+
+  defp isolators_computed?(column), do: is_boolean(column["isolated"])
+
+  defp isolators_failed?(column), do: column["isolated"] == "failed"
+
+  defp shadow_computed?(column), do: column["shadow_table"] == "ok"
+
+  defp shadow_failed?(column), do: column["shadow_table"] == "failed"
 end
--- air/lib/air_web/views/admin/analysis_view.ex
+++ air/lib/air_web/views/admin/analysis_view.ex
@@ -1,4 +1,6 @@
 defmodule AirWeb.Admin.AnalysisView do
   @moduledoc false
   use Air.Web, :view
+
+  def total(groups, field), do: groups |> Enum.map(& &1[field]) |> Enum.sum()
 end
