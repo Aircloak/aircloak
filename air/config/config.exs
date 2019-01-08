84ccd963d3f17ae424e530cb32822b3fe57d66ea
Paweł Obrok <pawel.obrok@gmail.com>
2019-01-08 14:23:06 +0100

    Remove appsignal
    Turns out our reporting to appsignal has not been working for some time
    now, most likely since the upgrade to Elixir 1.7. Because we got no
    information that this was happening and restoring it to working order
    would involve some amount of work, we decided to remove appsignal for
    now, possibly to be replaced with another monitoring service later on.

--- air/config/config.exs
+++ air/config/config.exs
@@ -17,8 +17,7 @@ config :air, AirWeb.Endpoint,
   http: [port: 8080],
   root: Path.dirname(__DIR__),
   render_errors: [accepts: ~w(html json)],
-  pubsub: [name: Air.PubSub, adapter: Phoenix.PubSub.PG2],
-  instrumenters: [Appsignal.Phoenix.Instrumenter]
+  pubsub: [name: Air.PubSub, adapter: Phoenix.PubSub.PG2]
 
 config :air, AirWeb.MonitoringEndpoint,
   check_origin: false,
@@ -54,7 +53,7 @@ config :air, Air.Repo,
   pool_size: 10,
   # We need it to work with `pgbouncer` (see https://github.com/elixir-ecto/postgrex#pgbouncer)
   prepare: :unnamed,
-  loggers: [Appsignal.Ecto, Ecto.LogEntry]
+  loggers: [Ecto.LogEntry]
 
 config :air, ecto_repos: [Air.Repo]
 
