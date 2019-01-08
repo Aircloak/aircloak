48ef2c11c6a186f3886aca10023f50fe30aeed9c
Paweł Obrok <pawel.obrok@gmail.com>
2019-01-08 17:42:49 +0100

    Allow grouping by data source on analysis page

--- air/lib/air_web/controllers/admin/analysis_controller.ex
+++ air/lib/air_web/controllers/admin/analysis_controller.ex
@@ -17,18 +17,36 @@ defmodule AirWeb.Admin.AnalysisController do
   # -------------------------------------------------------------------
 
   def index(conn, _params) do
-    render(conn, "index.html", groups: tables())
+    tables = tables()
+    render(conn, "index.html", by_table: tables, by_data_source: group(tables, & &1.data_source), by_host: tables)
   end
 
   # -------------------------------------------------------------------
   # Internal functions
   # -------------------------------------------------------------------
 
+  @stats ~w(
+    columns isolators_computed isolators_failed rare_values_computed rare_values_failed analyzed analysis_failed)a
+
+  defp group(tables, fun) do
+    tables
+    |> Enum.group_by(fun)
+    |> Enum.map(fn {name, tables} ->
+      initial = Enum.reduce(@stats, %{name: name}, &Map.put(&2, &1, 0))
+
+      Enum.reduce(tables, initial, fn table, acc ->
+        Enum.reduce(@stats, acc, fn key, acc ->
+          Map.put(acc, key, acc[key] + table[key])
+        end)
+      end)
+    end)
+  end
+
   defp tables() do
-    for data_source <- Air.Service.DataSource.all(),
-        table <- Air.Schemas.DataSource.tables(data_source) do
+    for data_source <- Air.Service.DataSource.all(), table <- Air.Schemas.DataSource.tables(data_source) do
       %{
         name: "#{data_source.name}/#{table["id"]}",
+        data_source: data_source.name,
         columns: length(table["columns"]),
         isolators_computed: Enum.count(table["columns"], &Column.isolators_computed?/1),
         isolators_failed: Enum.count(table["columns"], &Column.isolators_failed?/1),
