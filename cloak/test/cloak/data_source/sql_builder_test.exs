7f3db0d04beec94a1894512fa70b2ece965b4d83
Sasa Juric <sasa.juric@gmail.com>
2019-01-16 09:57:36 +0100

    replace unit tests with property tests

--- cloak/test/cloak/data_source/sql_builder_test.exs
+++ cloak/test/cloak/data_source/sql_builder_test.exs
@@ -1,9 +1,11 @@
 defmodule Cloak.DataSource.SqlBuilderTest do
   use ExUnit.Case, async: true
 
+  use ExUnitProperties
   alias Cloak.DataSource.{SqlBuilder, Table}
   alias Cloak.Sql.Query
   import Cloak.Test.QueryHelpers
+  import StreamData
 
   test "non-text column is not force casted",
     do: refute(sql_string("select int from table") =~ ~r/CAST\("table"\."int"/)
@@ -11,34 +13,35 @@ defmodule Cloak.DataSource.SqlBuilderTest do
   test "workaround for text comparisons on SQL Server ignoring trailing spaces",
     do: assert(sql_string("select count(*) from table where string = 'ab'", SQLServer) =~ "= N'ab.')")
 
-  test "table name quoting" do
-    assert SqlBuilder.quote_table_name("name") == "\"name\""
-    assert SqlBuilder.quote_table_name("\"name\"") == "\"name\""
-    assert SqlBuilder.quote_table_name("full.name") == "\"full\".\"name\""
-    assert SqlBuilder.quote_table_name("long.full.name") == "\"long\".\"full\".\"name\""
+  describe "quote_table_name" do
+    property "quotes each part" do
+      check all quote_char <- constant_of([?", ?']),
+                parts_data <- parts_data(),
+                {parts, unquoted_parts} = Enum.unzip(parts_data),
+                table_name = Enum.join(parts, ".") do
+        expected = unquoted_parts |> Stream.map(&to_string([quote_char, &1, quote_char])) |> Enum.join(".")
+        assert SqlBuilder.quote_table_name(table_name, quote_char) == expected
+      end
+    end
   end
 
   describe "table_name_parts" do
-    test "unqualified single part", do: assert(SqlBuilder.table_name_parts("foo") == ["foo"])
-    test "unqualified with whitespace", do: assert(SqlBuilder.table_name_parts("foo bar baz") == ["foo bar baz"])
-    test "unqualified multi parts", do: assert(SqlBuilder.table_name_parts("foo.bar.baz") == ["foo", "bar", "baz"])
-
-    test "qualified single part", do: assert(SqlBuilder.table_name_parts(~s/"foo"/) == ["foo"])
-    test "qualified with quotes in the name", do: assert(SqlBuilder.table_name_parts(~s/"f""oo"/) == [~s(f"oo)])
-    test "qualified multi part", do: assert(SqlBuilder.table_name_parts(~s/"foo"."bar"."baz"/) == ["foo", "bar", "baz"])
-
-    test "mixed multi part", do: assert(SqlBuilder.table_name_parts(~s/"foo".bar."baz"/) == ["foo", "bar", "baz"])
-
-    test "empty parts are not allowed" do
-      assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts("") end
-      assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts(".") end
-      assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts("foo.") end
-      assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts(".foo") end
-      assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts("foo..bar") end
+    property "returns the list of unquoted parts" do
+      check all parts_data <- parts_data(),
+                {parts, unquoted_parts} = Enum.unzip(parts_data),
+                table_name = Enum.join(parts, ".") do
+        assert SqlBuilder.table_name_parts(table_name) == unquoted_parts
+      end
     end
 
-    test "non-matching quotes are not allowed",
-      do: assert_raise(ArgumentError, fn -> SqlBuilder.table_name_parts(~s/"foobar/) end)
+    property "raises on invalid inputs" do
+      check all valid_parts <- valid_parts(),
+                invalid_pos <- integer(0..length(valid_parts)),
+                invalid_part <- invalid_part(),
+                table_name = valid_parts |> List.insert_at(invalid_pos, invalid_part) |> Enum.join(".") do
+        assert_raise ArgumentError, fn -> SqlBuilder.table_name_parts(table_name) end
+      end
+    end
   end
 
   defp sql_string(query, dialect \\ PostgreSQL) do
@@ -68,4 +71,41 @@ defmodule Cloak.DataSource.SqlBuilderTest do
       }
     }
   end
+
+  # -------------------------------------------------------------------
+  # Generators for table names
+  # -------------------------------------------------------------------
+
+  defp valid_parts(), do: map(parts_data(), &Enum.map(&1, fn {valid_part, _unqouted_part} -> valid_part end))
+
+  defp parts_data(), do: nonempty(list_of(one_of([unquoted_part_data(), quoted_part_data()])))
+
+  defp unquoted_part_data(), do: map(nonempty_string_of(unquoted_char()), &{&1, &1})
+
+  defp quoted_part_data() do
+    map(
+      nonempty_string_of(one_of([unquoted_char(), special_char()])),
+      &{~s/"#{&1}"/, String.replace(&1, ~s/""/, ~s/"/)}
+    )
+  end
+
+  defp unquoted_char(), do: constant_of(Enum.concat([?a..?z, ?A..?Z, ?0..?9, [?_]]))
+
+  defp special_char() do
+    frequency([
+      {2, constant_of([?\s, ~s/""/, ?', ?.])},
+      {1, constant_of(0xA0..0xD7FF)}
+    ])
+  end
+
+  defp constant_of(elements), do: elements |> Enum.map(&constant/1) |> one_of()
+
+  defp nonempty_string_of(char_generator), do: map(nonempty(list_of(char_generator)), &to_string/1)
+
+  defp invalid_part(), do: one_of([empty_unquoted(), non_closed_quote()])
+
+  defp empty_unquoted(), do: constant("")
+
+  defp non_closed_quote(),
+    do: map(quoted_part_data(), fn {quoted, _unquoted} -> String.replace(quoted, ~r/"$/, "") end)
 end
