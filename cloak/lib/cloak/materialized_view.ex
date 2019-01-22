07723f837c7593fa4ce026f370ad89644003fa2a
Sasa Juric <sasa.juric@gmail.com>
2019-01-22 14:44:01 +0100

    compute unique view name

--- cloak/lib/cloak/materialized_view.ex
+++ cloak/lib/cloak/materialized_view.ex
@@ -10,6 +10,32 @@ defmodule Cloak.MaterializedView do
          do: {:ok, %__MODULE__{id: id, data_source: data_source, db_select: Cloak.DataSource.SqlBuilder.build(query)}}
   end
 
+  @doc """
+  Computes the view name which can be used as the table name in the database.
+
+  The view name is derived from the view id and the generated sql. If any of these pieces of data changes, the view
+  name will be different. This helps us remove various conflicting situations such as:
+
+  - the new version of the cloak generates a different SQL
+  - the view has been modified while a previous query is running
+  - shields us from possible attacks (see https://github.com/Aircloak/aircloak/issues/3467#issuecomment-455563526 for
+    example)
+
+  Example:
+
+  ```
+  iex> MaterializedView.name(view!(1, "select user_id, x from mv1"))
+  "__ac_2tiqXfcCKW9iKREwe5aLfUFgf"
+  ```
+  """
+  def name(view) do
+    hash = :crypto.hash(:sha256, :erlang.term_to_binary([view.id, view.db_select]))
+    encoded_hash = Base.encode64(hash, padding: false)
+
+    # make sure the name is not longer than 30 characters to avoid possible issues with some databases, such as Oracle
+    String.slice("__ac_#{encoded_hash}", 0, 30)
+  end
+
   defp supports_materialized_views?(data_source) do
     if data_source.driver.supports_materialized_views?(),
       do: :ok,
--- cloak/test/cloak/materialized_view_test.exs
+++ cloak/test/cloak/materialized_view_test.exs
@@ -2,84 +2,103 @@ defmodule Cloak.MaterializedViewTest do
   use ExUnit.Case, async: true
 
   alias Cloak.MaterializedView
+  doctest MaterializedView
 
   setup_all do
     Cloak.Test.DB.create_table("mv1", "x integer, y text")
     Cloak.Test.DB.create_table("mv2", "x integer, y text", db_name: "db_name_mv2")
   end
 
-  test "simple materialized view" do
-    assert {:ok, view} = MaterializedView.new(1, "select user_id, x from mv1", data_source())
-    assert view.db_select == ~s/SELECT "mv1"."user_id" AS "user_id","mv1"."x" AS "x" FROM "cloak_test"."mv1" AS "mv1"/
+  describe "compiled sql - " do
+    test "simple materialized view" do
+      view = view!(1, "select user_id, x from mv1")
+      assert view.db_select == ~s/SELECT "mv1"."user_id" AS "user_id","mv1"."x" AS "x" FROM "cloak_test"."mv1" AS "mv1"/
+    end
+
+    test "function support" do
+      view = view!(1, "select user_id, hash(y) from mv1")
+
+      assert view.db_select ==
+               ~s/SELECT "mv1"."user_id" AS "user_id",SUBSTR(MD5("mv1"."y"::text), 5, 8) AS "hash" / <>
+                 ~s/FROM "cloak_test"."mv1" AS "mv1"/
+    end
+
+    test "subquery support" do
+      view = view!(1, "select user_id, x from (select * from mv1) sq")
+
+      assert view.db_select ==
+               ~s/SELECT "sq"."user_id" AS "user_id","sq"."x" AS "x" FROM / <>
+                 ~s/(SELECT "mv1"."user_id" AS "user_id","mv1"."x" AS "x" FROM "cloak_test"."mv1" AS "mv1") AS "sq"/
+    end
+
+    test "join support" do
+      view = view!(1, "select mv1.user_id, mv2.x from mv1 inner join mv2 on mv1.user_id = mv2.user_id")
+
+      assert view.db_select ==
+               ~s/SELECT "mv1"."user_id" AS "user_id","mv2"."x" AS "x" / <>
+                 ~s/FROM  "cloak_test"."mv1" AS "mv1" / <>
+                 ~s/INNER JOIN "cloak_test"."db_name_mv2" AS "mv2" ON "mv1"."user_id" = "mv2"."user_id" /
+    end
+
+    test "tables are referenced by the user name" do
+      view = view!(1, "select user_id, x from mv2")
+
+      assert view.db_select ==
+               ~s/SELECT "mv2"."user_id" AS "user_id","mv2"."x" AS "x" FROM "cloak_test"."db_name_mv2" AS "mv2"/
+    end
+
+    test "tables can't be referenced by the db name" do
+      assert {:error, error} = view(1, "select user_id, x from db_name_mv2")
+      assert error == "Table `db_name_mv2` doesn't exist."
+    end
+
+    test "parsing error is reported" do
+      assert {:error, error} = view(1, "select")
+      assert error == "Expected `column definition` at line 1, column 7."
+    end
+
+    test "compilation error is reported" do
+      assert {:error, error} = view(1, "select sqrt(y) from mv1")
+      assert error == "Function `sqrt` requires arguments of type (`integer` | `real`), but got (`text`)."
+    end
+
+    test "anonymized subqueries can't be materialized" do
+      assert {:error, error} = view(1, "select x from mv1")
+      assert error == "At least one user id column must be selected."
+    end
+
+    test "emulated subqueries can't be materialized" do
+      assert {:error, error} = view(1, "select user_id, dec_b64(y) from mv1")
+      assert error == "Emulated query can't be materialized."
+    end
+
+    test "materialized view can't be created if the driver doesn't support it" do
+      mongo_data_source = Map.put(data_source(), :driver, Cloak.DataSource.MongoDB)
+      assert {:error, error} = MaterializedView.new(1, "select * from mv1", mongo_data_source)
+      assert error == "This data source doesn't support materialized views."
+    end
   end
 
-  test "function support" do
-    assert {:ok, view} = MaterializedView.new(1, "select user_id, hash(y) from mv1", data_source())
+  describe "name - " do
+    test "different id leads to a different name" do
+      view1 = view!(1, "select user_id, x from mv1")
+      view2 = view!(2, "select user_id, x from mv1")
+      refute MaterializedView.name(view1) == MaterializedView.name(view2)
+    end
 
-    assert view.db_select ==
-             ~s/SELECT "mv1"."user_id" AS "user_id",SUBSTR(MD5("mv1"."y"::text), 5, 8) AS "hash" / <>
-               ~s/FROM "cloak_test"."mv1" AS "mv1"/
+    test "different query leads to a different name" do
+      view1 = view!(1, "select user_id, x from mv1")
+      view2 = view!(1, "select user_id, y from mv1")
+      refute MaterializedView.name(view1) == MaterializedView.name(view2)
+    end
   end
 
-  test "subquery support" do
-    assert {:ok, view} = MaterializedView.new(1, "select user_id, x from (select * from mv1) sq", data_source())
-
-    assert view.db_select ==
-             ~s/SELECT "sq"."user_id" AS "user_id","sq"."x" AS "x" FROM / <>
-               ~s/(SELECT "mv1"."user_id" AS "user_id","mv1"."x" AS "x" FROM "cloak_test"."mv1" AS "mv1") AS "sq"/
+  defp view!(id, statement) do
+    {:ok, view} = view(id, statement)
+    view
   end
 
-  test "join support" do
-    assert {:ok, view} =
-             MaterializedView.new(
-               1,
-               "select mv1.user_id, mv2.x from mv1 inner join mv2 on mv1.user_id = mv2.user_id",
-               data_source()
-             )
-
-    assert view.db_select ==
-             ~s/SELECT "mv1"."user_id" AS "user_id","mv2"."x" AS "x" / <>
-               ~s/FROM  "cloak_test"."mv1" AS "mv1" / <>
-               ~s/INNER JOIN "cloak_test"."db_name_mv2" AS "mv2" ON "mv1"."user_id" = "mv2"."user_id" /
-  end
-
-  test "tables are referenced by the user name" do
-    assert {:ok, view} = MaterializedView.new(1, "select user_id, x from mv2", data_source())
-
-    assert view.db_select ==
-             ~s/SELECT "mv2"."user_id" AS "user_id","mv2"."x" AS "x" FROM "cloak_test"."db_name_mv2" AS "mv2"/
-  end
-
-  test "tables can't be referenced by the db name" do
-    assert {:error, error} = MaterializedView.new(1, "select user_id, x from db_name_mv2", data_source())
-    assert error == "Table `db_name_mv2` doesn't exist."
-  end
-
-  test "parsing error is reported" do
-    assert {:error, error} = MaterializedView.new(1, "select", data_source())
-    assert error == "Expected `column definition` at line 1, column 7."
-  end
-
-  test "compilation error is reported" do
-    assert {:error, error} = MaterializedView.new(1, "select sqrt(y) from mv1", data_source())
-    assert error == "Function `sqrt` requires arguments of type (`integer` | `real`), but got (`text`)."
-  end
-
-  test "anonymized subqueries can't be materialized" do
-    assert {:error, error} = MaterializedView.new(1, "select x from mv1", data_source())
-    assert error == "At least one user id column must be selected."
-  end
-
-  test "emulated subqueries can't be materialized" do
-    assert {:error, error} = MaterializedView.new(1, "select user_id, dec_b64(y) from mv1", data_source())
-    assert error == "Emulated query can't be materialized."
-  end
-
-  test "materialized view can't be created if the driver doesn't support it" do
-    mongo_data_source = Map.put(data_source(), :driver, Cloak.DataSource.MongoDB)
-    assert {:error, error} = MaterializedView.new(1, "select * from mv1", mongo_data_source)
-    assert error == "This data source doesn't support materialized views."
-  end
+  defp view(id, statement), do: MaterializedView.new(id, statement, data_source())
 
   defp data_source(), do: hd(Cloak.DataSource.all())
 end
