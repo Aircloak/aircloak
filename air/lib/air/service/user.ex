a4655eb6a5443272ad5584cafc2152250350a7cd
Paweł Obrok <pawel.obrok@gmail.com>
2019-01-29 12:57:36 +0100

    Use the atomic API in reset_password
    I forgot to make the switch when introducing
    `RevokableToken.verify_and_revoke`.

--- air/lib/air/service/user.ex
+++ air/lib/air/service/user.ex
@@ -67,20 +67,19 @@ defmodule Air.Service.User do
   def reset_password(token, params) do
     one_week_in_seconds = 7 * :timer.hours(24) / :timer.seconds(1)
 
-    with {:ok, user_id} <- RevokableToken.verify(token, :password_reset, max_age: one_week_in_seconds) do
-      in_transaction(fn ->
+    in_transaction(fn ->
+      with {:ok, user_id} <- RevokableToken.verify_and_revoke(token, :password_reset, max_age: one_week_in_seconds) do
         user = load(user_id)
 
-        RevokableToken.revoke(token, :password_reset)
         RevokableToken.revoke_all(user, :session)
 
         user
         |> change_main_login(&password_reset_changeset(&1, params))
         |> update()
-      end)
-    else
-      _ -> {:error, :invalid_token}
-    end
+      else
+        _ -> {:error, :invalid_token}
+      end
+    end)
   end
 
   @doc "Returns a list of all users in the system."
