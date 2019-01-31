05a4daffa27dbab83d121e2a782d25ec9ee380d4
Paweł Obrok <pawel.obrok@gmail.com>
2019-01-31 16:11:44 +0100

    Parse commandline arguments in MongoCleanup

--- tools/MongoCleanup/Program.fs
+++ tools/MongoCleanup/Program.fs
@@ -1,6 +1,21 @@
-﻿open System
+﻿open Argu
+open System
+
+type CLIArguments =
+    | [<Mandatory>] Connection_String of string
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Connection_String _ -> "specify a connection string"
+
+let optionParser = ArgumentParser.Create<CLIArguments>(programName = "MongoCleanup")
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    try
+        let options = optionParser.ParseCommandLine(inputs = argv, raiseOnUsage= true)
+        printfn "%A" options
+    with e ->
+        printfn "%s" e.Message
+
     0 // return an integer exit code
