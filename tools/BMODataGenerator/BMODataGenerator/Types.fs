a3014c8ce706ae95cc73881f4e64e3c4fb0242b0
Sebastian Probst Eide <sebastian@aircloak.com>
2019-01-31 15:58:53 +0100

    Uniform comment style
    The automatic functionality built into JetBrains Rider for
    commenting out blocks of code sucks...

--- tools/BMODataGenerator/BMODataGenerator/Types.fs
+++ tools/BMODataGenerator/BMODataGenerator/Types.fs
@@ -9,13 +9,11 @@ type Row = {
 
     merchName: string
 
-    (*
-    MERCH_SIC text
-        MERCH_SIC contains numbers stored as strings such as:
-        ·         5964
-        ·         0
-       There There are no null values in that column.
-    *)
+    //MERCH_SIC text
+    //    MERCH_SIC contains numbers stored as strings such as:
+    //    ·         5964
+    //    ·         0
+    //   There There are no null values in that column.
     merchSic: string
     merchCity: string
     merchState: string
@@ -28,47 +26,44 @@ type Row = {
     // them as datetimes
     tranDt: string
 
-    (*
-    SPOUSE contains mostly spouse names, but there’s other values that seem nonsensical to me (like numbers?) in strings:
-    ·         HELEN
-    ·         MARTHA
-    ·         CHRISTINE
-    ·         ROBIN STRADINGE
-    ·         601-12-1596
-    ·         391-30-1913
-    ·         HERS:SEE MM01
-    ·         APP-N/COAPP-Y
-    ·         JEAN J*SHAW
-    No null values, but there are string values that are ‘NULL’, which I think make more sense as null values,
-    but that’s how it was in the parquet file.
-    *)
+    //  SPOUSE contains mostly spouse names, but there’s other values that seem nonsensical to me (like numbers?) in strings:
+    //·         HELEN
+    //·         MARTHA
+    //·         CHRISTINE
+    //·         ROBIN STRADINGE
+    //·         601-12-1596
+    //·         391-30-1913
+    //·         HERS:SEE MM01
+    //·         APP-N/COAPP-Y
+    //·         JEAN J*SHAW
+    //  No null values, but there are string values that are ‘NULL’, which I think make more sense as null values,
+    //  but that’s how it was in the parquet file.
     spouse: string
     workPlace: string
+
     // ANNUAL_INCOME looks like a decimal encoded as a string, that can also be null.
     annualInc: string
-    (*
-    CARD_NBR are string encoded credit card numbers.
-    I will not be posting examples of this column here, but they are 16 digit numbers.
-    CARD_NBR can also be null.       
-    *)
+
+    //  CARD_NBR are string encoded credit card numbers.
+    //  I will not be posting examples of this column here, but they are 16 digit numbers.
+    //  CARD_NBR can also be null.       
     cardNbr: string
-    (*
-    I think genesis_id is some sort of identifier? Looks like a bunch of numbers stored as strings.
-    0: jdbc:drill:zk=local> select distinct genesis_id from genesis2_parquet limit 10;
-    +-------------+
-    | genesis_id  |
-    +-------------+
-    | 67108933    |
-    | 67108960    |
-    | 67109307    |
-    | 67110026    |
-    | 67110105    |
-    | 67110223    |
-    | 67110255    |
-    | 67110286    |
-    | 67110801    |
-    | 67110853    |
-    +-------------+
-    *)
+
+    //  I think genesis_id is some sort of identifier? Looks like a bunch of numbers stored as strings.
+    //  0: jdbc:drill:zk=local> select distinct genesis_id from genesis2_parquet limit 10;
+    //  +-------------+
+    //  | genesis_id  |
+    //  +-------------+
+    //  | 67108933    |
+    //  | 67108960    |
+    //  | 67109307    |
+    //  | 67110026    |
+    //  | 67110105    |
+    //  | 67110223    |
+    //  | 67110255    |
+    //  | 67110286    |
+    //  | 67110801    |
+    //  | 67110853    |
+    //  +-------------+
     genesisId: int
 }
