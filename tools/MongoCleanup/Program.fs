0b1883c42500c192ef486499f7451c5f998aeca0
Paweł Obrok <pawel.obrok@gmail.com>
2019-02-01 16:58:09 +0100

    Parse cloak config in MongoCleanup

--- tools/MongoCleanup/Program.fs
+++ tools/MongoCleanup/Program.fs
@@ -1,18 +1,43 @@
 ﻿open Argu
 open MongoDB.Driver
+open System.IO
+open FSharp.Json
 
 type CLIArguments =
-    | [<Mandatory>] Connection_String of string
+    | [<Mandatory>] Cloak_Config of path:string
     interface IArgParserTemplate with
         member s.Usage =
             match s with
-            | Connection_String _ -> "specify a connection string"
+            | Cloak_Config _ -> "specify the cloak config describing the data source"
+
+type ConnectionProperties = {
+    hostname: string
+    database: string
+    port: int option
+    username: string option
+    password: string option
+}
+
+type CloakConfig = {
+    parameters: ConnectionProperties
+}
 
 let optionParser = ArgumentParser.Create<CLIArguments>(programName = "MongoCleanup")
 
+let buildMongoSettings (cloakConfig: CloakConfig): MongoClientSettings =
+    let options = cloakConfig.parameters
+
+    MongoClientSettings(
+        Server = MongoServerAddress(options.hostname, options.port |> Option.defaultValue 27017)
+    )
+
 let run (options: ParseResults<CLIArguments>): unit =
-    let conn = MongoClient (options.GetResult Connection_String)
-    let db = conn.GetDatabase "teambank_konto_service"
+    use stream = new StreamReader(options.GetResult Cloak_Config)
+
+    let config = stream.ReadToEnd () |> Json.deserialize<CloakConfig>
+    let mongoSettings = buildMongoSettings config
+    let conn = MongoClient mongoSettings
+    let db = conn.GetDatabase config.parameters.database
 
     db.ListCollectionNames().ToList()
     |> printfn "%A"
