c2221d1ca7626aa88a5ee52ffab08e1282cc110d
Cristian Berneanu <cristian@aircloak.com>
2019-02-06 13:34:10 +0200

    Adds visual indicators for the key type in the web UI

--- air/assets/test/filter_test.js
+++ air/assets/test/filter_test.js
@@ -2,8 +2,8 @@ import assert from "assert";
 
 import {Filter, EmptyFilter} from "selectable_info/filter";
 
-const column1 = {name: "name1", type: "type1", user_id: false};
-const column2 = {name: "name2", type: "type2", user_id: true};
+const column1 = {name: "name1", type: "type1", key_type: null};
+const column2 = {name: "name2", type: "type2", key_type: "user_id"};
 const columns = [column1, column2];
 
 it("constructs successfully", () => {
