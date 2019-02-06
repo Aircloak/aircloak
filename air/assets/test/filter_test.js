856000c8928b26df27deebfa875abf774afe3826
Cristian Berneanu <cristian@aircloak.com>
2019-02-06 16:36:24 +0200

    Include column with non-user_id key type in test

--- air/assets/test/filter_test.js
+++ air/assets/test/filter_test.js
@@ -4,7 +4,8 @@ import {Filter, EmptyFilter} from "selectable_info/filter";
 
 const column1 = {name: "name1", type: "type1", key_type: null};
 const column2 = {name: "name2", type: "type2", key_type: "user_id"};
-const columns = [column1, column2];
+const column3 = {name: "name3", type: "type3", key_type: "account_id"};
+const columns = [column1, column2, column3];
 
 it("constructs successfully", () => {
   assert.doesNotThrow(() => new Filter(/test/));
@@ -60,6 +61,6 @@ describe("filterColumns", () => {
 
   it("allows filters using regex capabilities", () => {
     const filter = new Filter(/name(1|3)/);
-    assert.deepEqual(filter.filterColumns(columns), [column1]);
+    assert.deepEqual(filter.filterColumns(columns), [column1, column3]);
   });
 });
