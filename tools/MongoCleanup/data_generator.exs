b2c7fe69ced9adb5a4e9c53f58ccfffd4156af7f
Paweł Obrok <pawel.obrok@gmail.com>
2019-02-07 18:28:32 +0100

    Add third table to MongoCleanup data generator

--- tools/MongoCleanup/data_generator.exs
+++ tools/MongoCleanup/data_generator.exs
@@ -2,8 +2,9 @@ defmodule DataGenerator do
   def generate(size) do
     users = Enum.map(1..size, fn _ -> generate_user() end)
     purchases = Enum.flat_map(users, &generate_purchases/1)
+    items = Enum.flat_map(purchases, &generate_items/1)
 
-    {users, purchases}
+    {users, purchases, items}
   end
 
   def write(data, filename) do
@@ -20,16 +21,30 @@ defmodule DataGenerator do
   end
 
   def generate_purchases(user) do
-    for _ <- 1..10 do
+    for _ <- 0..:rand.uniform(20) do
       %{
+        id: "id-#{:erlang.unique_integer([:positive])}",
         user_id: user.id,
         date: %{"$date" => "#{NaiveDateTime.utc_now() |> NaiveDateTime.to_iso8601()}Z"}
       }
     end
   end
+
+  def generate_items(purchase) do
+    for _ <- 0..:rand.uniform(20) do
+      %{
+        purchase_id: purchase.id,
+        itemname: "widget-#{:rand.uniform(1000)}",
+        quantity: :rand.uniform(100) + 1,
+        price: :rand.uniform()
+      }
+    end
+  end
 end
 
-{users, purchases} = DataGenerator.generate(10)
+size = (System.get_env("SIZE") || "10") |> String.to_integer()
+{users, purchases, items} = DataGenerator.generate(size)
 
 DataGenerator.write(users, "/tmp/users.json")
 DataGenerator.write(purchases, "/tmp/purchases.json")
+DataGenerator.write(items, "/tmp/items.json")
