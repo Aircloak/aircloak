f5df3144d63e2e04399e530d3d1d55d35f116675
Sebastian Probst Eide <sebastian@aircloak.com>
2019-02-08 10:31:11 +0100

    Reorder migration to make it executable

--- air/priv/repo/migrations/20190207122651_add_creation_status_to_analyst_table.exs
+++ air/priv/repo/migrations/20190207122651_add_creation_status_to_analyst_table.exs
@@ -23,17 +23,18 @@ defmodule Air.Repo.Migrations.AddCreationStatusToAnalystTable do
   end
 
   def down do
+    execute("drop view analyst_tables")
+    execute("drop view views")
+
     alter table(:user_selectables) do
       remove(:creation_status)
     end
 
     CreationStatus.drop_type()
 
-    execute("drop view analyst_tables")
     execute("create view analyst_tables as select * from user_selectables where type = 'analyst_table'")
     execute("alter view analyst_tables alter column type set default 'analyst_table'")
 
-    execute("drop view views")
     execute("create view views as select * from user_selectables where type = 'view'")
     execute("alter view views alter column type set default 'view'")
   end
