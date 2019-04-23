c6a656e00b5cafcb5eefe856aca542f92dcb6e02
Sebastian Probst Eide <sebastian@aircloak.com>
2019-04-23 11:13:34 +0200

    Add more features in central for things that can be licensed

--- central/lib/central_web/views/license_view.ex
+++ central/lib/central_web/views/license_view.ex
@@ -17,6 +17,34 @@ defmodule CentralWeb.LicenseView do
   defp feature_enabled?(%{data: %{features: features}}, feature), do: feature in features
 
   defp features() do
-    %{"LDAP" => "ldap"}
+    %{
+      "LDAP" => "ldap",
+      "Offline deployment" => "offline_deployment",
+      "Analyst tables" => "analyst_tables",
+      "Email support" => "email_support"
+    }
   end
+
+  defp datasource_features() do
+    %{
+      "Postgres" => "ds_postgres",
+      "MySQL and MariaDB" => "ds_mysql",
+      "SQL Server" => "ds_sqlserver",
+      "MongoDB" => "ds_mongodb",
+      "Apache Drill" => "ds_apachedrill",
+      "Oracle DB" => "ds_oracle"
+    }
+  end
+
+  defp name_features(mapping, license),
+    do:
+      mapping
+      |> Enum.filter(fn {name, key} -> Enum.member?(license.features, key) end)
+      |> Enum.map(fn {name, _} -> name end)
+
+  defp datasource_features(license), do: name_features(datasource_features(), license)
+
+  defp core_features(license), do: name_features(features(), license)
+
+  defp licensed_features(license), do: Aircloak.OxfordComma.join(core_features(license) ++ datasource_features(license))
 end
