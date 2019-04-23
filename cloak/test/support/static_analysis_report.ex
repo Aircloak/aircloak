37bbb98a837a9fcc22e3427fd2651ba3c7836699
Paweł Obrok <pawel.obrok@gmail.com>
2019-04-23 14:32:18 +0200

    Format the static analysis report

--- cloak/test/support/static_analysis_report.ex
+++ cloak/test/support/static_analysis_report.ex
@@ -6,7 +6,16 @@ defmodule Test.StaticAnalysisReport do
   def report(expression, input_bounds, output_bounds, actual_safe?, method, method_safe?, restriction_safe?) do
     GenServer.call(
       __MODULE__,
-      {:report, {expression, input_bounds, output_bounds, actual_safe?, method, method_safe?, restriction_safe?}}
+      {:report,
+       %{
+         expression: expression,
+         input_bounds: input_bounds,
+         output_bounds: output_bounds,
+         actual_safe?: actual_safe?,
+         method: method,
+         method_safe?: method_safe?,
+         restriction_safe?: restriction_safe?
+       }}
     )
   end
 
@@ -31,6 +40,28 @@ defmodule Test.StaticAnalysisReport do
 
   defp print_report(state) do
     IO.puts("\nSTATIC ANALYSIS REPORT\n")
-    IO.inspect(state)
+
+    state
+    |> Enum.group_by(&Map.take(&1, [:expression, :input_bounds, :output_bounds, :actual_safe?, :restriction_safe?]))
+    |> Enum.sort()
+    |> Enum.each(fn {%{
+                       expression: expression,
+                       input_bounds: input_bounds,
+                       output_bounds: output_bounds,
+                       actual_safe?: actual_safe?,
+                       restriction_safe?: restriction_safe?
+                     }, runs} ->
+      IO.puts("\n#{expression} on #{inspect(input_bounds)} restricted to #{inspect(output_bounds)}\n")
+      IO.puts("Safe?: #{actual_safe?}")
+      IO.puts("Rejected by restritions?: #{!restriction_safe?}")
+      IO.puts("Rejected by static analysis methods: #{count(runs, &(!&1.method_safe?))}")
+    end)
+  end
+
+  defp count(all, fun) do
+    total = length(all)
+    positive = Enum.count(all, fun)
+
+    "#{positive}/#{total} (#{round(100 * positive / total)}%)"
   end
 end
