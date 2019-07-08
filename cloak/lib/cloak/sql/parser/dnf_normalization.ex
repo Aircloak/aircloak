e740dc9dab1fc20b9cb69c961699b6793fcadfbe
Paweł Obrok <pawel.obrok@gmail.com>
2019-07-08 14:24:02 +0200

    Implement DNF normalization

--- cloak/lib/cloak/sql/parser/dnf_normalization.ex
+++ cloak/lib/cloak/sql/parser/dnf_normalization.ex
@@ -1,3 +1,34 @@
 defmodule Cloak.Sql.Parser.DNFNormalization do
-  def dnf(formula), do: formula
+  alias Cloak.Sql.Query
+
+  def dnf(formula), do: formula |> push_in_negation() |> push_in_conjunction()
+
+  defp push_in_conjunction(formula) do
+    update_in(formula, [Query.Lenses.all_conditions()], fn
+      {:and, {:or, a, b}, {:or, c, d}} ->
+        {:or, {:or, push_in_conjunction({:and, a, c}), push_in_conjunction({:and, a, d})},
+         {:or, push_in_conjunction({:and, b, c}), push_in_conjunction({:and, b, d})}}
+
+      {:and, {:or, a, b}, c} ->
+        {:or, push_in_conjunction({:and, a, c}), push_in_conjunction({:and, b, c})}
+
+      {:and, c, {:or, a, b}} ->
+        {:or, push_in_conjunction({:and, a, c}), push_in_conjunction({:and, b, c})}
+
+      other ->
+        other
+    end)
+  end
+
+  defp push_in_negation(formula) do
+    update_in(formula, [Query.Lenses.all_conditions()], fn
+      {:not, expr} -> negate(expr)
+      other -> other
+    end)
+  end
+
+  defp negate({:not, expr}), do: expr
+  defp negate({:and, lhs, rhs}), do: {:or, negate(lhs), negate(rhs)}
+  defp negate({:or, lhs, rhs}), do: {:and, negate(lhs), negate(rhs)}
+  defp negate(expr), do: {:not, expr}
 end
