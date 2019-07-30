ec1aaf8888002e151449fb36f59a4a863d12dccd
Cristian Berneanu <cristian@aircloak.com>
2019-07-30 15:34:50 +0300

    Remove obsolete validation code

--- cloak/lib/cloak/sql/compiler/validation.ex
+++ cloak/lib/cloak/sql/compiler/validation.ex
@@ -108,14 +108,6 @@ defmodule Cloak.Sql.Compiler.Validation do
       )
     end
 
-    if Function.has_attribute?(name, {:not_in, query.type}) do
-      raise(
-        CompilationError,
-        source_location: expression.source_location,
-        message: "Function `#{Function.readable_name(name)}` is not allowed in `#{query.type}` subqueries."
-      )
-    end
-
     if query.type != :standard and Function.unsafe?(name) do
       raise(
         CompilationError,
