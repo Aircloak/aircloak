8fa5356fb1d16c42b96a8ce531d4b55089832377
Cristian Berneanu <cristian@aircloak.com>
2019-08-14 15:41:04 +0300

    Add field for anonymization type in `Query` structure

--- cloak/lib/cloak/sql/query/features.ex
+++ cloak/lib/cloak/sql/query/features.ex
@@ -36,7 +36,7 @@ defmodule Cloak.Sql.Query.Features do
       driver_dialect: sql_dialect_name(query.data_source),
       shadow_tables_used: shadow_tables_used?(query),
       isolators_used: isolators_used?(query),
-      anonymization_type: anonymization_type(query)
+      anonymization_type: to_string(query.anonymization_type)
     }
   end
 
@@ -220,7 +220,4 @@ defmodule Cloak.Sql.Query.Features do
         Query.max_rare_negative_conditions(anonymized_query)
 
   defp isolators_used?(query), do: Enum.count(TypeChecker.Access.potential_unclear_isolator_usages(query)) > 0
-
-  defp anonymization_type(%Query{from: {:subquery, %{alias: "__ac_statistics"}}}), do: "Statistics"
-  defp anonymization_type(_query), do: "UserId"
 end
