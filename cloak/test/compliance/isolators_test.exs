d0c25e538938d587314154ab9b3bfa944f172ac4
Cristian Berneanu <cristian@aircloak.com>
2019-09-03 13:10:25 +0300

    Refactor compliance test for isolators

--- cloak/test/compliance/isolators_test.exs
+++ cloak/test/compliance/isolators_test.exs
@@ -1,27 +1,11 @@
 defmodule Compliance.Isolators.Test do
   use ComplianceCase, async: true
 
-  [
-    {"height", "users"},
-    {"age", "users"},
-    {"id", "notes"},
-    {"user_fk", "addresses"},
-    {"home.postal_code", "addresses"},
-    {"work.postal_code", "addresses"},
-    {"user_fk", "notes"},
-    {"note_id", "notes_changes"},
-    {"date", "notes_changes"},
-    {"birthday", "users"},
-    {"name", "users"},
-    {"column_with_a_very_long_name", "users"},
-    {"home.city", "addresses"},
-    {"work.city", "addresses"},
-    {"title", "notes"},
-    {"content", "notes"},
-    {"change", "notes_changes"},
-    {"nullable", "users"}
-  ]
-  |> Enum.each(fn {column, table} ->
+  all_columns()
+  |> Enum.each(fn {column, from} ->
+    [table | _] = String.split(from)
+    column = if column == "n.id", do: "id", else: column
+
     @tag compliance: "isolator query on #{column} in #{table}"
     test "isolator query on #{column} from #{table}", context do
       results =
