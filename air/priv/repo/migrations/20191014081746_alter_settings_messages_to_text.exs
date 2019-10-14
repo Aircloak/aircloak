786f7f7e56b87ddba88006b280be8981c56b72b0
Cristian Berneanu <cristian@aircloak.com>
2019-10-14 11:48:01 +0300

    Fix error in air migration tests

--- air/priv/repo/migrations/20191014081746_alter_settings_messages_to_text.exs
+++ air/priv/repo/migrations/20191014081746_alter_settings_messages_to_text.exs
@@ -1,10 +1,17 @@
 defmodule Air.Repo.Migrations.AlterSettingsMessagesToText do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:settings) do
       modify(:login_message, :text)
       modify(:main_message, :text)
+    end
+  end
+
+  def down do
+    alter table(:settings) do
+      modify(:login_message, :string)
+      modify(:main_message, :string)
     end
   end
 end
