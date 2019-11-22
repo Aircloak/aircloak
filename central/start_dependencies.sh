962be1510cd5ffad46ce1fbf6460540b1c4ddfa9
Edon Gashi <edon@aircloak.com>
2019-11-22 15:36:38 +0100

    Remove docker_volumes from gitignore

--- central/start_dependencies.sh
+++ central/start_dependencies.sh
@@ -7,7 +7,6 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 ./db/build-image.sh
 
-
 echo "Starting databases for central"
 DB_ENV="dev" ./db/container.sh ensure_started
 DB_ENV="test" ./db/container.sh ensure_started
