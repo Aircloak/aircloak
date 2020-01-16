4634028a212701af84f1227394b2e432ccb8ac6e
Sebastian Probst Eide <sebastian@aircloak.com>
2020-01-16 13:00:42 +0100

    Bootstrap tooltip requires jQuery available globally
    This is somewhat ugly. Also it doesn't prevent us from having
    to import jQuery everywhere it is needed in order to enable
    flow to type check the usage of the functions.

--- air/assets/js/tooltips.js
+++ air/assets/js/tooltips.js
@@ -2,5 +2,6 @@
 
 import _ from "lodash";
 import $ from "jquery";
+import bootstrap from "bootstrap";
 
 export const activateTooltips = () => _.defer(() => $("[data-toggle='tooltip']").tooltip());
