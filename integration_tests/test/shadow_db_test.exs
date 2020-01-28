4169efcf2031771132f10a55b18882d7cd0c5474
Edon Gashi <edon@aircloak.com>
2020-01-28 11:36:16 +0100

    Format dt query to be human readable

--- integration_tests/test/shadow_db_test.exs
+++ integration_tests/test/shadow_db_test.exs
@@ -6,7 +6,33 @@ defmodule IntegrationTest.ShadowDbTest do
   import IntegrationTest.Helpers
 
   # The following query is an intercepted \dt request.
-  @dt_query "SELECT n.nspname as \"Schema\", c.relname as \"Name\", CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' WHEN 'p' THEN 'partitioned table' WHEN 'I' THEN 'partitioned index' END as \"Type\", pg_catalog.pg_get_userbyid(c.relowner) as \"Owner\" FROM pg_catalog.pg_class c LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace WHERE c.relkind IN ('r','p','') AND n.nspname <> 'pg_catalog' AND n.nspname <> 'information_schema' AND n.nspname !~ '^pg_toast' AND pg_catalog.pg_table_is_visible(c.oid) ORDER BY 1,2;"
+  @dt_query """
+    SELECT
+      n.nspname AS "Schema",
+      c.relname AS "Name",
+      CASE c.relkind
+        WHEN 'r' THEN 'table'
+        WHEN 'v' THEN 'view'
+        WHEN 'm' THEN 'materialized view'
+        WHEN 'i' THEN 'index'
+        WHEN 'S' THEN 'sequence'
+        WHEN 's' THEN 'special'
+        WHEN 'f' THEN 'foreign table'
+        WHEN 'p' THEN 'partitioned table'
+        WHEN 'I' THEN 'partitioned index'
+      END AS "Type",
+      pg_catalog.Pg_get_userbyid(c.relowner) AS "Owner"
+    FROM
+      pg_catalog.pg_class c
+      LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+    WHERE
+      c.relkind IN ('r', 'p', '')
+      AND n.nspname <> 'pg_catalog'
+      AND n.nspname <> 'information_schema'
+      AND n.nspname !~ '^pg_toast'
+      AND pg_catalog.Pg_table_is_visible(c.oid)
+    ORDER BY 1, 2;
+  """
 
   setup do
     enable_shadowdb_synchronization()
