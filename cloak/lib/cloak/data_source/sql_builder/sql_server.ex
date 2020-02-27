865323f95a72cf643d07a4954fb0266995287d96
Edon Gashi <edon@aircloak.com>
2020-02-27 14:55:06 +0100

    Force emulation of ltrim/2 and rtrim/2 in SQL Server

--- cloak/lib/cloak/data_source/sql_builder/sql_server.ex
+++ cloak/lib/cloak/data_source/sql_builder/sql_server.ex
@@ -16,7 +16,7 @@ defmodule Cloak.DataSource.SqlBuilder.SQLServer do
       sqrt floor ceil abs round trunc mod ^ * / + - %
       unsafe_pow unsafe_mul unsafe_div unsafe_add unsafe_sub unsafe_sub unsafe_mod
       checked_mod checked_div checked_pow
-      length lower upper ltrim rtrim left right substring concat
+      length lower upper ltrim/1 rtrim/1 left right substring concat
       hex cast coalesce grouping_id case
     )
 
