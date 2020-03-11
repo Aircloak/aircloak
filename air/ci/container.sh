aa37fc5c1b724faafdfc08187118b15ad5728307
Sebastian Probst Eide <sebastian@aircloak.com>
2020-03-11 14:42:04 +0100

    Make cloak docs available during CI build

--- air/ci/container.sh
+++ air/ci/container.sh
@@ -36,7 +36,7 @@ function prepare_for_test {
 mount_to_aircloak VERSION common/elixir bom
 mount_to_component \
   .flowconfig .gitignore assets config datagen docs include lib perftest priv rel test mix.exs mix.lock Makefile \
-  README.md check_warnings.sh .formatter.exs
+  README.md check_warnings.sh .formatter.exs ../cloak/docs
 mount_cached_component deps _build .bash_history docs/_book docs/node_modules priv/static
 mount $(ci_tmp_folder)/air/.gitbook /root/.gitbook
 
