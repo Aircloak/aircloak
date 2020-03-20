b0d000f7f68dc963f50b21f6db27a0acea1f1506
Edon Gashi <edon@aircloak.com>
2020-03-20 13:08:49 +0100

    Ensure blacklisted columns are ignored in table discovery

--- cloak/test/cloak/data_source_test.exs
+++ cloak/test/cloak/data_source_test.exs
@@ -6,13 +6,16 @@ defmodule Cloak.DataSourceTest do
   import ExUnit.CaptureLog
 
   setup_all do
-    :ok = Cloak.Test.DB.create_table("test", "value INTEGER")
+    :ok =
+      Cloak.Test.DB.create_table("test", "value INTEGER, ignored_column INTEGER",
+        blacklisted_columns: ["ignored_column"]
+      )
 
     :ok =
-      Cloak.Test.DB.insert_data("test", ["user_id", "value"], [
-        ["user1", 10],
-        ["user1", 20],
-        ["user1", 30]
+      Cloak.Test.DB.insert_data("test", ["user_id", "value", "ignored_column"], [
+        ["user1", 10, 100],
+        ["user1", 20, 200],
+        ["user1", 30, 300]
       ])
 
     :ok
