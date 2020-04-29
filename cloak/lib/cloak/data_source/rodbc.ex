dcc13ece2ab6e4db7747740a58e653f7c2a650a8
Sebastian Probst Eide <sebastian@aircloak.com>
2020-04-29 11:49:04 +0200

    Don't print out credentials

--- cloak/lib/cloak/data_source/rodbc.ex
+++ cloak/lib/cloak/data_source/rodbc.ex
@@ -20,7 +20,6 @@ defmodule Cloak.DataSource.RODBC do
     normalized_parameters
     |> conn_params_extractor.()
     |> Map.merge(Map.get(normalized_parameters, :odbc_parameters, %{}))
-    |> IO.inspect()
     |> driver_connect(driver_params)
   end
 
