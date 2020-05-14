cf71c7f6bcc1a21c9670d1a61b4f32b9ff7eeb2f
Cristian Berneanu <cristian@aircloak.com>
2020-05-14 15:20:10 +0300

    Refactor type checker code use standard API.

--- cloak/lib/cloak/sql/range.ex
+++ cloak/lib/cloak/sql/range.ex
@@ -121,7 +121,7 @@ defmodule Cloak.Sql.Range do
   defp implicit_range?(%Expression{kind: :constant}, _query), do: false
 
   defp implicit_range?(function = %Expression{kind: :function, args: args}, query) do
-    if Function.has_attribute?(function, :implicit_range) do
+    if Function.implicit_range?(function) do
       true
     else
       Enum.any?(args, &implicit_range?(&1, query))
