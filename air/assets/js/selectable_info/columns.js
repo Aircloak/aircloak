9a6da3f38e25938f091ce510e1d25e0da3c63153
Jakub Hampl <kopomir@gmail.com>
2020-05-28 12:12:52 +0200

    Adds column icons

--- air/assets/js/selectable_info/columns.js
+++ air/assets/js/selectable_info/columns.js
@@ -10,12 +10,43 @@ export type Column = {
   key_type: string,
 };
 
-const potentiallyRenderColumnIcon = (column: Column) => {
+const typeColors = {
+  boolean: "#fffbc6",
+  integer: "#e8ffdd",
+  real: "#e7f9cf",
+  text: "#fff2f2",
+  date: "#e8f2ff",
+  datetime: "#e9e8ff",
+};
+
+const columnIcon = (column: Column) => {
   if (column.key_type) {
     const icon = column.key_type === "user_id" ? "user" : "link";
-    return <span className={`fas fa-${icon}`}>&nbsp;</span>;
+    return (
+      <span
+        className={`type-icon ${icon}-icon`}
+        data-toggle="tooltip"
+        style={{
+          backgroundColor: typeColors[column.type] || "transparent",
+        }}
+        title={`${column.key_type}:${column.type}`}
+      >
+        <span className={`fas fa-${icon}`}>&nbsp;</span>
+      </span>
+    );
   } else {
-    return null;
+    return (
+      <span
+        className="type-icon native-type"
+        style={{
+          backgroundColor: typeColors[column.type] || "transparent",
+        }}
+        data-toggle="tooltip"
+        title={`${column.type}`}
+      >
+        {column.type[0].toUpperCase()}
+      </span>
+    );
   }
 };
 
@@ -41,46 +72,35 @@ export const ColumnsView = ({
   columns: Column[],
 }) => {
   return (
-    <table className="table table-condensed">
-      <thead>
-        <tr>
-          <th>Column</th>
-          <th>Type</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {filterColumns(table, columns, filter).map((item) => {
-          return (
-            <tr key={item.name}>
-              {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions,
-                               jsx-a11y/click-events-have-key-events */}
-              <td
-                onClick={(event) => {
-                  event.preventDefault();
-                  window.insertWordInEditor(item.name);
-                }}
-                className={columnClassName(item)}
-              >
-                {potentiallyRenderColumnIcon(item)}
-                <Higlighted table={table} column={item} field="name" />
-              </td>
-              {/* eslint-enable jsx-a11y/no-noninteractive-element-interactions,
-                               jsx-a11y/click-events-have-key-events */}
-              <td>
-                <Higlighted table={table} column={item} field="type" />
-                {item.key_type && (
-                  <>
-                    {" ("}
-                    <Higlighted table={table} column={item} field="key_type" />
-                    {")"}
-                  </>
-                )}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <ul className="list-group list-group-flush">
+      {filterColumns(table, columns, filter).map((item) => {
+        return (
+          <li
+            className={`list-group-item p-1  pr-3 ${columnClassName(
+              item
+            )} d-flex justify-content-between align-items-center`}
+            key={item.name}
+          >
+            <button
+              className="btn"
+              onClick={(event) => {
+                event.preventDefault();
+                window.insertWordInEditor(item.name);
+              }}
+            >
+              {columnIcon(item)}
+              <Higlighted table={table} column={item} field="name" />{" "}
+              {item.key_type && (
+                <>
+                  {" ("}
+                  <Higlighted table={table} column={item} field="key_type" />
+                  {")"}
+                </>
+              )}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
