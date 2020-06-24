269a6d3141c95e8696efa76a73dae1169292d115
Edon Gashi <edon@aircloak.com>
2020-06-24 11:56:06 +0200

    Make shadow db reuse last queried schemas

--- air/lib/air/psql_server/shadow_db.ex
+++ air/lib/air/psql_server/shadow_db.ex
@@ -6,6 +6,8 @@ defmodule Air.PsqlServer.ShadowDb do
   We maintain these databases so we can offload PostgreSQL specific queries issued over the psql interface.
   """
 
+  use GenServer
+
   require Logger
   require Aircloak.DeployConfig
   alias Air.PsqlServer.ShadowDb.{Connection, Schema}
@@ -18,6 +20,8 @@ defmodule Air.PsqlServer.ShadowDb do
           password: String.t(),
           name: String.t()
         }
+
+  @idle_timeout :timer.seconds(30)
 
   # -------------------------------------------------------------------
   # API functions
@@ -35,17 +39,58 @@ defmodule Air.PsqlServer.ShadowDb do
   @doc "Executes the query on the given data source."
   @spec query(User.t(), String.t(), String.t(), [term]) :: Connection.query_result()
   def query(user, data_source_name, query, params) do
-    run(user, data_source_name, fn conn ->
-      Connection.query(conn, query, params)
-    end)
+    GenServer.call(__MODULE__, {:query, user, data_source_name, query, params})
   end
 
   @doc "Parses the query on the given data source."
   @spec parse(User.t(), String.t(), String.t()) :: Connection.parse_result()
   def parse(user, data_source_name, query) do
-    run(user, data_source_name, fn conn ->
-      Connection.parse(conn, query)
-    end)
+    GenServer.call(__MODULE__, {:parse, user, data_source_name, query})
+  end
+
+  # -------------------------------------------------------------------
+  # GenServer callbacks
+  # -------------------------------------------------------------------
+
+  @impl GenServer
+  def init(_arg),
+    do: {:ok, %{conn: nil, last_db: nil}}
+
+  @impl GenServer
+  def handle_call(
+        {:query, user, data_source_name, query, params},
+        _from,
+        state
+      ) do
+    new_state = prepare_connection(user, data_source_name, state)
+    result = Connection.query(new_state.conn, query, params)
+    {:reply, result, new_state, @idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_call(
+        {:parse, user, data_source_name, query},
+        _from,
+        state
+      ) do
+    new_state = prepare_connection(user, data_source_name, state)
+    result = Connection.parse(new_state.conn, query)
+    {:reply, result, new_state, @idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, %{last_db: nil} = state), do: {:noreply, state}
+
+  def handle_info(:timeout, %{conn: conn}) do
+    Connection.execute!(conn, "ROLLBACK")
+    {:noreply, %{conn: conn, last_db: nil}}
+  end
+
+  @impl GenServer
+  def terminate(_reason, %{conn: conn}) do
+    if conn do
+      Connection.close(conn)
+    end
   end
 
   # -------------------------------------------------------------------
@@ -63,36 +108,51 @@ defmodule Air.PsqlServer.ShadowDb do
     }
   end
 
+  defp prepare_connection(user, data_source_name, %{conn: conn, last_db: nil} = _state) do
+    conn = ensure_connection(conn)
+    Connection.execute!(conn, "BEGIN TRANSACTION")
+    build_schema!(conn, user, data_source_name)
+    %{conn: conn, last_db: {user, data_source_name}}
+  end
+
+  defp prepare_connection(user, data_source_name, %{last_db: {last_user, last_data_source_name}} = state)
+       when user == last_user and data_source_name == last_data_source_name do
+    state
+  end
+
+  defp prepare_connection(user, data_source_name, %{conn: conn} = _state) do
+    Connection.execute!(conn, "ROLLBACK")
+    Connection.execute!(conn, "BEGIN TRANSACTION")
+    build_schema!(conn, user, data_source_name)
+    %{conn: conn, last_db: {user, data_source_name}}
+  end
+
+  defp ensure_connection(nil), do: Connection.open!()
+  defp ensure_connection(conn), do: conn
+
   # We chunk updates in batches because the operation may fail if the SQL string becomes too long.
   @chunk_size 30
 
-  defp run(user, data_source_name, fun) do
-    :global.trans({__MODULE__, self()}, fn ->
-      {time, result} =
-        :timer.tc(fn ->
-          Connection.run!(fn conn ->
-            Connection.execute!(conn, "BEGIN TRANSACTION")
-
-            try do
-              build_schema!(conn, user, data_source_name)
-              fun.(conn)
-            after
-              Connection.execute!(conn, "ROLLBACK")
-            end
-          end)
+  defp build_schema!(conn, user, data_source_name) do
+    {time, _result} =
+      :timer.tc(fn ->
+        Schema.create_table_statements(user, data_source_name)
+        |> Enum.chunk_every(@chunk_size)
+        |> Enum.each(fn chunk ->
+          sql = Enum.join(chunk, "\n")
+          Connection.execute!(conn, sql)
         end)
+      end)
 
-      Logger.debug(fn -> "Shadow db operation took #{time / 1000}ms." end)
-      result
-    end)
+    Logger.debug(fn -> "Shadow db creation took #{time / 1000}ms." end)
   end
 
-  defp build_schema!(conn, user, data_source_name) do
-    Schema.create_table_statements(user, data_source_name)
-    |> Enum.chunk_every(@chunk_size)
-    |> Enum.each(fn chunk ->
-      sql = Enum.join(chunk, "\n")
-      Connection.execute!(conn, sql)
-    end)
+  # -------------------------------------------------------------------
+  # Supervision tree
+  # -------------------------------------------------------------------
+
+  @doc false
+  def start_link(_arg) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 end
