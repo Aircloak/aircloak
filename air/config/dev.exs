ed5fd0d3afcc4959c40e5ca2b99c759500a7abef
Jakub Hampl <kopomir@gmail.com>
2020-06-29 13:29:49 +0200

    Spurious change to make github cooperate

--- air/config/dev.exs
+++ air/config/dev.exs
@@ -38,7 +38,7 @@ config :air, :central,
   max_reconnect_interval: 1000
 
 # Use smaller pools in dev for endpoints and repo. We shouldn't issue a huge load in dev mode anyway, and
-# less processes makes the supervision tree view in observer nicer.
+# less processes makes the supervision tree view in observer easier to read.
 config :air, AirWeb.Endpoint,
   http: [transport_options: [num_acceptors: 2]],
   https: [transport_options: [num_acceptors: 2]]
