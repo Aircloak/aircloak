c8fb375e949430e24764e8457c904d5c4ba1ba85
Jakub Hampl <kopomir@gmail.com>
2020-06-30 16:02:04 +0200

    Fix downgrade BOM

--- bom/lib/bom/whitelist.ex
+++ bom/lib/bom/whitelist.ex
@@ -13,7 +13,7 @@ defmodule BOM.Whitelist do
       {"excoveralls", "0.5.7"} => %{type: :mit, text: :standard},
       {"file_system", "0.2.8"} => %{type: :wtfpl, text: :standard},
       {"makeup_elixir", "0.14.1"} => %{type: :bsd_4_clause, text: :standard},
-      {"scrivener_ecto", "2.4.0"} => %{type: :mit, text: :provided},
+      {"scrivener_ecto", "2.2.0"} => %{type: :mit, text: :provided},
       {"scrivener", "2.7.0"} => %{type: :mit, text: :provided}
     },
     :node => %{
