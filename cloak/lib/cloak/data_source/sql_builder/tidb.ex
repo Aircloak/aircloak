9f8a5c59f522793f40e3934d68d0614b10f3d100
Cristian Berneanu <cristian@aircloak.com>
2020-07-02 22:17:18 +0300

    Fix `ILIKE` implementation for the TiDB dialect.

--- cloak/lib/cloak/data_source/sql_builder/tidb.ex
+++ cloak/lib/cloak/data_source/sql_builder/tidb.ex
@@ -13,6 +13,7 @@ defmodule Cloak.DataSource.SqlBuilder.TiDB do
   def function_sql("stddev", [arg]), do: function_sql("sqrt", [function_sql("variance", [arg])])
   def function_sql("variance", [arg]), do: ["VAR_POP(", arg, ") * (1.0 + 1.0 / (COUNT(", arg, ") - 1))"]
   def function_sql("unsafe_div", [arg1, arg2]), do: ["(CAST(", arg1, " AS double) / ", arg2, ")"]
+  def function_sql("ilike", [subject, pattern]), do: ["(LOWER(", subject, ") LIKE LOWER(", pattern, "))"]
   def function_sql(name, args), do: MySQL.function_sql(name, args)
 
   defdelegate limit_sql(limit, offset), to: MySQL
