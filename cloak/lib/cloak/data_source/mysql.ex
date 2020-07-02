818a776cdb0fce16fdd901d5a3e3caf0c8068e05
Cristian Berneanu <cristian@aircloak.com>
2020-07-02 22:03:07 +0300

    Refactor after connect calls in MySQL driver.

--- cloak/lib/cloak/data_source/mysql.ex
+++ cloak/lib/cloak/data_source/mysql.ex
@@ -18,8 +18,8 @@ defmodule Cloak.DataSource.MySQL do
   @impl Driver
   def connect(parameters) do
     with {:ok, connection} <- do_connect(parameters) do
-      {:ok, %MyXQL.Result{}} = MyXQL.query(connection, "SET sql_mode = 'ANSI,NO_BACKSLASH_ESCAPES'", [])
-      {:ok, %MyXQL.Result{}} = MyXQL.query(connection, "SET div_precision_increment = 30", [])
+      MyXQL.query!(connection, "SET sql_mode = 'ANSI,NO_BACKSLASH_ESCAPES'")
+      MyXQL.query!(connection, "SET div_precision_increment = 30")
       {:ok, connection}
     end
   end
