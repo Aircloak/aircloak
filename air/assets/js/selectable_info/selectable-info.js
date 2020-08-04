ce632884a61e51325195bcb8249339547763ec2d
Jakub Hampl <kopomir@gmail.com>
2020-08-04 13:07:44 +0200

    Fixes invalid DOM nesting warning

--- air/assets/js/selectable_info/selectable-info.js
+++ air/assets/js/selectable_info/selectable-info.js
@@ -45,14 +45,14 @@ const SelectableDetails = React.forwardRef(
           <Popover.Content>
             <div style={{ maxHeight: "50vh", overflowY: "auto" }}>
               {selectable.comment && (
-                <p className="border rounded px-2 py-1">
+                <div className="border rounded px-2 py-1">
                   <strong className="float-right text-muted ml-3 mt-1 font-weight-bold text-uppercase small">
                     Comment
                   </strong>
                   <div style={{ whiteSpace: "pre-line" }}>
                     {selectable.comment}
                   </div>
-                </p>
+                </div>
               )}
 
               {isAnalystCreatedSelectable(selectable) && (
