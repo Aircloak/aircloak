7d256b11e7a982b2a70ccde1c8567de4aa4eb838
Cristian Berneanu <cristian@aircloak.com>
2020-11-05 14:07:08 +0200

    Fix copy to clipboard action on Firefox.

--- air/assets/js/copy_to_clipboard.js
+++ air/assets/js/copy_to_clipboard.js
@@ -1,14 +1,11 @@
 // @flow
 
 export default (targetId: string) => {
-  const range = document.createRange();
   const target = document.getElementById(targetId);
 
-  if (target) {
-    range.selectNode(target);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
+  if (target && target instanceof HTMLInputElement) {
+    target.focus();
+    target.select();
     document.execCommand("copy");
-    window.getSelection().removeAllRanges();
   }
 };
