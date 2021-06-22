33da60d4bbd1ae93e1b88e26551434708cbc831b
Sebastian Probst Eide <sebastian@aircloak.com>
2021-06-22 17:41:59 +0200

    Fix audit log links
    Use the URI encoder for the date, and use full date to allow hour resolution to work too

--- air/lib/air_web/views/admin/system_status_view.ex
+++ air/lib/air_web/views/admin/system_status_view.ex
@@ -3,14 +3,19 @@ defmodule AirWeb.Admin.SystemStatusView do
   use Air.Web, :view
 
   def audit_log_url(metadata) do
-    start_time = metadata.start_time
-    from = "from=#{start_time.year}-#{start_time.month}-#{start_time.day}"
+    time_range =
+      URI.encode_query(%{
+        from: date_time_to_string(metadata.start_time),
+        to: date_time_to_string(DateTime.utc_now())
+      })
+
     events =
       metadata.event_types
-      |> Enum.map(fn event_type ->
-        "events[]=#{event_type}"
-      end)
+      |> Enum.map(& "events[]=#{&1}")
       |> Enum.join("&")
-    "/admin/audit_log?#{from}&#{events}"
+
+    "/admin/audit_log?#{time_range}&#{events}"
   end
+
+  defp date_time_to_string(dt), do: Timex.format!(dt, "%F %T", :strftime)
 end
