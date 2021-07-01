09bd95dd7160193876b57ded689d21faa9193412
Cristian Berneanu <cristian@aircloak.com>
2021-07-01 18:32:08 +0300

    Get fewer max log entries per refresh cycle.
    Better protection against a flood of log messages making the UI unresponsive.

--- air/lib/air_web/live/admin/logs_live/index.ex
+++ air/lib/air_web/live/admin/logs_live/index.ex
@@ -4,7 +4,8 @@ defmodule AirWeb.Admin.LogsLive.Index do
 
   alias Air.Schemas.Log
 
-  @max_entries_per_refresh 1_000
+  @initial_entries 1_000
+  @max_entries_per_refresh 100
   @refresh_interval :timer.seconds(2)
 
   @impl true
@@ -12,7 +13,8 @@ defmodule AirWeb.Admin.LogsLive.Index do
     # prevent recursion from debug messages
     Logger.disable(self())
 
-    {last_timestamp, logs} = NaiveDateTime.new!(Date.utc_today(), Time.from_seconds_after_midnight(0)) |> logs_tail()
+    {last_timestamp, logs} =
+      NaiveDateTime.new!(Date.utc_today(), Time.from_seconds_after_midnight(0)) |> logs_tail(@initial_entries)
 
     if connected?(socket), do: :timer.send_interval(@refresh_interval, self(), :refresh)
 
@@ -29,7 +31,7 @@ defmodule AirWeb.Admin.LogsLive.Index do
 
   @impl true
   def handle_info(:refresh, socket) do
-    {last_timestamp, logs} = socket.assigns.last_timestamp |> logs_tail()
+    {last_timestamp, logs} = socket.assigns.last_timestamp |> logs_tail(@max_entries_per_refresh)
     socket = socket |> assign(:last_timestamp, last_timestamp) |> assign(:logs, logs)
     {:noreply, socket}
   end
@@ -38,8 +40,8 @@ defmodule AirWeb.Admin.LogsLive.Index do
   # Helpers
   # -------------------------------------------------------------------
 
-  defp logs_tail(since) do
-    case Air.Service.Logs.tail(since, @max_entries_per_refresh) do
+  defp logs_tail(since, max_entries) do
+    case Air.Service.Logs.tail(since, max_entries) do
       [] ->
         {since, []}
 
